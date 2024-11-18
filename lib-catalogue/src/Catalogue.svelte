d564a96ea83d5038fec25a98d9adf1649f97d151
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2024-11-18 09:57:28 +0100

    Envoie les ressources au catalogue

--- lib-catalogue/src/Catalogue.svelte
+++ lib-catalogue/src/Catalogue.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
-  import type { Service } from "./Catalogue.types";
+  import type { Ressource, Service } from "./Catalogue.types";
 
   export let services: Service[];
+  export let ressources: Ressource[];
 </script>
 
 {#each services as service}
   <div class="carte service">
     <figure>
-      <img src="/assets/images/illustrations-services/{service.illustration}" alt="Illustration du service"/>
+      <img
+        src="/assets/images/illustrations-services/{service.illustration}"
+        alt="Illustration du service"
+      />
       <figcaption>Service</figcaption>
     </figure>
     <div class="contenu">
@@ -15,7 +19,10 @@
       <span>{@html service.description}</span>
       <a href={service.lienInterne}>
         En savoir plus
-        <img src="/assets/images/icone-fleche-droite.svg" alt="En savoir plus"/>
+        <img
+          src="/assets/images/icone-fleche-droite.svg"
+          alt="En savoir plus"
+        />
       </a>
       <div class="labels">
         {#each service.sources as source}<span>{source}</span>{/each}
@@ -23,6 +30,11 @@
     </div>
   </div>
 {/each}
+
+{#each ressources as ressource}
+  - {ressource.nom}
+{/each}
+
 
 <style>
 </style>
--- lib-catalogue/src/main.ts
+++ lib-catalogue/src/main.ts
@@ -1,11 +1,14 @@
 import { mount } from "svelte";
 import Catalogue from "./Catalogue.svelte";
-import type { Service } from "./Catalogue.types";
+import type { Ressource, Service } from "./Catalogue.types";
 
 const donnees = document.getElementById("donnees")!.textContent;
 if (!donnees) throw new Error("Impossible de trouver les données du catalogue");
 
-const props = JSON.parse(donnees) as { services: Service[] };
+const props = JSON.parse(donnees) as {
+  services: Service[];
+  ressources: Ressource[];
+};
 
 const catalogue = mount(Catalogue, {
   target: document.getElementById("catalogue")!,
