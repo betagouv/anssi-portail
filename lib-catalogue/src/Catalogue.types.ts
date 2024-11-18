8bc365b3399bc430c27d657aa7801861dc99a10e
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2024-11-18 09:57:28 +0100

    Envoie les ressources au catalogue

--- lib-catalogue/src/Catalogue.types.ts
+++ lib-catalogue/src/Catalogue.types.ts
@@ -5,3 +5,11 @@ export type Service = {
   lienInterne: string;
   sources: string[];
 };
+
+export type Ressource = {
+  nom: string;
+  illustration: string;
+  description: string;
+  lienInterne: string;
+  sources: string[];
+};
