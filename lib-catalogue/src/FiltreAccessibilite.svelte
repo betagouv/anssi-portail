264219bd005483fcda8d1fa4298c16a48e1110e9
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2024-11-19 16:45:26 +0100

    Affiche les filtres sur l’accessibilité
    ... sans les brancher pour le moment

---
+++ lib-catalogue/src/FiltreAccessibilite.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+</script>
+
+<fieldset>
+    <legend>Accessibles pour</legend>
+    <label>
+        <input type="checkbox">
+        <span class="libelle">Accès libre</span>
+        <span class="compte">71</span>
+    </label>
+    <label>
+        <input type="checkbox">
+        <span class="libelle">Entités publiques</span>
+        <span class="compte">55</span>
+    </label>
+    <label>
+        <input type="checkbox">
+        <span class="libelle">Entités régulées / NIS2</span>
+        <span class="compte">4</span>
+    </label>
+</fieldset>
