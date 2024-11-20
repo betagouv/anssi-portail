e93a9bfcd24b6d1ea9f01d3349547573961942b7
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2024-11-20 09:28:41 +0100

    Branche le filtre des droits d’accès

--- lib-catalogue/src/FiltreAccessibilite.svelte
+++ lib-catalogue/src/FiltreAccessibilite.svelte
@@ -1,21 +1,30 @@
 <script lang="ts">
+  import { rechercheParDroitAcces } from "./stores/rechercheParDroitAcces.store";
 </script>
 
 <fieldset>
-    <legend>Accessibles pour</legend>
-    <label>
-        <input type="checkbox">
-        <span class="libelle">Accès libre</span>
-        <span class="compte">71</span>
-    </label>
-    <label>
-        <input type="checkbox">
-        <span class="libelle">Entités publiques</span>
-        <span class="compte">55</span>
-    </label>
-    <label>
-        <input type="checkbox">
-        <span class="libelle">Entités régulées / NIS2</span>
-        <span class="compte">4</span>
-    </label>
+  <legend>Accessibles pour</legend>
+  <label>
+    <input
+      type="checkbox"
+      value="ACCES_LIBRE"
+      bind:group={$rechercheParDroitAcces}
+    />
+    <span class="libelle">Accès libre</span>
+    <span class="compte">71</span>
+  </label>
+  <label>
+    <input
+      type="checkbox"
+      value="ENTITES_PUBLIQUES"
+      bind:group={$rechercheParDroitAcces}
+    />
+    <span class="libelle">Entités publiques</span>
+    <span class="compte">55</span>
+  </label>
+  <label>
+    <input type="checkbox"  value="REGULES_NIS2" bind:group={$rechercheParDroitAcces}/>
+    <span class="libelle">Entités régulées / NIS2</span>
+    <span class="compte">4</span>
+  </label>
 </fieldset>
