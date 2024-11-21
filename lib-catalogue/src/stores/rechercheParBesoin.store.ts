dc17d3a0db835e68a244eb2fb5282ed44e45cbfb
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2024-11-21 15:14:16 +0100

    [SOIN] Délègue aux recherches le prédicat de correspondance des items

--- lib-catalogue/src/stores/rechercheParBesoin.store.ts
+++ lib-catalogue/src/stores/rechercheParBesoin.store.ts
@@ -1,4 +1,13 @@
-import { writable } from "svelte/store";
-import type { BesoinCyber } from "../Catalogue.types";
+import { get, writable } from "svelte/store";
+import type { BesoinCyber, ItemCyber } from "../Catalogue.types";
 
-export const rechercheParBesoin = writable<BesoinCyber>();
+const selectionBesoins = writable<BesoinCyber>();
+
+export const rechercheParBesoin = {
+  subscribe: selectionBesoins.subscribe,
+  set: selectionBesoins.set,
+  ok: (item: ItemCyber) =>
+    !!item.besoins.find(
+      (b) => !get(selectionBesoins) || get(selectionBesoins) === b,
+    ),
+};
--- lib-catalogue/src/stores/rechercheParDroitAcces.store.ts
+++ lib-catalogue/src/stores/rechercheParDroitAcces.store.ts
@@ -1,4 +1,14 @@
-import { writable } from "svelte/store";
-import type { DroitAcces } from "../Catalogue.types";
+import { get, writable } from "svelte/store";
+import type { DroitAcces, ItemCyber } from "../Catalogue.types";
 
-export const rechercheParDroitAcces = writable<DroitAcces[]>([]);
+const selectionDroitAcces = writable<DroitAcces[]>([]);
+
+export const rechercheParDroitAcces = {
+  subscribe: selectionDroitAcces.subscribe,
+  set: selectionDroitAcces.set,
+  ok: (item: ItemCyber) =>
+    get(rechercheParDroitAcces).length === 0 ||
+    !!get(rechercheParDroitAcces).find((droitAcces) =>
+      item.droitsAcces.includes(droitAcces),
+    ),
+};
