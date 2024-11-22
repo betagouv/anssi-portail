6bcac1de590bc83b70d8243a2125351cacfa8a34
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2024-11-22 09:00:20 +0100

    Compte le nombre de résultats par source

--- lib-catalogue/src/stores/nombreResultats.store.ts
+++ lib-catalogue/src/stores/nombreResultats.store.ts
@@ -1,41 +1,48 @@
 import { derived } from "svelte/store";
 import { catalogueStore } from "./catalogue.store";
-import { DroitAcces, FormatRessource, Typologie } from "../Catalogue.types";
+import {
+  DroitAcces,
+  FormatRessource,
+  Source,
+  Typologie,
+} from "../Catalogue.types";
 
 type NombreResultats = {
   parDroitAcces: Partial<Record<DroitAcces, number>>;
   parTypologie: Partial<Record<Typologie, number>>;
   parFormatDeRessource: Partial<Record<FormatRessource, number>>;
+  parSource: Partial<Record<Source, number>>;
 };
 
 export const nombreResultats = derived<
   [typeof catalogueStore],
   NombreResultats
 >([catalogueStore], ([$catalogueStore]) => {
+  const creeObjetDepuisEnum = <T>(
+    type: { [s: string]: T },
+    calculNombre: (valeur: T) => number,
+  ) => Object.fromEntries(Object.values(type).map((f) => [f, calculNombre(f)]));
+
   const nombreParDroitAcces = (droitAcces: DroitAcces) =>
     $catalogueStore.filter((item) => item.droitsAcces.includes(droitAcces))
       .length;
 
-  const parDroitAcces = Object.fromEntries(
-    Object.values(DroitAcces).map((d) => [d, nombreParDroitAcces(d)]),
-  );
-
   const nombreParTypologie = (typologie: Typologie) =>
     $catalogueStore.filter((item) => item.typologie === typologie).length;
-
-  const parTypologie = Object.fromEntries(
-    Object.values(Typologie).map((t) => [t, nombreParTypologie(t)]),
-  );
 
   const nombreParFormatDeRessource = (format: FormatRessource) =>
     $catalogueStore.filter((item) => item.format === format).length;
 
-  const parFormatDeRessource = Object.fromEntries(
-    Object.values(FormatRessource).map((f) => [
-      f,
-      nombreParFormatDeRessource(f),
-    ]),
-  );
+  const nombreParSource = (source: Source) =>
+    $catalogueStore.filter((item) => item.sources.includes(source)).length;
 
-  return { parDroitAcces, parTypologie, parFormatDeRessource };
+  return {
+    parDroitAcces: creeObjetDepuisEnum(DroitAcces, nombreParDroitAcces),
+    parTypologie: creeObjetDepuisEnum(Typologie, nombreParTypologie),
+    parFormatDeRessource: creeObjetDepuisEnum(
+      FormatRessource,
+      nombreParFormatDeRessource,
+    ),
+    parSource: creeObjetDepuisEnum(Source, nombreParSource),
+  };
 });
