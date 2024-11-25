91b885e8ddbca206c1104ca1f12d9346ced04de8
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2024-11-25 11:54:29 +0100

    Affiche le badge correspondant à la typologie et au format

--- lib-catalogue/src/Catalogue.svelte
+++ lib-catalogue/src/Catalogue.svelte
@@ -11,6 +11,7 @@
   import { rechercheParFormat } from "./stores/rechercheParFormat.store";
   import { rechercheParSource } from "./stores/rechercheParSource.store";
   import { rechercheParTheme } from "./stores/rechercheParTheme.store";
+  import { type ItemCyber, Typologie } from "./Catalogue.types";
 
   const reinitialiseFiltres = () => {
     rechercheParBesoin.reinitialise();
@@ -20,15 +21,26 @@
     rechercheParSource.reinitialise();
     rechercheParTheme.reinitialise();
   };
+
+  const libelleBadge = (item: ItemCyber) =>
+    item.typologie === Typologie.SERVICE ? "Service" : item.format;
 </script>
 
 <div class="recherche">
   <div class="sommaire sommaire-replie">
     <details>
       <summary>
-        <img class="menu" src="/assets/images/icone-filtre-vide.svg" alt="Icône filtre"/>
+        <img
+          class="menu"
+          src="/assets/images/icone-filtre-vide.svg"
+          alt="Icône filtre"
+        />
         <span class="titre-menu">Filtres</span>
-        <img class="chevron" src="/assets/images/icone-chevron-bas.svg" alt="Chevron ouverture" />
+        <img
+          class="chevron"
+          src="/assets/images/icone-chevron-bas.svg"
+          alt="Chevron ouverture"
+        />
       </summary>
 
       <div class="barre-filtres">
@@ -48,19 +60,19 @@
   </div>
 
   <div class="liste">
-    {#each $catalogueFiltre.resultats as service}
+    {#each $catalogueFiltre.resultats as itemCyber}
       <div class="carte service">
         <figure>
           <img
-            src="/assets/images/illustrations-services/{service.illustration}"
+            src="/assets/images/illustrations-services/{itemCyber.illustration}"
             alt="Illustration du service"
           />
-          <figcaption>Service</figcaption>
+          <figcaption>{libelleBadge(itemCyber)}</figcaption>
         </figure>
         <div class="contenu">
-          <h3>{@html service.nom}</h3>
-          <span>{@html service.description}</span>
-          <a href={service.lienInterne}>
+          <h3>{@html itemCyber.nom}</h3>
+          <span>{@html itemCyber.description}</span>
+          <a href={itemCyber.lienInterne}>
             En savoir plus
             <img
               src="/assets/images/icone-fleche-droite.svg"
@@ -68,7 +80,7 @@
             />
           </a>
           <div class="labels">
-            {#each service.sources as source}<span>{source}</span>{/each}
+            {#each itemCyber.sources as source}<span>{source}</span>{/each}
           </div>
         </div>
       </div>
@@ -79,24 +91,44 @@
 <div class="encart fond-sombre">
   <h1>Nos parcours adaptés à votre maturité cyber</h1>
   <div class="carte parcours">
-    <img src="/assets/images/debuter-cyber.png" alt="Illustration débutant cyber"/>
+    <img
+      src="/assets/images/debuter-cyber.png"
+      alt="Illustration débutant cyber"
+    />
     <h2>Débuter en cyberbersécurité</h2>
-    <p>Vous débutez en cyber ou vous avez besoin de solidifier vous connaissances ? Quelques actions simples pourraient améliorer votre sécurité. L’ANSSI vous propose des solutions pour y arriver.</p>
+    <p>
+      Vous débutez en cyber ou vous avez besoin de solidifier vous connaissances
+      ? Quelques actions simples pourraient améliorer votre sécurité. L’ANSSI
+      vous propose des solutions pour y arriver.
+    </p>
     <a href="#">Découvrir le parcours</a>
   </div>
   <div class="carte parcours">
-    <img src="/assets/images/approfondir-cyber.png" alt="Illustration approfondir cyber"/>
+    <img
+      src="/assets/images/approfondir-cyber.png"
+      alt="Illustration approfondir cyber"
+    />
     <h2>Approfondir la cybersécurité</h2>
-    <p>Vous connaissez les bases de la cyber mais quelques actions simples pourraient améliorer votre sécurité. L’ANSSI vous propose des solutions pour y arriver.</p>
+    <p>
+      Vous connaissez les bases de la cyber mais quelques actions simples
+      pourraient améliorer votre sécurité. L’ANSSI vous propose des solutions
+      pour y arriver.
+    </p>
     <a href="#">Découvrir le parcours</a>
   </div>
 </div>
 
-<div  class="encart fond-clair">
+<div class="encart fond-clair">
   <div class="carte test-maturite">
-    <img src="/assets/images/trouver-parcours.png" alt="Illustration trouver parcours"/>
+    <img
+      src="/assets/images/trouver-parcours.png"
+      alt="Illustration trouver parcours"
+    />
     <h1>Trouvez le parcours cyber adapté</h1>
-    <p>Faites le test et découvrez nos parcours cyber adaptés au niveau de maturité de votre entité.</p>
+    <p>
+      Faites le test et découvrez nos parcours cyber adaptés au niveau de
+      maturité de votre entité.
+    </p>
     <a href="#" class="bouton primaire">Testez maturité cyber</a>
   </div>
 </div>
