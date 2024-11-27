b5657951501e4b18316cd87da2dff4843174ac06
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2024-11-27 15:12:38 +0100

    Affiche les filtres dans une barre latérale

--- lib-catalogue/src/stores/rechercheParSource.store.ts
+++ lib-catalogue/src/stores/rechercheParSource.store.ts
@@ -6,12 +6,23 @@ const selectionDeSources = writable<Source[]>([]);
 export const rechercheParSource = {
   subscribe: selectionDeSources.subscribe,
   set: selectionDeSources.set,
-  ajoute: (source: Source) =>
-    selectionDeSources.update((etatActuel) => [
-      ...new Set([...etatActuel, source]),
-    ]),
-  retire: (source: Source) =>
-    selectionDeSources.update((valeur) => valeur.filter((v) => v !== source)),
+  ajoute: (source: Source) => {
+    /* On fait ce test pour éviter d’appeler update si ce n’est pas nécessaire.
+       En effet, chaque appel à update va déclencher une boucle infinie de mise à jour pour les obervateurs,
+       ce qui provoque une erreur `effect_update_depth_exceeded` lorsque le composant FiltreSource est utilisé
+       plusieurs fois.  */
+    if (!get(selectionDeSources).includes(source)) {
+      selectionDeSources.update((etatActuel) => [
+        ...new Set([...etatActuel, source]),
+      ]);
+    }
+  },
+  retire: (source: Source) => {
+    // cf commentaire sur l’ajout
+    if (get(selectionDeSources).includes(source)) {
+      selectionDeSources.update((valeur) => valeur.filter((v) => v !== source));
+    }
+  },
   reinitialise: () => selectionDeSources.set([]),
   ok: (item: ItemCyber) => {
     const sources = get(rechercheParSource);
