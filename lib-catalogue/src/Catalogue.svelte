572f5e9c79ea1316e72551dc9fe899845be6ec72
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-01-03 15:18:39 +0100

    Limite le contenu du catalogue à 1200px

--- lib-catalogue/src/Catalogue.svelte
+++ lib-catalogue/src/Catalogue.svelte
@@ -13,18 +13,38 @@
 </script>
 
 <div class="barre-filtre-besoin">
-  <FiltreBesoin />
+  <div class="contenu-section">
+    <FiltreBesoin />
+  </div>
 </div>
 
 <div class="recherche">
-  <div class="sommaire sommaire-replie">
-    <details>
-      <summary>
-        <EnteteFiltres />
-      </summary>
+  <div class="contenu-section">
+    <div class="sommaire sommaire-replie">
+      <details>
+        <summary>
+          <EnteteFiltres />
+        </summary>
 
+        <div class="barre-filtres">
+          <FiltreBesoin />
+          <FiltreAccessibilite />
+          <FiltreTypologieEtFormat />
+          <FiltreSource />
+          <FiltreTheme />
+          <input
+            type="button"
+            class="bouton primaire"
+            value="Réinitialiser les filtres"
+            on:click={reinitialiseFiltres}
+          />
+        </div>
+      </details>
+    </div>
+
+    <div class="sommaire sommaire-deplie">
+      <EnteteFiltres />
       <div class="barre-filtres">
-        <FiltreBesoin />
         <FiltreAccessibilite />
         <FiltreTypologieEtFormat />
         <FiltreSource />
@@ -36,90 +56,78 @@
           on:click={reinitialiseFiltres}
         />
       </div>
-    </details>
-  </div>
-
-  <div class="sommaire sommaire-deplie">
-    <EnteteFiltres />
-    <div class="barre-filtres">
-      <FiltreAccessibilite />
-      <FiltreTypologieEtFormat />
-      <FiltreSource />
-      <FiltreTheme />
-      <input
-        type="button"
-        class="bouton primaire"
-        value="Réinitialiser les filtres"
-        on:click={reinitialiseFiltres}
-      />
     </div>
-  </div>
 
-  <div class="liste">
-    {#each $catalogueFiltre.resultats as itemCyber}
-      <CarteItem {itemCyber} />
-    {:else}
-      <div class="aucun-resultat">
-        <img
-          src="/assets/images/illustration-aucun-resultat.svg"
-          alt="Aucun résultat"
-        />
-        <h1>Désolé, aucun résultat trouvé</h1>
-        <input
-          type="button"
-          class="bouton primaire"
-          value="Réinitialiser les filtres"
-          on:click={reinitialiseFiltres}
-        />
-      </div>
-    {/each}
+    <div class="liste">
+      {#each $catalogueFiltre.resultats as itemCyber}
+        <CarteItem {itemCyber} />
+      {:else}
+        <div class="aucun-resultat">
+          <img
+            src="/assets/images/illustration-aucun-resultat.svg"
+            alt="Aucun résultat"
+          />
+          <h1>Désolé, aucun résultat trouvé</h1>
+          <input
+            type="button"
+            class="bouton primaire"
+            value="Réinitialiser les filtres"
+            on:click={reinitialiseFiltres}
+          />
+        </div>
+      {/each}
+    </div>
   </div>
 </div>
 
 <div class="encart fond-sombre">
-  <h1>Nos parcours adaptés à votre maturité cyber</h1>
-  <div class="liste-parcours">
-    <div class="carte parcours">
-      <img
-        src="/assets/images/debuter-cyber.png"
-        alt="Illustration débutant cyber"
-      />
-      <h2>Débuter en cyberbersécurité</h2>
-      <p>
-        Vous débutez en cyber ou vous avez besoin de solidifier vous
-        connaissances ? Quelques actions simples pourraient améliorer votre
-        sécurité. L’ANSSI vous propose des solutions pour y arriver.
-      </p>
-      <a href="/parcours-debuter">Découvrir le parcours</a>
-    </div>
-    <div class="carte parcours">
-      <img
-        src="/assets/images/approfondir-cyber.png"
-        alt="Illustration approfondir cyber"
-      />
-      <h2>Approfondir la cybersécurité</h2>
-      <p>
-        Vous connaissez les bases de la cyber mais quelques actions simples
-        pourraient améliorer votre sécurité. L’ANSSI vous propose des solutions
-        pour y arriver.
-      </p>
-      <a href="/parcours-approfondir">Découvrir le parcours</a>
+  <div class="contenu-section">
+    <h1>Nos parcours adaptés à votre maturité cyber</h1>
+    <div class="liste-parcours">
+      <div class="carte parcours">
+        <img
+          src="/assets/images/debuter-cyber.png"
+          alt="Illustration débutant cyber"
+        />
+        <h2>Débuter en cyberbersécurité</h2>
+        <p>
+          Vous débutez en cyber ou vous avez besoin de solidifier vous
+          connaissances ? Quelques actions simples pourraient améliorer votre
+          sécurité. L’ANSSI vous propose des solutions pour y arriver.
+        </p>
+        <a href="/parcours-debuter">Découvrir le parcours</a>
+      </div>
+      <div class="carte parcours">
+        <img
+          src="/assets/images/approfondir-cyber.png"
+          alt="Illustration approfondir cyber"
+        />
+        <h2>Approfondir la cybersécurité</h2>
+        <p>
+          Vous connaissez les bases de la cyber mais quelques actions simples
+          pourraient améliorer votre sécurité. L’ANSSI vous propose des
+          solutions pour y arriver.
+        </p>
+        <a href="/parcours-approfondir">Découvrir le parcours</a>
+      </div>
     </div>
   </div>
 </div>
 
 <div class="encart fond-clair">
-  <div class="carte test-maturite">
-    <img
-      src="/assets/images/trouver-parcours.png"
-      alt="Illustration trouver parcours"
-    />
-    <h1>Trouvez le parcours cyber adapté</h1>
-    <p>
-      Faites le test et découvrez nos parcours cyber adaptés au niveau de
-      maturité de votre entité.
-    </p>
-    <a href="/test" class="bouton primaire">Testez votre maturité cyber</a>
+  <div class="contenu-section">
+    <div class="carte test-maturite">
+      <img
+        src="/assets/images/trouver-parcours.png"
+        alt="Illustration trouver parcours"
+      />
+      <h1>Trouvez le parcours cyber adapté</h1>
+      <p>
+        Faites le test et découvrez nos parcours cyber adaptés au niveau de
+        maturité de votre entité.
+      </p>
+      <a href="/test" class="bouton primaire">Testez votre maturité cyber</a>
+    </div>
   </div>
 </div>
 
