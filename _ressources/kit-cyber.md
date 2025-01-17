204e7d7177b3189e82a078a247aaf40bb9e261f2
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-01-17 10:43:17 +0100

    Affiche les services disponibles

--- _ressources/kit-cyber.md
+++ _ressources/kit-cyber.md
@@ -6,6 +6,7 @@ titreHtml: KIT CYBER
 description: Être sensibiliser et découvrir les bonnes pratiques dans les usages personnels et améliorer ses usages dans le cadre professionnel.
 besoins:
   - SENSIBILISER_ET_FORMER
+  - REAGIR
 droitsAcces:
   - ACCES_LIBRE
 format: PDF
--- lib-catalogue/src/ExtraitItems.svelte
+++ lib-catalogue/src/ExtraitItems.svelte
@@ -1,4 +1,24 @@
+<script lang="ts">
+  import CarteItem from "./CarteItem.svelte";
+  import { catalogueFiltre } from "./stores/catalogueFiltre.store";
+  import FiltreBesoin from "./FiltreBesoin.svelte";
+</script>
+
+<div class="barre-filtre-besoin">
+  <div class="contenu-section">
+    <FiltreBesoin />
+  </div>
+</div>
+
 <h2>
   Des services pour comprendre la cyber, être sensibilisé, mais aussi se former
   et former ses équipes à la cybersécurité.
 </h2>
+
+<section class="services-dispos">
+  <div class="contenu-section">
+    {#each $catalogueFiltre.resultats as itemCyber}
+      <CarteItem {itemCyber}></CarteItem>
+    {/each}
+  </div>
+</section>
