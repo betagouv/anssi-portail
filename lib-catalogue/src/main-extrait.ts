bdac4abec97ef96892a45724d3165af39392bfc5
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-01-17 10:43:17 +0100

    Affiche les services disponibles

--- lib-catalogue/src/main-extrait.ts
+++ lib-catalogue/src/main-extrait.ts
@@ -1,5 +1,17 @@
 import { mount } from "svelte";
 import ExtraitItems from "./ExtraitItems.svelte";
+import type {ItemCyber} from "./Catalogue.types";
+import {catalogueStore} from "./stores/catalogue.store";
+
+const donnees = document.getElementById("donnees")!.textContent;
+if (!donnees) throw new Error("Impossible de trouver les données du catalogue");
+
+const { ressources, services } = JSON.parse(donnees) as {
+  services: ItemCyber[];
+  ressources: ItemCyber[];
+};
+
+catalogueStore.initialise(services, ressources);
 
 const extrait = mount(ExtraitItems, {
   target: document.getElementById("extrait")!,
