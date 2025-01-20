24aeeb9a0b16f60657abfe606c0f81dafda02b6f
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-01-20 16:52:48 +0100

    Ajoute une page pour le test de maturité

--- lib-svelte/vite.config.ts
+++ lib-svelte/vite.config.ts
@@ -1,20 +1,20 @@
-import {defineConfig} from "vite";
-import {svelte} from "@sveltejs/vite-plugin-svelte";
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [svelte()],
-    build: {
-        rollupOptions: {
-            input: {
-                catalogue: "src/main-catalogue.ts",
-                extrait: "src/main-extrait.ts",
-                testMaturite: "src/main-test-maturite.ts"
-            },
-            output: {
-                entryFileNames: `assets/[name].js`,
-                assetFileNames: `assets/[name].[ext]`,
-            },
-        },
+  plugins: [svelte()],
+  build: {
+    rollupOptions: {
+      input: {
+        catalogue: "src/main-catalogue.ts",
+        extrait: "src/main-extrait.ts",
+        "test-maturite": "src/main-test-maturite.ts",
+      },
+      output: {
+        entryFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
     },
+  },
 });
