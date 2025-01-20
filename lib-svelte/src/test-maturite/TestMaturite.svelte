bbe3bf00c74ae9a7bf5df2a80669d8340fd38287
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-01-20 17:32:24 +0100

    Pose la première question du test
    … en faisant émerger des types de base.

--- lib-svelte/src/test-maturite/TestMaturite.svelte
+++ lib-svelte/src/test-maturite/TestMaturite.svelte
@@ -1,1 +1,59 @@
+<script lang="ts">
+  type Question = {
+    titre: string;
+    question: string;
+    propositions: string[];
+  };
+
+  const questions: Question[] = [
+    {
+      titre: "Prise en compte du risque",
+      question:
+        "Quelle est la perception des risques cyber au sein de votre organisation ?",
+      propositions: [
+        "Les responsables considèrent que leur organisation n'est pas concernée par le risque de cyberattaques.",
+        "Les responsables ont conscience que leur organisation peut être victime de cyberattaques, comme d'autres avant elle, sans savoir exactement lesquelles ni les conséquences qu’elles entraîneraient.",
+        "Les données et les processus à protéger en priorité contre les risques cyber les plus courants ont été identifiés.",
+        "Une analyse de risque approfondie de l'organisation a été menée et un plan détaillé de traitement des risques a été établi.",
+        "Les objectifs de réduction des risques d'origine cyber sont intégrés dans la stratégie globale de gestion des risques de l'organisation.",
+      ],
+    },
+  ];
+
+  let questionCourante = 0;
+  let reponseDonnee: number | null = null;
+
+  const toutesReponses: number[] = [];
+
+  function reponds() {
+    toutesReponses.push(reponseDonnee!);
+    questionCourante++;
+    reponseDonnee = null;
+  }
+</script>
+
 <h1>Testez votre maturité Cyber</h1>
+
+<h5>{questions[questionCourante].titre}</h5>
+<hr />
+{reponseDonnee}
+<hr />
+<h4>{questions[questionCourante].question}</h4>
+{#each questions[questionCourante].propositions as proposition, index}
+  <label>
+    <input
+      type="radio"
+      bind:group={reponseDonnee}
+      value={index}
+    />
+    {proposition}
+  </label>
+{/each}
+
+<input
+  type="button"
+  class="bouton primaire"
+  value="Question suivante"
+  disabled={reponseDonnee === null}
+  on:click={reponds}
+/>
