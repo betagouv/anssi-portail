454aa511c82bf8dca29402e86d597eba16f8db10
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-01-21 09:28:14 +0100

    [SOIN] Change la manière de stocker les réponses
    ...en utilisant un tableau prérempli de réponses vides

--- lib-svelte/src/test-maturite/TestMaturite.svelte
+++ lib-svelte/src/test-maturite/TestMaturite.svelte
@@ -35,27 +35,23 @@
   let questionCourante = 0;
   let reponseDonnee: number | null = null;
 
-  let toutesReponses: number[] = [];
+  let toutesReponses: (number|null)[] = Array(6);
+  toutesReponses.fill(null)
 
   function reponds() {
-    toutesReponses.push(reponseDonnee!);
+    toutesReponses[questionCourante] = reponseDonnee;
     questionCourante++;
-    reponseDonnee = null;
+    reponseDonnee = toutesReponses[questionCourante];
   }
   function reviensEnArriere() {
-    toutesReponses = toutesReponses.slice(0, questionCourante - 1);
     questionCourante--;
     reponseDonnee = toutesReponses[questionCourante];
   }
 </script>
 
 <h1>Testez votre maturité Cyber</h1>
-
 <p>Étape {questionCourante + 1} sur 7</p>
 <h5>{questions[questionCourante].titre}</h5>
-<hr />
-{reponseDonnee} <br />
-<hr />
 <h4>{@html questions[questionCourante].question}</h4>
 {#each questions[questionCourante].propositions as proposition, index}
   <label>
