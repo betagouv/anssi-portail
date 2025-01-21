fb2a1b262e26734be26ada944901f2430193ed49
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-01-21 11:58:40 +0100

    Crée un store dédié au déroulement du questionnaire
    … pour lui ajouter les tests nécessaires.

---
+++ lib-svelte/src/test-maturite/stores/questionnaire.store.ts
@@ -0,0 +1,30 @@
+import { writable } from "svelte/store";
+
+export type Questionnaire = {
+  questionCourante: number;
+  toutesLesReponses: number[];
+};
+
+const { set, subscribe, update } = writable<Questionnaire>();
+
+export const questionnaireStore = {
+  subscribe,
+  initialise: () =>
+    set({ questionCourante: 0, toutesLesReponses: new Array(6).fill(null) }),
+
+  reponds(reponseQuestionCourante: number) {
+    update((state) => {
+      state.toutesLesReponses[state.questionCourante] = reponseQuestionCourante;
+      state.questionCourante++;
+
+      return state;
+    });
+  },
+
+  reviensEnArriere() {
+    update((state) => {
+      state.questionCourante--;
+      return state;
+    });
+  },
+};
