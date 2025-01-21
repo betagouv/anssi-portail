b0f8665f3b9990a5e91605fb63eac49429da57f6
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-01-21 11:28:50 +0100

    [WIP] Affiche un radar de maturité

---
+++ lib-svelte/src/test-maturite/RadarMaturite.svelte
@@ -0,0 +1,82 @@
+<script lang="ts">
+    export let resultats = [2, 1, 4, 5, 3, 5]
+    export let rubriques = [{
+        label: "Pilotage de la sécurité",
+        ancrageTexte: "start",
+        alignementVertical: "middle"
+    }, {
+        label: "Budget", ancrageTexte: "start", alignementVertical: "hanging"
+    }, {
+        label: "Ressources humaines",
+        ancrageTexte: "end", alignementVertical: "hanging"
+    }, {label: "Adoption des solutions cyber", ancrageTexte: "end", alignementVertical: "middle"}, {
+        label: "Prise en compte du risque",
+        ancrageTexte: "end", alignementVertical: "auto"
+    }, {label: "Posture à l'égard de la cyber", ancrageTexte: "start", alignementVertical: "auto"}]
+
+    const tailleRadar = 200;
+    const polaireVersCartesien = (r: number, theta: number) => ({
+        x: r * Math.cos(theta),
+        y: r * Math.sin(theta),
+    });
+    type Point = { x: number, y: number };
+
+    const tableauEnPointsPolygone = (coordonnees: Point[]) => coordonnees.map((c) => `${c.x},${c.y}`).join(" ")
+
+    const pointsDuPolygone: Point[] = resultats.map((valeur, index) => {
+        const r = valeur / 5 * tailleRadar
+        const theta = (index * 2 * Math.PI) / 6
+        return polaireVersCartesien(r, theta)
+    });
+</script>
+
+<svg
+        id="radar"
+        width="880"
+        height="350"
+        viewBox="-440 -175 880 350"
+        xmlns="http://www.w3.org/2000/svg"
+>
+    {#each new Array(6).fill(0).map((_, index) => index) as index}
+        {@const theta = (index * 2 * Math.PI) / 6}
+        {@const coordonnees = polaireVersCartesien(tailleRadar, theta)}
+        <line x1="0" y1="0" x2={coordonnees.x} y2={coordonnees.y} stroke="#ddd"/>
+        {#each new Array(6).fill(0).map((_, index) => index) as index2}
+            {@const thetaSuivant = ((index + 1) * 2 * Math.PI) / 6}
+            {@const decalage = tailleRadar / 5 * index2}
+            {@const coordonneesActuelles = polaireVersCartesien(decalage, theta)}
+            {@const coordonneesSuivantes = polaireVersCartesien(
+                decalage,
+                thetaSuivant,
+            )}
+            <line
+                    x1={coordonneesActuelles.x}
+                    y1={coordonneesActuelles.y}
+                    x2={coordonneesSuivantes.x}
+                    y2={coordonneesSuivantes.y}
+                    stroke="#ddd"
+            />
+        {/each}
+    {/each}
+
+    <polygon points={tableauEnPointsPolygone(pointsDuPolygone)} fill="#FED98099" stroke="#0D0C21" stroke-width="3"/>
+    {#each new Array(6).fill(0).map((_, index) => index) as index}
+        {#if index !== 0 }
+            {@const r = (tailleRadar / 5 * index)}
+            {@const y = -r * Math.cos(-Math.PI / 6)}
+            <text x="0" y={y} fill="#0D0C21" text-anchor="middle" font-size="15" stroke="white" stroke-width="0.6"
+                  font-weight="bold" dominant-baseline="middle">
+                {index}
+            </text>
+        {/if}
+    {/each}
+
+    {#each rubriques as rubrique, index}
+        {@const coordonnees = polaireVersCartesien(tailleRadar * 1.1, index * 2 * Math.PI / 6)}
+        <text x={coordonnees.x} y={coordonnees.y} text-anchor={rubrique.ancrageTexte}
+              dominant-baseline={rubrique.alignementVertical} font-size="12" fill="#0D0C21">
+            <tspan>{rubrique.label} - </tspan>
+            <tspan font-weight="bold">5/5</tspan>
+        </text>
+    {/each}
+</svg>
