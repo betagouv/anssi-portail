3e71fedd75af97a155fcfa0eb136b3f901787eb3
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-01-21 11:42:48 +0100

    [SOIN] Déplace les tests
    … qui avaient été oubliés.

--- lib-svelte/test/catalogue/stores/rechercheParTheme.store.spec.ts
+++ lib-svelte/test/catalogue/stores/rechercheParTheme.store.spec.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from "vitest";
-import { ThemeCyber } from "../../src/catalogue/Catalogue.types";
+import { ThemeCyber } from "../../../src/catalogue/Catalogue.types";
 import { get } from "svelte/store";
-import { rechercheParTheme } from "../../src/catalogue/stores/rechercheParTheme.store";
+import { rechercheParTheme } from "../../../src/catalogue/stores/rechercheParTheme.store";
 
 describe("La recherche par thème", () => {
   it("est vide quand on la réinitialise", () => {
