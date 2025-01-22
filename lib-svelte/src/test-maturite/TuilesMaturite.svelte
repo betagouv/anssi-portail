1ce2ecfb06ff06fe4c285fe68885d989f8d7d5b9
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-01-22 17:33:08 +0100

    Ajoute un composant pour les tuiles correspondant au niveau

---
+++ lib-svelte/src/test-maturite/TuilesMaturite.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { type NiveauMaturite, niveauxMaturite } from "./TestMaturite.donnees";
+
+  export let niveauCourant: NiveauMaturite;
+
+  $: indexNiveauCourant = niveauxMaturite.indexOf(niveauCourant);
+</script>
+
+<div class="tuiles-niveau">
+  {#each niveauxMaturite as niveau, index}
+    <div
+      class="tuile-niveau"
+      class:actif={index < indexNiveauCourant}
+      class:courant={index === indexNiveauCourant}
+      class:inactif={index > indexNiveauCourant}
+    >
+      <img class="illustration" src="/assets/images/test-maturite/niveaux/{niveau.id}.svg" alt="Niveau de maturité">
+      <img class="coche" alt="" src="/assets/images/coche-ronde.svg">
+      <span>{niveau.label}</span>
+    </div>
+  {/each}
+</div>
