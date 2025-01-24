b2e6aa55933e1854f659ecf6ae9eb6edca20ce4a
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-01-24 20:54:07 +0100

    Extrais 2 <select> pour gagner en lisibilité

---
+++ lib-svelte/src/test-maturite/SelectSecteurActivite.svelte
@@ -0,0 +1,25 @@
+<select>
+  <option disabled selected>Sélectionner une option</option>
+  <option>Banques (secteur bancaire)</option>
+  <option>Eau potable</option>
+  <option>Eaux usées</option>
+  <option>Énergie</option>
+  <option>Espace</option>
+  <option>Fabrication</option>
+  <option>
+    Fabrication, production et distribution de produits chimiques
+  </option>
+  <option>Fournisseurs numériques</option>
+  <option>Gestion des déchets</option>
+  <option>Gestion des services TIC</option>
+  <option>Infrastructure des marchés financiers</option>
+  <option>Infrastructure numérique</option>
+  <option>
+    Production transformation et distribution de denrées alimentaires
+  </option>
+  <option>Recherche</option>
+  <option>Santé</option>
+  <option>Services postaux et d'expédition</option>
+  <option>Transports</option>
+  <option>Autre secteur d'activité</option>
+</select>
