192bbfa07cd2a87a68ea9b9a67f95670886a41c4
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-01-24 10:36:33 +0100

    Affiche une tuile adaptée au niveau
     ...vers le parcours ou le catalogue

---
+++ lib-svelte/src/test-maturite/TuileVersParcours.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let parcours: "debuter" | "approfondir";
+</script>
+
+<div class="tuile parcours">
+  <img src="/assets/images/{parcours}-cyber.png" alt="">
+  <h3>Votre parcours cyber</h3>
+  <p>
+    Identifiez vos actions prioritaires grâce à un parcours dédié, regroupant
+    des services et ressources adaptés à vos besoins et à la maturité cyber de
+    votre organisation.
+  </p>
+  <a href="/parcours-{parcours}" class="bouton primaire">Accéder à mon parcours cyber</a>
+</div>
