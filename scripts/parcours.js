8319128573b8548e6e38cd062f308d793b097643
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-01-28 15:50:43 +0100

    Synchronise le scroll et l'élément de menu actif
    On sait qu'il reste un souci en mobile : l'élément de menu actif n'est pas forcément visible à l'écran.
    On a tenté de le faire scroller dans la fenêtre… mais sans succès pour l'instant. Ça fait des collisions de scroll vertical / horizontal.

--- scripts/parcours.js
+++ scripts/parcours.js
@@ -1,30 +1,18 @@
+import { elementLePlusVisible } from "./scroll.js";
 
-const changeSectionActive = () => {
-    const boutons = Array.from(
-        document.querySelectorAll(".sommaire .groupe-boutons a"),
-    );
+window.addEventListener("scroll", () => {
+  const sections = document.getElementsByClassName("action");
+  const laPlusVisible = elementLePlusVisible([...sections], window.innerHeight);
 
-    const trouveLesBoutons = (hash) =>
-        boutons.filter((bouton) => new URL(bouton.href).hash === hash);
+  if (!laPlusVisible) return;
 
-    boutons.forEach((s) => s.classList.remove("actif"));
-    if (location.hash) {
-        trouveLesBoutons(location.hash).forEach((bouton) =>
-            bouton.classList.add("actif"),
-        );
-    } else {
-        boutons[0].classList.add("actif");
-    }
-};
+  document
+    .querySelectorAll(".sommaire a")
+    .forEach((element) => element.classList.remove("actif"));
 
-window.addEventListener("hashchange", changeSectionActive);
-window.addEventListener("DOMContentLoaded", changeSectionActive);
-
-// window.addEventListener("scroll", (e) => {
-//     const element = document.getElementById("progresser")
-//
-//     let rect = element.getBoundingClientRect();
-//     let ratio = ratioEspaceOccupe({top: rect.top, hauteur: rect.height, hauteurFenetre: window.innerHeight});
-//     console.log(ratio);
-// });
-
+  const idPourSurlignage = `#${laPlusVisible.id}`;
+  const cible = document.querySelector(
+    `.sommaire a[href='${idPourSurlignage}']`,
+  );
+  cible.classList.add("actif");
+});
