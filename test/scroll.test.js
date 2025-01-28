427650ba90795f4721e51b3a8c91fb204b1c22aa
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-01-28 15:49:20 +0100

    Étoffe le calcul de ratio
    … qui sera utilisé pour mettre en surbrillance des éléments de menu.

--- test/scroll.test.js
+++ test/scroll.test.js
@@ -1,48 +1,79 @@
 import { describe, expect, test } from "vitest";
-import { ratioEspaceOccupe } from "../scripts/scroll";
+import { elementLePlusVisible, ratioEspaceOccupe } from "../scripts/scroll";
 
-describe("le calcul de visibilite d'un element", () => {
-  const etat = (top, hauteur) => ({ top, hauteur, hauteurFenetre: 100 });
+describe("Pour la synchronisation avec le scroll", () => {
+  describe("le calcul de visibilite d'un element", () => {
+    const etat = (top, hauteur) => ({ top, hauteur, hauteurFenetre: 100 });
 
-  test("retourne 0 lorsque l'element est au-dessus", () => {
-    let ratio = ratioEspaceOccupe(etat(-100, 10));
+    test("retourne 0 lorsque l'element est au-dessus", () => {
+      const ratio = ratioEspaceOccupe(etat(-100, 10));
 
-    expect(ratio).toBe(0);
+      expect(ratio).toBe(0);
+    });
+
+    test("retourne 0.5 lorsque l'element occupe la moitie superieure de la fenetre", () => {
+      const ratio = ratioEspaceOccupe(etat(0, 50));
+
+      expect(ratio).toBe(0.5);
+    });
+
+    test("retourne 0 lorsque l'element est au-dessous de la fenetre", () => {
+      const ratio = ratioEspaceOccupe(etat(200, 50));
+
+      expect(ratio).toBe(0);
+    });
+
+    test("retourne la proportion visible lorsque l'element depasse par en-dessous", () => {
+      const ratio = ratioEspaceOccupe(etat(0, 250));
+
+      expect(ratio).toBe(1);
+    });
+
+    test("retourne la proportion visible lorsque l'element depasse par au-dessus et occupe tout l'espace", () => {
+      const ratio = ratioEspaceOccupe(etat(-50, 150));
+
+      expect(ratio).toBe(1);
+    });
+
+    test("retourne la proportion visible lorsque l'element depasse par au-dessus et n'occupe pas tout l'espace", () => {
+      const ratio = ratioEspaceOccupe(etat(-50, 100));
+
+      expect(ratio).toBe(0.5);
+    });
+
+    test("retourne la proportion visible lorsque l'element depasse par en-dessous et n'occupe pas tout l'espace", () => {
+      const ratio = ratioEspaceOccupe(etat(60, 100));
+
+      expect(ratio).toBe(0.4);
+    });
   });
 
-  test("retourne 0.5 lorsque l'element occupe la moitie superieure de la fenetre", () => {
-    let ratio = ratioEspaceOccupe(etat(0, 50));
+  describe("le calcul de l'élément le plus visible", () => {
+    test("renvoie l'élément qui occupe le plus d'espace", () => {
+      const fenetre = 100;
+      const mini = { getBoundingClientRect: () => ({ top: 0, height: 30 }) };
+      const maxi = { getBoundingClientRect: () => ({ top: 30, height: 70 }) };
 
-    expect(ratio).toBe(0.5);
-  });
+      const lePlusVisible = elementLePlusVisible([mini, maxi], fenetre);
 
-  test("retourne 0 lorsque l'element est au-dessous de la fenetre", () => {
-    let ratio = ratioEspaceOccupe(etat(200, 50));
+      expect(lePlusVisible).toEqual(maxi);
+    });
 
-    expect(ratio).toBe(0);
-  });
+    test("renvoie null si tous les éléments sont invisibles", () => {
+      const fenetre = 100;
+      const invisible1 = {
+        getBoundingClientRect: () => ({ top: -100, height: 20 }),
+      };
+      const invisible2 = {
+        getBoundingClientRect: () => ({ top: -80, height: 40 }),
+      };
 
-  test("retourne la proportion visible lorsque l'element depasse par en-dessous", () => {
-    let ratio = ratioEspaceOccupe(etat(0, 250));
+      const lePlusVisible = elementLePlusVisible(
+        [invisible1, invisible2],
+        fenetre,
+      );
 
-    expect(ratio).toBe(1);
-  });
-
-  test("retourne la proportion visible lorsque l'element depasse par au-dessus et occupe tout l'espace", () => {
-    let ratio = ratioEspaceOccupe(etat(-50, 150));
-
-    expect(ratio).toBe(1);
-  });
-
-  test("retourne la proportion visible lorsque l'element depasse par au-dessus et n'occupe pas tout l'espace", () => {
-    let ratio = ratioEspaceOccupe(etat(-50, 100));
-
-    expect(ratio).toBe(0.5);
-  });
-
-  test("retourne la proportion visible lorsque l'element depasse par en-dessous et n'occupe pas tout l'espace", () => {
-    let ratio = ratioEspaceOccupe(etat(50, 100));
-
-    expect(ratio).toBe(0.5);
+      expect(lePlusVisible).toBe(null);
+    });
   });
 });
