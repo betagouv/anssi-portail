c7b0a6f1e54440e44a015528286a0c0cb39420f8
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-01-29 17:24:55 +0100

    [SOIN] Extrais l’illustration dans un composant

---
+++ lib-svelte/src/niveaux-maturite/IllustrationNiveauMaturite.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import type { NiveauMaturite } from "./NiveauxMaturite.donnees";
+
+  export let niveauSelectionne: NiveauMaturite;
+</script>
+
+<div class="illustration">
+  <img
+    src="/assets/images/test-maturite/niveaux/{niveauSelectionne.id}.svg"
+    alt={`Illustration niveau ${niveauSelectionne.label}`}
+  />
+</div>
