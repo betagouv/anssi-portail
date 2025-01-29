103f0afdcf7d70d09c5275390429753dd19ecd24
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-01-29 17:22:12 +0100

    [SOIN] Extrais la sélection du niveau dans un composant

---
+++ lib-svelte/src/niveaux-maturite/SelectNiveauMaturite.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import {
+    type NiveauMaturite,
+    niveauxMaturite,
+  } from "./NiveauxMaturite.donnees";
+
+  export let niveauSelectionne: NiveauMaturite;
+</script>
+
+<select bind:value={niveauSelectionne}>
+  {#each niveauxMaturite as niveau}
+    <option value={niveau}>{niveau.label}</option>
+  {/each}
+</select>
