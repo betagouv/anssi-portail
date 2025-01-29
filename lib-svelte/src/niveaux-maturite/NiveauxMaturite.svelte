fb9eecb1d80f0116425e6d39aa37e2ee3f249bc8
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-01-29 17:07:28 +0100

    Affiche la grille haute pour les mobiles larges (>440px)

--- lib-svelte/src/niveaux-maturite/NiveauxMaturite.svelte
+++ lib-svelte/src/niveaux-maturite/NiveauxMaturite.svelte
@@ -5,51 +5,82 @@
   } from "./NiveauxMaturite.donnees";
 
   let niveauSelectionne: NiveauMaturite = niveauxMaturite[0];
+  let niveauSelectionne2: NiveauMaturite = niveauxMaturite[1];
 </script>
 
 <section class="niveaux">
   <div class="contenu-section">
-    <select bind:value={niveauSelectionne}>
-      {#each niveauxMaturite as niveau}
-        <option value={niveau}>{niveau.label}</option>
-      {/each}
-    </select>
+    <div class="grille-haute">
+      <select bind:value={niveauSelectionne}>
+        {#each niveauxMaturite as niveau}
+          <option value={niveau}>{niveau.label}</option>
+        {/each}
+      </select>
+      <select bind:value={niveauSelectionne2}>
+        {#each niveauxMaturite as niveau}
+          <option value={niveau}>{niveau.label}</option>
+        {/each}
+      </select>
 
-    <div class="illustration">
-      <img
-        src="/assets/images/test-maturite/niveaux/{niveauSelectionne.id}.svg"
-        alt={`Illustration niveau ${niveauSelectionne.label}`}
-      />
+      <div class="illustration">
+        <img
+          src="/assets/images/test-maturite/niveaux/{niveauSelectionne.id}.svg"
+          alt={`Illustration niveau ${niveauSelectionne.label}`}
+        />
+      </div>
+      <div class="illustration">
+        <img
+          src="/assets/images/test-maturite/niveaux/{niveauSelectionne2.id}.svg"
+          alt={`Illustration niveau ${niveauSelectionne.label}`}
+        />
+      </div>
+
+      <p class="description">{niveauSelectionne.description}</p>
+      <p class="description">{niveauSelectionne2.description}</p>
     </div>
-
-    <p class="description">{niveauSelectionne.description}</p>
 
     <h2>Les 6 critères pour évaluer votre niveau de maturité cyber</h2>
 
     <div class="details">
-      <h3>Prise en compte du risque</h3>
-      <p>{niveauSelectionne.priseEnCompteRisque}</p>
+      <div class="detail">
+        <h3>Prise en compte du risque</h3>
+        <p>{niveauSelectionne.priseEnCompteRisque}</p>
+      </div>
 
-      <h3>Posture à l'égard de la cyber</h3>
-      <p>{niveauSelectionne.posture}</p>
+      <div class="detail">
+        <h3>Posture à l'égard de la cyber</h3>
+        <p>{niveauSelectionne.posture}</p>
+      </div>
 
-      <h3>Pilotage de la sécurité</h3>
-      <p>{niveauSelectionne.pilotage}</p>
+      <div class="detail">
+        <h3>Pilotage de la sécurité</h3>
+        <p>{niveauSelectionne.pilotage}</p>
+      </div>
 
-      <h3>Resources humaines</h3>
-      <p>{niveauSelectionne.ressourcesHumaines}</p>
+      <div class="detail">
+        <h3>Resources humaines</h3>
+        <p>{niveauSelectionne.ressourcesHumaines}</p>
+      </div>
 
-      <h3>Budget</h3>
-      <p>{niveauSelectionne.budget}</p>
+      <div class="detail">
+        <h3>Budget</h3>
+        <p>{niveauSelectionne.budget}</p>
+      </div>
 
-      <h3>Adoption des solutions cyber</h3>
-      <p>{niveauSelectionne.adoptionSolutions}</p>
+      <div class="detail">
+        <h3>Adoption des solutions cyber</h3>
+        <p>{niveauSelectionne.adoptionSolutions}</p>
+      </div>
 
-      <h3>Niveau de sécurité</h3>
-      <p>{niveauSelectionne.niveauSecurite}</p>
+      <div class="detail">
+        <h3>Niveau de sécurité</h3>
+        <p>{niveauSelectionne.niveauSecurite}</p>
+      </div>
 
-      <h3>La priorité</h3>
-      <p>{niveauSelectionne.priorite}</p>
+      <div class="detail">
+        <h3>La priorité</h3>
+        <p>{niveauSelectionne.priorite}</p>
+      </div>
     </div>
   </div>
 </section>
