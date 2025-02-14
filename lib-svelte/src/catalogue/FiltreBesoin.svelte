dabc5fef8c9766805056b20e136b67a6155701a2
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-02-14 15:19:18 +0100

    Corrige les erreurs d’accessibilité
    ...relevées par Wave

--- lib-svelte/src/catalogue/FiltreBesoin.svelte
+++ lib-svelte/src/catalogue/FiltreBesoin.svelte
@@ -8,70 +8,65 @@
 <div class="choix-filtre-besoin">
   {#if filtreTousVisible}
     <label class:actif={!$rechercheParBesoin}>
-      <img
-        src="/assets/images/illustration-filtre-besoins-tous.svg"
-        alt="illustration tous"
-      />
       <input
         type="radio"
         name="filtre-besoin"
         value=""
         bind:group={$rechercheParBesoin}
-      /> <span>Tous les besoins</span>
+      />
+      <img
+        src="/assets/images/illustration-filtre-besoins-tous.svg"
+        alt="Illustration pour pour le filtre sur tous les besoins"
+      />
+      <span>Tous les besoins</span>
     </label>
   {/if}
-  <label
-    class:actif={$rechercheParBesoin === BesoinCyber.ETRE_SENSIBILISE}
-  >
-    <img
-      src="/assets/images/illustration-filtre-besoins-sensibiliser.svg"
-      alt="illustration Être sensibilisé"
-    />
+  <label class:actif={$rechercheParBesoin === BesoinCyber.ETRE_SENSIBILISE}>
     <input
       type="radio"
       name="filtre-besoin"
       value={BesoinCyber.ETRE_SENSIBILISE}
       bind:group={$rechercheParBesoin}
-    /> <span>Être sensibilisé</span>
-  </label>
-  <label
-          class:actif={$rechercheParBesoin === BesoinCyber.SE_FORMER}
-  >
-    <img
-            src="/assets/images/illustration-filtre-besoins-former.svg"
-            alt="illustration Se former"
     />
+    <img
+      src="/assets/images/illustration-filtre-besoins-sensibiliser.svg"
+      alt="Illustration pour le filtre des besoins Être sensibilisé"
+    /><span>Être sensibilisé</span>
+  </label>
+  <label class:actif={$rechercheParBesoin === BesoinCyber.SE_FORMER}>
     <input
-            type="radio"
-            name="filtre-besoin"
-            value={BesoinCyber.SE_FORMER}
-            bind:group={$rechercheParBesoin}
-    /> <span>Se former</span>
-  </label>
-  <label
-    class:actif={$rechercheParBesoin === BesoinCyber.SECURISER}
-  >
-    <img
-      src="/assets/images/illustration-filtre-besoins-securiser.svg"
-      alt="illustration Sécuriser"
+      type="radio"
+      name="filtre-besoin"
+      value={BesoinCyber.SE_FORMER}
+      bind:group={$rechercheParBesoin}
     />
+    <img
+      src="/assets/images/illustration-filtre-besoins-former.svg"
+      alt="Illustration pour le filtre des besoins Se former"
+    /><span>Se former</span>
+  </label>
+  <label class:actif={$rechercheParBesoin === BesoinCyber.SECURISER}>
     <input
       type="radio"
       name="filtre-besoin"
       value={BesoinCyber.SECURISER}
       bind:group={$rechercheParBesoin}
-    /> <span>Sécuriser</span>
+    />
+    <img
+      src="/assets/images/illustration-filtre-besoins-securiser.svg"
+      alt="Illustration pour le filtre des besoins Sécuriser"
+    /><span>Sécuriser</span>
   </label>
   <label class:actif={$rechercheParBesoin === BesoinCyber.REAGIR}>
-    <img
-      src="/assets/images/illustration-filtre-besoins-reagir.svg"
-      alt="illustration réagir"
-    />
     <input
       type="radio"
       name="filtre-besoin"
       value={BesoinCyber.REAGIR}
       bind:group={$rechercheParBesoin}
-    /> <span>Réagir</span>
+    />
+    <img
+      src="/assets/images/illustration-filtre-besoins-reagir.svg"
+      alt="Illustration pour le filtre des besoins Réagir"
+    /><span>Réagir</span>
   </label>
 </div>
