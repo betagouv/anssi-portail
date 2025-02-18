f16c07838024537b77758883f0f43de6c0d0c361
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-02-18 15:08:57 +0100

    Adapte l’affichage du partage pour les écrans large

--- lib-svelte/src/test-maturite/PartageTest.svelte
+++ lib-svelte/src/test-maturite/PartageTest.svelte
@@ -16,38 +16,45 @@
   };
 </script>
 
-<h2>Encouragez d’autres organisations à agir</h2>
-<p>
-  Aidez d’autres organisations à accéder à des services et ressources adaptés à
-  leur maturité cyber.
-</p>
-<h3>Partager</h3>
-<div class="boutons-partage">
-  <a
-    class="bouton secondaire"
-    target="_blank"
-    rel="noopener"
-    title="Partager sur LinkedIn"
-    on:click|preventDefault={partageLinkedIn}
-    href={urlLinkedIn}
-  >
-    <img src="/assets/images/icone-linkedin.svg" alt="Partager sur LinkedIn" />
-  </a>
+<div class="description">
+  <h2>Encouragez d’autres organisations à agir</h2>
+  <p>
+    Aidez d’autres organisations à accéder à des services et ressources adaptés
+    à leur maturité cyber.
+  </p>
+</div>
+<div class="partage">
+  <h3>Partager</h3>
+  <div class="boutons-partage">
+    <a
+      class="bouton secondaire"
+      target="_blank"
+      rel="noopener"
+      title="Partager sur LinkedIn"
+      on:click|preventDefault={partageLinkedIn}
+      href={urlLinkedIn}
+    >
+      <img
+        src="/assets/images/icone-linkedin.svg"
+        alt="Partager sur LinkedIn"
+      />
+    </a>
 
-  <a
-    class="bouton secondaire"
-    target="_blank"
-    rel="noopener external"
-    title="Partager par email"
-    href="mailto:?subject=Test de maturité Cyber&body=Lien vers le test : {lienTest}"
-  >
-    <img src="/assets/images/icone-email.svg" alt="Partager par email" />
-  </a>
+    <a
+      class="bouton secondaire"
+      target="_blank"
+      rel="noopener external"
+      title="Partager par email"
+      href="mailto:?subject=Test de maturité Cyber&body=Lien vers le test : {lienTest}"
+    >
+      <img src="/assets/images/icone-email.svg" alt="Partager par email" />
+    </a>
 
-  <button class="bouton secondaire" on:click={partageLien}>
-    <img
-      src="/assets/images/icone-copie-lien.svg"
-      alt="Copier dans le presse-papier"
-    />
-  </button>
+    <button class="bouton secondaire" on:click={partageLien}>
+      <img
+        src="/assets/images/icone-copie-lien.svg"
+        alt="Copier dans le presse-papier"
+      />
+    </button>
+  </div>
 </div>
