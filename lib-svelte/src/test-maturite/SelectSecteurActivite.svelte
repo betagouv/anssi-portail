71b57b22cdcf8bda41064f6a9564e4d40297b63e
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-02-18 16:49:53 +0100

    Utilise les sections de la nomenclature d’activités française (NAF) pour les secteurs d’activité

--- lib-svelte/src/test-maturite/SelectSecteurActivite.svelte
+++ lib-svelte/src/test-maturite/SelectSecteurActivite.svelte
@@ -1,25 +1,25 @@
 <select>
   <option disabled selected>Sélectionner une option</option>
-  <option>Banques (secteur bancaire)</option>
-  <option>Eau potable</option>
-  <option>Eaux usées</option>
-  <option>Énergie</option>
-  <option>Espace</option>
-  <option>Fabrication</option>
-  <option>
-    Fabrication, production et distribution de produits chimiques
-  </option>
-  <option>Fournisseurs numériques</option>
-  <option>Gestion des déchets</option>
-  <option>Gestion des services TIC</option>
-  <option>Infrastructure des marchés financiers</option>
-  <option>Infrastructure numérique</option>
-  <option>
-    Production transformation et distribution de denrées alimentaires
-  </option>
-  <option>Recherche</option>
-  <option>Santé</option>
-  <option>Services postaux et d'expédition</option>
-  <option>Transports</option>
-  <option>Autre secteur d'activité</option>
+  <option>Agriculture, sylviculture et pêche</option>
+  <option>Industries extractives</option>
+  <option>Industrie manufacturière</option>
+  <option>Production et distribution d'électricité, de gaz, de vapeur et d'air conditionné</option>
+  <option>Production et distribution d'eau ; assainissement, gestion des déchets et dépollution</option>
+  <option>Construction</option>
+  <option>Commerce ; réparation d'automobiles et de motocycles</option>
+  <option>Transports et entreposage</option>
+  <option>Hébergement et restauration</option>
+  <option>Information et communication</option>
+  <option>Activités financières et d'assurance</option>
+  <option>Activités immobilières</option>
+  <option>Activités spécialisées, scientifiques et techniques</option>
+  <option>Activités de services administratifs et de soutien</option>
+  <option>Administration publique</option>
+  <option>Enseignement</option>
+  <option>Santé humaine et action sociale</option>
+  <option>Arts, spectacles et activités récréatives</option>
+  <option>Autres activités de services</option>
+  <option>Activités des ménages en tant qu'employeurs ; activités indifférenciées des ménages en tant que producteurs de biens
+    et services pour usage propre</option>
+  <option>Activités extra-territoriales</option>
 </select>
