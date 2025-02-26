18b550bc0b026145bdb1b16536004ec2665037fc
Alexis Brones <alexis.brones@beta.gouv.fr>
2025-02-26 16:50:06 +0100

    Stocke les informations de connexion dans un cookie

---
+++ back/tests/api/cookie.ts
@@ -0,0 +1,12 @@
+const enObjet = (cookie: string) =>
+  cookie.split("; ").reduce((acc: Record<string, any>, v) => {
+    const [cle, valeur] = v.split("=");
+    try {
+      acc[cle] = JSON.parse(decodeURIComponent(valeur).slice(2));
+    } catch (error) {
+      acc[cle] = valeur;
+    }
+    return acc;
+  }, {});
+
+export { enObjet };
--- back/tests/api/ressourceConnexionOIDC.spec.ts
+++ back/tests/api/ressourceConnexionOIDC.spec.ts
@@ -1,17 +1,17 @@
 import { beforeEach, describe, it } from "node:test";
 import { Express } from "express";
-import { join } from "path";
 import request from "supertest";
 import assert from "node:assert";
 import { creeServeur } from "../../src/api/msc";
 import { ConfigurationServeur } from "../../src/api/configurationServeur";
 import { fauxFournisseurDeChemin } from "./fauxObjets";
 import { fabriqueMiddleware } from "../../src/api/middleware";
-import {adaptateurOIDC} from "../../src/api/adaptateurOIDC";
+import { enObjet } from "./cookie";
 
 describe("La ressource connexion OIDC", () => {
   describe("quand on requete GET sur /oidc/connexion", () => {
-    it("redirige vers l'adresse proconnect", async () => {
+    let serveur: Express;
+    beforeEach(() => {
       const configurationServeur: ConfigurationServeur = {
         fournisseurChemin: fauxFournisseurDeChemin,
         middleware: fabriqueMiddleware(),
@@ -25,12 +25,25 @@ describe("La ressource connexion OIDC", () => {
           },
         },
       };
-      let serveur = creeServeur(configurationServeur);
+      serveur = creeServeur(configurationServeur);
+    });
 
+    it("redirige vers l'adresse proconnect", async () => {
       const reponse = await request(serveur).get("/oidc/connexion");
 
       assert.equal(reponse.status, 302);
       assert.equal(reponse.headers.location, "une-adresse-proconnect");
+    });
+
+    it("ecrit un cookie avec le state et le nonce", async () => {
+      const reponse = await request(serveur).get("/oidc/connexion");
+
+      const cookieHeader = reponse.headers["set-cookie"];
+      assert.notEqual(cookieHeader, undefined);
+      const cookie = enObjet(cookieHeader[0]);
+      const { state, nonce } = cookie.AgentConnectInfo;
+      assert.equal(state, "un faux state");
+      assert.equal(nonce, "un faux nonce");
     });
   });
 });
