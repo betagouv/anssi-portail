e9e57dbcc15c59082bb748e5a4765807950d284e
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-02-28 12:06:39 +0100

    Écris les informations de l’utilisateur dans la session

--- back/src/api/oidc/ressourceApresAuthentificationOIDC.ts
+++ back/src/api/oidc/ressourceApresAuthentificationOIDC.ts
@@ -5,7 +5,12 @@ const ressourceApresAuthentificationOIDC = (
   configurationServeur: ConfigurationServeur
 ) => {
   let routeur = Router();
-  routeur.get('/', async (_requete, reponse) => {
+  routeur.get('/', async (requete, reponse) => {
+    const { adaptateurOIDC } = configurationServeur;
+    let { accessToken } = await adaptateurOIDC.recupereJeton(requete);
+    let informationsUtilisateur =
+      await adaptateurOIDC.recupereInformationsUtilisateur(accessToken);
+    requete.session! = { ...requete.session!, ...informationsUtilisateur };
     reponse.sendFile(
       configurationServeur.fournisseurChemin.cheminPageJekyll(
         'apres-authentification'
--- back/tests/api/oidc/ressourceApresAuthentificationOIDC.spec.ts
+++ back/tests/api/oidc/ressourceApresAuthentificationOIDC.spec.ts
@@ -7,37 +7,70 @@ import { fabriqueMiddleware } from '../../../src/api/middleware';
 import { creeServeur } from '../../../src/api/msc';
 import { fauxAdaptateurOIDC, fauxFournisseurDeChemin } from '../fauxObjets';
 import { join } from 'node:path';
+import { AdaptateurOIDC } from '../../../src/api/oidc/adaptateurOIDC';
+import { decodeSessionDuCookie } from '../cookie';
 
 describe('La ressource apres authentification OIDC', () => {
   describe('quand on fait un GET sur /oidc/apres-authentification', () => {
     let serveur: Express;
     let fournisseurChemin = fauxFournisseurDeChemin;
+    let adaptateurOIDC: AdaptateurOIDC;
+
     beforeEach(() => {
+      adaptateurOIDC = fauxAdaptateurOIDC;
       const configurationServeur: ConfigurationServeur = {
         fournisseurChemin,
         middleware: fabriqueMiddleware(),
-        adaptateurOIDC: fauxAdaptateurOIDC,
+        adaptateurOIDC,
       };
       serveur = creeServeur(configurationServeur);
     });
-    it('recois 200', async () => {
+
+    it('reçoit 200', async () => {
       const reponse = await request(serveur).get(
         '/oidc/apres-authentification'
       );
 
       assert.equal(reponse.status, 200);
     });
-    it('sers la page apres-authentification', async () => {
+
+    it('sert la page apres-authentification', async () => {
       let nomPageDemande;
       fournisseurChemin.cheminPageJekyll = (nomPage) => {
         nomPageDemande = nomPage;
         return join(process.cwd(), 'tests', 'ressources', 'factice.html');
       };
-      const reponse = await request(serveur).get(
+      await request(serveur).get('/oidc/apres-authentification');
+
+      assert.equal(nomPageDemande, 'apres-authentification');
+    });
+
+    it('vérifie la récupération de jeton', async () => {
+      adaptateurOIDC.recupereJeton = async () => {
+        return { idToken: 'xx', accessToken: 'y' };
+      };
+      adaptateurOIDC.recupereInformationsUtilisateur = async (accessToken) => {
+        if (accessToken === 'y') {
+          return {
+            prenom: 'Jeanne',
+            nom: 'Dupont',
+            email: 'jeanne.dupont',
+            siret: '1234',
+          };
+        }
+        throw new Error('Aurait du être appelé avec le bon access token');
+      };
+
+      const reponse: any = await request(serveur).get(
         '/oidc/apres-authentification'
       );
 
-      assert.equal(nomPageDemande, 'apres-authentification');
+      const session = decodeSessionDuCookie(reponse, 0);
+      assert.notEqual(session, undefined);
+      assert.equal(session.prenom, 'Jeanne');
+      assert.equal(session.nom, 'Dupont');
+      assert.equal(session.email, 'jeanne.dupont');
+      assert.equal(session.siret, '1234');
     });
   });
 });
