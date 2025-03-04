65647e018ba83f91946ebf2d826aea2c711b1194
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-03-04 10:36:24 +0100

    Affiche la zone d’identification
    ...avec le prénom et le nom si l’utilisateur ou un lien de connexion

--- front/lib-svelte/src/identification/ZoneIdentification.svelte
+++ front/lib-svelte/src/identification/ZoneIdentification.svelte
@@ -1,4 +1,31 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+  import axios from "axios";
+
+  type Profil = {
+    prenom: string;
+    nom: string;
+    email: string;
+    siret: string;
+  };
+
+  let profil: Profil | undefined;
+
+  onMount(async () => {
+    let reponse = await axios.get("/profil");
+    let data = reponse.data as any;
+    if (data.email) {
+      profil = data as Profil;
+    } else {
+      profil = undefined;
+    }
+  });
 </script>
 
-<span></span>
+<span>
+  {#if profil === undefined}
+    <a href="/connexion">Se connecter</a>
+  {:else}
+    {profil.prenom} {profil.nom}
+  {/if}
+</span>
