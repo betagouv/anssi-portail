8ae5a6318ede24486b81c4e357d369796bc5273a
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-03-06 16:51:41 +0100

    Ajoute le code des secteurs

--- front/lib-svelte/src/test-maturite/SelectSecteurActivite.svelte
+++ front/lib-svelte/src/test-maturite/SelectSecteurActivite.svelte
@@ -1,25 +1,10 @@
+<script lang="ts">
+  import { secteurs } from "./TestMaturite.donnees";
+</script>
+
 <select>
-  <option disabled selected>Sélectionner une option</option>
-  <option>Agriculture, sylviculture et pêche</option>
-  <option>Industries extractives</option>
-  <option>Industrie manufacturière</option>
-  <option>Production et distribution d'électricité, de gaz, de vapeur et d'air conditionné</option>
-  <option>Production et distribution d'eau ; assainissement, gestion des déchets et dépollution</option>
-  <option>Construction</option>
-  <option>Commerce ; réparation d'automobiles et de motocycles</option>
-  <option>Transports et entreposage</option>
-  <option>Hébergement et restauration</option>
-  <option>Information et communication</option>
-  <option>Activités financières et d'assurance</option>
-  <option>Activités immobilières</option>
-  <option>Activités spécialisées, scientifiques et techniques</option>
-  <option>Activités de services administratifs et de soutien</option>
-  <option>Administration publique</option>
-  <option>Enseignement</option>
-  <option>Santé humaine et action sociale</option>
-  <option>Arts, spectacles et activités récréatives</option>
-  <option>Autres activités de services</option>
-  <option>Activités des ménages en tant qu'employeurs ; activités indifférenciées des ménages en tant que producteurs de biens
-    et services pour usage propre</option>
-  <option>Activités extra-territoriales</option>
+  <option disabled selected value="null">Sélectionner une option</option>
+  {#each secteurs as secteur}
+    <option value={secteur.code}>{secteur.libelle}</option>
+  {/each}
 </select>
