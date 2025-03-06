fe23c25a0156e788f0dd659b4199e1d2a0e15b14
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-03-06 17:17:54 +0100

    Utilise les tranches d’effectif de l’INSEE
    ... et transforme les 3 options de taille en une liste déroulante. Ceci est fait afin de coller au mieux avec ce qui nous sera retourné depuis l’API recherche entreprise.

---
+++ front/lib-svelte/src/test-maturite/SelectTailleOrganisation.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { tranchesEffectifEtablissement } from "./TestMaturite.donnees";
+</script>
+
+<select>
+  <option disabled selected value="null">Sélectionner une option</option>
+  {#each tranchesEffectifEtablissement as tranche}
+    <option value={tranche.code}>{tranche.libelle}</option>
+  {/each}
+</select>
