682b4f98d51254ec325d1c8dad0bbc71fcb9c14c
Stéfanie LOISELEUR <stloiseleur@ippon.fr>
2025-03-10 12:10:13 +0100

    Ajoute une ressource pour interroger la Recherche Entreprise
    Note: il faudra remplacer les console.error par sentry quand ce sera disponible

---
+++ back/tests/api/ressourceAnnuaireOrganisations.spec.ts
@@ -0,0 +1,78 @@
+import {beforeEach, describe, it} from "node:test";
+import request from "supertest";
+import {Express} from "express";
+import {creeServeur} from "../../src/api/msc";
+import {
+  fauxAdaptateurJWT,
+  fauxAdaptateurOIDC,
+  fauxAdaptateurRechercheEntreprise,
+  fauxFournisseurDeChemin
+} from "./fauxObjets";
+import {fabriqueMiddleware} from "../../src/api/middleware";
+import {EntrepotUtilisateurMemoire} from "../persistance/entrepotUtilisateurMemoire";
+import {
+  AdaptateurRechercheEntreprise,
+  ResultatRechercheEntreprise
+} from "../../src/infra/adaptateurRechercheEntreprise";
+import assert from "node:assert";
+
+describe('quand requête GET sur `/api/annuaire/organisations`', () => {
+  let serveur: Express;
+  let configurationServeur: any;
+
+  beforeEach(() => {
+    configurationServeur = {
+      fournisseurChemin: fauxFournisseurDeChemin,
+      middleware: fabriqueMiddleware(),
+      adaptateurOIDC: fauxAdaptateurOIDC,
+      adaptateurJWT: fauxAdaptateurJWT,
+      adaptateurRechercheEntreprise: fauxAdaptateurRechercheEntreprise,
+      entrepotUtilisateur: new EntrepotUtilisateurMemoire(),
+    };
+    serveur = creeServeur(configurationServeur);
+  });
+
+  it('aseptise les paramètres de la requête', async (done) => {
+    let termeCherche;
+    let departementCherche;
+    let unAdaptateurRechercheEntreprise: AdaptateurRechercheEntreprise = {
+      rechercheOrganisations: async (terme: string, departement: string | null) => {
+        termeCherche = terme;
+        departementCherche = departement;
+        return ([]);
+      }
+    };
+    serveur = creeServeur({...configurationServeur, adaptateurRechercheEntreprise: unAdaptateurRechercheEntreprise});
+
+    await request(serveur).get('/api/annuaire/organisations?recherche=ma>recherche&departement=mon>departement');
+
+    assert.equal(termeCherche, 'ma&gt;recherche');
+    assert.equal(departementCherche, 'mon&gt;departement');
+  });
+
+  it('retourne une erreur HTTP 400 si le terme de recherche est vide', async () => {
+    let reponse = await request(serveur).get('/api/annuaire/organisations?recherche=&departement=mon>departement');
+    assert.equal(reponse.status, 400);
+  });
+
+  it("recherche les organisations correspondantes grâce au service d'annuaire", async (done) => {
+    let adaptateurAppele = false;
+    let termeCherche;
+    let departementCherche;
+    let unAdaptateurRechercheEntreprise: AdaptateurRechercheEntreprise = {
+      rechercheOrganisations: async (terme: string, departement: string | null): Promise<ResultatRechercheEntreprise[]> => {
+        adaptateurAppele = true;
+        termeCherche = terme;
+        departementCherche = departement;
+        return [{nom: 'un résultat', departement: '01', siret: '1234'}];
+      }
+    };
+    serveur = creeServeur({...configurationServeur, adaptateurRechercheEntreprise: unAdaptateurRechercheEntreprise});
+
+    await request(serveur).get('/api/annuaire/organisations?recherche=marecherche&departement=mondepartement');
+
+    assert.equal(adaptateurAppele, true);
+    assert.equal(termeCherche, 'marecherche');
+    assert.equal(departementCherche, 'mondepartement');
+  });
+});
