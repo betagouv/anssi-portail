4cb4391feefaf477bb78e4a42b5afcd00421067a
Alexis Brones <alexis.brones@beta.gouv.fr>
2025-03-10 17:13:18 +0100

    Ajoute la possibilité de faire de mettre du style directement dans les composants
    ... On ajoute une option au build vite pour généré un fichier CSS global pour tous les composants Svelte. On peut faire du SCSS directement dans les balises `<style lang="scss>`.

--- front/lib-svelte/vite.config.ts
+++ front/lib-svelte/vite.config.ts
@@ -5,6 +5,7 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 export default defineConfig({
   plugins: [svelte()],
   build: {
+    cssCodeSplit: false,
     rollupOptions: {
       input: {
         catalogue: "src/main-catalogue.ts",
