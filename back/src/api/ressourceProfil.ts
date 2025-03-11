8a1c18eded22bc37cda2128cf5343502a0d4ca12
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-03-11 16:33:44 +0100

    Utilise le siret des données du token "création de compte" pour compléter les informations de l'organisation

--- back/src/api/ressourceProfil.ts
+++ back/src/api/ressourceProfil.ts
@@ -1,7 +1,8 @@
 import { ConfigurationServeur } from './configurationServeur';
 import { Request, Response, Router } from 'express';
+import { ResultatRechercheEntreprise } from '../infra/adaptateurRechercheEntreprise';
 
-const ressourceProfil = ({adaptateurJWT}: ConfigurationServeur) => {
+const ressourceProfil = ({adaptateurJWT, adaptateurRechercheEntreprise}: ConfigurationServeur) => {
   const routeur = Router();
   routeur.get('/', (requete: Request, reponse: Response) => {
     reponse.send({
@@ -12,12 +13,23 @@ const ressourceProfil = ({adaptateurJWT}: ConfigurationServeur) => {
     });
   });
 
-  routeur.get('/verification-token-creation-compte', (requete: Request, reponse: Response) => {
+  routeur.get('/verification-token-creation-compte', async (requete: Request, reponse: Response) => {
     const { token } = requete.query;
     try {
       if(!token) throw new Error('Token manquant');
       const informationsUtilisateur = adaptateurJWT.decode(token as string);
-      reponse.send(informationsUtilisateur);
+
+      let organisation: ResultatRechercheEntreprise | undefined;
+      if(informationsUtilisateur.siret) {
+        organisation = (await adaptateurRechercheEntreprise.rechercheOrganisations(informationsUtilisateur.siret, null))[0];
+      }
+
+      reponse.send({
+        prenom: informationsUtilisateur.prenom,
+        nom: informationsUtilisateur.nom,
+        email: informationsUtilisateur.email,
+        ...(organisation && { organisation })
+      });
     } catch(e) {
       reponse.sendStatus(401);
     }
