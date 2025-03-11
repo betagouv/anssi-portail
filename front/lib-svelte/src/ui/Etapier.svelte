dddcf7bc4cda4f712a440041b295e17a54d4da58
Alexis Brones <alexis.brones@beta.gouv.fr>
2025-03-11 11:53:54 +0100

    Résout un "warning" Svelte concernant l'ambiguité des balises autofermantes

--- front/lib-svelte/src/ui/Etapier.svelte
+++ front/lib-svelte/src/ui/Etapier.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-    export let etapeCourante: number;
-    export let nombreEtapes: number;
+  export let etapeCourante: number;
+  export let nombreEtapes: number;
 </script>
 
 <div class="etapier">
-    {#each new Array(nombreEtapes).fill(0) as _, i}
-        <span class:accessible={i <= etapeCourante} />
-    {/each}
+  {#each new Array(nombreEtapes).fill(0) as _, i}
+    <span class:accessible={i <= etapeCourante}></span>
+  {/each}
 </div>
