897d54325e5048790269c1d047012622dcb82890
Alexis Brones <alexis.brones@beta.gouv.fr>
2025-03-12 15:12:19 +0100

    Ajoute les informations restantes venant de ProConnect à l'utilisateur pour le persister

--- back/src/metier/utilisateur.ts
+++ back/src/metier/utilisateur.ts
@@ -1,3 +1,10 @@
 export interface Utilisateur {
   email: string;
+  prenom: string;
+  nom: string;
+  telephone: string;
+  postes: string[];
+  siretEntite: string;
+  cguAcceptees: boolean;
+  infolettreAcceptee: boolean;
 }
