cfc4db4edb62bb35e11991877070bd8299555f7b
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-03-12 11:23:48 +0100

    Ajoute un adaptateur de gestion d'erreur Sentry

---
+++ back/src/infra/adaptateurGestionErreurSentry.ts
@@ -0,0 +1,30 @@
+import { Express, Request, Response, NextFunction } from 'express';
+import * as Sentry from '@sentry/node';
+import { adaptateurEnvironnement } from './adaptateurEnvironnement';
+
+export interface AdaptateurGestionErreur {
+  initialise(applicationExpress: Express): void;
+  controleurErreurs(erreur: Error, requete: Request, reponse: Response, suite: NextFunction): void;
+}
+
+export const adaptateurGestionErreurSentry: AdaptateurGestionErreur = {
+  initialise: (applicationExpress: Express) => {
+    const config = adaptateurEnvironnement.sentry();
+
+    Sentry.init({
+      dsn: config.dsn(),
+      environment: config.environnement(),
+      integrations: [
+        new Sentry.Integrations.Express({ app: applicationExpress }),
+        new Sentry.Integrations.Postgres(),
+        ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
+      ],
+    });
+    Sentry.setTag('msc-source', 'backend');
+
+    applicationExpress.use(Sentry.Handlers.requestHandler());
+    applicationExpress.use(Sentry.Handlers.tracingHandler());
+  },
+  controleurErreurs: (erreur: Error, requete: Request, reponse: Response, suite: NextFunction) =>
+    Sentry.Handlers.errorHandler()(erreur, requete, reponse, suite),
+};
