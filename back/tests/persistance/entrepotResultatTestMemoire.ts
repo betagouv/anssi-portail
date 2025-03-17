d124e202728d023b9b39deacb3170517786d2b44
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-03-17 16:37:56 +0100

    Sauvegarde le résultat du test de maturité lorsque l’utilisateur n’est pas connecté

--- back/tests/persistance/entrepotResultatTestMemoire.ts
+++ back/tests/persistance/entrepotResultatTestMemoire.ts
@@ -1,5 +1,5 @@
-import {EntrepotResultatTest} from '../../src/metier/entrepotResultatTest';
-import {ResultatTestMaturite} from '../../src/metier/resultatTestMaturite';
+import { EntrepotResultatTest } from '../../src/metier/entrepotResultatTest';
+import { ResultatTestMaturite } from '../../src/metier/resultatTestMaturite';
 
 export class EntrepotResultatTestMemoire implements EntrepotResultatTest {
   entites: ResultatTestMaturite[] = [];
@@ -7,9 +7,14 @@ export class EntrepotResultatTestMemoire implements EntrepotResultatTest {
   async ajoute(resultatTest: ResultatTestMaturite): Promise<void> {
     this.entites.push(resultatTest);
   }
+
   async dernierPourUtilisateur(
     email: string
   ): Promise<ResultatTestMaturite | undefined> {
-    return this.entites.find(entity => entity.emailUtilisateur === email) ;
+    return this.entites.find((entite) => entite.emailUtilisateur === email);
   }
+
+  tous = async (): Promise<ResultatTestMaturite[]> => {
+    return [...this.entites];
+  };
 }
