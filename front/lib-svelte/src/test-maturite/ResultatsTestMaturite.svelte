d3df9045239c84adbc281ceb20e6cc250293c6e2
LauraMondoloni31 <154080608+LauraMondoloni31@users.noreply.github.com>
2025-03-17 12:52:16 +0100

    modif-wording

--- front/lib-svelte/src/test-maturite/ResultatsTestMaturite.svelte
+++ front/lib-svelte/src/test-maturite/ResultatsTestMaturite.svelte
@@ -34,7 +34,7 @@
 
 <Hero
   titre="Résultat de maturité cyber"
-  description="Ce résultat nous permet de vous guider et de vous fournir les informations et les outils essentiels pour agir et améliorer votre niveau de maturité cyber."
+  description="Ce résultat nous permet de vous guider et de vous fournir les informations et les outils essentiels pour agir sur votre maturité cyber."
 />
 <section class="resultats-test">
   <div class="contenu-section">
