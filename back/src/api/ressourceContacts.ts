8082ff38b1520a7cd62a7985638d72bd8a55ee9c
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-03-17 17:14:52 +0100

    Ajoute une ressource permettant d'obtenir les contacts pour l'utilisateur courant
    ... en fonction de sa région

--- back/src/api/ressourceContacts.ts
+++ back/src/api/ressourceContacts.ts
@@ -1,19 +1,30 @@
 import { Request, Response, Router } from 'express';
 import { ConfigurationServeur } from './configurationServeur';
-import { regionDuDepartement } from '../metier/referentielRegions';
+import { contactsParRegion } from '../metier/referentielContacts';
+import { CodeDepartement, regionDuDepartement } from '../metier/referentielDepartements';
 
-const ressourceContacts = ({ middleware, adaptateurProfilAnssi }: ConfigurationServeur) => {
+const ressourceContacts = ({
+  middleware,
+  adaptateurProfilAnssi,
+}: ConfigurationServeur) => {
   const routeur = Router();
-  routeur.get('/', middleware.verifieJWT, async (requete: Request, reponse: Response) => {
-    const profilANSSI = await adaptateurProfilAnssi.recupere(requete.emailUtilisateurCourant!);
-    if(!profilANSSI) {
-      reponse.sendStatus(404);
-      return;
-    }
+  routeur.get(
+    '/',
+    middleware.verifieJWT,
+    async (requete: Request, reponse: Response) => {
+      const profilANSSI = await adaptateurProfilAnssi.recupere(
+        requete.emailUtilisateurCourant!
+      );
+      if (!profilANSSI) {
+        reponse.sendStatus(404);
+        return;
+      }
 
-    const region = regionDuDepartement(profilANSSI.organisation.departement);
-    reponse.sendStatus(200);
-  });
+      const codeRegion = regionDuDepartement(profilANSSI.organisation.departement as CodeDepartement);
+      
+      reponse.json(contactsParRegion[codeRegion]);
+    }
+  );
   return routeur;
 };
 
