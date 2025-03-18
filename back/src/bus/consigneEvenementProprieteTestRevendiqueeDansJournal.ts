65762862e313140a6ca3894529a3a6146f80e9c1
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2025-03-18 11:10:06 +0100

    Câble l'abonné de l'évènement ProprieteTestRevendiquee

---
+++ back/src/bus/consigneEvenementProprieteTestRevendiqueeDansJournal.ts
@@ -0,0 +1,19 @@
+import { AdaptateurHorloge } from '../infra/adaptateurHorloge';
+import { AdaptateurJournal } from '../infra/adaptateurJournal';
+import { ProprieteTestRevendiquee } from './proprieteTestRevendiquee';
+
+export const consigneEvenementProprieteTestRevendiqueeDansJournal = ({
+  adaptateurJournal,
+  adaptateurHorloge,
+}: {
+  adaptateurJournal: AdaptateurJournal;
+  adaptateurHorloge: AdaptateurHorloge;
+}) => {
+  return async function (evenement: ProprieteTestRevendiquee) {
+    await adaptateurJournal.consigneEvenement({
+      donnees: evenement,
+      type: 'PROPRIETE_TEST_REVENDIQUEE',
+      date: adaptateurHorloge.maintenant(),
+    });
+  };
+};
