64438c5ca7ea7ae5237cdd7ea6c558b163a5b1c2
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-03-19 12:13:08 +0100

    Affiche la page du résultat de test
    ...avec un contenu fictif pour l’instant

--- front/lib-svelte/src/test-maturite/MaturiteUtilisateur.svelte
+++ front/lib-svelte/src/test-maturite/MaturiteUtilisateur.svelte
@@ -1,4 +1,23 @@
 <script lang="ts">
+  import ResultatsTestMaturite from './ResultatsTestMaturite.svelte';
+  import { questionnaireStore } from './stores/questionnaire.store';
+  import { onMount } from 'svelte';
+  import type { IdRubrique } from './TestMaturite.donnees';
+  import axios from 'axios';
 
+  questionnaireStore.initialise();
+
+  type DernierResultatTest = {
+    reponses: Record<IdRubrique, number>;
+  };
+
+  onMount(async () => {
+    const reponseHttp = await axios.get<DernierResultatTest>(
+      '/api/resultats-test/dernier'
+    );
+    const reponses = reponseHttp.data.reponses;
+    questionnaireStore.chargeReponses(reponses);
+  });
 </script>
-Bientôt disponible
+
+<ResultatsTestMaturite />
