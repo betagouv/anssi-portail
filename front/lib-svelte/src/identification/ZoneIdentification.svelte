41b154625e42ff8207f9c1679a9ebad8ba94a38a
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-03-19 19:18:30 +0100

    [SOIN] Déplace le style de la zone d'identification dans le composant Svelte

--- front/lib-svelte/src/identification/ZoneIdentification.svelte
+++ front/lib-svelte/src/identification/ZoneIdentification.svelte
@@ -12,3 +12,39 @@
     <a href="/oidc/deconnexion">Se déconnecter</a>
   </div>
 {/if}
+
+<style lang="scss">
+  .profil-connecte,
+  .profil-deconnecte,
+  .profil-connecte > a,
+  .libelle-profil {
+    display: flex;
+    gap: 8px;
+    align-items: center;
+    padding: 4px 8px;
+
+    &:before {
+      width: 16px;
+      height: 16px;
+      line-height: 16px;
+    }
+  }
+
+  .profil-connecte > a,
+  .profil-deconnecte > a,
+  .libelle-profil {
+    color: #000091;
+  }
+
+  .profil-deconnecte > a:before {
+    content: url('/assets/images/icone-profil-non-connecte.svg');
+  }
+
+  .profil-connecte > a:before {
+    content: url('/assets/images/icone-profil-deconnecte.svg');
+  }
+
+  .libelle-profil:before {
+    content: url('/assets/images/icone-profil-connecte.svg');
+  }
+</style>
