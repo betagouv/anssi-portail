a9f4e575d5ab54a3b77e4ae3bd44716126c5fb04
Alexis Brones <alexis.brones@beta.gouv.fr>
2025-03-19 12:32:00 +0100

    Ajoute un utilisateur par défaut pour le développement local

--- back/src/infra/adaptateurProfilAnssiVide.ts
+++ back/src/infra/adaptateurProfilAnssiVide.ts
@@ -1,7 +1,12 @@
 import { AdaptateurProfilAnssi } from "./adaptateurProfilAnssi";
 
-export const adaptateurProfilAnssiVide = (): AdaptateurProfilAnssi => (
-{
-  recupere: async () => undefined,
+export const adaptateurProfilAnssiVide = (): AdaptateurProfilAnssi => ({
+  recupere: async () => ({
+    prenom: 'Jeanne',
+    nom: 'Dupont',
+    email: 'jeanne.dupont@mail.fr',
+    domainesSpecialite:['RSSI'],
+    organisation: {nom:'ANSSI', siret:'12345678901234', departement: '33'}
+  }),
   metsAJour: async () => {},
 });
