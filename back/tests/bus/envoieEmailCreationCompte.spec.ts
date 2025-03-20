921bf21a533140051b549d5b0e2cb0fe3e8daec5
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-03-20 17:18:33 +0100

    Ajoute une abonné qui envoie un email en cas de création de compte

---
+++ back/tests/bus/envoieEmailCreationCompte.spec.ts
@@ -0,0 +1,27 @@
+import { describe, it } from 'node:test';
+import assert from 'node:assert';
+import { AdaptateurEmail } from '../../src/metier/adaptateurEmail';
+import { envoieEmailCreationCompte } from '../../src/bus/envoieEmailCreationCompte';
+import { CompteCree } from '../../src/bus/compteCree';
+
+describe("L'abonnement qui envoie un email de création de compte", () => {
+  it('envoie un email de bienvenue', () => {
+    let donneesRecues;
+    const adaptateurEmail: AdaptateurEmail = {
+      envoieEmailBienvenue: async ({ email, prenom }) => {
+        donneesRecues = { email, prenom };
+      },
+    };
+
+    envoieEmailCreationCompte({ adaptateurEmail })(
+      new CompteCree({
+        email: 'jeanne.dupond@mail.fr',
+        prenom: 'Jeanne'
+      })
+    );
+
+    assert.notEqual(donneesRecues, undefined);
+    assert.equal(donneesRecues!.email, 'jeanne.dupond@mail.fr');
+    assert.equal(donneesRecues!.prenom, 'Jeanne');
+  });
+});
