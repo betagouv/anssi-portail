f1765e3f1fb23c254503c28b86b0d9655850e393
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2025-03-20 16:48:12 +0100

    Ajoute l'entrepôt Postgres des favoris

---
+++ back/src/infra/entrepotFavoriPostgres.ts
@@ -0,0 +1,25 @@
+import Knex from 'knex';
+import config from '../../knexfile';
+import { EntrepotFavori } from '../metier/entrepotFavori';
+import { Favori } from '../metier/favori';
+
+export class EntrepotFavoriPostgres implements EntrepotFavori {
+  knex: Knex.Knex;
+
+  constructor() {
+    this.knex = Knex(config);
+  }
+
+  async tousCeuxDeUtilisateur(emailUtilisateur: string): Promise<Favori[]> {
+    return this.knex('favoris')
+      .where('emailUtilisateur', emailUtilisateur)
+      .orderBy('date_ajout', 'desc');
+  }
+
+  async ajoute(favori: Favori): Promise<void> {
+    await this.knex('favoris').insert({
+      email_utilisateur: favori.emailUtilisateur,
+      id: favori.id,
+    });
+  }
+}
