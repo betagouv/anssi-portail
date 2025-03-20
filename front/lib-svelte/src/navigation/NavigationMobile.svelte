f035e7a48c3d9654501f276e4e0019cc47835bef
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-03-20 10:30:28 +0100

    Modifie les liens de la navigation en mode connecté

--- front/lib-svelte/src/navigation/NavigationMobile.svelte
+++ front/lib-svelte/src/navigation/NavigationMobile.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   import { fly } from 'svelte/transition';
 
+  import { profilStore } from '../stores/profil.store';
   import LienNavigationMobile from './LienNavigationMobile.svelte';
   import { mount, onDestroy, onMount, unmount } from 'svelte';
   import MenuBurger from './MenuBurger.svelte';
   import ZoneIdentification from '../identification/ZoneIdentification.svelte';
+  import LienNavigation from './LienNavigation.svelte';
 
   const cheminRelatif = window.location.pathname;
   let ouvert = false;
@@ -24,24 +26,28 @@
   onDestroy(() => {
     unmount(menuBurger);
   });
+
+  $: estConnecte = !!$profilStore;
 </script>
 
 {#if ouvert}
   <nav class="menu-mobile" transition:fly={{ x: 300 }}>
-    <a class="fermer" id="menu-mobile-fermer" on:click={() => ouvert = false}>Fermer</a>
+    <button class="fermer" on:click={() => ouvert = false}>Fermer</button>
     <ZoneIdentification estMobile/>
     <div class="choix">
-      <LienNavigationMobile href="/" label="Accueil" />
-      <LienNavigationMobile href="/catalogue/" label="Explorer le catalogue complet" />
+      {#if !estConnecte}
+        <LienNavigation href="/" label="Accueil"/>
+      {/if}
+      <LienNavigationMobile href="/catalogue/" label={estConnecte ? 'Le catalogue des services' : 'Explorer le catalogue complet'}/>
       <LienNavigationMobile href="/nis2/" label="Vous accompagner avec NIS2" />
       <details class:actif={ cheminRelatif === '/parcours-debuter/' ||  cheminRelatif === '/parcours-approfondir/'}>
-        <summary>Découvrir notre sélection</summary>
+        <summary>{estConnecte ? 'Notre sélection' : 'Découvrir notre sélection'}</summary>
         <div class="choix">
           <LienNavigationMobile href="/parcours-debuter/" label="Les services pour se lancer" dansMenuDeroulant />
           <LienNavigationMobile href="/parcours-approfondir/" label="Les services pour approfondir" dansMenuDeroulant />
         </div>
       </details>
-      <LienNavigationMobile href="/test-maturite/" label="Tester votre maturité cyber" />
+      <LienNavigationMobile href={estConnecte ? '/ma-maturite' : '/test-maturite/'} label={estConnecte ? 'Votre maturité cyber' : 'Tester votre maturité cyber'} />
     </div>
   </nav>
 {/if}
@@ -59,10 +65,16 @@
     top: 0;
     left: 0;
 
-    #menu-mobile-fermer {
+    button.fermer {
       align-self: flex-end;
       color: #000091;
       border: none;
+      background: none;
+      display: flex;
+      gap: 8px;
+      align-items: center;
+      padding: 0;
+      line-height: 24px;
 
       &:after {
         content: url('/assets/images/icone-croix.svg');
