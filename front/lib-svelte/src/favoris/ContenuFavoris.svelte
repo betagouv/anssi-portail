b1a5a3930c789e9f3a51363a13732a32701c6476
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2025-03-24 08:53:51 +0100

    [SOIN] Extrais le contenu des favoris
    ...afin de préparer la page Favoris Partagés, qui ressemble mais n'a pas les mêmes bannières/CTA

---
+++ front/lib-svelte/src/favoris/ContenuFavoris.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import CarteItem from '../catalogue/CarteItem.svelte';
+  import type { ItemCyber } from '../catalogue/Catalogue.types.js';
+
+  export let itemsEnFavori: ItemCyber[] = [];
+  export let avecBoutonFavori: boolean;
+</script>
+
+<div class="grille">
+  {#each itemsEnFavori as itemCyber}
+    <CarteItem {itemCyber} {avecBoutonFavori} />
+  {/each}
+</div>
