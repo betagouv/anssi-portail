521a6958ac95d79eeab82c22beb1a5b1a2daedb5
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-03-26 11:07:43 +0100

    [MATURITE] Affiche des onglets
    ... permettant d’accéder à la comparaison qui contient une annonce sur la disponibilité prochaine de cette fonctionnalité

--- front/lib-svelte/src/test-maturite/ResultatsTestMaturite.svelte
+++ front/lib-svelte/src/test-maturite/ResultatsTestMaturite.svelte
@@ -1,17 +1,15 @@
 <script lang="ts">
   import Hero from '../Hero.svelte';
   import TuilesMaturite from './TuilesMaturite.svelte';
-  import { type IdNiveau } from '../niveaux-maturite/NiveauxMaturite.donnees';
-  import {
-    questionnaireStore,
-    resultatsQuestionnaire,
-  } from './stores/questionnaire.store';
+  import { type IdNiveau, niveauxMaturite } from '../niveaux-maturite/NiveauxMaturite.donnees';
+  import { questionnaireStore, resultatsQuestionnaire } from './stores/questionnaire.store';
   import PubliciteMesServicesCyber from './PubliciteMesServicesCyber.svelte';
   import RadarMaturite from './RadarMaturite.svelte';
   import TuileVersParcours from './TuileVersParcours.svelte';
-  import { niveauxMaturite } from '../niveaux-maturite/NiveauxMaturite.donnees';
   import PartageTest from './PartageTest.svelte';
   import { questions } from './TestMaturite.donnees';
+  import Onglet from '../ui/Onglet.svelte';
+  import ConteneurOnglets from '../ui/ConteneurOnglets.svelte';
 
   export let affichePubMsc = true;
   export let afficheRappelReponses = false;
@@ -28,7 +26,7 @@
   $: moyenne =
     $questionnaireStore.toutesLesReponses.reduce(
       (acc, valeur) => acc + valeur,
-      0
+      0,
     ) / $questionnaireStore.toutesLesReponses.length;
 
   $: idNiveau = calculeIdNiveau(moyenne);
@@ -36,6 +34,8 @@
   $: niveau =
     niveauxMaturite.find((niveau) => niveau.id === idNiveau) ||
     niveauxMaturite[0];
+
+  let ongletActif: 'votre-organisation' | 'comparaison' = 'votre-organisation';
 </script>
 
 <Hero
@@ -43,77 +43,111 @@
   description="Ce résultat nous permet de vous guider et de vous fournir les informations et les outils essentiels pour agir sur votre maturité cyber."
   ariane="Tester votre maturité cyber"
 />
-<section class="resultats-test">
+<section class="section-onglets">
   <div class="contenu-section">
-    <h2>Niveau de maturité le plus proche : {niveau.label}</h2>
-    <TuilesMaturite niveauCourant={niveau} {animeTuiles} />
-    <div class="description-niveau">
-      <h5>{niveau.label}</h5>
-      <p>{niveau.description}</p>
-    </div>
+    <ConteneurOnglets>
+      <Onglet
+        bind:ongletActif
+        cetOnglet="votre-organisation"
+        labelOnglet="Maturité cyber de votre organisation"
+      ></Onglet>
+      <Onglet
+        bind:ongletActif
+        cetOnglet="comparaison"
+        labelOnglet="Comparaison avec d’autres entités"
+      ></Onglet>
+    </ConteneurOnglets>
+  </div>
+</section>
 
-    <a href="/niveaux-maturite" class="lien" target="_blank"
+{#if ongletActif === 'votre-organisation'}
+  <section class="resultats-test">
+    <div class="contenu-section">
+      <h2>Niveau de maturité le plus proche : {niveau.label}</h2>
+      <TuilesMaturite niveauCourant={niveau} {animeTuiles} />
+      <div class="description-niveau">
+        <h5>{niveau.label}</h5>
+        <p>{niveau.description}</p>
+      </div>
+
+      <a href="/niveaux-maturite" class="lien" target="_blank"
       >Les niveaux de maturité cyber</a
-    >
-  </div>
-</section>
-
-{#if affichePubMsc}
-  <section class="pour-aller-plus-loin">
-    <div class="contenu-section">
-      <PubliciteMesServicesCyber />
+      >
     </div>
   </section>
-{/if}
 
-<section class="repartition">
-  <div class="contenu-section">
-    <h2>Répartition de votre maturité cyber</h2>
-    <RadarMaturite resultats={$resultatsQuestionnaire} />
-    <div class="note">
-      Ce résultat est une évaluation indicative basé sur un modèle élaboré par
-      l’ANSSI. La maturité cyber n’est pas une évaluation du niveau de sécurité des systèmes d’information d’une organisation.
-    </div>
-  </div>
-</section>
+  {#if affichePubMsc}
+    <section class="pour-aller-plus-loin">
+      <div class="contenu-section">
+        <PubliciteMesServicesCyber />
+      </div>
+    </section>
+  {/if}
 
-{#if afficheRappelReponses}
-  <section class="rappel-reponses">
+  <section class="repartition">
     <div class="contenu-section">
-      <h2>Rappel de vos réponses</h2>
-      <div class="reponses">
-        {#each questions as question}
-          <div class="reponse">
-            <h3>{question.titre}</h3>
-            <p>
-              {question.propositions[($resultatsQuestionnaire[question.id] || 1) - 1]}
-            </p>
-          </div>
-        {/each}
+      <h2>Répartition de votre maturité cyber</h2>
+      <RadarMaturite resultats={$resultatsQuestionnaire} />
+      <div class="note">
+        Ce résultat est une évaluation indicative basé sur un modèle élaboré par
+        l’ANSSI. La maturité cyber n’est pas une évaluation du niveau de
+        sécurité des systèmes d’information d’une organisation.
       </div>
     </div>
   </section>
-{/if}
 
-<section class="votre-parcours">
-  <div class="contenu-section">
-    {#if niveau.id === 'intermediaire'}
-      <TuileVersParcours parcours="approfondir" />
-    {:else if niveau.id === 'optimal' || niveau.id === 'confirme'}
-      <div class="tuile">
-        <img src="/assets/images/debuter-cyber.png" alt="" />
-        <h3>Les services et ressources cyber</h3>
-        <p>
-          Trouvez les services et les ressources adaptés à vos besoins et votre
-          maturité cyber.
-        </p>
-        <a href="/catalogue" class="bouton primaire">Découvrir</a>
+  {#if afficheRappelReponses}
+    <section class="rappel-reponses">
+      <div class="contenu-section">
+        <h2>Rappel de vos réponses</h2>
+        <div class="reponses">
+          {#each questions as question}
+            <div class="reponse">
+              <h3>{question.titre}</h3>
+              <p>
+                {question.propositions[
+                ($resultatsQuestionnaire[question.id] || 1) - 1
+                  ]}
+              </p>
+            </div>
+          {/each}
+        </div>
       </div>
-    {:else}
-      <TuileVersParcours parcours="debuter" />
-    {/if}
-  </div>
-</section>
+    </section>
+  {/if}
+
+  <section class="votre-parcours">
+    <div class="contenu-section">
+      {#if niveau.id === 'intermediaire'}
+        <TuileVersParcours parcours="approfondir" />
+      {:else if niveau.id === 'optimal' || niveau.id === 'confirme'}
+        <div class="tuile">
+          <img src="/assets/images/debuter-cyber.png" alt="" />
+          <h3>Les services et ressources cyber</h3>
+          <p>
+            Trouvez les services et les ressources adaptés à vos besoins et
+            votre maturité cyber.
+          </p>
+          <a href="/catalogue" class="bouton primaire">Découvrir</a>
+        </div>
+      {:else}
+        <TuileVersParcours parcours="debuter" />
+      {/if}
+    </div>
+  </section>
+{:else}
+  <section class="comparaison">
+    <div class="contenu-section">
+      <img
+        src="/assets/images/illustration-dragon-aucun-resultat.svg"
+        alt="Aucun favori sauvegardé"
+      />
+      <h4>Bientôt disponible pour votre organisation.</h4>
+      <p>Contribuez à enrichir l’expérience pour vous et les autres organisations en evaluant votre maturité cyber.</p>
+      <a href="/test-maturite" class="bouton primaire">Tester votre maturité cyber</a>
+    </div>
+  </section>
+{/if}
 
 <section class="partage-test">
   <div class="contenu-section">
---
+++ front/lib-svelte/src/ui/Onglet.svelte
@@ -0,0 +1,59 @@
+<script lang="ts" generics="T extends string">
+  export let ongletActif: T;
+  export let cetOnglet: T;
+  export let labelOnglet: string;
+  export let sansBordureEnBas: boolean = false;
+</script>
+
+<button
+  type="button"
+  class="onglet"
+  class:active={ongletActif === cetOnglet}
+  class:sansBordureEnBas
+  on:click={() => (ongletActif = cetOnglet)}
+>
+  <span class="label">{labelOnglet}</span>
+</button>
+
+<style>
+  img {
+    width: 16px;
+    height: 16px;
+  }
+
+  .onglet {
+    padding: 9px 12px;
+    background: #f6f6f6;
+    border: none;
+    cursor: pointer;
+    border-top: 2px solid transparent;
+    border-left: 1px solid transparent;
+    border-right: 1px solid transparent;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+
+    transform: translateY(1px);
+    z-index: 3;
+    text-wrap: nowrap;
+    min-width: min-content;
+    font-weight: bold;
+  }
+
+  .onglet:not(.sansBordureEnBas):not(.active) {
+    border-bottom: 1px solid var(--gris-clair);
+  }
+
+  .onglet:hover {
+    background: #dfdfdf;
+  }
+
+  .onglet.active {
+    background: white;
+    border-top: 2px solid var(--jaune-msc);
+    border-left: 1px solid var(--gris-clair);
+    border-right: 1px solid var(--gris-clair);
+    border-bottom: 1px solid white;
+  }
+</style>
