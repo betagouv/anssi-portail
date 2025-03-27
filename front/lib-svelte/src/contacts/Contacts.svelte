83ca24a8e3429fe4195b3556f6ba795711f16520
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-03-27 08:27:56 +0100

    Ajoute le fil d’ariane sur la page des contacts
    ...en version connectée, donc qui considère le catalogue comme la racine du site

--- front/lib-svelte/src/contacts/Contacts.svelte
+++ front/lib-svelte/src/contacts/Contacts.svelte
@@ -2,6 +2,7 @@
   import { onMount } from 'svelte';
   import axios from 'axios';
   import type { Contacts } from './contacts';
+  import Hero from '../Hero.svelte';
 
   let contacts: Contacts;
 
@@ -11,70 +12,83 @@
   });
 </script>
 
-{#if contacts}
-  <div class="contacts">
+<Hero
+  titre="Vos contacts"
+  description="Des contacts cyber utiles de proximité pour vous orienter."
+  ariane="Vos contacts"
+></Hero>
 
-    {#if contacts.COT}
-      {@const { nom, email } = contacts.COT}
-      <div class="carte-contact">
-        <h2>Délégué(e)s ANSSI de votre région</h2>
-        <p class="nom">{nom}</p>
-        <p class="email"><a href="mailto:{email}">{email}</a></p>
+<section>
+  <div class="contenu-section">
+    {#if contacts}
+      <div class="contacts">
+        {#if contacts.COT}
+          {@const { nom, email } = contacts.COT}
+          <div class="carte-contact">
+            <h2>Délégué(e)s ANSSI de votre région</h2>
+            <p class="nom">{nom}</p>
+            <p class="email"><a href="mailto:{email}">{email}</a></p>
+          </div>
+        {/if}
+
+        {#if contacts.CSIRT}
+          {@const { nom, telephone, siteWeb, adresse } = contacts.CSIRT}
+          <div class="carte-contact">
+            <h2>{nom}</h2>
+            {#if telephone}
+              <p class="telephone"><a href="tel:{telephone}">{telephone}</a></p>
+            {/if}
+            <p class="site-web">
+              <a href={siteWeb} target="_blank">{siteWeb}</a>
+            </p>
+            {#if adresse}
+              <p class="adresse">{adresse}</p>
+            {/if}
+          </div>
+        {/if}
+
+        {#if contacts.campus}
+          {@const { nom, siteWeb, adresse, email } = contacts.campus}
+          <div class="carte-contact">
+            <h2>{nom}</h2>
+            {#if email}
+              <p class="email"><a href="mailto:{email}">{email}</a></p>
+            {/if}
+            <p class="site-web">
+              <a href={siteWeb} target="_blank">{siteWeb}</a>
+            </p>
+            <p class="adresse">{adresse}</p>
+          </div>
+        {/if}
+
+        <div class="carte-contact">
+          <h2>CERT-FR</h2>
+          <ul class="telephone">
+            <li>
+              Depuis la France métropolitaine au <a href="tel:3218">3218</a>
+              (service gratuit + prix d’un appel) ou
+              <a href="tel:09 70 83 32 18">09 70 83 32 18</a>
+            </li>
+            <li>
+              Depuis certaines collectivités territoriales situées en Outre-mer
+              ou depuis l’étranger au <a href="tel:+33 9 70 83 32 18"
+                >+33 9 70 83 32 18</a
+              >
+            </li>
+          </ul>
+          <p class="email">
+            <a href="mailto:cert-fr@ssi.gouv.fr">cert-fr@ssi.gouv.fr</a>
+          </p>
+          <p class="site-web">
+            <a href="https://www.cert.ssi.gouv.fr" target="_blank"
+              >https://www.cert.ssi.gouv.fr</a
+            >
+          </p>
+        </div>
       </div>
     {/if}
-
-    {#if contacts.CSIRT}
-      {@const { nom, telephone, siteWeb, adresse } = contacts.CSIRT}
-      <div class="carte-contact">
-        <h2>{nom}</h2>
-        {#if telephone}
-          <p class="telephone"><a href="tel:{telephone}">{telephone}</a></p>
-        {/if}
-        <p class="site-web"><a href={siteWeb} target="_blank">{siteWeb}</a></p>
-        {#if adresse}
-          <p class="adresse">{adresse}</p>
-        {/if}
-      </div>
-    {/if}
-
-    {#if contacts.campus}
-      {@const { nom, siteWeb, adresse, email } = contacts.campus}
-      <div class="carte-contact">
-        <h2>{nom}</h2>
-        {#if email}
-          <p class="email"><a href="mailto:{email}">{email}</a></p>
-        {/if}
-        <p class="site-web"><a href={siteWeb} target="_blank">{siteWeb}</a></p>
-        <p class="adresse">{adresse}</p>
-      </div>
-    {/if}
-    
-    <div class="carte-contact">
-      <h2>CERT-FR</h2>
-      <ul class="telephone">
-        <li>
-          Depuis la France métropolitaine au <a href="tel:3218">3218</a>
-          (service gratuit + prix d’un appel) ou
-          <a href="tel:09 70 83 32 18">09 70 83 32 18</a>
-        </li>
-        <li>
-          Depuis certaines collectivités territoriales situées en Outre-mer ou
-          depuis l’étranger au <a href="tel:+33 9 70 83 32 18"
-            >+33 9 70 83 32 18</a
-          >
-        </li>
-      </ul>
-      <p class="email">
-        <a href="mailto:cert-fr@ssi.gouv.fr">cert-fr@ssi.gouv.fr</a>
-      </p>
-      <p class="site-web">
-        <a href="https://www.cert.ssi.gouv.fr" target="_blank"
-          >https://www.cert.ssi.gouv.fr</a
-        >
-      </p>
-    </div>
   </div>
-{/if}
+</section>
 
 <style lang="scss">
   @use '../../../assets/styles/responsive.scss' as *;
@@ -95,24 +109,30 @@
     display: flex;
     gap: 8px;
     align-items: start;
+
     &:before {
       width: 16px;
       height: 16px;
       display: block;
       padding: 4px 0 3px 0;
     }
+
     &.telephone:before {
       content: url('/assets/images/icone-contact-telephone.svg');
     }
+
     &.email:before {
       content: url('/assets/images/icone-contact-email.svg');
     }
+
     &.nom:before {
       content: url('/assets/images/icone-contact-nom.svg');
     }
+
     &.site-web:before {
       content: url('/assets/images/icone-contact-site-web.svg');
     }
+
     &.adresse:before {
       content: url('/assets/images/icone-contact-adresse.svg');
     }
