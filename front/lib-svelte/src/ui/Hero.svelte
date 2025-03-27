33f55d9dbc8b950cd5de0cffb8278db5c69590b3
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-03-27 08:48:32 +0100

    Extrais un composant pour le fil d’ariane

---
+++ front/lib-svelte/src/ui/Hero.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import FilAriane from './FilAriane.svelte';
+
+  export let titre: string;
+  export let description: string;
+  export let ariane: string;
+</script>
+
+<section class="chapeau fond-sombre">
+  <div class="contenu-section">
+    <FilAriane {ariane} />
+    <h1>{titre}</h1>
+    <p>{description}</p>
+  </div>
+</section>
