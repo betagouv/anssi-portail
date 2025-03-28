293904f9ef8a86a8d303658a9355dfa93531b7e0
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-03-28 09:28:29 +0100

    [SOIN] Utilise le fil d’ariane dans le fil d’ariane du produit

--- front/lib-svelte/src/ui/FilArianeProduit.svelte
+++ front/lib-svelte/src/ui/FilArianeProduit.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
+  import FilAriane from './FilAriane.svelte';
+
   export let nom: string;
+  let branche = {
+    nom: 'Les services et ressources cyber',
+    lien: '/catalogue',
+  };
 </script>
 
-<div class="fil-ariane">
-  <a href="/" class="lien">Accueil</a>
-  <span><img src="/assets/images/icone-chevron-droite.svg" alt="" /> </span>
-  <a href="/catalogue/" class="lien">Les services et ressources cyber</a>
-  <span><img src="/assets/images/icone-chevron-droite.svg" alt="" /></span>
-  <span>{@html nom}</span>
-</div>
+<FilAriane feuille={nom} {branche} />
