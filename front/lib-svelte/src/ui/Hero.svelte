f699b1bd1952cb78a8760e6a0dcb8537fb3c7e76
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2025-03-28 10:12:39 +0100

    N'affiche pas le fil d'Ariane sur le catalogue en mode connecté

--- front/lib-svelte/src/ui/Hero.svelte
+++ front/lib-svelte/src/ui/Hero.svelte
@@ -3,12 +3,14 @@
 
   export let titre: string;
   export let description: string;
-  export let ariane: string;
+  export let ariane: string | undefined;
 </script>
 
 <section class="chapeau fond-sombre">
   <div class="contenu-section">
-    <FilAriane feuille={ariane} />
+    {#if ariane}
+      <FilAriane feuille={ariane} />
+    {/if}
     <h1>{titre}</h1>
     <p>{description}</p>
   </div>
