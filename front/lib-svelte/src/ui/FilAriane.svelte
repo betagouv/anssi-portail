7d6bbfb759592bd439b88c864ca4c190d4567b57
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-03-28 09:28:29 +0100

    [SOIN] Utilise le fil d’ariane dans le fil d’ariane du produit

--- front/lib-svelte/src/ui/FilAriane.svelte
+++ front/lib-svelte/src/ui/FilAriane.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
   import { profilStore } from '../stores/profil.store.js';
+
   export let feuille: string;
+  type Branche = {
+    nom: string;
+    lien: string;
+  };
+  export let branche: undefined | Branche = undefined;
 </script>
+
 <div class="fil-ariane">
   {#if $profilStore}
     <a href="/catalogue" class="lien">Catalogue des services</a>
@@ -9,11 +16,17 @@
     <a href="/" class="lien">Accueil</a>
   {/if}
   <span><img src="/assets/images/icone-chevron-droite.svg" alt="" /> </span>
-  <span>{feuille}</span>
+
+  {#if branche}
+    <a href={branche.lien} class="lien">{branche.nom}</a>
+    <span><img src="/assets/images/icone-chevron-droite.svg" alt="" /></span>
+  {/if}
+
+  <span>{@html feuille}</span>
 </div>
 
 <style>
   .fil-ariane {
-      grid-area: ariane;
+    grid-area: ariane;
   }
 </style>
