be07ae33765a035b416c60cc6aba041d2d20c8ff
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-03-28 09:32:49 +0100

    [SOIN] Utilise directement le fil d’ariane dans la fiche produit
    ...afin de supprimer le fil-ariane-produit

--- front/lib-svelte/src/main-fil-ariane-produit.ts
+++
@@ -1,9 +0,0 @@
-import { mount } from 'svelte';
-import FilArianeProduit from './ui/FilArianeProduit.svelte';
-
-const donnees = document.getElementById('donnees')!.textContent;
-if (!donnees) throw new Error('Impossible de trouver les données');
-
-const props = JSON.parse(donnees);
-
-mount(FilArianeProduit, { target: document.getElementById('fil-ariane-produit')!, props });
