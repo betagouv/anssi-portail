47a2f5871d4a3a64f191f8e417200013e5b33695
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-04-03 15:00:13 +0200

    N’affiche par la carte sous forme de lien si l’item n’en a pas

--- front/lib-svelte/src/catalogue/CarteItem.svelte
+++ front/lib-svelte/src/catalogue/CarteItem.svelte
@@ -6,16 +6,23 @@
   export let avecBoutonFavori: boolean = false;
 </script>
 
-<a
-  class="carte {itemCyber.typologie}"
-  target={itemCyber.lienInterne ? '' : '_blank'}
-  href={itemCyber.lienInterne ?? itemCyber.lienExterne}
-  class:lien-externe-produit={!itemCyber.lienInterne}
-  data-source="Catalogue"
-  data-cible={itemCyber.nom}
->
-  <ContenuCarteItem {itemCyber} {avecBoutonFavori} />
-</a>
+{#if itemCyber.lienInterne || itemCyber.lienExterne}
+  <a
+    class="carte {itemCyber.typologie}"
+    target={itemCyber.lienInterne ? '' : '_blank'}
+    href={itemCyber.lienInterne ?? itemCyber.lienExterne}
+    class:lien-externe-produit={!itemCyber.lienInterne}
+    data-source="Catalogue"
+    data-cible={itemCyber.nom}
+  >
+    <ContenuCarteItem {itemCyber} {avecBoutonFavori} />
+  </a>
+{:else}
+  <div class="carte {itemCyber.typologie}">
+    <ContenuCarteItem {itemCyber} {avecBoutonFavori} />
+  </div>
+
+{/if}
 
 <style lang="scss">
   .carte.service.lien-externe-produit > * {
