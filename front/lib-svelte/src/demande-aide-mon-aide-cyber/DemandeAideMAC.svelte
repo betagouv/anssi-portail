ce5e65e02b5378e6a614ed14555356eb1688c119
Marc ETOURNEAU <marc.etourneau@beta.gouv.fr>
2025-04-03 10:41:04 +0200

    Ajoute une page pour le POC de MonAideCyber

---
+++ front/lib-svelte/src/demande-aide-mon-aide-cyber/DemandeAideMAC.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import ChampTexte from '../ui/ChampTexte.svelte';
+  import Bouton from '../ui/Bouton.svelte';
+  import axios from 'axios';
+
+  let email: string;
+  let enCoursEnvoi = false;
+
+  const soumetFormulaire = () => {
+    enCoursEnvoi = true;
+    try {
+      axios.post('/api/mon-aide-cyber/demandes-aide', { email });
+    } finally {
+      enCoursEnvoi = false;
+    }
+  };
+</script>
+
+<section class="test-maturite">
+  <div class="contenu-section">
+    <h2>Le diagnostic</h2>
+    <div class="formulaire">
+      <label for="nom">Votre email</label>
+      <ChampTexte
+        bind:valeur={email}
+        nom="nom"
+        id="nom"
+        aideSaisie="Ex: jean.dupont@email.com"
+      />
+    </div>
+    <Bouton
+      type="primaire"
+      titre="Envoyer"
+      on:click={soumetFormulaire}
+      {enCoursEnvoi}
+    />
+  </div>
+</section>
---
+++ front/lib-svelte/src/main-demande-aide-mon-aide-cyber.ts
@@ -0,0 +1,6 @@
+import { mount } from 'svelte';
+import DemandeAideMAC from './demande-aide-mon-aide-cyber/DemandeAideMAC.svelte';
+
+mount(DemandeAideMAC, {
+  target: document.getElementById('demande-aide-mon-aide-cyber')!,
+});
