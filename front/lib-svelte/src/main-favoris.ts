21f2cf2b83a50a18d4855e977d29c4d8820a0b71
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-04-03 14:05:14 +0200

    [SOIN] Permet d’initialiser le store du catalogue sans répartition des besoins

--- front/lib-svelte/src/main-favoris.ts
+++ front/lib-svelte/src/main-favoris.ts
@@ -1,18 +1,14 @@
 import { mount } from 'svelte';
-import MesFavoris from "./favoris/MesFavoris.svelte";
-import type { ItemCyber, RepartitionParBesoin } from "./catalogue/Catalogue.types";
-import { catalogueStore } from "./catalogue/stores/catalogue.store";
+import MesFavoris from './favoris/MesFavoris.svelte';
+import type { ItemCyber } from './catalogue/Catalogue.types';
+import { catalogueStore } from './catalogue/stores/catalogue.store';
 
+const donnees = document.getElementById('donnees')!.textContent;
+if (!donnees) throw new Error('Impossible de trouver les données du catalogue');
 
-const donnees = document.getElementById("donnees")!.textContent;
-if (!donnees) throw new Error("Impossible de trouver les données du catalogue");
+const { itemsCyber } = JSON.parse(donnees) as { itemsCyber: ItemCyber[] };
 
-const { itemsCyber, repartition } = JSON.parse(donnees) as {
-  itemsCyber: ItemCyber[];
-  repartition: RepartitionParBesoin;
-};
-
-catalogueStore.initialise(itemsCyber, repartition);
+catalogueStore.initialise(itemsCyber);
 
 const favoris = mount(MesFavoris, {
   target: document.getElementById('favoris')!,
