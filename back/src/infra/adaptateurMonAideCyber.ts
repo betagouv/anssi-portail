4803fe0ccacc37d798004b65dc12237229c4dc6a
Marc ETOURNEAU <marc.etourneau@beta.gouv.fr>
2025-04-04 09:48:48 +0200

    Ajoute un faux adaptateur MonAideCyber

--- back/src/infra/adaptateurMonAideCyber.ts
+++ back/src/infra/adaptateurMonAideCyber.ts
@@ -1,17 +1,28 @@
 import axios from 'axios';
+import { adaptateurMonAideCyberVide } from './adaptateurMonAideCyberVide';
 
-export interface AdaptateurMonAideCyber {
-  creeDemandeAide: (demandeAide: DemandeAide) => Promise<void>;
-}
 export type DemandeAide = {
   email: string;
 };
 
-export const fabriqueAdaptateurMonAideCyber = () => ({
-  creeDemandeAide: async (demandeAide: DemandeAide) => {
+export interface AdaptateurMonAideCyber {
+  creeDemandeAide: (demandeAide: DemandeAide) => Promise<void>;
+}
+
+const adaptateurMonAideCyber = (): AdaptateurMonAideCyber => {
+  const creeDemandeAide = async ({ email }: DemandeAide) => {
     await axios.post(
       `${process.env.MON_AIDE_CYBER_URL_BASE}/api/demandes/dummy-etre-aide`,
-      demandeAide
+      {
+        email,
+      }
     );
-  },
-});
+  };
+
+  return { creeDemandeAide };
+};
+
+export const fabriqueAdaptateurMonAideCyber = () =>
+  process.env.MON_AIDE_CYBER_URL_BASE
+    ? adaptateurMonAideCyber()
+    : adaptateurMonAideCyberVide();
