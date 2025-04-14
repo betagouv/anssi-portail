22047ec2effde396552ab9e98a4209102c58d963
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-04-14 13:08:15 +0200

    Rajoute les icônes manquantes
    Sur le lien « Retour », on passe en mode `border-bottom` pour le souligné : le `text-decoration: underline` ne s'applique *pas* aux images.
    Pour obtenir le souligné, on utilise une bordure.

---
+++ front/lib-svelte/src/ui/Icone.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  type Icone = 'check' | 'fleche-gauche';
+
+  const srcDesIcones: Record<Icone, string> = {
+    'fleche-gauche': '/assets/icones/icone-fleche-gauche.svg',
+    check: '/assets/icones/icone-check-line.svg',
+  };
+
+  export let type: Icone;
+
+</script>
+
+<img src={srcDesIcones[type]} alt="" />
+
+<style lang="scss">
+  .icone {
+    display: inline-block;
+    width: 1rem;
+    height: 1rem;
+  }
+</style>
