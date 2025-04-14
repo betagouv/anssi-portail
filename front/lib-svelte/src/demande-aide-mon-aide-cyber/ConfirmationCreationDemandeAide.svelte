22d540ddc9c2c18a700b338057b4cb1e00ec5b1c
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-04-14 18:19:59 +0200

    Ajuste l'affichage du bloc de confirmation

--- front/lib-svelte/src/demande-aide-mon-aide-cyber/ConfirmationCreationDemandeAide.svelte
+++ front/lib-svelte/src/demande-aide-mon-aide-cyber/ConfirmationCreationDemandeAide.svelte
@@ -3,52 +3,48 @@
 </script>
 
 <div class="carte-confirmation">
-  <div class="contenu-confirmation">
-    <h3>Votre demande a bien été envoyée !</h3>
-    <p>
-      Un Aidant cyber prendra bientôt contact avec vous pour vous accompagner
-      dans les prochaines étapes.
-    </p>
-    <Bouton
-      type="primaire"
-      titre="Explorer les services pour se lancer"
-      on:click={() => (window.location.pathname = '/')}
-    />
-    <a href="/" class="lien">Revenir à la page d'accueil</a>
-  </div>
+  <h3>Votre demande a bien été envoyée&nbsp;!</h3>
+  <p>
+    Un Aidant cyber prendra bientôt contact avec vous pour vous accompagner dans
+    les prochaines étapes.
+  </p>
+  <Bouton
+    type="primaire"
+    taille="md"
+    titre="Explorer les services pour se lancer"
+    on:click={() => (window.location.pathname = '/')}
+  />
+  <a href="/" class="lien">Revenir à la page d'accueil</a>
 </div>
 
 <style lang="scss">
   @use '../../../assets/styles/responsive' as *;
 
   .carte-confirmation {
-    max-width: 100%;
-    border-radius: 8px;
-    border: 1px solid
-      var(--Couleurs-Clair-Decisions-Border-_border-default-grey, #ddd);
-    background: #fff;
-    padding: 48px 24px 72px 24px;
-
-    margin-top: -75px;
-
     display: flex;
     flex-direction: column;
-    gap: 32px;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    max-width: 792px;
+    padding: 48px 24px 72px 24px;
+    margin: auto;
+    border-radius: 8px;
+    border: 1px solid #ddd;
+    background: #fff;
 
     @include a-partir-de(md) {
       padding: 48px 80px 72px 80px;
     }
 
-    @include a-partir-de(lg) {
-      padding: 48px 80px 72px 80px;
+    h3 {
+      margin: 24px 0 10px 0;
     }
-  }
-
-  .contenu-confirmation {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    text-align: center;
-    gap: 16px;
+    p {
+      margin: 0 0 40px;
+    }
+    a {
+      margin-top: 16px;
+    }
   }
 </style>
