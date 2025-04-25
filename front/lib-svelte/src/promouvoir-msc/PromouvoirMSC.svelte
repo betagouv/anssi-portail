b7d6d917a279e4daee9b21b08b8d3357974362c7
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-04-25 18:10:46 +0200

    Affiche le contenu de la page Crisp `Promouvoir MSC`

--- front/lib-svelte/src/promouvoir-msc/PromouvoirMSC.svelte
+++ front/lib-svelte/src/promouvoir-msc/PromouvoirMSC.svelte
@@ -1,9 +1,35 @@
 <script lang="ts">
   import Hero from '../ui/Hero.svelte';
+  import { onMount } from 'svelte';
+  import axios from 'axios';
+
+  type PageHtmlCrisp = {
+    titre: string;
+    contenu: string | null;
+    description: string;
+    tableDesMatieres: unknown[];
+  };
+
+  let pageCrisp :PageHtmlCrisp = {
+    titre:"",
+    description:"",
+    contenu:"",
+    tableDesMatieres:[]
+  }
+
+  onMount(async () => {
+    const reponse = await axios.get<PageHtmlCrisp>('/api/pages-crisp/promouvoir_msc');
+    pageCrisp = reponse.data;
+  });
 </script>
 
 <Hero
-  titre="Promouvoir MesServicesCyber"
-  description="Vous appréciez MesServicesCyber ? N'hésitez pas à en parler autour de vous !"
-  ariane="Promouvoir MesServicesCyber"
+  titre={pageCrisp.titre}
+  description={pageCrisp.description}
+  ariane={pageCrisp.titre}
 />
+
+<div class="article-promouvoir-msc">
+  <!-- eslint-disable-next-line svelte/no-at-html-tags-->
+  {@html pageCrisp.contenu}
+</div>
