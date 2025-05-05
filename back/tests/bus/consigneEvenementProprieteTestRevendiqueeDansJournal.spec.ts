0b995003f271feaaf061f51e92253a27f8f796ab
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-05-05 16:55:28 +0200

    [JOURNAL] Hache l’email de l’utilisateur
    ...pour les événements de revendication de propriété de test

--- back/tests/bus/consigneEvenementProprieteTestRevendiqueeDansJournal.spec.ts
+++ back/tests/bus/consigneEvenementProprieteTestRevendiqueeDansJournal.spec.ts
@@ -1,26 +1,42 @@
-import { describe, it } from 'node:test';
+import { beforeEach, describe, it } from 'node:test';
 import assert from 'node:assert';
 import { AdaptateurHorloge } from '../../src/infra/adaptateurHorloge';
 import { AdaptateurJournal } from '../../src/infra/adaptateurJournal';
 import { ProprieteTestRevendiquee } from '../../src/bus/evenements/proprieteTestRevendiquee';
 import { consigneEvenementProprieteTestRevendiqueeDansJournal } from '../../src/bus/consigneEvenementProprieteTestRevendiqueeDansJournal';
+import { AdaptateurChiffrement } from '../../src/infra/adaptateurChiffrement';
 
 describe("L'abonnement qui consigne la revendication de la propriété d'un test dans le journal", () => {
+  let adaptateurHorloge: AdaptateurHorloge;
+  let adaptateurJournal: AdaptateurJournal;
+  let adaptateurChiffrement: AdaptateurChiffrement;
+
+  const consigneEvenementDansJournal = () =>
+    consigneEvenementProprieteTestRevendiqueeDansJournal({
+      adaptateurJournal,
+      adaptateurHorloge,
+      adaptateurChiffrement,
+    });
+
+  beforeEach(() => {
+    adaptateurHorloge = { maintenant: () => new Date() };
+    adaptateurChiffrement = {
+      hacheSha256: (chaineEnClair: string) => `${chaineEnClair}-hache`,
+    };
+  });
+
   it('consigne un évènement de ProprieteTestRevendiquee', async () => {
     let evenementRecu;
-    const adaptateurJournal: AdaptateurJournal = {
+    adaptateurJournal = {
       consigneEvenement: async (donneesEvenement: unknown) => {
         evenementRecu = donneesEvenement;
       },
     };
-    const adaptateurHorloge: AdaptateurHorloge = {
+    adaptateurHorloge = {
       maintenant: () => new Date('2025-03-10'),
     };
 
-    await consigneEvenementProprieteTestRevendiqueeDansJournal({
-      adaptateurJournal,
-      adaptateurHorloge,
-    })(
+    await consigneEvenementDansJournal()(
       new ProprieteTestRevendiquee({
         emailUtilisateur: 'u1@mail.com',
         idResultatTest: '12345',
@@ -29,8 +45,26 @@ describe("L'abonnement qui consigne la revendication de la propriété d'un test
 
     assert.notEqual(evenementRecu, undefined);
     assert.equal(evenementRecu!.type, 'PROPRIETE_TEST_REVENDIQUEE');
-    assert.equal(evenementRecu!.donnees.emailUtilisateur, 'u1@mail.com');
     assert.equal(evenementRecu!.donnees.idResultatTest, '12345');
     assert.deepEqual(evenementRecu!.date, new Date('2025-03-10'));
+  });
+
+  it("hache l'email de l'utilisateur", async () => {
+    let evenementRecu;
+    adaptateurJournal = {
+      consigneEvenement: async (donneesEvenement: unknown) => {
+        evenementRecu = donneesEvenement;
+      },
+    };
+
+    await consigneEvenementDansJournal()(
+      new ProprieteTestRevendiquee({
+        emailUtilisateur: 'u1@mail.com',
+        idResultatTest: '1',
+      })
+    );
+
+    assert.equal(evenementRecu!.donnees.idUtilisateur, 'u1@mail.com-hache');
+    assert.equal(evenementRecu!.donnees.emailUtilisateur, undefined);
   });
 });
