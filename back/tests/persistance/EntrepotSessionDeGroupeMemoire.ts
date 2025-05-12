f150cfbb79d493e0eec058252c452e74d92c1780
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-05-12 17:43:33 +0200

    Génère un code de session de groupe aléatoire

--- back/tests/persistance/EntrepotSessionDeGroupeMemoire.ts
+++ back/tests/persistance/EntrepotSessionDeGroupeMemoire.ts
@@ -4,4 +4,8 @@ import { SessionDeGroupe } from '../../src/metier/sessionDeGroupe';
 
 export class EntrepotSessionDeGroupeMemoire
   extends EntrepotMemoire<SessionDeGroupe>
-  implements EntrepotSessionDeGroupe {}
+  implements EntrepotSessionDeGroupe
+{
+  parCode = async (code: string): Promise<SessionDeGroupe | undefined> =>
+    this.entites.find((e) => e.code === code);
+}
