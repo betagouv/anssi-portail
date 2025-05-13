e0291c4f6a19313a09c68c34b3c221d6ed463535
Alexis Brones <alexis.brones@beta.gouv.fr>
2025-05-13 16:50:12 +0200

    Ajoute une page d'accueil pour les sessions de groupe

---
+++ front/lib-svelte/src/main-session-groupe.ts
@@ -0,0 +1,6 @@
+import { mount } from 'svelte';
+import SessionGroupe from './test-maturite/SessionGroupe.svelte';
+
+mount(SessionGroupe, {
+  target: document.getElementById('session-groupe')!,
+});
