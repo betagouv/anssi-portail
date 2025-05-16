0ba71baa1638d60cd268939284d4b196061811cc
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-05-16 10:08:07 +0200

    [RESULTATS SESSION GROUPE] Affiche les 5 niveaux de maturité cyber

---
+++ front/lib-svelte/src/test-maturite/ResultatsSessionGroupe.svelte
@@ -0,0 +1,35 @@
+<script>
+  import TuilesMaturiteSessionGroupe from './TuilesMaturiteSessionGroupe.svelte';
+</script>
+
+<section>
+  <div class="contenu-section">
+    <h2>Les 5 niveaux de maturité cyber</h2>
+    <TuilesMaturiteSessionGroupe />
+    <a href="/niveaux-maturite" class="lien" target="_blank">
+      Les niveaux de maturité cyber
+    </a>
+  </div>
+</section>
+
+<style lang="scss">
+  @use '../../../assets/styles/responsive' as *;
+
+  section {
+    padding: 48px var(--gouttiere) 0;
+
+    .contenu-section {
+      display: flex;
+      flex-direction: column;
+      gap: 32px;
+      padding-bottom: 48px;
+      border-bottom: 1px solid var(--gris-clair);
+    }
+  }
+
+  h2 {
+    margin: 0;
+    font-size: 2rem;
+    line-height: 2.5rem;
+  }
+</style>
