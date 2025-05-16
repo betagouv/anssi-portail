2ec448759e90bc45d337750f8c7f6df618924bc7
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2025-05-16 16:06:09 +0200

    Affiche la légende des résultats d'une session de groupe

---
+++ front/lib-svelte/src/test-maturite/Serie.ts
@@ -0,0 +1,11 @@
+type ElementSerie = {
+  valeur: number;
+  libelle: string;
+};
+export type Serie = ElementSerie[];
+
+export const totalSerie = (serie: Serie) =>
+  serie.reduce((valeurCumulee, element) => valeurCumulee + element.valeur, 0);
+
+export const pourcentagesSerie = (serie: Serie) =>
+  (serie.map((d) => (Math.round((d.valeur / totalSerie(serie)) * 100))));
