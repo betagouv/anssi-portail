edbf9f55fae55afebe6e83bb405159e4e8d751e6
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2025-05-16 16:06:09 +0200

    Affiche la légende des résultats d'une session de groupe

--- front/lib-svelte/src/test-maturite/ResultatsSessionGroupe.svelte
+++ front/lib-svelte/src/test-maturite/ResultatsSessionGroupe.svelte
@@ -1,6 +1,7 @@
 <script>
   import GraphiqueAnneau from './GraphiqueAnneau.svelte';
   import TuilesMaturiteSessionGroupe from './TuilesMaturiteSessionGroupe.svelte';
+  import { pourcentagesSerie } from './Serie';
   const serie = [
     { libelle: 'Insuffisant', valeur: 16 },
     { libelle: 'Émergent', valeur: 11 },
@@ -23,7 +24,21 @@
 <section>
   <div class="contenu-section">
     <h2>Répartition des niveaux de maturité cyber de cette session</h2>
-    <GraphiqueAnneau {serie} />
+    <div class="repartition-niveaux-maturite">
+      <GraphiqueAnneau {serie} />
+      <div class="legende">
+        {#each pourcentagesSerie(serie) as pourcentage, index}
+          {@const element = serie[index]}
+          <div class="ligne-legende ligne-legende-{index}">
+            <span class="libelle">{element.libelle}</span>
+            <div>
+              <span class="total">{element.valeur}</span>
+              <span class="pourcentage">({pourcentage}%)</span>
+            </div>
+          </div>
+        {/each}
+      </div>
+    </div>
   </div>
 </section>
 
@@ -52,5 +67,62 @@
     margin: 0;
     font-size: 2rem;
     line-height: 2.5rem;
+  }
+
+  .repartition-niveaux-maturite {
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    gap: 48px;
+  }
+
+  .legende {
+    width: 242px;
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+  }
+
+  .ligne-legende {
+    display: grid;
+    gap: 10px;
+    grid-template-columns: 24px 2fr 1fr;
+    align-items: center;
+
+    div {
+      text-wrap: nowrap;
+    }
+
+    .total {
+      font-weight: bold;
+    }
+  }
+
+  .ligne-legende:before {
+    width: 14px;
+    height: 14px;
+    border-radius: 7px;
+    content: '';
+    background-color: var(--couleur-puce);
+  }
+
+  .ligne-legende-0 {
+    --couleur-puce: #6369f1;
+  }
+
+  .ligne-legende-1 {
+    --couleur-puce: #fec54b;
+  }
+
+  .ligne-legende-2 {
+    --couleur-puce: #8248a1;
+  }
+
+  .ligne-legende-3 {
+    --couleur-puce: #f26c85;
+  }
+
+  .ligne-legende-4 {
+    --couleur-puce: #8ed4a3;
   }
 </style>
