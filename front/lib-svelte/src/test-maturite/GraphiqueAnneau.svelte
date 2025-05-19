3fbf7eefa2693cf8611caceed8c25c8d23fd60fd
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2025-05-19 19:17:26 +0200

    [CORRECTION] Corrige l'affichage du graphique anneau
    - la légende était répétée 6 fois
    - en cas de secteur à 100%, le secteur ne s'affichait pas car le point de départ et d'arrivée étaient confondus
    - en cas d'arc large (>50% de l'anneau), il faut indiquer à SVG que l'arc est large pour qu'il dessine le bon arc

--- front/lib-svelte/src/test-maturite/GraphiqueAnneau.svelte
+++ front/lib-svelte/src/test-maturite/GraphiqueAnneau.svelte
@@ -28,8 +28,19 @@
     const resultat = [];
     for (let index = 0; index < pourcentagesCumules.length - 1; index++) {
       const angleCourant = pourcentageVersRadian(pourcentagesCumules[index]);
-      const nouvelAngle = pourcentageVersRadian(pourcentagesCumules[index + 1]);
+      let nouvelAngle = pourcentageVersRadian(pourcentagesCumules[index + 1]);
+      // si on passe directement de 0 à 100, les points de départ et d'arrivée sont les mêmes
+      // et svg ne dessine rien. Les prochaines lignes permettent de contourner ce problème.
+      if (
+        pourcentagesCumules[index] === 0 &&
+        pourcentagesCumules[index + 1] === 100
+      ) {
+        nouvelAngle = pourcentageVersRadian(99.9999);
+      }
+      const pourcentageSecteur =
+        pourcentagesCumules[index + 1] - pourcentagesCumules[index];
       resultat.push({
+        arcLarge: pourcentageSecteur > 50,
         premierPointExterieur: polaireVersCartesien(
           rayonExterieur,
           angleCourant
@@ -50,33 +61,30 @@
 </script>
 
 <svg id="radar" viewBox="-130 -130 260 260" xmlns="http://www.w3.org/2000/svg">
+  <text
+    class="total-participants"
+    x="0"
+    y="0"
+    text-anchor="middle"
+    font-size="40">{totalSerie(serie)}</text
+  >
+  <text
+    class="libelle-total-participants"
+    x="0"
+    y="15px"
+    font-size="16"
+    dominant-baseline="hanging"
+    text-anchor="middle"
+    >Participants
+  </text>
   {#each calculeLesPointsDesSecteurs() as secteur, index (index)}
-    <text
-      class="total-participants"
-      x="0"
-      y="0"
-      text-anchor="middle"
-      font-size="40">{totalSerie(serie)}</text
-    >
-    <text
-      class="libelle-total-participants"
-      x="0"
-      y="15px"
-      font-size="16"
-      dominant-baseline="hanging"
-      text-anchor="middle"
-      >Participants
-    </text>
     <path
       class={`secteur-${index} secteur`}
       d="M {secteur.premierPointExterieur.x} {secteur.premierPointExterieur.y}
-             A {rayonExterieur} {rayonExterieur} 0 0 1 {secteur
-        .secondPointExterieur.x} {secteur.secondPointExterieur.y}
-             L {secteur.premierPointInterieur.x} {secteur.premierPointInterieur
-        .y}
-             A {rayonInterieur} {rayonInterieur} 0 0 0 {secteur
-        .secondPointInterieur.x} {secteur.secondPointInterieur.y}
-             Z"
+         A {rayonExterieur} {rayonExterieur} 0 {secteur.arcLarge ? 1 : 0} 1 {secteur.secondPointExterieur.x} {secteur.secondPointExterieur.y}
+         L {secteur.premierPointInterieur.x} {secteur.premierPointInterieur.y}
+         A {rayonInterieur} {rayonInterieur} 0 {secteur.arcLarge ? 1 : 0} 0 {secteur.secondPointInterieur.x} {secteur.secondPointInterieur.y}
+         Z"
     />
   {/each}
 </svg>
