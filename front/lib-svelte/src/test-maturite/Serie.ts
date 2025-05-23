5e38b330bc80b9655c3e7f98bb9ac190a02b8756
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-05-23 09:45:23 +0200

    Affiche le radar des résultats de la session de groupe

--- front/lib-svelte/src/test-maturite/Serie.ts
+++ front/lib-svelte/src/test-maturite/Serie.ts
@@ -1,8 +1,17 @@
+import type { IdNiveau } from '../niveaux-maturite/NiveauxMaturite.donnees';
+import type { IdRubrique } from './TestMaturite.donnees';
+
 type ElementSerie = {
   valeur: number;
   libelle: string;
 };
 export type Serie = ElementSerie[];
+
+export type SerieRadar = {
+  id: IdNiveau;
+  valeurs: Record<IdRubrique, number>;
+  couleur: string;
+}
 
 export const totalSerie = (serie: Serie) =>
   serie.reduce((valeurCumulee, element) => valeurCumulee + element.valeur, 0);
