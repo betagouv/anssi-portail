d46f77616bab8f4ed21dcc2b92efe07a1b64414d
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2025-06-06 10:27:00 +0200

    [SOIN] Utilise le composant Ui Kit Page Crisp
    ... sur la page Promouvoir MSC

--- front/lib-svelte/src/promouvoir-msc/PromouvoirMSC.svelte
+++ front/lib-svelte/src/promouvoir-msc/PromouvoirMSC.svelte
@@ -41,43 +41,4 @@
   ariane={pageCrisp.titre}
 />
 
-<div class="sommaire sommaire-replie">
-  <details>
-    <summary>
-      <div class="entete-filtres">
-        <img class="menu" src="/assets/images/icone-menu-lateral.svg" alt="" />
-        <span id="section-active" class="titre-menu">
-          {tableDesMatieres[0]?.texte}
-        </span>
-        <img
-          class="chevron"
-          src="/assets/images/icone-chevron-bas.svg"
-          alt=""
-        />
-      </div>
-    </summary>
-
-    <ul>
-      {#each tableDesMatieres as entree (entree.id)}
-        <li><a href={`#${entree.id}`}>{entree.texte}</a></li>
-      {/each}
-    </ul>
-  </details>
-</div>
-
-<div class="article">
-  <div class="contenu-section">
-    <div class="sommaire sommaire-deplie">
-      <ul>
-        {#each tableDesMatieres as entree (entree.id)}
-          <li><a href={`#${entree.id}`}>{entree.texte}</a></li>
-        {/each}
-      </ul>
-    </div>
-
-    <div class="contenu">
-      <!-- eslint-disable-next-line svelte/no-at-html-tags-->
-      {@html pageCrisp.contenu}
-    </div>
-  </div>
-</div>
+<lab-anssi-page-crisp {tableDesMatieres} contenu={pageCrisp.contenu}></lab-anssi-page-crisp>
