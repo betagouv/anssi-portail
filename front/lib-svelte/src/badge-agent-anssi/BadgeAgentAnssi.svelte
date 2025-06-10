924e4106b1ef61ef55c270ee7c438b3a0fefe846
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-06-10 15:20:19 +0200

    Réorganise l’entête de page
    ...afin que le badge `Agent ANSSI` puisse s’afficher correctement dans toutes les situations

--- front/lib-svelte/src/badge-agent-anssi/BadgeAgentAnssi.svelte
+++ front/lib-svelte/src/badge-agent-anssi/BadgeAgentAnssi.svelte
@@ -6,3 +6,16 @@
 {#if $profilStore && profilStore.utilisateurEstConnecte() && get(profilStore)?.estAgentAnssi}
   <span class="badge-agent-anssi">Agent ANSSI</span>
 {/if}
+
+<style lang="scss">
+  .badge-agent-anssi {
+    text-transform: uppercase;
+    padding: 0 6px;
+    border-radius: 4px;
+    color: #2f4077ff;
+    font-weight: bold;
+    font-size: 0.8rem;
+    background-color: #e9edfeff;
+    text-wrap: nowrap;
+  }
+</style>
