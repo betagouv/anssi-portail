60d5b656421b02a0e176f7db6440ba282d6c79bf
Marc ETOURNEAU <marc.etourneau@beta.gouv.fr>
2025-06-27 11:23:40 +0200

    [DIAG] Vérifie la validité du retour d'expérience

--- back/src/api/ressourceRetoursExperience.ts
+++ back/src/api/ressourceRetoursExperience.ts
@@ -1,13 +1,29 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
 import { ConfigurationServeur } from './configurationServeur';
+import { check } from 'express-validator';
 
 export const ressourceRetoursExperience = ({
   messagerieInstantanee,
+  middleware,
 }: ConfigurationServeur): Router => {
   const routeur = Router();
-  routeur.post('/', (requete, reponse) => {
-    messagerieInstantanee.notifieUnRetourExperience(requete.body);
-    reponse.sendStatus(201);
-  });
+  routeur.post(
+    '/',
+    [
+      check('raison')
+        .isIn(['pas-clair', 'pas-le-temps', 'pas-besoin', 'autre'])
+        .withMessage('La raison est invalide'),
+      check('emailDeContact')
+        .optional({ values: 'falsy' })
+        .isEmail()
+        .withMessage("L'email est invalide"),
+    ],
+    middleware.valide(),
+    middleware.aseptise('precision'),
+    (requete: Request, reponse: Response) => {
+      messagerieInstantanee.notifieUnRetourExperience(requete.body);
+      reponse.sendStatus(201);
+    }
+  );
   return routeur;
 };
--- back/tests/api/ressourceRetoursExperience.spec.ts
+++ back/tests/api/ressourceRetoursExperience.spec.ts
@@ -21,9 +21,14 @@ describe("La ressource des retours d'expérience", () => {
       messagerieInstantanee,
     });
   });
+
   describe('sur demande de création', () => {
     it('retourne un 201', async () => {
-      const reponse = await request(serveur).post('/api/retours-experience');
+      const reponse = await request(serveur)
+        .post('/api/retours-experience')
+        .send({
+          raison: 'pas-clair',
+        });
       assert.equal(reponse.status, 201);
     });
 
@@ -46,6 +51,64 @@ describe("La ressource des retours d'expérience", () => {
         precision: 'flou',
         emailDeContact: 'mail@mail.com',
       });
+    });
+
+    it('renvoie une erreur si la raison est invalide', async () => {
+      let retourExperienceEnvoye: RetourExperience | null = null;
+      messagerieInstantanee.notifieUnRetourExperience = async (
+        retourExperience: RetourExperience
+      ) => {
+        retourExperienceEnvoye = retourExperience;
+      };
+
+      const reponse = await request(serveur)
+        .post('/api/retours-experience')
+        .send({
+          raison: 'raison-invalide',
+        });
+
+      assert.equal(reponse.status, 400);
+      assert.equal(reponse.body.erreur, 'La raison est invalide');
+      assert.equal(retourExperienceEnvoye, null);
+    });
+
+    it('renvoie une erreur si le mail est invalide', async () => {
+      const reponse = await request(serveur)
+        .post('/api/retours-experience')
+        .send({
+          emailDeContact: 'email-invalide',
+          raison: 'pas-clair',
+        });
+
+      assert.equal(reponse.status, 400);
+      assert.equal(reponse.body.erreur, "L'email est invalide");
+    });
+
+    it("ignore l'email s'il est vide", async () => {
+      const reponse = await request(serveur)
+        .post('/api/retours-experience')
+        .send({
+          emailDeContact: '',
+          raison: 'pas-clair',
+        });
+
+      assert.equal(reponse.status, 201);
+    });
+
+    it('aseptise les paramètres', async () => {
+      let retourExperienceEnvoye: RetourExperience | null = null;
+      messagerieInstantanee.notifieUnRetourExperience = async (
+        retourExperience: RetourExperience
+      ) => {
+        retourExperienceEnvoye = retourExperience;
+      };
+
+      await request(serveur).post('/api/retours-experience').send({
+        raison: 'pas-clair',
+        precision: '  <>',
+      });
+
+      assert.equal(retourExperienceEnvoye!.precision, '&lt;&gt;');
     });
   });
 });
