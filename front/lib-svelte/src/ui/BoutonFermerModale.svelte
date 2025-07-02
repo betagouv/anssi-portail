63a8725cfb73960fd65e7b2dc78b1769c421a03b
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-07-02 11:24:11 +0200

    [SOIN] Utilise le bouton de l'UI KIT pour fermer les modales

--- front/lib-svelte/src/ui/BoutonFermerModale.svelte
+++ front/lib-svelte/src/ui/BoutonFermerModale.svelte
@@ -1,33 +1,22 @@
 <script lang="ts">
-
 </script>
 
-<button class="fermer" on:click type="button">Fermer</button>
+<lab-anssi-bouton
+  icone="close-line"
+  on:click
+  on:keypress
+  role="button"
+  sans-bordure
+  taille="sm"
+  tabindex={0}
+  titre="Fermer"
+  variante="tertiaire"
+>
+</lab-anssi-bouton>
 
 <style lang="scss">
-  .fermer {
-    font-size: 0.875rem;
-    line-height: 1.5rem;
-    font-weight: 500;
-    border: none;
-    background: none;
-    display: flex;
-    gap: 8px;
-    align-items: center;
-    padding: 4px 0 4px 12px;
-    cursor: pointer;
+  lab-anssi-bouton {
     align-self: flex-end;
-
-    &:after {
-      content: '';
-      display: inline-block;
-      background-repeat: no-repeat;
-      background-size: contain;
-      width: 16px;
-      height: 16px;
-      line-height: 1.5rem;
-      background-image: url('/assets/images/icone-croix.svg');
-      filter: brightness(0) saturate(100%);
-    }
+    margin-right: -12px;
   }
 </style>
