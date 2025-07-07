de5f693a5f263222fe10dda380e9a734fba64e55
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-07-07 17:31:08 +0200

    Récupère les statistiques depuis l’API

--- front/lib-svelte/src/statistiques/Statistiques.svelte
+++ front/lib-svelte/src/statistiques/Statistiques.svelte
@@ -1,29 +1,55 @@
-<script>
+<script lang="ts">
   import Tuile from './Tuile.svelte';
-</script>
+  import { onMount } from 'svelte';
+  import axios from 'axios';
 
-<div class="tuiles">
-  <Tuile
-    description="Utilisateurs inscrits"
-    image="stat-utilisateurs-inscrits"
-    mesure={1200}
-  />
-  <Tuile
-    description="Test de maturité cyber"
-    image="stat-test-maturite"
-    mesure={1200}
-  />
-  <Tuile
-    description="Diagnostics cyber"
-    image="stat-diagnostics-cyber"
-    mesure={1200}
-  />
-  <Tuile
-    description="Services et ressources consultés"
-    image="stat-services-et-ressources-consultes"
-    mesure={1200}
-  />
-</div>
+  type Statistiques = {
+    utilisateursInscrits: number;
+    testsMaturite: {
+      total: number;
+      parNiveau: {
+        insuffisant: number;
+        emergent: number;
+        intermediaire: number;
+        confirme: number;
+        optimal: number;
+      }
+    };
+    diagnosticsCyber: number;
+    servicesEtRessourcesConsultes: number;
+  }
+
+  let mesures: Statistiques | undefined = undefined;
+
+  onMount(async () => {
+    const reponse = await axios.get<Statistiques>('/api/statistiques');
+    mesures = reponse.data;
+  });
+</script>
+{#if mesures}
+  <div class="tuiles">
+    <Tuile
+      description="Utilisateurs inscrits"
+      image="stat-utilisateurs-inscrits"
+      mesure={mesures.utilisateursInscrits}
+    />
+    <Tuile
+      description="Test de maturité cyber"
+      image="stat-test-maturite"
+      mesure={mesures.testsMaturite.total}
+    />
+    <Tuile
+      description="Diagnostics cyber"
+      image="stat-diagnostics-cyber"
+      mesure={mesures.diagnosticsCyber}
+    />
+    <Tuile
+      description="Services et ressources consultés"
+      image="stat-services-et-ressources-consultes"
+      mesure={mesures.servicesEtRessourcesConsultes}
+    />
+  </div>
+{/if}
 
 <style lang="scss">
   @use '../../../assets/styles/responsive' as *;
