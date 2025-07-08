42b2b3f25e1808134556fd2031899f5ab72dcc7b
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-07-08 15:27:16 +0200

    Renforce la validation des réponses en écartant les entiers dans un tableau

--- back/tests/api/testMaturite/ressourceResultatsDeTest.spec.ts
+++ back/tests/api/testMaturite/ressourceResultatsDeTest.spec.ts
@@ -313,6 +313,18 @@ describe('La ressource qui gère les résultats de test de maturité', () => {
           );
         });
 
+        it('resiste aux réponses sous forme de tableau', async () => {
+          const reponse = await requeteAvecDonneeIncorrecte({
+            reponses: { ...donneesCorrectes.reponses, pilotage: [5] },
+          });
+
+          assert.equal(reponse.status, 400);
+          assert.equal(
+            reponse.body.erreur,
+            'Les valeurs de réponses doivent être comprises entre 1 et 5'
+          );
+        });
+
         it('aseptise le code de session de groupe', async () => {
           await requeteAvecDonneeIncorrecte({
             codeSessionGroupe: ' ABC2ED ',
