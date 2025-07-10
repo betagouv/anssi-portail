3bb0ac2328b806da0c19c519d6a628aa3bc803d2
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-07-10 16:03:47 +0200

    Adapte l’affichage en mode bureau

--- front/lib-svelte/src/test-maturite/CarteTestMaturite.svelte
+++ front/lib-svelte/src/test-maturite/CarteTestMaturite.svelte
@@ -14,15 +14,24 @@
   export let resultatTest: ResultatTest;
 
   $: niveau = niveauxMaturite.find(
-    (niveau) => niveau.id === resultatTest.niveau,
+    (niveau) => niveau.id === resultatTest.niveau
   );
 
   $: description = niveau?.description;
   $: libelleNiveau = niveau?.label;
-  $: dateFormatee = new Intl.DateTimeFormat('fr-FR', { dateStyle: 'long' }).format(new Date(resultatTest.dateRealisation));
+  $: dateFormatee = new Intl.DateTimeFormat('fr-FR', {
+    dateStyle: 'long',
+  }).format(new Date(resultatTest.dateRealisation));
 </script>
 
 <a href="#historique" class="carte">
+  <div class="illustration-niveau">
+    <img
+      class="plante"
+      src="/assets/images/test-maturite/niveaux/{resultatTest.niveau}.svg"
+      alt="Niveau de maturité"
+    />
+  </div>
   <span class="date">{dateFormatee}</span>
   <h3>{libelleNiveau}</h3>
   <p class="description">{description}</p>
@@ -33,15 +42,25 @@
 </a>
 
 <style lang="scss">
+  @use '../../../assets/styles/responsive' as *;
+
   .carte {
     padding: 24px;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
+    display: grid;
+    grid-template-areas:
+      'illustration date'
+      'illustration niveau'
+      'illustration description'
+      'illustration lien';
     border: 1px solid #ddd;
     border-radius: 8px;
     cursor: pointer;
     background: white;
+
+    @include a-partir-de(md) {
+      padding: 12px;
+      gap: 0 24px;
+    }
 
     &:hover {
       background: #f6f6f6;
@@ -57,14 +76,48 @@
     }
   }
 
+  .illustration-niveau {
+    grid-area: illustration;
+    display: none;
+    @include a-partir-de(md) {
+      background-color: #fff7db;
+      width: 120px;
+      height: 176px;
+      display: flex;
+      flex-direction: column-reverse;
+      padding-bottom: 16px;
+      align-items: center;
+      position: relative;
+      img {
+        width: 72px;
+        z-index: 1;
+      }
+
+      &:before {
+        content: '';
+        position: absolute;
+        z-index: 0;
+        bottom: 26px;
+        border: 1px solid #0d0c21;
+        left: 0;
+        right: 0;
+      }
+    }
+  }
+
   .date {
+    grid-area: date;
     color: #666;
     font-size: 0.875rem;
     line-height: 1.5rem;
     margin-bottom: 8px;
+    @include a-partir-de(md) {
+      margin-top: 12px;
+    }
   }
 
   h3 {
+    grid-area: niveau;
     color: #161616;
     font-size: 1.125rem;
     font-weight: bold;
@@ -73,12 +126,26 @@
   }
 
   .description {
+    grid-area: description;
     color: #3a3a3a;
     margin: 0 0 8px;
     text-align: left;
   }
 
   .lien {
-    align-self: flex-end;
+    grid-area: lien;
+    justify-self: flex-end;
+    @include a-partir-de(md) {
+      margin-bottom: 12px;
+    }
+  }
+
+  @include a-partir-de(md) {
+    .date,
+    h3,
+    .description,
+    .lien {
+      margin-right: 12px;
+    }
   }
 </style>
