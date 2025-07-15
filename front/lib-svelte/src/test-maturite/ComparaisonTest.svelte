e23f4cc3692fa28523ef401389ec8b6719641d46
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-07-15 11:29:47 +0200

    [COMPARAISON] Réorganise les sections

--- front/lib-svelte/src/test-maturite/ComparaisonTest.svelte
+++ front/lib-svelte/src/test-maturite/ComparaisonTest.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
-  import BoutonsPartageTest from './BoutonsPartageTest.svelte';
   import GraphiqueAnneau from './GraphiqueAnneau.svelte';
   import LegendeAnneau from './LegendeAnneau.svelte';
-  import LegendeRadarSessionGroupe from './LegendeRadarSessionGroupe.svelte';
   import PartageTest from './PartageTest.svelte';
   import RadarSessionGroupe from './RadarSessionGroupe.svelte';
   import type { Serie, SerieRadar } from './Serie';
+  import LegendeRadarComparaison from './LegendeRadarComparaison.svelte';
 
   export let testRealise = false;
 
@@ -31,33 +30,33 @@
   ];
 </script>
 
-<section class="comparaison">
-  <div class="contenu-section">
-    {#if testRealise}
-      <section>
-        <div class="contenu-section">
-          <h2>Répartition des niveaux de maturité cyber de cette session</h2>
-          <div class="repartition-niveaux-maturite">
-            <GraphiqueAnneau {serie} nomDeLaDonnee="Participants" />
-            <LegendeAnneau {serie} />
-          </div>
-        </div>
-      </section>
+{#if testRealise}
+  <section class="repartion-organisations">
+    <div class="contenu-section">
+      <h2>Répartition des organisations</h2>
+      <div class="repartition-niveaux-maturite">
+        <GraphiqueAnneau {serie} nomDeLaDonnee="organisations" />
+        <LegendeAnneau {serie} />
+      </div>
+    </div>
+  </section>
 
-      <section>
-        <div class="contenu-section repartition-reponses">
-          <h2>Répartition des réponses</h2>
-          <RadarSessionGroupe series={seriesRadar} />
-          <LegendeRadarSessionGroupe />
-          <div class="message-information">
-            Le résultat obtenu est une évaluation indicative basée sur un modèle
-            élaboré par l’ANSSI. La maturité cyber n’est pas une évaluation du
-            niveau de sécurité des systèmes d’information d’une organisation
-            mais de sa posture à l’égard des enjeux cyber.
-          </div>
-        </div>
-      </section>
-    {:else}
+  <section class="repartition-reponses">
+    <div class="contenu-section">
+      <h2>Répartition des réponses</h2>
+      <RadarSessionGroupe series={seriesRadar} />
+      <LegendeRadarComparaison />
+      <div class="message-information">
+        Le résultat obtenu est une évaluation indicative basée sur un modèle
+        élaboré par l’ANSSI. La maturité cyber n’est pas une évaluation du
+        niveau de sécurité des systèmes d’information d’une organisation mais de
+        sa posture à l’égard des enjeux cyber.
+      </div>
+    </div>
+  </section>
+{:else}
+  <section class="pas-de-test">
+    <div class="contenu-section">
       <img
         src="/assets/images/illustration-dragon-aucun-resultat.svg"
         alt="Bientôt disponible"
@@ -65,33 +64,84 @@
       <h4>Vous souhaitez comparer la maturité cyber de votre organisation ?</h4>
       <p>Pour cela vous devez d’abord réaliser le test de maturité cyber.</p>
       <a href="/test-maturite" class="bouton primaire"> Débuter le test </a>
-    {/if}
-  </div>
-</section>
+    </div>
+  </section>
+{/if}
 <PartageTest />
 
 <style lang="scss">
   @use '../../../assets/styles/responsive' as *;
-  .description-test-realise {
-    margin-bottom: 16px;
-  }
 
-  .repartition-niveaux-maturite {
-    display: flex;
-    flex-direction: column;
-    align-self: center;
-    gap: 48px;
-    @include a-partir-de(sm) {
+  .repartion-organisations {
+    padding: 32px var(--gouttiere) 48px;
+
+    .contenu-section {
+      display: flex;
+      flex-direction: column;
       align-items: center;
-      justify-content: space-around;
-      flex-direction: row;
+      gap: 48px;
+
+      .repartition-niveaux-maturite {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-evenly;
+        gap: 56px;
+      }
     }
   }
 
-  .message-information {
-    font-size: 0.75rem;
-    line-height: 1.5rem;
-    padding: 24px 0;
-    color: #666666;
+  .repartition-reponses {
+    padding: 48px var(--gouttiere);
+
+    .contenu-section {
+      display: flex;
+      flex-direction: column;
+      gap: 32px;
+
+      h2 {
+        margin-bottom: 32px;
+      }
+
+      @include a-partir-de(sm) {
+        align-items: center;
+        justify-content: space-around;
+        flex-direction: row;
+      }
+
+      .message-information {
+        margin-top: 16px;
+        font-size: 0.75rem;
+        line-height: 1.5rem;
+        padding: 24px 0;
+        color: #666666;
+      }
+    }
+  }
+
+  .pas-de-test {
+    .contenu-section {
+      display: flex;
+      flex-direction: column;
+      gap: 16px;
+      align-items: center;
+      text-align: center;
+    }
+
+    img {
+      max-width: 282px;
+    }
+
+    h4 {
+      font-size: 1.5rem;
+      line-height: 1.75rem;
+      max-width: 588px;
+    }
+
+    p {
+      font-size: 1.125rem;
+      line-height: 1.75rem;
+      max-width: 588px;
+    }
   }
 </style>
