21f8bff0beecc47da335660c3d042462e947ddc5
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-07-16 11:21:46 +0200

    [SOIN] Extrait le calcul de la répartition

--- back/src/api/testMaturite/ressourceRepartitionDesResultatsDeTest.ts
+++ back/src/api/testMaturite/ressourceRepartitionDesResultatsDeTest.ts
@@ -1,15 +1,6 @@
 import { Request, Response, Router } from 'express';
-import {
-  IdNiveauMaturite,
-  IdRubrique,
-  tousLesIdRubrique,
-} from '../../metier/resultatTestMaturite';
+import { RepartitionResultatsTest } from '../../metier/repartitionResultatsTest';
 import { ConfigurationServeur } from '../configurationServeur';
-
-type Serie = {
-  id: IdNiveauMaturite;
-  valeurs: Record<IdRubrique, number[]>;
-};
 
 export const ressourceRepartitionDesResultatsDeTest = ({
   entrepotResultatTest,
@@ -18,59 +9,10 @@ export const ressourceRepartitionDesResultatsDeTest = ({
   routeur.get('/', async (_requete: Request, reponse: Response) => {
     const tousLesResultats =
       await entrepotResultatTest.tousEnOmettantUtilisateur();
-    const series = tousLesResultats.reduce((accumulateur, test) => {
-      const niveau = test.niveau();
-      let serieCourante = accumulateur.find((serie) => serie.id === niveau);
-      if (!serieCourante) {
-        serieCourante = {
-          id: niveau,
-          valeurs: tousLesIdRubrique.reduce(
-            (rubriqueAccumulateur, rubrique) => {
-              return {
-                ...rubriqueAccumulateur,
-                [rubrique]: [],
-              };
-            },
-            {}
-          ),
-        };
-        accumulateur.push(serieCourante);
-      }
-      tousLesIdRubrique.forEach((rubrique) => {
-        serieCourante.valeurs[rubrique].push(test.reponses[rubrique]);
-      });
-      return accumulateur;
-    }, [] as Serie[]);
-
-    const calculeValeurMoyenne = (
-      idRubrique: IdRubrique,
-      serie: Serie
-    ): number => {
-      return (
-        serie.valeurs[idRubrique].reduce((accumulateur, valeur) => {
-          return accumulateur + valeur;
-        }, 0) / serie.valeurs[idRubrique].length
-      );
-    };
-    const seriesMoyenne = series.map((serie) => ({
-      id: serie.id,
-      valeurs: {
-        'adoption-solutions': calculeValeurMoyenne('adoption-solutions', serie),
-        'prise-en-compte-risque': calculeValeurMoyenne(
-          'prise-en-compte-risque',
-          serie
-        ),
-        'ressources-humaines': calculeValeurMoyenne(
-          'ressources-humaines',
-          serie
-        ),
-        budget: calculeValeurMoyenne('budget', serie),
-        pilotage: calculeValeurMoyenne('pilotage', serie),
-        posture: calculeValeurMoyenne('posture', serie),
-      },
-      totalNombreTests: serie.valeurs['adoption-solutions'].length,
-    }));
-    reponse.send(seriesMoyenne);
+    const repartitions = new RepartitionResultatsTest(
+      tousLesResultats
+    ).calculeRepartitionParNiveau();
+    reponse.send(repartitions);
   });
   return routeur;
 };
---
+++ back/src/metier/repartitionResultatsTest.ts
@@ -0,0 +1,76 @@
+import {
+  IdNiveauMaturite,
+  IdRubrique,
+  ResultatTestMaturite,
+  tousLesIdRubrique,
+} from './resultatTestMaturite';
+
+type ScoresPourUnNiveau = {
+  id: IdNiveauMaturite;
+  valeurs: Record<IdRubrique, number[]>;
+};
+
+export type RepartitionResultatsTestPourUnNiveau = {
+  id: IdNiveauMaturite;
+  valeurs: Record<IdRubrique, number>;
+  totalNombreTests: number;
+};
+
+export class RepartitionResultatsTest {
+  constructor(private readonly resultatsDeTest: ResultatTestMaturite[]) {}
+
+  calculeRepartitionParNiveau = () => {
+    const listeDesScoresParNiveau = this.resultatsDeTest.reduce(
+      (accumulateur, test) => {
+        const niveau = test.niveau();
+        let scoresParNiveauCourant = accumulateur.find(
+          (scoreParNiveau) => scoreParNiveau.id === niveau
+        );
+        if (!scoresParNiveauCourant) {
+          scoresParNiveauCourant = {
+            id: niveau,
+            valeurs: this.creeObjetParRubrique(() => []),
+          };
+          accumulateur.push(scoresParNiveauCourant);
+        }
+        tousLesIdRubrique.forEach((rubrique) => {
+          scoresParNiveauCourant.valeurs[rubrique].push(
+            test.reponses[rubrique]
+          );
+        });
+        return accumulateur;
+      },
+      [] as ScoresPourUnNiveau[]
+    );
+
+    return listeDesScoresParNiveau.map((scoresParNiveau) => ({
+      id: scoresParNiveau.id,
+      valeurs: this.creeObjetParRubrique((rubrique) =>
+        this.calculeValeurMoyenne(rubrique, scoresParNiveau)
+      ),
+      totalNombreTests: scoresParNiveau.valeurs['adoption-solutions'].length,
+    }));
+  };
+
+  private calculeValeurMoyenne(
+    idRubrique: IdRubrique,
+    scoresParNiveau: ScoresPourUnNiveau
+  ): number {
+    return (
+      scoresParNiveau.valeurs[idRubrique].reduce((accumulateur, valeur) => {
+        return accumulateur + valeur;
+      }, 0) / scoresParNiveau.valeurs[idRubrique].length
+    );
+  }
+
+  private creeObjetParRubrique<T>(
+    valeurDeLaRubrique: (rubrique: IdRubrique) => T
+  ): Record<IdRubrique, T> {
+    return tousLesIdRubrique.reduce((rubriqueAccumulateur, rubrique) => {
+      return {
+        ...rubriqueAccumulateur,
+        [rubrique]: valeurDeLaRubrique(rubrique),
+      };
+    }, {});
+  }
+}
