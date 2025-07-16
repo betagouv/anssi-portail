f592817334cd9bd81fb5e95ede8d7d66c4fdb7b0
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-07-16 10:22:10 +0200

    Remonte les données de répartition dans le front

--- front/lib-svelte/src/niveaux-maturite/NiveauxMaturite.donnees.ts
+++ front/lib-svelte/src/niveaux-maturite/NiveauxMaturite.donnees.ts
@@ -1,9 +1,9 @@
 export type IdNiveau =
-  | "insuffisant"
-  | "emergent"
-  | "intermediaire"
-  | "confirme"
-  | "optimal";
+  | 'insuffisant'
+  | 'emergent'
+  | 'intermediaire'
+  | 'confirme'
+  | 'optimal';
 
 export type NiveauMaturite = {
   id: IdNiveau;
@@ -21,8 +21,8 @@ export type NiveauMaturite = {
 
 export const niveauxMaturite: NiveauMaturite[] = [
   {
-    id: "insuffisant",
-    label: "Insuffisant",
+    id: 'insuffisant',
+    label: 'Insuffisant',
     description:
       "Une conscience insuffisante des risques cyber pesant sur l'organisation. Pas ou peu de mesures spécifiques mises en oeuvre en matière de cybersécurité.",
     priorite:
@@ -42,12 +42,12 @@ export const niveauxMaturite: NiveauMaturite[] = [
       "L'entité est vulnérable aux menaces cyber les plus courantes.",
   },
   {
-    id: "emergent",
-    label: "Émergent",
+    id: 'emergent',
+    label: 'Émergent',
     description:
       "Une prise de conscience de l'existence de risques cyber pour l'organisation et une volonté de mettre en place des premières mesures cyber pertinentes.",
     priorite:
-      "Identifier les mesures de cybersécurité prioritaires et rapides à mettre en oeuvre pour corriger les principales vulnérabilités.",
+      'Identifier les mesures de cybersécurité prioritaires et rapides à mettre en oeuvre pour corriger les principales vulnérabilités.',
     priseEnCompteRisque:
       "Les responsables ont conscience que l’organisation pourrait être victime de cyberattaques sans mesurer l'ampleur du risque.",
     posture:
@@ -55,46 +55,46 @@ export const niveauxMaturite: NiveauMaturite[] = [
     pilotage:
       "Des actions en matière de cybersécurité sont parfois mises en oeuvre mais sans réelle priorisation et sans plan d'action structuré.",
     ressourcesHumaines:
-      "Une personne est identifiée comme référente pour les enjeux cyber en plus de ses missions actuelles.",
+      'Une personne est identifiée comme référente pour les enjeux cyber en plus de ses missions actuelles.',
     budget:
-      "Des dépenses dédiées à la cybersécurité peuvent être occasionnellement autorisées.",
+      'Des dépenses dédiées à la cybersécurité peuvent être occasionnellement autorisées.',
     adoptionSolutions:
-      "Une ou plusieurs solutions cyber ont pu être déployées en fonction des opportunités.",
+      'Une ou plusieurs solutions cyber ont pu être déployées en fonction des opportunités.',
     niveauSecurite:
       "L'entité est vulnérable aux menaces cyber les plus courantes.",
   },
   {
-    id: "intermediaire",
-    label: "Intermédiaire",
+    id: 'intermediaire',
+    label: 'Intermédiaire',
     description:
       "Une conscience partagée des risques cyber pour l'organisation et la volonté de formaliser un plan d'action cyber complet tout en ajustant les moyens en conséquence.",
     priorite:
       "Structurer le plan d'action cyber et dédier une ligne budgétaire à la cybersécurité. ",
     priseEnCompteRisque:
-      "Les données et les processus à protéger en priorité contre les risques cyber les plus courants ont été identifiés.",
+      'Les données et les processus à protéger en priorité contre les risques cyber les plus courants ont été identifiés.',
     posture:
-      "Les responsables manifestent un réel intérêt pour le renforcement de la cybersécurité et les directions métiers sont sensibilisées.",
+      'Les responsables manifestent un réel intérêt pour le renforcement de la cybersécurité et les directions métiers sont sensibilisées.',
     pilotage:
       "Un premier plan d'action avec des actions prioritaires à mettre en oeuvre a été établi et est suivi.",
     ressourcesHumaines:
       "La personne référente dispose d'un temps dédié au suivi des enjeux cyber et peut compter sur des soutiens ponctuels.",
-    budget: "Une part du budget informatique est alloué à la cybersécurité.",
+    budget: 'Une part du budget informatique est alloué à la cybersécurité.',
     adoptionSolutions:
-      "L’organisation dispose de plusieurs solutions adaptées aux actions prioritaires identifiées dans le plan d’action.",
+      'L’organisation dispose de plusieurs solutions adaptées aux actions prioritaires identifiées dans le plan d’action.',
     niveauSecurite:
       "Des vulnérabilités importantes ont été corrigées mais l'organisation demeure vulnérable et n'est souvent pas préparée à faire face à une crise d'origine cyber.",
   },
   {
-    id: "confirme",
-    label: "Confirmé",
+    id: 'confirme',
+    label: 'Confirmé',
     description:
       "Une cartographie précise de ses risques cyber pour l'organisation, un plan d'action cyber approfondi et des moyens humains et budgétaires dédiés.",
     priorite:
-      "Élargir la prise en compte des enjeux de cybersécurité notamment en matière de défense et de résilience et renforcer les moyens dédiés à la cybersécurité.",
+      'Élargir la prise en compte des enjeux de cybersécurité notamment en matière de défense et de résilience et renforcer les moyens dédiés à la cybersécurité.',
     priseEnCompteRisque:
       "Une analyse de risque approfondie de l'organisation a été menée et un plan détaillé de traitement des risques a été établi.",
     posture:
-      "La cybersécurité est un sujet prioritaire évoqué en CODIR, les directions métiers sont responsabilisées sur le sujet et les agents sensibilisés.",
+      'La cybersécurité est un sujet prioritaire évoqué en CODIR, les directions métiers sont responsabilisées sur le sujet et les agents sensibilisés.',
     pilotage:
       "Un plan d'action cyber étoffé est établi et fait l’objet d’un suivi régulier.",
     ressourcesHumaines:
@@ -107,8 +107,8 @@ export const niveauxMaturite: NiveauMaturite[] = [
       "L'organisation a mis en oeuvre des mesures efficaces contre les risques cyber les plus courants et dispose d'un niveau de préparation satisfaisant face à une crise d'origine cyber, mais demeure vulnérable à des attaques plus élaborées.",
   },
   {
-    id: "optimal",
-    label: "Optimal",
+    id: 'optimal',
+    label: 'Optimal',
     description:
       "Des objectifs de réduction des risques cyber intégrés à la stratégie de l'organisation. Des moyens humains, budgétaires conséquents, un plan d'action cyber exhaustif, adapté aux menaces stratégiques. ",
     priorite:
@@ -116,7 +116,7 @@ export const niveauxMaturite: NiveauMaturite[] = [
     priseEnCompteRisque:
       "Les objectifs de réduction des risques d'origine cyber sont intégrés dans la stratégie globale de gestion des risques de l'organisation.",
     posture:
-      "L’organisation est un acteur clé de la cybersécurité au sein de son écosystème ou de son secteur d’activité.",
+      'L’organisation est un acteur clé de la cybersécurité au sein de son écosystème ou de son secteur d’activité.',
     pilotage:
       "Un plan d'action cyber complet est établi et ses objectifs prioritaires sont intégrés dans la stratégie globale de l'organisation.",
     ressourcesHumaines:
@@ -129,3 +129,11 @@ export const niveauxMaturite: NiveauMaturite[] = [
       "L'organisation a mis en oeuvre des mesures lui permettant de se protéger contre des attaques élaborées et est parfaitement préparée à réagir à une crise d'origine cyber.",
   },
 ];
+
+export const couleursDeNiveau: Record<IdNiveau, string> = {
+  insuffisant: '#6369f1',
+  emergent: '#fec54b',
+  intermediaire: '#8248a1',
+  confirme: '#f26c85',
+  optimal: '#8ed4a3',
+};
