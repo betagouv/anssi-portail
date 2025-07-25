8911ed9704cf12a2e4da103d4ee9db21622535aa
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-07-25 08:59:57 +0200

    Ignore les erreurs Axios `Request aborted`
    ...car elles correspondent à un rafraîchissement du navigateur par l’utilisateur, ce qui n’est pas une erreur pour nous

--- front/scripts/sentry.js
+++ front/scripts/sentry.js
@@ -2,9 +2,21 @@ const dsn = document.getElementById('script-sentry').dataset.dsn;
 const environment =
   document.getElementById('script-sentry').dataset.environnement;
 
+// Voir l'issue https://github.com/axios/axios/issues/6209#issuecomment-2299747509
+const avantEnvoiSentry = (evenement, detail) => {
+  if (
+    axios?.isAxiosError(detail?.originalException) &&
+    detail?.originalException?.code === 'ECONNABORTED'
+  ) {
+    return null;
+  }
+  return evenement;
+};
+
 Sentry.init({
   dsn,
   environment,
+  beforeSend: avantEnvoiSentry,
 });
 
 Sentry.setTag('msc-source', 'frontend');
