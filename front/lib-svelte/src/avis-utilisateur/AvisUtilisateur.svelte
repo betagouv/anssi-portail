3580cef06010d8e692239e432695e494434dbe68
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2025-07-31 17:59:05 +0200

    [FEEDBACK] Crée le visuel du CTA

--- front/lib-svelte/src/avis-utilisateur/AvisUtilisateur.svelte
+++ front/lib-svelte/src/avis-utilisateur/AvisUtilisateur.svelte
@@ -2,7 +2,10 @@
 </script>
 
 <div class="avis-utilisateur-cta">
-  Votre avis nous intéresse.
+  <div class="illustration">
+    <img src="/assets/images/dragon-coeur.svg" alt="Dragon coeur" />
+  </div>
+    <p class="texte">Votre avis nous intéresse&nbsp;!</p>
 </div>
 
 <style lang="scss">
@@ -16,5 +19,28 @@
     right: 0;
     top: 208px;
     width: 124px;
+
+    .illustration {
+      background-color: var(--jaune-clair-msc);
+      border-top-left-radius: 8px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: flex-end;
+      height: 120px;
+
+      img {
+        width: 97px;
+      }
+    }
+
+    .texte {      
+      color: #161616;
+      font-size: 14px;
+      font-weight: 700;
+      line-height: normal;
+      margin: 8px 8px 12px;
+      text-align: center;
+    }
   }
 </style>
