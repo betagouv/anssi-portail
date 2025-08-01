4d871b867048f57d4f83446210d0f6c66e342293
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-08-01 11:15:16 +0200

    [FEEDBACK] Cache le CTA avis utilisateur derrière un feature flag

--- front/lib-svelte/src/main-avis-utilisateur.ts
+++ front/lib-svelte/src/main-avis-utilisateur.ts
@@ -1,6 +1,12 @@
 import { mount } from 'svelte';
 import AvisUtilisateur from './avis-utilisateur/AvisUtilisateur.svelte';
 
+const donnees = document.getElementById('donneesAvisUtilisateur')!.textContent;
+if (!donnees) throw new Error('Impossible de trouver les données');
+
+const props = JSON.parse(donnees);
+
 mount(AvisUtilisateur, {
   target: document.getElementById('avis-utilisateur')!,
+  props,
 });
