1054f67b03e30cb03ce39e09281d0d3d9e04bade
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-08-05 14:19:57 +0200

    [FEEDBACK] Controle l'affichage de l'avis utilisateur

---
+++ front/lib-svelte/src/avis-utilisateur/controleAffichage.ts
@@ -0,0 +1,26 @@
+const UNE_JOURNEE_EN_MILLISECONDE = 1000 * 60 * 60 * 24;
+
+export const proposeAvisUtilisteur = ({
+  cheminCourant,
+  dateDernierAvis,
+  dateDerniereFermeture,
+}: {
+  cheminCourant: string;
+  dateDernierAvis?: Date;
+  dateDerniereFermeture?: Date;
+}): boolean => {
+  if (RegExp(/(\/?)(cyberdepart|test-maturite)(\/?)$/).exec(cheminCourant)) {
+    return false;
+  }
+  if (dateDernierAvis) {
+    return false;
+  }
+  if (
+    dateDerniereFermeture &&
+    dateDerniereFermeture.getTime() >=
+      new Date().getTime() - UNE_JOURNEE_EN_MILLISECONDE
+  ) {
+    return false;
+  }
+  return true;
+};
