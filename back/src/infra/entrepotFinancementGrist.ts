adbb53c74fdc812a6c379eea4a49287c6a10c1dd
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-08-11 17:23:05 +0200

    [FINANCEMENTS] Créer l'adaptateur financement Grist
    ...en charge d'appeler Grist pour récupérer une liste de financements

--- back/src/infra/entrepotFinancementGrist.ts
+++ back/src/infra/entrepotFinancementGrist.ts
@@ -1,7 +1,21 @@
+import axios, { AxiosResponse } from 'axios';
 import { EntrepotFinancement } from '../metier/entrepotFinancement';
+import { Financement } from '../metier/financement';
+
+export type ClientHttp = {
+  get: (url: string) => Promise<unknown>;
+};
 
 export class EntrepotFinancementGrist implements EntrepotFinancement {
+  clientHttp: ClientHttp;
+  constructor({ clientHttp = axios }: { clientHttp?: ClientHttp }) {
+    this.clientHttp = clientHttp;
+  }
+
   tous = async () => {
-    throw new Error('Pas encore implémentée');
+    const reponse = (await this.clientHttp.get(
+      'la vraie url'
+    )) as AxiosResponse;
+    return reponse.data as Financement[];
   };
 }
