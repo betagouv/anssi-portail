7edc042da6fec5079241352e80fe1631ad2c9aa5
Marc ETOURNEAU <marc.etourneau@beta.gouv.fr>
2025-08-12 16:53:32 +0200

    [FINANCEMENTS] Ajoute le bandeau Financements cyber

--- front/lib-svelte/src/financements/Financements.svelte
+++ front/lib-svelte/src/financements/Financements.svelte
@@ -1,4 +1,10 @@
 <script lang="ts">
+  import Hero from '../ui/Hero.svelte';
+  import { profilStore } from '../stores/profil.store';
 </script>
 
-<div>Financements placeholder</div>
+<Hero
+  titre="Financements cyber"
+  description="Bénéficiez d’aides et de subventions pour renforcer la maturité cyber de votre organisation."
+  ariane={$profilStore ? undefined : 'Financements cyber'}
+/>
