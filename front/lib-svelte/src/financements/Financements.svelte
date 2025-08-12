87dd393c355f1e93811f894722a3b7561e6c7595
Marc ETOURNEAU <marc.etourneau@beta.gouv.fr>
2025-08-12 18:08:46 +0200

    [FINANCEMENTS] Met en place le layout desktop et tablette de la page

--- front/lib-svelte/src/financements/Financements.svelte
+++ front/lib-svelte/src/financements/Financements.svelte
@@ -8,3 +8,57 @@
   description="Bénéficiez d’aides et de subventions pour renforcer la maturité cyber de votre organisation."
   ariane={$profilStore ? undefined : 'Financements cyber'}
 />
+
+<section class="financements">
+  <div class="entete-filtres"></div>
+  <div class="contenu-section">
+    <div class="grille-cartes">
+      <p>test</p>
+      <p>sg</p>
+      <p>gds</p>
+      <p>gsdgds</p>
+    </div>
+  </div>
+</section>
+
+<style lang="scss">
+  @use '../../../assets/styles/responsive' as *;
+
+  section {
+    padding: 0 var(--gouttiere) 40px;
+    .contenu-section {
+      display: flex;
+      gap: 1.5rem;
+      padding-top: 3rem;
+      padding-bottom: 3rem;
+
+      .filtres {
+        display: none;
+
+        @include a-partir-de(md) {
+          display: flex;
+          flex-direction: column;
+          flex: 1 0 0;
+          max-width: 282px;
+        }
+      }
+
+      .grille-cartes {
+        display: grid;
+        row-gap: 24px;
+        column-gap: 24px;
+        flex: 1 0 0;
+        grid-template-rows:
+          fit-content(100%) fit-content(100%) fit-content(100%) fit-content(
+            100%
+          )
+          fit-content(100%) 216px;
+        grid-template-columns: 1fr;
+
+        @include a-partir-de(sm) {
+          grid-template-columns: repeat(2, minmax(0, 1fr));
+        }
+      }
+    }
+  }
+</style>
