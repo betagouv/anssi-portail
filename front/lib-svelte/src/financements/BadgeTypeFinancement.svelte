cb05dbbf94881cd0b3efd0f06cefdef43f8f2f6c
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-08-13 10:51:30 +0200

    [FINANCEMENTS] Utilise des badges
    ...pour afficher les types de financement, à la place de tags

---
+++ front/lib-svelte/src/financements/BadgeTypeFinancement.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+</script>
+
+<span>
+  <slot />
+</span>
+
+<style lang="scss">
+  span {
+    font-size: 0.75rem;
+    font-weight: bold;
+    line-height: 1.25rem;
+
+    color: var(--tag-couleur-texte);
+    background-color: var(--tag-couleur-fond);
+    padding: 0 6px;
+    border-radius: 4px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: fit-content;
+    white-space: nowrap;
+    box-sizing: border-box;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    text-transform: uppercase;
+  }
+</style>
