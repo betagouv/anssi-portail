0572efe24bb6ff7abbe20b301a5e9f34651332d1
Marc ETOURNEAU <marc.etourneau@beta.gouv.fr>
2025-08-14 16:29:49 +0200

    [FIANCEMENTS] Affiche les sections de détail d'un financement

---
+++ front/lib-svelte/src/financements/SectionDetailsFinancement.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  export let ancre: string;
+  export let titre: string;
+  export let detail: string;
+</script>
+
+{#if detail}
+  <section id={ancre}>
+    <div class="contenu-section">
+      <h2>{titre}</h2>
+      <!-- On affiche des données provenant d'une source interne -->
+      <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+      <div>{@html detail}</div>
+    </div>
+  </section>
+{/if}
+
+<style lang="scss">
+  section {
+    .contenu-section {
+      display: flex;
+      flex-direction: column;
+      gap: 16px;
+    }
+  }
+</style>
