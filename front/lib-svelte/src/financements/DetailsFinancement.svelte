ad63e2b89210954c7af4e83f23825d5f0d51d789
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-08-18 16:11:32 +0200

    [FINANCEMENTS] Mise en place du sommaire mobile ...et refonte du refonte du sommaire desktop

--- front/lib-svelte/src/financements/DetailsFinancement.svelte
+++ front/lib-svelte/src/financements/DetailsFinancement.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import axios from 'axios';
-  import { onMount } from 'svelte';
+  import { onDestroy, onMount, tick } from 'svelte';
   import FilAriane from '../ui/FilAriane.svelte';
   import BadgeTypeFinancement from './BadgeTypeFinancement.svelte';
   import type { Financement, ResumeFinancement } from './financement';
@@ -23,8 +23,13 @@
     contact: string;
   };
 
-  export let resumeFinancement: ResumeFinancement;
-  let financement: Financement | undefined;
+  interface Props {
+    resumeFinancement: ResumeFinancement;
+  }
+
+  let { resumeFinancement }: Props = $props();
+  let financement: Financement | undefined = $state();
+  let detailsElement: HTMLDetailsElement | undefined = $state();
 
   onMount(async () => {
     try {
@@ -36,6 +41,82 @@
       financement = undefined;
     }
   });
+
+  let contenu: HTMLDivElement;
+  let observateurDIntersection: IntersectionObserver;
+
+  const observeLesSections = () => {
+    observateurDIntersection = new IntersectionObserver(
+      (sections) => {
+        sections.forEach((section) => {
+          const titreDeLaSection = section.target.querySelector('h2');
+          const lesLiens = contenu.querySelectorAll(
+            `.sommaire ul li a[href='#${titreDeLaSection!.id}']`
+          );
+
+          if (!lesLiens || !lesLiens.length) return;
+
+          if (section.isIntersecting) {
+            lesLiens.forEach((l) => l.parentElement!.classList.add('actif'));
+
+            const menuMobileVisible = contenu.querySelector('#section-active');
+            if (menuMobileVisible)
+              menuMobileVisible.textContent = titreDeLaSection!.textContent;
+          } else
+            lesLiens.forEach((l) => l.parentElement!.classList.remove('actif'));
+        });
+      },
+      { rootMargin: '-30% 0% -70% 0%' }
+    );
+
+    const lesSections = contenu.querySelectorAll('section');
+    lesSections.forEach((s) => observateurDIntersection.observe(s));
+  };
+
+  const attendChargementImages = async () => {
+    const images = contenu.querySelectorAll('img');
+    await Promise.all(
+      Array.from(images).map((image) => {
+        if (image.complete) return Promise.resolve();
+        return new Promise((res) => {
+          image.addEventListener('load', res);
+          image.addEventListener('error', res);
+        });
+      })
+    );
+  };
+
+  const scroll = (cible: string) => {
+    if (cible) {
+      tick().then(async () => {
+        const ancre = contenu && contenu.querySelector(cible);
+        if (ancre) {
+          await attendChargementImages();
+          ancre.scrollIntoView(true);
+        }
+      });
+    }
+  };
+
+  $effect(() => {
+    if (financement) {
+      scroll(window.location.hash);
+      tick().then(observeLesSections);
+    }
+  });
+
+  onDestroy(() => {
+    const lesSections = contenu!.querySelectorAll('section');
+    lesSections.forEach((s) => {
+      if (observateurDIntersection) observateurDIntersection.unobserve(s);
+    });
+  });
+
+  const fermeSommaire = () => {
+    if (detailsElement) {
+      detailsElement.open = false;
+    }
+  };
 </script>
 
 <section class="chapeau">
@@ -68,81 +149,239 @@
   </div>
 </section>
 
-{#if financement}
-  <section class="corps">
-    <div class="contenu-section">
-      <div class="sommaire">
-        {#if financement.objectifs}
-          <a href="#objectifs">Objectifs</a>
+<div bind:this={contenu}>
+  <div class="sommaire sommaire-replie">
+    <details bind:this={detailsElement}>
+      <summary>
+        <div class="entete-filtres">
+          <img
+            class="menu"
+            src="/assets/images/icone-menu-lateral.svg"
+            alt=""
+          />
+          <span id="section-active" class="titre-menu">Objectifs</span>
+          <img
+            class="chevron"
+            src="/assets/images/icone-chevron-bas.svg"
+            alt=""
+          />
+        </div>
+      </summary>
+
+      <ul>
+        {#if financement?.objectifs}
+          <li class="actif">
+            <a href="#objectifs" onclick={fermeSommaire}>Objectifs</a>
+          </li>
         {/if}
-        {#if financement.operationsEligibles}
-          <a href="#operations-eligibles">Opérations éligibles</a>
+        {#if financement?.operationsEligibles}
+          <li>
+            <a href="#operations-eligibles" onclick={fermeSommaire}
+              >Opérations éligibles</a
+            >
+          </li>
         {/if}
-        {#if financement.benificiaires}
-          <a href="#beneficiaires">Bénéficiaires</a>
+        {#if financement?.benificiaires}
+          <li>
+            <a href="#beneficiaires" onclick={fermeSommaire}>Bénéficiaires</a>
+          </li>
         {/if}
-        {#if financement.montant}
-          <a href="#montant">Montant</a>
+        {#if financement?.montant}
+          <li><a href="#montant" onclick={fermeSommaire}>Montant</a></li>
         {/if}
-        {#if financement.condition}
-          <a href="#conditions">Conditions</a>
+        {#if financement?.condition}
+          <li><a href="#conditions" onclick={fermeSommaire}>Conditions</a></li>
         {/if}
-        <p class="titreTags">tags</p>
-        <div class="tags">
-          {#each financement.entitesElligibles as entite (entite)}
-            <lab-anssi-tag label={entite} taille="sm" type="defaut"
-            ></lab-anssi-tag>
-          {/each}
+      </ul>
+    </details>
+  </div>
+
+  {#if financement}
+    <section class="corps">
+      <div class="contenu-section">
+        <div class="sommaire sommaire-deplie">
+          <ul>
+            {#if financement.objectifs}
+              <li class="actif">
+                <a href="#objectifs">Objectifs</a>
+              </li>
+            {/if}
+            {#if financement.operationsEligibles}
+              <li>
+                <a href="#operations-eligibles">Opérations éligibles</a>
+              </li>
+            {/if}
+            {#if financement.benificiaires}
+              <li>
+                <a href="#beneficiaires">Bénéficiaires</a>
+              </li>
+            {/if}
+            {#if financement.montant}
+              <li>
+                <a href="#montant">Montant</a>
+              </li>
+            {/if}
+            {#if financement.condition}
+              <li>
+                <a href="#conditions">Conditions</a>
+              </li>
+            {/if}
+            <p class="titreTags">tags</p>
+            <div class="tags">
+              {#each financement.entitesElligibles as entite (entite)}
+                <lab-anssi-tag label={entite} taille="sm" type="defaut"
+                ></lab-anssi-tag>
+              {/each}
+            </div>
+          </ul>
+        </div>
+        <div class="fiche">
+          <div class="financePar">
+            <p>Financé par : <strong>{financement.financeur}</strong></p>
+          </div>
+          <SectionDetailsFinancement
+            ancre="objectifs"
+            titre="Objectifs"
+            detail={financement.objectifs}
+          />
+          <SectionDetailsFinancement
+            ancre="operations-eligibles"
+            titre="Opérations éligibles"
+            detail={financement.operationsEligibles}
+          />
+
+          <SectionDetailsFinancement
+            ancre="beneficiaires"
+            titre="Bénéficiaires"
+            detail={financement.benificiaires}
+          />
+
+          <SectionDetailsFinancement
+            ancre="montant"
+            titre="Montant"
+            detail={financement.montant}
+          />
+
+          <SectionDetailsFinancement
+            ancre="conditions"
+            titre="Conditions"
+            detail={financement.condition}
+          />
+          <lab-anssi-lien
+            href="#"
+            titre="Haut de page"
+            icone="arrow-up-fill"
+            positionIcone="gauche"
+          ></lab-anssi-lien>
         </div>
       </div>
-      <div class="fiche">
-        <div class="financePar">
-          <p>Financé par : <strong>{financement.financeur}</strong></p>
-        </div>
-        <SectionDetailsFinancement
-          ancre="objectifs"
-          titre="Objectifs"
-          detail={financement.objectifs}
-        />
-        <SectionDetailsFinancement
-          ancre="operations-eligibles"
-          titre="Opérations éligibles"
-          detail={financement.operationsEligibles}
-        />
-
-        <SectionDetailsFinancement
-          ancre="beneficiaires"
-          titre="Bénéficiaires"
-          detail={financement.benificiaires}
-        />
-
-        <SectionDetailsFinancement
-          ancre="montant"
-          titre="Montant"
-          detail={financement.montant}
-        />
-
-        <SectionDetailsFinancement
-          ancre="conditions"
-          titre="Conditions"
-          detail={financement.condition}
-        />
-        <lab-anssi-lien
-          href="#"
-          titre="Haut de page"
-          icone="arrow-up-fill"
-          positionIcone="gauche"
-        ></lab-anssi-lien>
-      </div>
-    </div>
-  </section>
-{/if}
+    </section>
+  {/if}
+</div>
 
 <style lang="scss">
   @use '../../../assets/styles/responsive' as *;
 
   section {
     padding: 0 var(--gouttiere) 40px;
+  }
+
+  .sommaire-replie {
+    padding: 0;
+    border-top: 1px solid #ddd;
+    border-bottom: 1px solid #ddd;
+    background: white;
+    position: sticky;
+    top: 0;
+    z-index: 2;
+
+    @include a-partir-de(desktop) {
+      display: none;
+    }
+
+    &:has(details[open]) {
+      position: fixed;
+      top: 0;
+      height: 100vh;
+      width: 100%;
+      box-sizing: border-box;
+      overflow: auto;
+    }
+
+    details {
+      .entete-filtres {
+        padding: 12px 16px;
+        background: white;
+        color: var(--sommaire-actif-couleur);
+        font-size: 1rem;
+        font-style: normal;
+        font-weight: 500;
+        line-height: 1.5rem;
+      }
+
+      ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 16px;
+
+        li {
+          border-bottom: 1px solid #ddd;
+          padding-top: 12px;
+          padding-bottom: 12px;
+
+          a {
+            border-left: 2px solid transparent;
+            text-decoration: none;
+            padding-left: 14px;
+            display: inline-block;
+            border-bottom: none;
+            font-size: 1rem;
+            font-weight: 700;
+            line-height: 1.5rem;
+          }
+
+          &.actif {
+            a {
+              color: var(--sommaire-actif-couleur);
+              border-left-color: var(--sommaire-actif-indicateur-couleur);
+            }
+          }
+        }
+      }
+
+      &[open] {
+        summary {
+          .entete-filtres {
+            background: var(--sommaire-mobile-fond);
+
+            :global(.chevron) {
+              transform: rotate(180deg);
+            }
+          }
+        }
+      }
+
+      summary {
+        list-style: none;
+        &::marker {
+          content: '';
+        }
+
+        &::-webkit-details-marker {
+          display: none;
+        }
+
+        .entete-filtres {
+          display: flex;
+          align-items: center;
+          gap: 8px;
+        }
+      }
+
+      .titre-menu {
+        flex-grow: 1;
+      }
+    }
   }
 
   .corps {
@@ -157,35 +396,59 @@
       gap: 24px;
       align-self: stretch;
 
-      .sommaire {
+      .sommaire-deplie {
         display: none;
+        width: 282px;
+        flex: 0 0 auto;
+        align-self: flex-start;
 
-        @include a-partir-de(md) {
+        @include a-partir-de(lg) {
           display: flex;
           flex-direction: column;
-          align-items: flex-start;
           position: sticky;
           top: 0;
-          max-width: 282px;
-          flex: 1 0 0;
         }
 
-        a {
-          display: flex;
-          align-self: stretch;
-          width: auto;
-          font-weight: bold;
-          font-size: 1rem;
+        ul {
+          list-style-type: none;
+          padding: 0;
+          margin: 0 0 40px;
+
+          li {
+            &.actif {
+              a {
+                color: var(--sommaire-actif-couleur);
+                border-left: 2px solid var(--sommaire-actif-indicateur-couleur);
+                padding-left: 6px;
+              }
+            }
+
+            a {
+              width: 100%;
+              border-bottom: none;
+              text-decoration: none;
+              padding: 12px 8px;
+              display: inline-block;
+              font-size: 1rem;
+              font-style: normal;
+              font-weight: 700;
+              line-height: 1.5rem;
+
+              &:hover {
+                background: rgb(0, 0, 0, 4%);
+              }
+
+              &:active {
+                background: rgb(0, 0, 0, 8%);
+              }
+            }
+          }
+        }
+
+        span {
+          margin-bottom: 16px;
+          font-size: 0.85rem;
           line-height: 1.5rem;
-          padding: 12px 8px;
-
-          &:hover {
-            background: rgb(0, 0, 0, 4%);
-          }
-
-          &:active {
-            background: rgb(0, 0, 0, 8%);
-          }
         }
 
         .titreTags {
@@ -203,7 +466,6 @@
           gap: 8px;
         }
       }
-
       .fiche {
         display: flex;
         flex-direction: column;
