5d96fb6aac0c1cc61af88137447b2fd1ba5bd064
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-08-20 18:34:30 +0200

    [SOIN] Extrais la logique de filtrage
    ...dans des fonctions de contruction de filtre

--- back/src/api/ressourceFinancements.ts
+++ back/src/api/ressourceFinancements.ts
@@ -1,7 +1,55 @@
 import { Request, Response, Router } from 'express';
+import { Financement } from '../metier/financement';
 import { estCodeRegion } from '../metier/referentielRegions';
 import { trancheEffectifParCode } from '../metier/referentielTranchesEffectifEtablissement';
 import { ConfigurationServeur } from './configurationServeur';
+
+const construitFiltreFinancementParRegion = (codeRegion?: string) => {
+  if (!estCodeRegion(codeRegion)) {
+    return () => true;
+  }
+  return (financement: Financement): boolean =>
+    financement.regions.includes('FRANCE') ||
+    financement.regions.includes(codeRegion);
+};
+
+const construitFiltreFinancementParEntite = ({
+  estAssociation,
+  estCollectivite,
+  codeTrancheEffectif,
+}: {
+  estAssociation: boolean;
+  estCollectivite: boolean;
+  codeTrancheEffectif?: string;
+}) => {
+  if (estCollectivite) {
+    return (financement: Financement): boolean =>
+      financement.entitesElligibles.includes('Collectivités');
+  }
+  if (estAssociation) {
+    return (financement: Financement): boolean =>
+      financement.entitesElligibles.includes('Associations');
+  }
+  const trancheEffectif = trancheEffectifParCode(codeTrancheEffectif);
+  if (trancheEffectif.code !== 'NN') {
+    const codeEffectifNombre = Number(trancheEffectif.code);
+    if (codeEffectifNombre < 12) {
+      return (financement: Financement): boolean =>
+        financement.entitesElligibles.includes('TPE');
+    }
+    if (codeEffectifNombre < 32) {
+      return (financement: Financement): boolean =>
+        financement.entitesElligibles.includes('PME');
+    }
+    if (codeEffectifNombre < 52) {
+      return (financement: Financement): boolean =>
+        financement.entitesElligibles.includes('ETI');
+    }
+    return (financement: Financement): boolean =>
+      financement.entitesElligibles.includes('Entreprises');
+  }
+  return () => true;
+};
 
 export const ressourceFinancements = ({
   entrepotFinancement,
@@ -33,41 +81,16 @@ export const ressourceFinancements = ({
             estCollectivite,
             estAssociation,
           } = resultatRechercheOrga[0];
-          if (estCodeRegion(codeRegion)) {
-            financements = financements.filter(
-              (f) =>
-                f.regions.includes('FRANCE') || f.regions.includes(codeRegion)
-            );
-          }
-          if (estCollectivite) {
-            financements = financements.filter((f) =>
-              f.entitesElligibles.includes('Collectivités')
-            );
-          } else if (estAssociation) {
-            financements = financements.filter((f) =>
-              f.entitesElligibles.includes('Associations')
-            );
-          } else {
-            const trancheEffectif = trancheEffectifParCode(codeTrancheEffectif);
-            if (trancheEffectif.code !== 'NN') {
-              const codeEffectifNombre = Number(trancheEffectif.code);
-              let entite = '';
-              if (codeEffectifNombre < 12) {
-                entite = 'TPE';
-              } else if (codeEffectifNombre < 32) {
-                entite = 'PME';
-              } else if (codeEffectifNombre < 52) {
-                entite = 'ETI';
-              } else {
-                entite = 'Entreprises';
-              }
-              if (entite) {
-                financements = financements.filter((f) =>
-                  f.entitesElligibles.includes(entite)
-                );
-              }
-            }
-          }
+
+          financements = financements
+            .filter(
+              construitFiltreFinancementParEntite({
+                estAssociation,
+                estCollectivite,
+                codeTrancheEffectif,
+              })
+            )
+            .filter(construitFiltreFinancementParRegion(codeRegion));
         }
         reponse.send(
           financements.map((financement) => ({
