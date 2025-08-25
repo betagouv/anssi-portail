1876b8ad22fa8a72187ea7d19d39db1f86d2121f
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-08-25 12:02:08 +0200

    [NAVIGATION] Transforme le widget utilisateur en menu

--- front/lib-svelte/src/identification/ZoneIdentification.svelte
+++ front/lib-svelte/src/identification/ZoneIdentification.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { slide } from 'svelte/transition';
+  import LienNavigation from '../navigation/LienNavigation.svelte';
   import { profilStore } from '../stores/profil.store';
 
   export let estMobile = false;
@@ -11,23 +13,66 @@
       <a href="/connexion" class="lien-connexion">Se connecter</a>
     </div>
   {:else}
-    <div class="profil-connecte">
-      <span class="libelle-profil"
-        >{$profilStore.prenom} {$profilStore.nom}</span
-      >
-      <a href="/oidc/deconnexion">Se déconnecter</a>
-    </div>
+    <details>
+      <summary class="libelle-profil">
+        {$profilStore.prenom}
+        {$profilStore.nom}
+      </summary>
+      <div class="contenu" transition:slide>
+        <LienNavigation
+          href="/services-anssi/"
+          label="Services ANSSI utilisés"
+          dansMenuDeroulant
+        />
+      </div>
+    </details>
+    <a class="deconnexion" href="/oidc/deconnexion">Se déconnecter</a>
   {/if}
 </div>
 
 <style lang="scss">
   @use '../../../assets/styles/responsive' as *;
 
+  * {
+    box-sizing: border-box;
+  }
   .conteneur-zone-indentification:not(.mobile) {
     display: none;
 
     @include a-partir-de(lg) {
-      display: block;
+      display: flex;
+      align-items: center;
+
+      details {
+        position: relative;
+        summary {
+          cursor: pointer;
+          color: #000091;
+
+          &:after {
+            content: '';
+            display: block;
+            width: 16px;
+            height: 16px;
+            mask-image: url('/assets/images/icone-chevron-bas.svg');
+            transition: transform 0.15s linear;
+            background-color: #000091;
+          }
+        }
+
+        &[open] summary:after {
+          transform: rotate(-180deg);
+        }
+
+        .contenu {
+          position: absolute;
+          top: 100%;
+          width: 320px;
+          background-color: white;
+          z-index: 5;
+          box-shadow: 0 4px 12px 0 rgba(0, 0, 18, 0.16);
+        }
+      }
     }
   }
 
@@ -37,7 +82,7 @@
 
     display: block;
 
-    .profil-connecte > span.libelle-profil {
+    details {
       display: none;
     }
 
@@ -46,14 +91,13 @@
     }
   }
 
-  .profil-connecte,
   .profil-deconnecte,
-  .profil-connecte > a,
   .libelle-profil {
     display: flex;
     gap: 8px;
     align-items: center;
     padding: 4px 8px;
+    color: #000091;
 
     &:before {
       width: 16px;
@@ -62,7 +106,6 @@
     }
   }
 
-  .profil-connecte > a,
   .profil-deconnecte > a,
   .libelle-profil {
     padding: 4px 12px;
@@ -73,8 +116,18 @@
     content: url('/assets/images/icone-profil-non-connecte.svg');
   }
 
-  .profil-connecte > a:before {
-    content: url('/assets/images/icone-profil-deconnecte.svg');
+  .deconnexion {
+    display: flex;
+    gap: 8px;
+    align-items: center;
+    padding: 4px 12px;
+    color: #000091;
+    &:before {
+      width: 16px;
+      height: 16px;
+      line-height: 16px;
+      content: url('/assets/images/icone-profil-deconnecte.svg');
+    }
   }
 
   .libelle-profil:before {
