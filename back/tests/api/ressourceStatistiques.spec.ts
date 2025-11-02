2d9a22ea5df4ef0e324a191dcd9cee0e7cb59ca8
LauraMondoloni31 <154080608+LauraMondoloni31@users.noreply.github.com>
2025-11-02 21:45:55 +0100

    mise à jour hebdo

--- back/tests/api/ressourceStatistiques.spec.ts
+++ back/tests/api/ressourceStatistiques.spec.ts
@@ -52,7 +52,7 @@ describe('La ressource Statistiques', () => {
     it('renvoie le nombre de services et de ressources consultés', async () => {
       const reponse = await request(serveur).get('/api/statistiques');
 
-      assert.equal(reponse.body.servicesEtRessourcesConsultes, 7300);
+      assert.equal(reponse.body.servicesEtRessourcesConsultes, 7800);
     });
 
     it('renvoie le nombre de tests de maturité', async () => {
