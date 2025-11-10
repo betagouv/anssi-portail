1bb7e02a17d7f34b5ebc3907039022e90724afb2
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-11-10 15:27:17 +0100

    [SOIN] Simplifie la récupération des textes

--- recuperation-guides/src/recuperateurGuide.ts
+++ recuperation-guides/src/recuperateurGuide.ts
@@ -1,4 +1,4 @@
-import { parse } from 'node-html-parser';
+import { HTMLElement, parse } from 'node-html-parser';
 import { LecteurSite } from './lecteurDeSiteHttp';
 
 type Guide = {
@@ -10,20 +10,23 @@ type Guide = {
 
 export class RecuperateurGuide {
   constructor(private lecteurSite: LecteurSite) {}
+
   async recupere(urlGuide: string): Promise<Guide> {
     const contenuHtml = await this.lecteurSite.lis(urlGuide);
     const document = parse(contenuHtml);
     return {
-      titre: document.querySelector('h1')!.innerText,
-      resume: document.querySelector('.banniere-group p')!.innerText,
-      datePublication: document
-        .querySelector('.published-on')!
-        .innerText.trim()
-        .substring(10),
-      dateMiseAJour: document
-        .querySelector('.updated-on')!
-        .innerText.trim()
-        .substring(14),
+      titre: this.recupereTexte(document, 'h1'),
+      resume: this.recupereTexte(document, '.banniere-group p'),
+      datePublication: this.recupereTexte(document, '.published-on', 10),
+      dateMiseAJour: this.recupereTexte(document, '.updated-on', 14),
     };
+  }
+
+  private recupereTexte(
+    document: HTMLElement,
+    selecteur: string,
+    debut: number = 0
+  ) {
+    return document.querySelector(selecteur)!.innerText.trim().substring(debut);
   }
 }
