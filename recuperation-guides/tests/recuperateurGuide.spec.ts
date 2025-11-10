a210d6c720473b212961d90e8108a61fa2a69938
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-11-10 15:18:23 +0100

    [GUIDES] Récupères les informations basiques
    ...dont le titre, le résumé et les dates

---
+++ recuperation-guides/tests/recuperateurGuide.spec.ts
@@ -0,0 +1,59 @@
+import assert from 'node:assert';
+import { describe, it , beforeEach} from 'node:test';
+import { RecuperateurGuide } from '../src/recuperateurGuide';
+
+describe("Le récupérateur d'un guide", () => {
+  const htmlGuide = `
+    <p>Pas le bon résumé</p>
+    <div class="banniere-group">
+      <H1>Guide 1</H1>
+      <p>Dans un contexte de prolifération…</p>
+    </div>
+    <span class="published-on"> Publié le 26 août 2025 </span>
+    <span class="updated-on"> Mis à jour le 2 octobre 2025 </span>
+`;
+
+  let recuperateurGuide: RecuperateurGuide;
+
+  beforeEach(() => {
+    recuperateurGuide = new RecuperateurGuide({ lis: async (_) => htmlGuide });
+  });
+
+  it('récupère le titre', async () => {
+    const guide = await recuperateurGuide.recupere('/guide1');
+
+    assert.equal(guide.titre, 'Guide 1');
+  });
+
+  it('appelle avec la bonne url', async () => {
+    let urlAppelee = '';
+    const recuperateurGuide = new RecuperateurGuide({
+      lis: async (url) => {
+        urlAppelee = url;
+        return htmlGuide;
+      },
+    });
+
+    await recuperateurGuide.recupere('/guide1');
+
+    assert.equal(urlAppelee, '/guide1');
+  });
+
+  it('récupère le résumé', async () => {
+    const guide = await recuperateurGuide.recupere('/guide1');
+
+    assert.equal(guide.resume, 'Dans un contexte de prolifération…');
+  });
+
+  it('récupère la date de publication', async () => {
+    const guide = await recuperateurGuide.recupere('/guide1');
+
+    assert.equal(guide.datePublication, "26 août 2025");
+  });
+
+  it('récupère la date de mise à jour', async () => {
+    const guide = await recuperateurGuide.recupere('/guide1');
+
+    assert.equal(guide.dateMiseAJour, "2 octobre 2025");
+  });
+});
