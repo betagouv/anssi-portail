bf3fe923990fe2963ce2e8b8b8f4fc476f2cf25f
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2025-11-10 17:40:03 +0100

    [GUIDES] Récupère les guides de toutes les pages

--- recuperation-guides/src/index.ts
+++ recuperation-guides/src/index.ts
@@ -9,7 +9,7 @@ const recuperateurDeGuides = new RecuperateurDAdressesDesGuides(
 
 const adresses = await recuperateurDeGuides.recupere(
   'https://cyber.gouv.fr/publications?field_type_de_publication_target_id%5B934%5D=934&field_type_de_publication_target_id%5B936%5D=936',
-  1
+  12
 );
 
 const recuperateurGuide = new RecuperateurGuide(lecteurDeSiteHttp);
