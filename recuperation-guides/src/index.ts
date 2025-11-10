edeba14f59f4f3405e63fa94ab7cc2bcf25592fe
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-11-10 14:49:56 +0100

    [SOIN] Extrais le lecteur de sites

--- recuperation-guides/src/index.ts
+++ recuperation-guides/src/index.ts
@@ -1,26 +1,14 @@
-import axios from 'axios';
+import { lecteurDeSiteHttp } from './lecteurDeSiteHttp';
 import { RecuperateurDAdressesDesGuides } from './recuperateurDAdressesDesGuides';
 
 console.log('Récupération des guides de sécurité...');
-const recuperateurDeGuides = new RecuperateurDAdressesDesGuides({
-  lis: async (url: string) => {
-    const reponseChallenge = await axios.get(url);
-    const challengeExtrait = (reponseChallenge.data as string).match(
-      /var __blnChallengeStore=\{.*,"value":"([^"]*)"/
-    )!;
-    const challenge = challengeExtrait[1];
-    const reponse = await axios.get(url, {
-      headers: {
-        cookie: `bln_challengejs=${challenge}`,
-      },
-    });
-    return reponse.data;
-  },
-});
+const recuperateurDeGuides = new RecuperateurDAdressesDesGuides(
+  lecteurDeSiteHttp
+);
 
 const adresses = await recuperateurDeGuides.recupere(
   'https://cyber.gouv.fr/publications?field_type_de_publication_target_id%5B934%5D=934&field_type_de_publication_target_id%5B936%5D=936',
-  12
+  2
 );
 
 console.log(adresses);
