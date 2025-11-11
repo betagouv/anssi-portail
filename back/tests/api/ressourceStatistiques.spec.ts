3fd7ff979a1156a7018e3d5f379c7588d3f50500
LauraMondoloni31 <154080608+LauraMondoloni31@users.noreply.github.com>
2025-11-11 13:33:57 +0100

    modifie la page stats

--- back/tests/api/ressourceStatistiques.spec.ts
+++ back/tests/api/ressourceStatistiques.spec.ts
@@ -52,7 +52,7 @@ describe('La ressource Statistiques', () => {
     it('renvoie le nombre de services et de ressources consultés', async () => {
       const reponse = await request(serveur).get('/api/statistiques');
 
-      assert.equal(reponse.body.servicesEtRessourcesConsultes, 7800);
+      assert.equal(reponse.body.servicesEtRessourcesConsultes, 8300);
     });
 
     it('renvoie le nombre de tests de maturité', async () => {
