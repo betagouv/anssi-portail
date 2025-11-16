aca4f55fb0a38f1aec1994c674b33925cbefbf4b
LauraMondoloni31 <154080608+LauraMondoloni31@users.noreply.github.com>
2025-11-16 16:49:27 +0100

    ajuste le nombre des services cyber consultés

--- back/src/metier/statistiques.ts
+++ back/src/metier/statistiques.ts
@@ -19,7 +19,7 @@ export type Statistiques = {
   servicesEtRessourcesConsultes: number;
 };
 
-const SERVICES_ET_RESSOURCES_CONSULTES = 8300;
+const SERVICES_ET_RESSOURCES_CONSULTES = 8900;
 
 export const calculeStatistiques = async ({
   entrepotUtilisateur,
