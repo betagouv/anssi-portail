e3ded5c6a0eedc01ff6e8d1bd1a8a643efd6a84f
LauraMondoloni31 <154080608+LauraMondoloni31@users.noreply.github.com>
2025-11-16 16:49:27 +0100

    ajuste le nombre des services cyber consultés

--- back/tests/api/ressourceStatistiques.spec.ts
+++ back/tests/api/ressourceStatistiques.spec.ts
@@ -52,7 +52,7 @@ describe('La ressource Statistiques', () => {
     it('renvoie le nombre de services et de ressources consultés', async () => {
       const reponse = await request(serveur).get('/api/statistiques');
 
-      assert.equal(reponse.body.servicesEtRessourcesConsultes, 8300);
+      assert.equal(reponse.body.servicesEtRessourcesConsultes, 8900);
     });
 
     it('renvoie le nombre de tests de maturité', async () => {
