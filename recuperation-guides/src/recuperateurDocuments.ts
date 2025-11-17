8e4bd776d4bdc8bba350b6c6ed5ed20847ac6d22
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-11-17 14:17:37 +0100

    [SOIN] Extrais la récupération d'image

--- recuperation-guides/src/recuperateurDocuments.ts
+++ recuperation-guides/src/recuperateurDocuments.ts
@@ -4,18 +4,23 @@ import { Guide } from './recuperateurGuide';
 
 export const recupereDocuments = async (guides: Guide[]): Promise<void> => {
   for (const guide of guides) {
-    if (guide.image) {
-      const reponseImage = await axios.get(guide.image, {
-        responseType: 'blob',
-      });
-      await fs.access(`sortie/${guide.id}`).catch(async () => {
-        await fs.mkdir(`sortie/${guide.id}`);
-      });
-      await fs.writeFile(
-        `sortie/${guide.id}/${guide.image.split('/').at(-1)}`,
-        reponseImage.data
-      );
-      console.log('Image récupérée : ', guide.image);
-    }
+    await recupereIllustration(guide);
   }
+};
+
+const recupereIllustration = async ({ id, image }: Guide): Promise<void> => {
+  if (!image) {
+    return;
+  }
+  const reponseImage = await axios.get(image, {
+    responseType: 'blob',
+  });
+  await fs.access(`sortie/${id}`).catch(async () => {
+    await fs.mkdir(`sortie/${id}`);
+  });
+  await fs.writeFile(
+    `sortie/${id}/${image.split('/').at(-1)}`,
+    reponseImage.data
+  );
+  console.log('Image récupérée : ', image);
 };
