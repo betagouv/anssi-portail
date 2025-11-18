2ad50428ccd8511c167c356c093c8d1fd6edb1cb
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-11-18 17:53:10 +0100

    [FILTRES] Réinitialise les filtres de collection

--- front/lib-svelte/src/catalogue/stores/recherches.store.ts
+++ front/lib-svelte/src/catalogue/stores/recherches.store.ts
@@ -1,10 +1,11 @@
 import { derived } from 'svelte/store';
 import { rechercheParBesoin } from './rechercheParBesoin.store';
+import { rechercheParCollection } from './rechercheParCollection.store';
 import { rechercheParDroitAcces } from './rechercheParDroitAcces.store';
-import { rechercheParTypologie } from './rechercheParTypologie.store';
 import { rechercheParFormat } from './rechercheParFormat.store';
-import { rechercheParSource } from './rechercheParSource.store';
 import { rechercheParLangue } from './rechercheParLangue.store';
+import { rechercheParSource } from './rechercheParSource.store';
+import { rechercheParTypologie } from './rechercheParTypologie.store';
 
 const recherches2 = derived(
   [
@@ -14,6 +15,7 @@ const recherches2 = derived(
     rechercheParFormat,
     rechercheParSource,
     rechercheParLangue,
+    rechercheParCollection,
   ],
   ([
     $rechercheParDroitAcces,
@@ -22,6 +24,7 @@ const recherches2 = derived(
     $rechercheParFormat,
     $rechercheParSource,
     $rechercheParLangue,
+    $rechercheParCollection,
   ]) => ({
     filtreActif:
       $rechercheParDroitAcces.length !== 0 ||
@@ -29,7 +32,8 @@ const recherches2 = derived(
       $rechercheParTypologie.length !== 0 ||
       $rechercheParFormat.length !== 0 ||
       $rechercheParSource.length !== 0 ||
-      $rechercheParLangue.length !== 0,
+      $rechercheParLangue.length !== 0 ||
+      $rechercheParCollection.length !== 0,
   })
 );
 
@@ -42,5 +46,6 @@ export const recherches = {
     rechercheParFormat.reinitialise();
     rechercheParSource.reinitialise();
     rechercheParLangue.reinitialise();
+    rechercheParCollection.reinitialise();
   },
 };
--- front/lib-svelte/test/catalogue/stores/recherches.store.spec.ts
+++ front/lib-svelte/test/catalogue/stores/recherches.store.spec.ts
@@ -1,30 +1,36 @@
-import { beforeEach, describe, expect, it } from "vitest";
-import { rechercheParDroitAcces } from "../../../src/catalogue/stores/rechercheParDroitAcces.store";
+import { beforeEach, describe, expect, it } from 'vitest';
+import { rechercheParDroitAcces } from '../../../src/catalogue/stores/rechercheParDroitAcces.store';
 import {
   BesoinCyber,
+  CollectionGuide,
   DroitAcces,
   FormatRessource,
+  Langue,
   Source,
   Typologie,
-} from "../../../src/catalogue/Catalogue.types";
-import { get } from "svelte/store";
-import { recherches } from "../../../src/catalogue/stores/recherches.store";
-import { rechercheParBesoin } from "../../../src/catalogue/stores/rechercheParBesoin.store";
-import { rechercheParFormat } from "../../../src/catalogue/stores/rechercheParFormat.store";
-import { rechercheParTypologie } from "../../../src/catalogue/stores/rechercheParTypologie.store";
-import { rechercheParSource } from "../../../src/catalogue/stores/rechercheParSource.store";
+} from '../../../src/catalogue/Catalogue.types';
+import { get } from 'svelte/store';
+import { recherches } from '../../../src/catalogue/stores/recherches.store';
+import { rechercheParBesoin } from '../../../src/catalogue/stores/rechercheParBesoin.store';
+import { rechercheParFormat } from '../../../src/catalogue/stores/rechercheParFormat.store';
+import { rechercheParTypologie } from '../../../src/catalogue/stores/rechercheParTypologie.store';
+import { rechercheParSource } from '../../../src/catalogue/stores/rechercheParSource.store';
+import { rechercheParLangue } from '../../../src/catalogue/stores/rechercheParLangue.store';
+import { rechercheParCollection } from '../../../src/catalogue/stores/rechercheParCollection.store';
 
-describe("Le store des recherches", () => {
+describe('Le store des recherches', () => {
   beforeEach(() => {
     rechercheParDroitAcces.set([]);
     rechercheParBesoin.set(null);
     rechercheParFormat.set([]);
     rechercheParTypologie.set([]);
     rechercheParSource.set([]);
+    rechercheParLangue.set([]);
+    rechercheParCollection.set([]);
   });
 
   describe("indique qu'un filtre est actif lorsque ", () => {
-    it("la recherche par besoin est active", () => {
+    it('la recherche par besoin est active', () => {
       rechercheParBesoin.set(BesoinCyber.REAGIR);
 
       const { filtreActif } = get(recherches);
@@ -40,7 +46,7 @@ describe("Le store des recherches", () => {
       expect(filtreActif).toBe(true);
     });
 
-    it("la recherche par typologie est active", () => {
+    it('la recherche par typologie est active', () => {
       rechercheParTypologie.set([Typologie.SERVICE]);
 
       const { filtreActif } = get(recherches);
@@ -48,7 +54,7 @@ describe("Le store des recherches", () => {
       expect(filtreActif).toBe(true);
     });
 
-    it("la recherche par format est active", () => {
+    it('la recherche par format est active', () => {
       rechercheParFormat.set([FormatRessource.PUBLICATION]);
 
       const { filtreActif } = get(recherches);
@@ -56,8 +62,24 @@ describe("Le store des recherches", () => {
       expect(filtreActif).toBe(true);
     });
 
-    it("la recherche par source est active", () => {
+    it('la recherche par source est active', () => {
       rechercheParSource.set([Source.INNOVATION_ANSSI]);
+
+      const { filtreActif } = get(recherches);
+
+      expect(filtreActif).toBe(true);
+    });
+
+    it('la recherche par langue est active', () => {
+      rechercheParLangue.set([Langue.FR]);
+
+      const { filtreActif } = get(recherches);
+
+      expect(filtreActif).toBe(true);
+    });
+
+    it('la recherche par collection est active', () => {
+      rechercheParCollection.set([CollectionGuide.CRISE_CYBER]);
 
       const { filtreActif } = get(recherches);
 
