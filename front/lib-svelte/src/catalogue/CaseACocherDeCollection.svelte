ab4625b1c3a1c53e0142eddc39bd0d3931e6a23d
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-11-19 09:43:56 +0100

    [SOIN] Factorise les case à cocher
    ...du filtre des collections

---
+++ front/lib-svelte/src/catalogue/CaseACocherDeCollection.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import type { CollectionGuide } from './Catalogue.types';
+  import { nombreGuides } from './stores/nombreGuides.store';
+  import { rechercheParCollection } from './stores/rechercheParCollection.store';
+
+  export let collection: CollectionGuide;
+</script>
+
+<label>
+  <input
+    type="checkbox"
+    value={collection}
+    bind:group={$rechercheParCollection}
+  />
+  <span class="libelle">{collection}</span>
+  <span class="compte">
+    {$nombreGuides.parCollection[collection]}
+  </span>
+</label>
--- front/lib-svelte/src/catalogue/FiltreCollection.svelte
+++ front/lib-svelte/src/catalogue/FiltreCollection.svelte
@@ -1,19 +1,22 @@
 <script lang="ts">
+  import CaseACocherDeCollection from './CaseACocherDeCollection.svelte';
   import { CollectionGuide } from './Catalogue.types';
   import { nombreGuides } from './stores/nombreGuides.store';
   import { rechercheParCollection } from './stores/rechercheParCollection.store';
 
-  $: toutesLesExpertiseTechnique =
+  $: toutesLesExpertisesTechniques =
     $rechercheParCollection.includes(CollectionGuide.LES_ESSENTIELS) &&
     $rechercheParCollection.includes(CollectionGuide.LES_FONDAMENTAUX);
 
-  $: quelquesExpertiseTechnique =
-    !toutesLesExpertiseTechnique &&
-    ($rechercheParCollection.includes(CollectionGuide.LES_ESSENTIELS) ||
-      $rechercheParCollection.includes(CollectionGuide.LES_FONDAMENTAUX));
+  $: quelquesExpertisesTechniques =
+    $rechercheParCollection.includes(CollectionGuide.LES_ESSENTIELS) ||
+    $rechercheParCollection.includes(CollectionGuide.LES_FONDAMENTAUX);
+
+  $: unePartieSeulementDesExpertisesTechniques =
+    !toutesLesExpertisesTechniques && quelquesExpertisesTechniques;
 
   const gereCocheExpertiseTechnique = () => {
-    if (toutesLesExpertiseTechnique) {
+    if (toutesLesExpertisesTechniques) {
       rechercheParCollection.retire([
         CollectionGuide.LES_ESSENTIELS,
         CollectionGuide.LES_FONDAMENTAUX,
@@ -31,8 +34,8 @@
   <legend>Collection</legend>
   <label>
     <input
-      checked={toutesLesExpertiseTechnique || quelquesExpertiseTechnique}
-      indeterminate={quelquesExpertiseTechnique}
+      checked={quelquesExpertisesTechniques}
+      indeterminate={unePartieSeulementDesExpertisesTechniques}
       on:click={gereCocheExpertiseTechnique}
       type="checkbox"
     />
@@ -43,71 +46,15 @@
     </span>
   </label>
   <fieldset>
-    <label>
-      <input
-        type="checkbox"
-        value={CollectionGuide.LES_ESSENTIELS}
-        bind:group={$rechercheParCollection}
-      />
-      <span class="libelle">{CollectionGuide.LES_ESSENTIELS}</span>
-      <span class="compte">
-        {$nombreGuides.parCollection[CollectionGuide.LES_ESSENTIELS]}
-      </span>
-    </label>
-    <label>
-      <input
-        type="checkbox"
-        value={CollectionGuide.LES_FONDAMENTAUX}
-        bind:group={$rechercheParCollection}
-      />
-      <span class="libelle">{CollectionGuide.LES_FONDAMENTAUX}</span>
-      <span class="compte">
-        {$nombreGuides.parCollection[CollectionGuide.LES_FONDAMENTAUX]}
-      </span>
-    </label>
+    <CaseACocherDeCollection collection={CollectionGuide.LES_ESSENTIELS} />
+    <CaseACocherDeCollection collection={CollectionGuide.LES_FONDAMENTAUX} />
   </fieldset>
-  <label>
-    <input
-      type="checkbox"
-      value={CollectionGuide.CRISE_CYBER}
-      bind:group={$rechercheParCollection}
-    />
-    <span class="libelle">{CollectionGuide.CRISE_CYBER}</span>
-    <span class="compte">
-      {$nombreGuides.parCollection[CollectionGuide.CRISE_CYBER]}
-    </span>
-  </label>
-  <label>
-    <input
-      type="checkbox"
-      value={CollectionGuide.GESTION_DES_RISQUES_CYBER}
-      bind:group={$rechercheParCollection}
-    />
-    <span class="libelle">{CollectionGuide.GESTION_DES_RISQUES_CYBER}</span>
-    <span class="compte">
-      {$nombreGuides.parCollection[CollectionGuide.GESTION_DES_RISQUES_CYBER]}
-    </span>
-  </label>
-  <label>
-    <input
-      type="checkbox"
-      value={CollectionGuide.SUPERVISION_DE_SECURITE}
-      bind:group={$rechercheParCollection}
-    />
-    <span class="libelle">{CollectionGuide.SUPERVISION_DE_SECURITE}</span>
-    <span class="compte">
-      {$nombreGuides.parCollection[CollectionGuide.SUPERVISION_DE_SECURITE]}
-    </span>
-  </label>
-  <label>
-    <input
-      type="checkbox"
-      value={CollectionGuide.REMEDIATION}
-      bind:group={$rechercheParCollection}
-    />
-    <span class="libelle">{CollectionGuide.REMEDIATION}</span>
-    <span class="compte">
-      {$nombreGuides.parCollection[CollectionGuide.REMEDIATION]}
-    </span>
-  </label>
+  <CaseACocherDeCollection collection={CollectionGuide.CRISE_CYBER} />
+  <CaseACocherDeCollection
+    collection={CollectionGuide.GESTION_DES_RISQUES_CYBER}
+  />
+  <CaseACocherDeCollection
+    collection={CollectionGuide.SUPERVISION_DE_SECURITE}
+  />
+  <CaseACocherDeCollection collection={CollectionGuide.REMEDIATION} />
 </fieldset>
