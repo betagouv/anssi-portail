1d89dc0bc77839c28f8f367588d1dd13cf389a58
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-11-20 10:32:36 +0100

    Convertis les images en AVIF
    ...et ne conserve que les versions en 234px et 588px de large

--- recuperation-guides/src/recuperateurDocuments.ts
+++ recuperation-guides/src/recuperateurDocuments.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { createWriteStream } from 'node:fs';
 import fs from 'node:fs/promises';
 import { join } from 'node:path';
+import sharp from 'sharp';
 import { Guide } from './recuperateurGuide';
 
 export const recupereDocuments = async (guides: Guide[]): Promise<void> => {
@@ -11,7 +12,11 @@ export const recupereDocuments = async (guides: Guide[]): Promise<void> => {
   }
 };
 
-async function telecharge(url: string, id: string = ''): Promise<void> {
+async function telecharge(
+  url: string,
+  id: string = '',
+  fnTransformation?: (chemin: string, fluxEntree: any) => Promise<void>
+): Promise<void> {
   const chemin = join('sortie', id, decodeURI(url.split('/').at(-1)!));
   const reponse = await axios.get(url, {
     responseType: 'stream',
@@ -20,23 +25,39 @@ async function telecharge(url: string, id: string = ''): Promise<void> {
     await fs.mkdir(join(`sortie`, id));
   });
 
-  const fluxDeSortie = createWriteStream(chemin);
-  reponse.data.pipe(fluxDeSortie);
-
-  return new Promise((resolve, reject) => {
-    fluxDeSortie.on('finish', resolve);
-    fluxDeSortie.on('error', (erreur) => {
-      console.log('Erreur de récupération du document', id, erreur);
-      resolve();
+  if (fnTransformation) {
+    await fnTransformation(chemin, reponse.data);
+  } else {
+    const fluxDeSortie = createWriteStream(chemin);
+    reponse.data.pipe(fluxDeSortie);
+    return new Promise((resolve, reject) => {
+      fluxDeSortie.on('finish', resolve);
+      fluxDeSortie.on('error', (erreur) => {
+        console.log('Erreur de récupération du document', id, erreur);
+        resolve();
+      });
     });
-  });
+  }
 }
 
 const recupereIllustration = async ({ id, image }: Guide): Promise<void> => {
   if (!image) {
     return;
   }
-  await telecharge(image, id);
+  await telecharge(image, id, async (chemin, flux) => {
+    const retailleEn234px = sharp().resize(234).avif({ quality: 80 });
+    const retailleEn588px = sharp().resize(588).avif({ quality: 80 });
+
+    const cheminSansExtension = chemin.slice(0, chemin.lastIndexOf('.'));
+    const flux234 = createWriteStream(cheminSansExtension + '-234.avif');
+    const flux588 = createWriteStream(cheminSansExtension + '-588.avif');
+
+    flux.pipe(retailleEn234px).pipe(flux234);
+    flux.pipe(retailleEn588px).pipe(flux588);
+    return new Promise((resolve) => {
+      flux588.on('finish', resolve);
+    });
+  });
   console.log('Image récupérée : ', image);
 };
 
