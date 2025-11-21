9e83f27a97d6d2fc0d2832750aa134f831d24500
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-11-21 16:54:28 +0100

    [GUIDE] N’affiche le bouton que s’il n’y a qu’un seul document

--- front/lib-svelte/src/catalogue/guides/BoutonTelechargementGuide.svelte
+++ front/lib-svelte/src/catalogue/guides/BoutonTelechargementGuide.svelte
@@ -4,6 +4,8 @@
   export let guide: Guide;
 </script>
 
-<a href={guide.documents[0].url} target="_blank" class="bouton primaire">
-  Télécharger le guide
-</a>
+{#if guide.documents.length === 1}
+  <a href={guide.documents[0].url} target="_blank" class="bouton primaire">
+    Télécharger le guide
+  </a>
+{/if}
