27641be7e84fe510d09202766b4ba5c19ebf73ab
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-11-21 16:52:06 +0100

    [SOIN] Extrais le bouton de téléchargement d’un guide
    ...dans un composant

---
+++ front/lib-svelte/src/catalogue/guides/BoutonTelechargementGuide.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import type { Guide } from '../Guide.types';
+
+  export let guide: Guide;
+</script>
+
+<a href={guide.documents[0].url} target="_blank" class="bouton primaire">
+  Télécharger le guide
+</a>
