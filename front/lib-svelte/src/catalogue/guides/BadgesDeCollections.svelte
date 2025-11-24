f463ee72f2b3b4ae142cabe643e59b3a8cf47a62
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-11-24 17:44:35 +0100

    [SOIN] Crée un composant de badge de collection

---
+++ front/lib-svelte/src/catalogue/guides/BadgesDeCollections.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import type { Guide } from '../Guide.types';
+  import { laCouleurDuBadgeSelonLaCollection } from './guide';
+
+  export let guide: Guide;
+  const badges = guide.collections.map((collection) => ({
+    label: collection,
+    accent: laCouleurDuBadgeSelonLaCollection(collection),
+  }));
+</script>
+
+<dsfr-badges-group {badges} size="sm"></dsfr-badges-group>
