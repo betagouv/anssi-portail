a76664cc82169abd6fd42b397dedf4b9957a9908
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-11-25 15:31:07 +0100

    [SOIN] Extrais la ressource des guides de même collection
    ...dans son propre fichier

---
+++ back/src/api/guides/ressourceGuidesMemesCollections.ts
@@ -0,0 +1,27 @@
+import { Request, Response, Router } from 'express';
+import { ConfigurationServeur } from '../configurationServeur';
+import { guidePresentation } from './guidePresentation';
+
+export const ressourceGuidesMemesCollections = ({
+  adaptateurEnvironnement,
+  entrepotGuide,
+}: ConfigurationServeur) => {
+  const routeur = Router();
+
+  routeur.get(
+    '/:slug/memes-collections',
+    async (requete: Request, reponse: Response) => {
+      const guideCible = await entrepotGuide.parId(requete.params.slug);
+      if (!guideCible) {
+        reponse.sendStatus(404);
+        return;
+      }
+      const guides = await entrepotGuide.parCollections(guideCible.collections);
+      reponse
+        .status(200)
+        .send(guides.map(guidePresentation(adaptateurEnvironnement)));
+    }
+  );
+
+  return routeur;
+};
