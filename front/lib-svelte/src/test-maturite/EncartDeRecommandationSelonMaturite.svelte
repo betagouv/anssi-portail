1317d88d8f0da57e8f01f8b01abe048fc949eaab
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2025-11-28 16:06:25 +0100

    [RESULTATS MATURITE] Retire le encart cyber départ

--- front/lib-svelte/src/test-maturite/EncartDeRecommandationSelonMaturite.svelte
+++ front/lib-svelte/src/test-maturite/EncartDeRecommandationSelonMaturite.svelte
@@ -9,32 +9,7 @@
     niveau.id === 'intermediaire';
 </script>
 
-{#if afficheDiagnostic}
-  <section class="encart fond-clair">
-    <div class="contenu-section">
-      <div class="carte parcours cyber-depart">
-        <img
-          src="/assets/images/dragon-kart.svg"
-          alt="Illustration diagnostic cyber"
-          class="illustration"
-        />
-        <h2>Bénéficiez d’un diagnostic cyber gratuit</h2>
-        <p>
-          Vous souhaitez
-          <b>protéger votre organisation contre les cyberattaques</b>
-          mais ne savez pas par quoi commencer ?
-          <b>Prenez votre cyberdépart</b> avec un
-          <b>premier diagnostic gratuit</b>
-          et anonyme d'une heure dans vos locaux ou en visio
-          <b>accompagné par un Aidant cyber</b>.
-        </p>
-        <a href="/cyberdepart" class="bouton primaire">
-          Demander mon diagnostic cyber
-        </a>
-      </div>
-    </div>
-  </section>
-{:else}
+{#if !afficheDiagnostic}
   <section class="votre-parcours">
     <div class="contenu-section">
       <div class="tuile">
