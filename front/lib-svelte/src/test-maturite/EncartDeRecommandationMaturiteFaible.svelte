8a1213c021b61d50935f929ca322e2238c23d05b
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-12-04 16:54:20 +0100

    Adpate la demande de diagnostic
    ...sur la page des résultats, aux grands écrans

--- front/lib-svelte/src/test-maturite/EncartDeRecommandationMaturiteFaible.svelte
+++ front/lib-svelte/src/test-maturite/EncartDeRecommandationMaturiteFaible.svelte
@@ -4,69 +4,81 @@
 </script>
 
 <section>
-  <hgroup>
-    <h1>Passez à l'étape suivante</h1>
-    <p>
-      Votre résultat montre que votre organisation n’est pas suffisamment
-      protégée face aux cyber menaces. L’ANSSI vous recommande de réaliser un
-      diagnostic cyber (gratuit) afin de définir rapidement les actions
-      prioritaires à mettre en place et renforcer votre cybersécurité.
-    </p>
-  </hgroup>
-  <lab-anssi-icone nom="arrow-down-s-line" taille="lg"></lab-anssi-icone>
-  <div class="demande-diagnostic">
-    <hgroup>
-      <h2>Protégez rapidement votre organisation des cyberattaques.</h2>
+  <div class="contenu-section">
+    <hgroup class="preambule">
+      <h1>Passez à l'étape suivante</h1>
       <p>
-        Profitez d’un premier diagnostic cyber gratuit accompagné par un Aidant
-        cyber et recevez 6 recommandations prioritaires à mettre en place pour
-        améliorer la cybersécurité de votre organisation.
+        Votre résultat montre que votre organisation n’est pas suffisamment
+        protégée face aux cyber menaces. L’ANSSI vous recommande de réaliser un
+        diagnostic cyber (gratuit) afin de définir rapidement les actions
+        prioritaires à mettre en place et renforcer votre cybersécurité.
       </p>
     </hgroup>
-    <ol>
-      <li>Complétez et envoyez votre demande</li>
-      <li><span>Un <u>Aidant cyber</u> vous contacte rapidement</span></li>
-      <li>Réalisez votre diagnostic (en visio ou sur site – 1h)</li>
-      <li>
-        Recevez vos 6 recommandations prioritaires pour renforcer votre
-        cybersécurité
-      </li>
-    </ol>
-    <FormulaireDemandeSimplifiee />
-    <details>
-      <summary>
-        <lab-anssi-icone nom="questionnaire-line"></lab-anssi-icone>
-        Questions fréquentes
-      </summary>
-      <div class="faq">
-        <h6>Questions les plus fréquentes</h6>
-        <FoireAuxQuestions />
+    <lab-anssi-icone nom="arrow-down-s-line" taille="lg"></lab-anssi-icone>
+    <div class="demande-diagnostic">
+      <hgroup>
+        <h2>Protégez rapidement votre organisation des cyberattaques.</h2>
+        <p>
+          Profitez d’un premier diagnostic cyber gratuit accompagné par un
+          Aidant cyber et recevez 6 recommandations prioritaires à mettre en
+          place pour améliorer la cybersécurité de votre organisation.
+        </p>
+      </hgroup>
+      <ol>
+        <li>Complétez et envoyez votre demande</li>
+        <li><span>Un <u>Aidant cyber</u> vous contacte rapidement</span></li>
+        <li>Réalisez votre diagnostic (en visio ou sur site – 1h)</li>
+        <li>
+          Recevez vos 6 recommandations prioritaires pour renforcer votre
+          cybersécurité
+        </li>
+      </ol>
+      <div class="formulaire">
+        <FormulaireDemandeSimplifiee />
       </div>
-    </details>
+      <details>
+        <summary>
+          <lab-anssi-icone nom="questionnaire-line"></lab-anssi-icone>
+          Questions fréquentes
+        </summary>
+        <div class="faq">
+          <h6>Questions les plus fréquentes</h6>
+          <FoireAuxQuestions />
+        </div>
+      </details>
+    </div>
   </div>
 </section>
 
 <style lang="scss">
   @use '../../../assets/styles/responsive' as *;
-  section {
+  @use '../../../assets/styles/grille' as *;
+  .contenu-section {
     display: flex;
     flex-direction: column;
     gap: 24px;
-    h1 {
-      font-size: 2rem;
-      line-height: 2.5rem;
-      text-align: center;
-      margin: 0 0 24px;
-
-      @include a-partir-de(md) {
-        font-size: 2.5rem;
-        line-height: 3rem;
-      }
-    }
 
     p {
       font-size: 1.125rem;
       line-height: 1.75rem;
+    }
+
+    hgroup.preambule {
+      text-align: center;
+      align-self: center;
+      @include a-partir-de(lg) {
+        max-width: taille-pour-colonnes(10);
+      }
+      h1 {
+        font-size: 2rem;
+        line-height: 2.5rem;
+        margin: 0 0 24px;
+
+        @include a-partir-de(md) {
+          font-size: 2.5rem;
+          line-height: 3rem;
+        }
+      }
     }
 
     lab-anssi-icone {
