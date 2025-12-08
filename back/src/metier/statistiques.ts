c2c57f20c19e97ecc7654ad1f8630811643fc7b4
LauraMondoloni31 <154080608+LauraMondoloni31@users.noreply.github.com>
2025-12-08 08:48:08 +0100

    mise à jour page stats

--- back/src/metier/statistiques.ts
+++ back/src/metier/statistiques.ts
@@ -19,7 +19,7 @@ export type Statistiques = {
   servicesEtRessourcesConsultes: number;
 };
 
-const SERVICES_ET_RESSOURCES_CONSULTES = 9250;
+const SERVICES_ET_RESSOURCES_CONSULTES = 11600;
 
 export const calculeStatistiques = async ({
   entrepotUtilisateur,
