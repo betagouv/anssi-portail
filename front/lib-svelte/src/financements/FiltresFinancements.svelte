8c53ba86368c9c079d9f5761cc230b6470a5a9d8
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2025-12-19 11:26:18 +0100

    [SOIN] Mutualise les styles des filtres

--- front/lib-svelte/src/financements/FiltresFinancements.svelte
+++ front/lib-svelte/src/financements/FiltresFinancements.svelte
@@ -95,84 +95,11 @@
   .filtres {
     display: flex;
     flex-direction: column;
-    width: 100%;
   }
 
-  fieldset {
-    margin: 0;
-    padding: 0 0 2rem;
-    border: 0;
-    border-bottom: 1px solid var(--gris-clair);
-
-    legend {
-      text-transform: uppercase;
-      font-weight: bold;
-      font-size: 0.875rem;
-      line-height: 1.25rem;
-      margin-bottom: 1rem;
-      padding: 0;
-    }
-
-    ul {
-      display: grid;
-      list-style: none;
-      padding: 0;
-      margin: 0;
-      gap: 1rem;
-    }
-
-    label {
-      display: flex;
-      gap: 0.5rem;
-      align-items: center;
-      cursor: pointer;
-
-      .libelle {
-        display: flex;
-        gap: 8px;
-        align-items: center;
-      }
-
-      input[type='checkbox'] {
-        appearance: none;
-        border: 1px solid var(--noir);
-        border-radius: 4px;
-        width: 24px;
-        height: 24px;
-        margin: 0;
-        cursor: pointer;
-
-        &:checked {
-          background-color: var(--jaune-msc);
-
-          &::before {
-            content: '';
-            display: block;
-            margin: auto;
-            width: 6px;
-            height: 12px;
-            border-right: 2px var(--noir) solid;
-            border-bottom: 2px var(--noir) solid;
-            transform: translateY(2px) rotate(0.12turn);
-          }
-        }
-
-        &:indeterminate {
-          /* Ce style est prévu pour être cumulatif avec l'état coché */
-          &::before {
-            width: 9px;
-            height: 10px;
-            border-right: none;
-            transform: none;
-          }
-        }
-      }
-
-      &.colonne {
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-      }
-    }
+  label.colonne {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
   }
 </style>
