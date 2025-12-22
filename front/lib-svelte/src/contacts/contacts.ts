a65d27d1abcdc2f60cce8cb704e0fc182270153b
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-12-22 16:59:06 +0100

    Calcule le secteur de contact
    ...en fonction du code d’activité

---
+++ front/lib-svelte/src/contacts/contacts.ts
@@ -0,0 +1,51 @@
+import type { Profil } from '../stores/profil.store';
+import { type CodeSecteurContact, codesSecteurContact } from './contacts.type';
+
+export const estCodeSecteurContact = (
+  codeSecteur: string
+): codeSecteur is CodeSecteurContact => {
+  return (codesSecteurContact as readonly string[]).includes(codeSecteur);
+};
+
+export const calculeCodeSecteurContact = (
+  codeActivite: string
+): CodeSecteurContact | undefined => {
+  if (codeActivite === '25.40Z' || codeActivite === '84.22Z') {
+    return 'defense';
+  }
+  if (codeActivite.startsWith('86.') || codeActivite.startsWith('84.')) {
+    return 'sante';
+  }
+
+  if (
+    codeActivite.startsWith('50.') ||
+    codeActivite.startsWith('52.22') ||
+    codeActivite === '52.24A' ||
+    codeActivite.startsWith('42.91') ||
+    codeActivite.startsWith('77.34')
+  ) {
+    return 'maritime';
+  }
+
+  if (codeActivite.startsWith('85.') || codeActivite.startsWith('72.1')) {
+    return 'enseignement-recherche';
+  }
+
+  if (codeActivite.startsWith('51.')) {
+    return 'aviation';
+  }
+
+  return undefined;
+};
+
+export const creeLienContactsUtiles = (profil: Profil | undefined) => {
+  if (profil) {
+    const codeSecteurContact = calculeCodeSecteurContact(profil.codeActivite);
+    const parametres = new URLSearchParams();
+    if (profil?.codeRegion) parametres.set('region', profil?.codeRegion);
+    if (codeSecteurContact) parametres.set('secteur', codeSecteurContact);
+    return `/contacts/#?${parametres.toString()}`;
+  } else {
+    return '/contacts';
+  }
+};
