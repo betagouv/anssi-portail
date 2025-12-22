c7f7fdf6f1adbdcee58c12035e1798fb7a61bac8
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-12-22 11:46:12 +0100

    [CONTACTS] Affiche le filtre sectoriel
    ...en mode déconnecté avec les 6 secteurs pour lesquels nous souhaitons afficher des contacts sectoriels

---
+++ front/lib-svelte/src/contacts/SelectSecteurContacts.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type { Option } from '../test-maturite/SelecteurSimple';
+  import SelecteurSimple from '../test-maturite/SelecteurSimple.svelte';
+  import { secteursContacts } from './contacts.donnees';
+
+  export let secteur: string;
+
+  const options: Option[] = secteursContacts;
+</script>
+
+<SelecteurSimple
+  {options}
+  bind:valeurSeclectionne={secteur}
+  optionDefautSelectionnable
+/>
