244c0dfb948e51c4f5f2d11dbe516f2e7df24bd6
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-01-05 17:38:41 +0100

    [SOIN] Déplace les données d'événement
    ...dans un fichier dédié, en vue de les utiliser dans un autre adaptateur que l'adaptateurJournal

---
+++ back/src/infra/donneesEvenement.ts
@@ -0,0 +1,72 @@
+import { NiveauDeSatisfaction } from '../metier/niveauDeSatisfaction';
+import { CodeRegion } from '../metier/referentielRegions';
+import { CodeSecteur } from '../metier/referentielSecteurs';
+import { CodeTrancheEffectif } from '../metier/referentielTranchesEffectifEtablissement';
+import { ReponsesTestMaturite } from '../metier/resultatTestMaturite';
+
+export type DonneesEvenement =
+  | DonneesEvenementNouvelUtilisateur
+  | DonneesEvenementProprieteTestRevendiquee
+  | DonneesEvenementTestRealise
+  | DonneesEvenementMiseAJourFavorisUtilisateur
+  | DonneesEvenementRetourExperienceDonne
+  | DonneesEvenementAvisUtilisateurDonne;
+
+interface DonneesCommunesEvenement {
+  date: Date;
+}
+
+interface DonneesEvenementNouvelUtilisateur extends DonneesCommunesEvenement {
+  donnees: {
+    idUtilisateur: string;
+  };
+  type: 'NOUVEL_UTILISATEUR_INSCRIT';
+}
+
+interface DonneesEvenementMiseAJourFavorisUtilisateur
+  extends DonneesCommunesEvenement {
+  donnees: {
+    idUtilisateur: string;
+    listeIdFavoris: string[];
+  };
+  type: 'MISE_A_JOUR_FAVORIS_UTILISATEUR';
+}
+
+interface DonneesEvenementProprieteTestRevendiquee
+  extends DonneesCommunesEvenement {
+  donnees: {
+    idUtilisateur: string;
+    idResultatTest: string;
+  };
+  type: 'PROPRIETE_TEST_REVENDIQUEE';
+}
+
+interface DonneesEvenementTestRealise extends DonneesCommunesEvenement {
+  donnees: {
+    region: CodeRegion | undefined;
+    secteur: CodeSecteur | undefined;
+    tailleOrganisation: CodeTrancheEffectif | undefined;
+    reponses: ReponsesTestMaturite;
+    codeSessionGroupe?: string;
+    idResultatTest: string;
+  };
+  type: 'TEST_REALISE';
+}
+
+interface DonneesEvenementRetourExperienceDonne
+  extends DonneesCommunesEvenement {
+  donnees: {
+    raison: string;
+    idUtilisateur: string | undefined;
+  };
+  type: 'RETOUR_EXPERIENCE_DONNE';
+}
+
+interface DonneesEvenementAvisUtilisateurDonne
+  extends DonneesCommunesEvenement {
+  donnees: {
+    niveauDeSatisfaction: NiveauDeSatisfaction;
+    idUtilisateur: string | undefined;
+  };
+  type: 'AVIS_UTILISATEUR_DONNE';
+}
