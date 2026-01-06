da18e888ed4074551136778edfa96aab859a02d8
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2026-01-06 13:02:27 +0100

    [SOIN] Reprends les scripts Node
    ... afin d'avoir la même structure que DSC

--- back/eslint.config.mjs
+++ back/eslint.config.mjs
@@ -3,21 +3,26 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config({
-  files: ['**/*.ts'],
-  extends: [eslint.configs.recommended, tseslint.configs.recommended],
-  rules: {
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      {
-        args: 'all',
-        argsIgnorePattern: '^_',
-        caughtErrors: 'all',
-        caughtErrorsIgnorePattern: '^_',
-        destructuredArrayIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-        ignoreRestSiblings: false,
-      },
-    ],
+export default tseslint.config(
+  {
+    ignores: ['dist/**', 'node_modules/**'],
   },
-});
+  {
+    files: ['**/*.ts'],
+    extends: [eslint.configs.recommended, tseslint.configs.recommended],
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: false,
+        },
+      ],
+    },
+  }
+);
