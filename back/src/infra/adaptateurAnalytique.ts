071897649d9ad4fa853c422ad04af097266794b8
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-01-06 10:15:39 +0100

    Envoie les événements de téléchargement de guide
    ...sur Matomo

--- back/src/infra/adaptateurAnalytique.ts
+++ back/src/infra/adaptateurAnalytique.ts
@@ -1,5 +1,34 @@
-import { DonneesEvenement } from './donneesEvenement';
+import { URLSearchParams } from 'node:url';
+import { AdaptateurEnvironnement } from './adaptateurEnvironnement';
+import { ClientHttpPosteur } from './clientHttp';
+import {
+  DonneesEvenement,
+  DonneesEvenementGuideTelecharge,
+} from './donneesEvenement';
 
 export type AdaptateurAnalytique = {
-  rapporteEvenement: (donneesEvenement: DonneesEvenement) => Promise<void>;
+  rapporteEvenement: (
+    donneesEvenement: DonneesEvenementGuideTelecharge
+  ) => Promise<void>;
 };
+
+export const fabriqueAdaptateurMatamo = (
+  clientHttpPosteur: ClientHttpPosteur<DonneesEvenement, unknown>,
+  adaptateurEnvironnement: AdaptateurEnvironnement
+): AdaptateurAnalytique => ({
+  rapporteEvenement: async (
+    donneesEvenement: DonneesEvenementGuideTelecharge
+  ) => {
+    const parametres = new URLSearchParams();
+    parametres.append('rec', '1');
+    parametres.append('action_name', 'Guide Téléchargé');
+    parametres.append('idsite', adaptateurEnvironnement.matomo().idSite());
+    parametres.append('c_n', donneesEvenement.donnees.nom);
+    parametres.append('c_t', donneesEvenement.donnees.id);
+
+    await clientHttpPosteur.post(
+      'https://stats.beta.gouv.fr/matomo.php?' + parametres.toString(),
+      donneesEvenement
+    );
+  },
+});
---
+++ back/tests/infra/adaptateurMatomo.spec.ts
@@ -0,0 +1,39 @@
+import { describe, it } from 'node:test';
+import { fabriqueAdaptateurMatamo } from '../../src/infra/adaptateurAnalytique';
+import { ClientHttpPosteur } from '../../src/infra/clientHttp';
+import { DonneesEvenement } from '../../src/infra/donneesEvenement';
+import assert from 'node:assert';
+import { fauxAdaptateurEnvironnement } from '../api/fauxObjets';
+
+describe("L'adpatateur Matomo", () => {
+  const evenement: DonneesEvenement = {
+    type: 'GUIDE_TELECHARGE',
+    donnees: {
+      id: 'zero-trust',
+      nom: 'Zero Trust',
+    },
+    date: new Date('2026-01-01T10:11:12.000'),
+  };
+  describe("Lors du rapport d'un événement", () => {
+    it('envoie une requete POST à la bonne URL', () => {
+      let urlAppelee: string = '';
+      const posteur: ClientHttpPosteur<DonneesEvenement, undefined> = {
+        post: async (url, _corps) => {
+          urlAppelee = url;
+          return { data: undefined };
+        },
+      };
+      const adaptateurMatomo = fabriqueAdaptateurMatamo(
+        posteur,
+        fauxAdaptateurEnvironnement
+      );
+
+      adaptateurMatomo.rapporteEvenement(evenement);
+
+      assert.equal(
+        urlAppelee,
+        'https://stats.beta.gouv.fr/matomo.php?rec=1&action_name=Guide+T%C3%A9l%C3%A9charg%C3%A9&idsite=227&c_n=Zero+Trust&c_t=zero-trust'
+      );
+    });
+  });
+});
