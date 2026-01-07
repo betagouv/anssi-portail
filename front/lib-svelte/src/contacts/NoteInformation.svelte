fc1ceb5eb09d42e04b29de9c645be176b7115060
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-01-07 15:44:50 +0100

    [SOIN] Extrais les notes informatives
    ...dans un composant dédié

---
+++ front/lib-svelte/src/contacts/NoteInformation.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+</script>
+
+<p class="note-information">
+  Contactez en priorité les personnes responsables des questions numériques et
+  de cybersécurité au sein de votre organisation.
+</p>
+
+<style lang="scss">
+  @use '../../../assets/styles/responsive' as *;
+
+  .note-information {
+    align-self: flex-start;
+    margin-bottom: 40px;
+
+    @include a-partir-de(md) {
+      margin-bottom: 24px;
+    }
+
+    :global(.contacts) & {
+      margin-bottom: 0;
+    }
+  }
+</style>
