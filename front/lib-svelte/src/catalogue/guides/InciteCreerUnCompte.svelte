6322356fea30687f8170ce1eeadea6402772ce4d
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2026-01-08 14:54:36 +0100

    [GUIDES] Crée le composant avec le message pour se créer un compte

---
+++ front/lib-svelte/src/catalogue/guides/InciteCreerUnCompte.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+</script>
+
+<p class="note-information">
+  <strong>Newsletter MesServicesCyber.</strong>
+  Restez informé des nouveaux guides de l’ANSSI et autres actualités MesServicesCyber.
+  <lab-anssi-lien href="/inscription" titre="Créer mon compte"></lab-anssi-lien>
+</p>
+
+<style lang="scss">
+  .note-information {
+    color: var(--gris-aide-saisie);
+    font-size: 1rem;
+    line-height: 1.5rem;
+    margin-bottom: 1.5rem;
+  }
+</style>
