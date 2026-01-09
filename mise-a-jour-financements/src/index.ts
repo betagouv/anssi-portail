702101dcd4ca9034668dcf08d709bf7650563315
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-01-09 14:02:15 +0100

    Initialise le module mise-a-jour-financements

---
+++ mise-a-jour-financements/src/index.ts
@@ -0,0 +1,4 @@
+console.info('Récupération des financements sur Grist...');
+console.info('...compare avec les financements sur aides-entreprise...');
+console.info('...cherche de nouveaux financement cyber...');
+console.info('... Terminé !');
