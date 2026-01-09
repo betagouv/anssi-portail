e210a69bba9829c6dfdb6fdc582fac39c3540383
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-01-09 14:55:24 +0100

    Compare les financements depuis 2 sources

---
+++ mise-a-jour-financements/tests/metier/comparateurFinancement.spec.ts
@@ -0,0 +1,73 @@
+import assert from 'node:assert';
+import { beforeEach, describe, it } from 'node:test';
+import { AdaptateurSourceExterne } from '../../src/infra/adaptateurSourceExterne.js';
+import { EntrepotFinancement } from '../../src/infra/entrepotFinancement.js';
+import { ComparateurFinancement } from '../../src/metier/comparateurFinancement.js';
+import { Financement } from '../../src/metier/financement.js';
+
+describe('Le comparateur de financement', () => {
+  const financement1: Financement = {
+    id: 1,
+    nom: 'Cyber PME',
+    financeur: 'BPI France',
+    operationsEligibles: 'opération 2',
+    benificiaires: 'Tout le monde',
+    montant: 'Mille milliards',
+    condition: 'Avoir 10 doigts',
+  };
+
+  let entrepotFinancement: EntrepotFinancement;
+  let adaptateurSourceExterne: AdaptateurSourceExterne;
+  let comparateur: ComparateurFinancement;
+
+  beforeEach(() => {
+    entrepotFinancement = {
+      tous: async () => [financement1],
+    };
+    adaptateurSourceExterne = {
+      parId: async () => financement1,
+    };
+    comparateur = new ComparateurFinancement(
+      entrepotFinancement,
+      adaptateurSourceExterne
+    );
+  });
+
+  it("sait récupérer tous les financements de l'entrepot", async () => {
+    await comparateur.chargeFinancements();
+
+    assert.deepEqual(comparateur.financements, [financement1]);
+  });
+
+  describe("lorsqu'on compare un financement", () => {
+    it("ne retourne rien si aucune différence n'est trouvée", async () => {
+      await comparateur.chargeFinancements();
+      const resultatComparaison = await comparateur.compareSourceExterne();
+
+      assert.deepEqual(resultatComparaison, []);
+    });
+  });
+
+  describe('consulte les détails de chaque financement sur la source externe pour trouver des différences', () => {
+    it('sur le montant', async () => {
+      adaptateurSourceExterne.parId = async () => ({
+        ...financement1,
+        montant: 'Dix mille',
+      });
+
+      await comparateur.chargeFinancements();
+      const resultatComparaison = await comparateur.compareSourceExterne();
+
+      assert.deepEqual(resultatComparaison, [
+        {
+          idFinancement: 1,
+          donneesDifferentes: {
+            nomDeLaDonnee: 'Montant',
+            valeurSurGrist: 'Mille milliards',
+            nouvelleValeur: 'Dix mille',
+          },
+        },
+      ]);
+    });
+  });
+});
