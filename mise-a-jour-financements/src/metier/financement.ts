77afbf7b393bcda7c7095abb85440f54c239079a
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-01-09 14:54:47 +0100

    Ajoute le modèle des financements

---
+++ mise-a-jour-financements/src/metier/financement.ts
@@ -0,0 +1,9 @@
+export type Financement = {
+  id: number;
+  nom: string;
+  financeur: string;
+  operationsEligibles: string;
+  benificiaires: string;
+  montant: string;
+  condition: string;
+};
