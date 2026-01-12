41c534ec4e5bf690113a6f5e32040ed65f05d3d4
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-01-12 16:55:27 +0100

    [CORRECTION] Replie le bon menu
    ...à l'arrivée sur la page d'un guide

--- front/scripts/service.js
+++ front/scripts/service.js
@@ -1,46 +1,49 @@
 import { elementLePlusVisible } from './scroll.js';
 
-  const sectionsSommaireReplie = () =>
-    Array.from(document.querySelectorAll('.sommaire.sommaire-replie ul li'));
+const sectionsSommaireReplie = () =>
+  Array.from(document.querySelectorAll('.sommaire.sommaire-replie ul li'));
 
-  const trouveLesLiDuSommaireReplie = (hash) =>
-    sectionsSommaireReplie().filter(
-      (li) => new URL(li.querySelector('a').href).hash === hash
-    );
+const trouveLesLiDuSommaireReplie = (hash) =>
+  sectionsSommaireReplie().filter(
+    (li) => new URL(li.querySelector('a').href).hash === hash
+  );
 
-  const trouveLesLiDesSommaires = (hash) =>
-    Array.from(document.querySelectorAll('.sommaire ul li')).filter(
-      (li) => new URL(li.querySelector('a').href).hash === hash
-    );
+const trouveLesLiDesSommaires = (hash) =>
+  Array.from(document.querySelectorAll('.sommaire ul li')).filter(
+    (li) => new URL(li.querySelector('a').href).hash === hash
+  );
 
-  const trouveLeTexte = (hash) =>
-    trouveLesLiDuSommaireReplie(hash)[0]?.textContent;
+const trouveLeTexte = (hash) =>
+  trouveLesLiDuSommaireReplie(hash)[0]?.textContent;
 
-  const desactiveTousLesItems = () => {
-    const sections = Array.from(document.querySelectorAll('.sommaire ul li'));
-    sections.forEach((s) => s.classList.remove('actif'));
-  };
+const desactiveTousLesItems = () => {
+  const sections = Array.from(document.querySelectorAll('.sommaire ul li'));
+  sections.forEach((s) => s.classList.remove('actif'));
+};
 
-  const activeItems = (idPourSurlignage) =>
-    trouveLesLiDesSommaires(idPourSurlignage).forEach((li) =>
-      li.classList.add('actif')
-    );
+const activeItems = (idPourSurlignage) =>
+  trouveLesLiDesSommaires(idPourSurlignage).forEach((li) =>
+    li.classList.add('actif')
+  );
 
-  const metsAJourLeTexteDeLaSectionActive = (hash) => {
-    document.querySelector('#section-active').textContent =
-      trouveLeTexte(hash) ?? '';
-  };
+const metsAJourLeTexteDeLaSectionActive = (hash) => {
+  document.querySelector('#section-active').textContent =
+    trouveLeTexte(hash) ?? '';
+};
 
-  const metsAJourSectionActive = (hashSectionActive) => {
-    desactiveTousLesItems();
-    metsAJourLeTexteDeLaSectionActive(hashSectionActive);
-    activeItems(hashSectionActive);
-  };
+const metsAJourSectionActive = (hashSectionActive) => {
+  desactiveTousLesItems();
+  metsAJourLeTexteDeLaSectionActive(hashSectionActive);
+  activeItems(hashSectionActive);
+};
 
-  const changeSectionActive = () => {
-    document.querySelector('details').removeAttribute('open');
-    if (location.hash) metsAJourSectionActive(location.hash);
-  };
+const changeSectionActive = () => {
+  const details = document.querySelector('.sommaire-replie > details');
+  if (details) {
+    details.removeAttribute('open');
+  }
+  if (location.hash) metsAJourSectionActive(location.hash);
+};
 
 let scrollEnAttenteDeMiseAJour = false;
 
