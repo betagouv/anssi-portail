b4c728e64df99f7fb6eb467ab81bad913bdb19fa
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2026-01-16 14:45:04 +0100

    [CORRECTION] Ajoute un cache pour la recherche d’entreprises
    ...afin d’éviter de sursolliciter cette API qui nous renvoie des erreurs (429) le cas échéant. Le cache a une durée de 30 minutes, arbitrairement.

---
+++ back/src/infra/RechercheEntrepriseAvecCache.ts
@@ -0,0 +1,26 @@
+import {
+  AdaptateurRechercheEntreprise,
+  ResultatRechercheEntreprise,
+} from './adaptateurRechercheEntreprise';
+import { Cache } from './cache';
+
+const TRENTE_MINUTES = 60 * 30;
+
+export class RechercheEntrepriseAvecCache
+  implements AdaptateurRechercheEntreprise
+{
+  cache: Cache<Promise<ResultatRechercheEntreprise[]>>;
+
+  constructor(private adaptateurDecore: AdaptateurRechercheEntreprise) {
+    this.cache = new Cache({ ttl: TRENTE_MINUTES });
+  }
+
+  async rechercheOrganisations(
+    terme: string,
+    departement: string | null
+  ): Promise<ResultatRechercheEntreprise[]> {
+    return this.cache.get(`${terme}-${departement}`, () =>
+      this.adaptateurDecore.rechercheOrganisations(terme, departement)
+    );
+  }
+}
