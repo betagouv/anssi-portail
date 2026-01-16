97cad5d1e3457fe039d64e146ea3d9e6de4dd9bb
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2026-01-16 11:31:42 +0100

    [SOIN] Extrais la demande de diagnostic simplifiée

--- front/lib-svelte/src/test-maturite/EncartDeRecommandationMaturiteFaible.svelte
+++ front/lib-svelte/src/test-maturite/EncartDeRecommandationMaturiteFaible.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-  import FoireAuxQuestions from '../demande-aide-mon-aide-cyber/FoireAuxQuestions.svelte';
-  import FormulaireDemandeSimplifiee from '../demande-aide-mon-aide-cyber/FormulaireDemandeSimplifiee.svelte';
+  import DemandeDiagnosticSimplifiee from '../demande-aide-mon-aide-cyber/DemandeDiagnosticSimplifiee.svelte';
 </script>
 
 <section>
@@ -15,47 +14,14 @@
       </p>
     </hgroup>
     <lab-anssi-icone nom="arrow-down-s-line" taille="lg"></lab-anssi-icone>
-    <div class="demande-diagnostic">
-      <div class="explications">
-        <hgroup>
-          <h2>Protégez rapidement votre organisation des cyberattaques.</h2>
-          <p>
-            Profitez d’un premier diagnostic cyber gratuit accompagné par un
-            Aidant cyber et recevez 6 recommandations prioritaires à mettre en
-            place pour améliorer la cybersécurité de votre organisation.
-          </p>
-        </hgroup>
-        <ol>
-          <li>Complétez et envoyez votre demande</li>
-          <li>Un aidant cyber vous contacte rapidement</li>
-          <li>Réalisez votre diagnostic (en visio ou sur site – 1h)</li>
-          <li>
-            Recevez vos 6 recommandations prioritaires pour renforcer votre
-            cybersécurité
-          </li>
-        </ol>
-      </div>
-
-      <div class="formulaire">
-        <FormulaireDemandeSimplifiee />
-      </div>
-      <details>
-        <summary>
-          <lab-anssi-icone nom="questionnaire-line"></lab-anssi-icone>
-          Questions fréquentes
-        </summary>
-        <div class="faq">
-          <h6>Questions les plus fréquentes</h6>
-          <FoireAuxQuestions />
-        </div>
-      </details>
-    </div>
+    <DemandeDiagnosticSimplifiee></DemandeDiagnosticSimplifiee>
   </div>
 </section>
 
 <style lang="scss">
   @use '../../../assets/styles/responsive' as *;
   @use '../../../assets/styles/grille' as *;
+
   .contenu-section {
     display: flex;
     flex-direction: column;
@@ -72,6 +38,7 @@
       @include a-partir-de(lg) {
         max-width: taille-pour-colonnes(10);
       }
+
       h1 {
         font-size: 2rem;
         line-height: 2.5rem;
@@ -86,112 +53,6 @@
 
     lab-anssi-icone {
       align-self: center;
-    }
-
-    .demande-diagnostic {
-      display: grid;
-      grid-template-areas:
-        'explications'
-        'etapes'
-        'formulaire'
-        'faq';
-      background-color: #fef6e3;
-      padding: 40px 16px;
-      border-radius: 8px;
-      gap: 24px;
-
-      @include a-partir-de(md) {
-        padding: 40px;
-      }
-
-      @include a-partir-de(lg) {
-        grid-template-areas:
-          'explications formulaire'
-          'faq faq';
-      }
-
-      .explications {
-        grid-area: explications;
-        hgroup {
-          h2 {
-            margin-bottom: 16px;
-          }
-
-          p {
-            margin-bottom: 32px;
-          }
-        }
-      }
-      ol {
-        display: flex;
-        flex-direction: column;
-        gap: 16px;
-        counter-reset: li;
-        list-style: none;
-        padding: 0;
-        li {
-          display: flex;
-          gap: 12px;
-
-          &::before {
-            flex: 0 0 16px;
-            align-self: flex-start;
-            height: 16px;
-            line-height: 100%;
-            text-align: center;
-            font-weight: bold;
-            border-radius: 4px;
-            padding: 4px;
-            background-color: var(--noir);
-            color: white;
-            content: counter(li);
-            counter-increment: li;
-          }
-        }
-      }
-
-      .formulaire {
-        grid-area: formulaire;
-        background-color: white;
-        padding: 24px;
-        border: 1px solid #ddd;
-      }
-      details {
-        grid-area: faq;
-        summary {
-          padding: 8px 16px 8px 12px;
-          border-radius: 8px;
-          width: max-content;
-          display: flex;
-          gap: 4px;
-
-          &:hover {
-            background-color: rgb(from var(--noir) r g b / 4%);
-            cursor: pointer;
-          }
-          &:active {
-            background-color: rgb(from var(--noir) r g b / 8%);
-            cursor: pointer;
-          }
-          &::marker {
-            content: '';
-          }
-          &::-webkit-details-marker {
-            display: none;
-          }
-        }
-        .faq {
-          background-color: white;
-          padding: 48px 16px;
-          margin-top: 32px;
-          h6 {
-            font-weight: bold;
-            font-size: 1.125rem;
-            line-height: 1.5rem;
-            margin-block: 0 24px;
-          }
-        }
-      }
     }
   }
 </style>
