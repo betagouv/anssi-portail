24a0122d12c79447dc477cd4fc7f47da3502a8f1
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-01-20 10:43:01 +0100

    Crée un barometre simplifie

---
+++ front/lib-svelte/src/main-barometre-simplifie.ts
@@ -0,0 +1,6 @@
+import { mount } from 'svelte';
+import BarometreSimplifie from './test-maturite/BarometreSimplifie.svelte';
+
+mount(BarometreSimplifie, {
+  target: document.getElementById('barometre-simplifie')!,
+});
---
+++ front/lib-svelte/src/test-maturite/BarometreSimplifie.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import GraphiqueAnneau from './GraphiqueAnneau.svelte';
+  import LegendeAnneau from './LegendeAnneau.svelte';
+  import type { Serie } from './Serie';
+
+  let serie: Serie = [
+    {
+      libelle: 'libelle1',
+      valeur: 10,
+    },
+    {
+      libelle: 'libelle2',
+      valeur: 10,
+    },
+    {
+      libelle: 'libelle3',
+      valeur: 10,
+    },
+  ];
+</script>
+
+<div class="">
+  <GraphiqueAnneau {serie} nomDeLaDonnee="tests réalisés" />
+  <LegendeAnneau {serie} />
+</div>
