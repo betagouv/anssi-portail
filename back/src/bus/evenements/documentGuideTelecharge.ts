d5647f9680579328f9fe9128afefc78694057cbb
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2026-01-23 15:26:59 +0100

    Ajoute une origine de téléchargement des guides

--- back/src/bus/evenements/documentGuideTelecharge.ts
+++ back/src/bus/evenements/documentGuideTelecharge.ts
@@ -1,7 +1,23 @@
+const originesConnues = ['msc', 'mqc'] as const;
+type Origines = (typeof originesConnues)[number];
+const estUneOrigineConnue = (
+  origine: string | undefined
+): origine is Origines => {
+  return originesConnues.some((o) => o === origine);
+};
+
 export class DocumentGuideTelecharge {
   readonly nomFichier: string;
+  readonly origine: Origines;
 
-  constructor({ nomFichier }: { nomFichier: string }) {
+  constructor({
+    nomFichier,
+    origine,
+  }: {
+    nomFichier: string;
+    origine: string | undefined;
+  }) {
     this.nomFichier = nomFichier;
+    this.origine = estUneOrigineConnue(origine) ? origine : 'msc';
   }
 }
