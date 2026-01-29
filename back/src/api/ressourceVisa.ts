3882de4472d54450ccd4bdf91ec84bb1cb3fd214
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-01-29 11:18:58 +0100

    Spécifie la mise en cache des visas

--- back/src/api/ressourceVisa.ts
+++ back/src/api/ressourceVisa.ts
@@ -20,6 +20,14 @@ export const ressourceVisa = ({
 
         reponse.contentType(fluxCellar.typeDeContenu);
         reponse.setHeader('content-length', fluxCellar.tailleDuContenu);
+        reponse.set({
+          'cache-control':
+            'public, max-age=3600, s-maxage=3600, must-revalidate, proxy-revalidate',
+          pragma: '',
+          expires: '3600',
+          'surrogate-control':
+            'public, max-age=3600, s-maxage=3600, must-revalidate, proxy-revalidate',
+        });
         fluxCellar.flux.pipe(reponse);
 
         fluxCellar.flux.on('error', (err) => {
