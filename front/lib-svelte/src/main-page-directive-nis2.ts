461335e9fd9d331c757db895c7dba2b160a14369
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2026-02-02 14:53:28 +0100

    [NIS2] Ajoute la page "Directive NIS 2"

---
+++ front/lib-svelte/src/main-page-directive-nis2.ts
@@ -0,0 +1,6 @@
+import { mount } from 'svelte';
+import Page from './nis2/PageNis2.svelte';
+
+mount(Page, {
+  target: document.getElementById('page-directive-nis2')!,
+});
--- front/lib-svelte/src/ui/FilAriane.svelte
+++ front/lib-svelte/src/ui/FilAriane.svelte
@@ -5,9 +5,10 @@
   export let feuille: string;
   export let branche: undefined | Branche = undefined;
   export let brancheConnectee: undefined | Branche = undefined;
+  export let fondSombre = false;
 </script>
 
-<div class="fil-ariane">
+<div class="fil-ariane {fondSombre ? 'fond-sombre' : ''}">
   {#if $profilStore}
     <a href="/catalogue" class="lien">Catalogue des services</a>
   {:else}
@@ -39,5 +40,16 @@
 <style>
   .fil-ariane {
     grid-area: ariane;
+
+    &.fond-sombre {
+      a {
+        color: #ffffff;
+        text-decoration-color: #ffffff;
+      }
+
+      img {
+        filter: invert(1);
+      }
+    }
   }
 </style>
