bf95ba3a35ff5761a707acdecb2d9a7e22f5317a
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2026-02-03 16:52:54 +0100

    [NIS2] Affiche les items des solutions

--- front/lib-svelte/src/page-directive-nis2.ts
+++ front/lib-svelte/src/page-directive-nis2.ts
@@ -1,6 +1,15 @@
 import { mount } from 'svelte';
+import type { ItemCyber } from './catalogue/Catalogue.types';
 import Page from './nis2/PageNis2.svelte';
+
+const donnees = document.getElementById('donnees')!.textContent;
+if (!donnees) throw new Error('Impossible de trouver les données du catalogue');
+
+const { itemsCyber } = JSON.parse(donnees) as {
+  itemsCyber: ItemCyber[];
+};
 
 mount(Page, {
   target: document.getElementById('page-directive-nis2')!,
+  props: { itemsCyber },
 });
