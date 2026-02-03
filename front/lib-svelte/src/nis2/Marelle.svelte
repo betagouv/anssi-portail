fada0a24034a2f12445c894293b5e1103c7691ed
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2026-02-03 11:20:16 +0100

    [NIS2][SOIN] Déplace l'accordéon et la marelle
    ... dans des composants spécifiques

---
+++ front/lib-svelte/src/nis2/Marelle.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+  const etapesMarelle = [
+    {
+      titre: 'Savoir si votre entité est concernée',
+      description:
+        'Réalisez un test en ligne pour déterminer si votre entité est régulée par la directive NIS 2 et à quelle catégorie elle appartient.',
+      lien: {
+        href: '#',
+        texte: 'Débuter le test',
+        target: '_self',
+      },
+      illustration: {
+        lien: '/assets/images/personne-reflechissant-devant-un-ecran.svg',
+        alt: '',
+      },
+    },
+
+    {
+      titre: "Enregistrer votre entité auprès de l'ANSSI",
+      description:
+        'Accédez dès à présent au service de pré-enregistrement en ligne.',
+      lien: {
+        href: '#',
+        texte: 'Se déclarer',
+        target: '_self',
+      },
+      illustration: {
+        lien: '/assets/images/homme-remplissant-ecran-login.svg',
+        alt: '',
+      },
+    },
+    {
+      titre: 'Agir pour réduire les risques cyber',
+      description:
+        'Mettez en oeuvre les exigences de sécurité issues de la directive pour réduire vos risques cyber et signalez à l’ANSSI vos incidents de sécurité. ',
+      lien: {
+        href: '#',
+        texte: "Découvrir les solutions pour m'aider",
+        target: '_self',
+      },
+      illustration: {
+        lien: '/assets/images/index-touchant-ecran-demandant-empreinte-digitale.svg',
+        alt: '',
+      },
+    },
+  ];
+</script>
+
+<lab-anssi-marelle
+  titre="NIS&nbsp;2&nbsp;: Comment se lancer&nbsp;?"
+  etapesmarelle={etapesMarelle}
+></lab-anssi-marelle>
+
+<style lang="scss">
+  @use '../../../assets/styles/responsive' as *;
+
+  .marelle {
+    padding-block: 72px;
+    background: #f6f6f6;
+
+    lab-anssi-marelle {
+      justify-self: center;
+      @include a-partir-de(lg) {
+        max-width: taille-pour-colonnes(10);
+      }
+    }
+  }
+</style>
