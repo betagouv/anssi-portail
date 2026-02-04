f9441049c0836c4bf7a318346e72d69d6ee5710e
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-02-04 16:38:11 +0100

    [LANDING-DIAG] Affiche le Hero

--- front/lib-svelte/src/demande-aide-mon-aide-cyber/PresentationDemandeDiagnostic.svelte
+++ front/lib-svelte/src/demande-aide-mon-aide-cyber/PresentationDemandeDiagnostic.svelte
@@ -1,4 +1,20 @@
 <script lang="ts">
+  import FilAriane from '../ui/FilAriane.svelte';
+  import Heros from '../ui/Heros.svelte';
 </script>
 
-<p>Presentation Demande Diagnostic</p>
+<Heros
+  format="heros"
+  theme="sombre"
+  cacheTags
+  titre="Protéger ma collectivité contre les cyberattaques"
+  description="Les collectivités sont exposées aux cyberattaques, notamment aux rançongiciels. En 2024, elles ont représenté 17 % des cibles de rançongiciels, ce qui en fait le deuxième type d’entités le plus visé."
+  cacheActions
+  illustrationSource="/assets/images/image-generique.avif"
+  illustrationAlt=""
+  cacheIllustration={false}
+>
+  {#snippet filAriane()}
+    <FilAriane fondSombre={true} feuille="Protéger ma collectivité" />
+  {/snippet}
+</Heros>
