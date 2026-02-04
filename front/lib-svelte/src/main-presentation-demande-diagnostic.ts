4be9e0db4a2cb2d009d11fa7f5dac626a3019561
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-02-04 16:44:33 +0100

    [SOIN] Déplace le composant dans son propre sous dossier

--- front/lib-svelte/src/main-presentation-demande-diagnostic.ts
+++ front/lib-svelte/src/main-presentation-demande-diagnostic.ts
@@ -1,5 +1,5 @@
 import { mount } from 'svelte';
-import Page from './demande-aide-mon-aide-cyber/PresentationDemandeDiagnostic.svelte';
+import Page from './demande-aide-mon-aide-cyber/presentation/PresentationDemandeDiagnostic.svelte';
 
 mount(Page, {
   target: document.getElementById('page-presentation-demande-diagnostic')!,
