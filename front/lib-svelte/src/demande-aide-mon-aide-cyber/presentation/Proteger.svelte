c8338ab2393b2a3d641aae4e9add61fb0d51b996
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-02-04 17:09:01 +0100

    [LANDING-DIAG][PROTEGER] Affiche l'introduction

--- front/lib-svelte/src/demande-aide-mon-aide-cyber/presentation/Proteger.svelte
+++ front/lib-svelte/src/demande-aide-mon-aide-cyber/presentation/Proteger.svelte
@@ -1,5 +1,43 @@
 <script lang="ts"></script>
 
 <dsfr-container>
-  <h2>Découvrez les risques pour<br />votre collectivité en cas d’attaques</h2>
+  <div class="introduction">
+    <h2>
+      Découvrez les risques pour<br />votre collectivité en cas d’attaques
+    </h2>
+    <!-- Les sous-titres sont intégrés dans la vidéo -->
+    <!-- svelte-ignore a11y_media_has_caption -->
+    <video
+      id="video-risques"
+      src="https://messervicescyber-ressources.cellar-c2.services.clever-cloud.com/Video_Risques.mp4"
+      controls
+    ></video>
+  </div>
 </dsfr-container>
+
+<style lang="scss">
+  @use '../../../../assets/styles/responsive' as *;
+  @use '../../../../assets/styles/grille' as *;
+
+  .introduction {
+    padding-bottom: 72px;
+    margin-inline: auto;
+    max-width: 100%;
+    @include a-partir-de(md) {
+      max-width: taille-pour-colonnes(10);
+    }
+    @include a-partir-de(lg) {
+      padding-top: 24px;
+      max-width: taille-pour-colonnes(6);
+    }
+
+    h2 {
+      text-align: center;
+      margin-bottom: 48px;
+    }
+
+    video {
+      width: 100%;
+    }
+  }
+</style>
