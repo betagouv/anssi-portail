c2eb9f5e7f3af22f24bd4c1fd4079769fa6775a5
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2026-02-05 17:42:05 +0100

    [THEME] Applique les thèmes

--- demande-diag/src/DemandeDiagnostic.svelte
+++ demande-diag/src/DemandeDiagnostic.svelte
@@ -3,19 +3,18 @@
     tag: 'lab-anssi-demande-diagnostic',
     shadow: 'none',
     props: {
-      siretAidant: {type:'String', attribute: "siret-aidant"}
-    }
+      siretAidant: { type: 'String', attribute: 'siret-aidant' },
+    },
   }}
 />
 
 <script lang="ts">
-  import DemandeDiagnosticSimplifiee
-    from '@anssi-portail/svelte/src/demande-aide-mon-aide-cyber/DemandeDiagnosticSimplifiee.svelte';
+  import DemandeDiagnosticSimplifiee from '@anssi-portail/svelte/src/demande-aide-mon-aide-cyber/DemandeDiagnosticSimplifiee.svelte';
 
   const { siretAidant }: { siretAidant?: string } = $props();
 </script>
 
-<div class="lab-anssi-demande-diagnostic-simplifiee">
+<div class="lab-anssi-demande-diagnostic-simplifiee" data-themeable>
   <DemandeDiagnosticSimplifiee
     origine={window.location.hostname}
     urlBase={import.meta.env.VITE_API_URL}
