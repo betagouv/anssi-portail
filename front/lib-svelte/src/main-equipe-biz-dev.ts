d3543ced45e13aade1ab2a2092448b5564bde720
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-02-05 09:48:11 +0100

    [SOIN] Extrais l'encart des biz dev
    ...actuellement utilisé dans l'accueil, pour le réutilisé en Svelte

---
+++ front/lib-svelte/src/main-equipe-biz-dev.ts
@@ -0,0 +1,6 @@
+import { mount } from 'svelte';
+import EquipeBizDev from './interlocuteurs/EquipeBizDev.svelte';
+
+mount(EquipeBizDev, {
+  target: document.getElementById('equipe-biz-dev')!,
+});
