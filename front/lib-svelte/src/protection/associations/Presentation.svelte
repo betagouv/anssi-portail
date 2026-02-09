649b4c410967c6e10e695cf00548c5418596d481
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-02-09 18:18:30 +0100

    [LANDING][ASSOCIATIONS] Affiche la présentation

---
+++ front/lib-svelte/src/protection/associations/Presentation.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import FilAriane from '../../ui/FilAriane.svelte';
+  import Heros from '../../ui/Heros.svelte';
+  import Proteger from '../Proteger.svelte';
+</script>
+
+<Heros
+  cacheActions
+  cacheIllustration={false}
+  cacheTags
+  description=""
+  format="heros"
+  illustrationAlt=""
+  illustrationSource="/assets/images/personne-avec-micro-consultant-sites-cyber.svg"
+  theme="sombre"
+  titre=""
+>
+  {#snippet preambule()}
+    <hgroup class="preambule">
+      <h1 class="alternatif-xs">
+        Protéger mon association contre les cyberattaques
+      </h1>
+      <p class="texte-chapo-xl">
+        Tout comme les entreprises et les administrations publiques, les
+        associations sont exposées aux cyberattaques&nbsp;: rançongiciels
+        (ransomwares), fuites de données, hameçonnage… Les impacts peuvent être
+        financiers, juridiques et réputationnels, parfois majeurs.
+      </p>
+      <p class="texte-chapo-xl">
+        Bonne nouvelle&nbsp;: il est possible de réduire les risques avec des
+        actions simples et concrètes. Découvrez les solutions gratuites
+        proposées par l’État pour renforcer la cybersécurité de votre
+        association et vous accompagner pas à pas dans votre démarche de
+        sécurisation.
+      </p>
+    </hgroup>
+  {/snippet}
+  {#snippet filAriane()}
+    <FilAriane fondSombre={true} feuille="Protéger mon association" />
+  {/snippet}
+</Heros>
+
+<Proteger />
+
+<style lang="scss">
+  .preambule {
+    padding-right: 32px;
+  }
+</style>
