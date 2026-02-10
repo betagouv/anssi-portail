6eb5947ff076023317d68cb4843db558e0b793bb
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-02-10 09:57:48 +0100

    [LANDING][ENTREPRISES] Affiche la présentation

---
+++ front/lib-svelte/src/main-entreprises.ts
@@ -0,0 +1,6 @@
+import { mount } from 'svelte';
+import Page from './protection/entreprises/Presentation.svelte';
+
+mount(Page, {
+  target: document.getElementById('page-entreprises')!,
+});
---
+++ front/lib-svelte/src/protection/entreprises/Presentation.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  import FilAriane from '../../ui/FilAriane.svelte';
+  import Heros from '../../ui/Heros.svelte';
+  import Proteger from '../Proteger.svelte';
+</script>
+
+<Heros
+  cacheActions
+  cacheIllustration={false}
+  cacheTags
+  description=""
+  format="heros"
+  illustrationAlt=""
+  illustrationSource="/assets/images/personne-avec-laptop-entoure-de-diagrammes.svg"
+  theme="sombre"
+  titre=""
+>
+  {#snippet preambule()}
+    <hgroup class="preambule">
+      <h1 class="alternatif-xs">
+        Protéger mon entreprise contre les cyberattaques
+      </h1>
+      <p class="texte-chapo-xl">
+        Quelle que soit leur taille - TPE/PME/ETI - ou leur secteur, toutes les
+        entreprises sont exposées au risque de cyberattaques, comme les
+        rançongiciels (ransomwares) ou les fuites de données. Les conséquences
+        financières, juridiques ou réputationnelles peuvent être très graves.
+      </p>
+      <p class="texte-chapo-xl">
+        Comprendre les risques cyber et agir concrètement est essentiel et
+        possible ! Découvrez toutes les solutions gratuites proposées par l'Etat
+        pour vous aider dans vos efforts de sécurisation.
+      </p>
+    </hgroup>
+  {/snippet}
+  {#snippet filAriane()}
+    <FilAriane
+      fondSombre={true}
+      feuille="Protéger mon entreprise contre les cyberattaques"
+    />
+  {/snippet}
+</Heros>
+
+<Proteger />
+
+<style lang="scss">
+  .preambule {
+    padding-right: 32px;
+  }
+</style>
