29cbc788d644c72146a57e02fdecaf3a168414a9
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2026-02-11 12:14:47 +0100

    [NIS2] Affiche la vidéo

--- front/lib-svelte/src/nis2/DocumentationNis2.svelte
+++ front/lib-svelte/src/nis2/DocumentationNis2.svelte
@@ -9,9 +9,28 @@
     <img src="/assets/images/plaquette-nis2.avif" alt="Plaquette NIS 2" />
 
     <h2>Vidéo de présentation de la directive NIS 2</h2>
-    <!-- Les sous-titres sont intégrés dans la vidéo -->
-    <!-- svelte-ignore a11y_media_has_caption -->
-    <video controls preload="metadata" poster=""> </video>
+    <div class="video">
+      <!-- Les sous-titres sont intégrés dans la vidéo -->
+      <!-- svelte-ignore a11y_media_has_caption -->
+      <video
+        controls
+        preload="metadata"
+        poster="https://messervicescyber-ressources.cellar-c2.services.clever-cloud.com/focus-nis2-episode2.avif"
+      >
+        <source
+          src="https://messervicescyber-ressources.cellar-c2.services.clever-cloud.com/focus-nis2-episode2.av1.mp4"
+          type="video/mp4; codecs=av01.0.05M.08"
+        />
+        <source
+          src="https://messervicescyber-ressources.cellar-c2.services.clever-cloud.com/focus-nis2-episode2.webm"
+          type="video/webm; codecs='vp9,opus'"
+        />
+        <source
+          src="https://messervicescyber-ressources.cellar-c2.services.clever-cloud.com/focus-nis2-episode2.mp4"
+          type="video/mp4; codecs=avc1.42E01E"
+        />
+      </video>
+    </div>
   </div>
 </dsfr-container>
 
@@ -35,6 +54,16 @@
 
       img {
         margin-bottom: 48px;
+      }
+
+      .video {
+        background: black;
+
+        video {
+          aspect-ratio: 16 / 10;
+          height: 100%;
+          width: 100%;
+        }
       }
 
       @include a-partir-de(lg) {
