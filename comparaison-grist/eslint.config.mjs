263c09dd86e0d55234110800566bf36fd63753dd
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-02-11 14:46:43 +0100

    Initie le projet comparaison-grist

---
+++ comparaison-grist/eslint.config.mjs
@@ -0,0 +1,28 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+  {
+    files: ['**/*.ts'],
+    extends: [eslint.configs.recommended, tseslint.configs.recommended],
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: false,
+        },
+      ],
+    },
+  }
+);
