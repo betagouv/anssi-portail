9d84e0ee814a10776e7f5bd93e68fb4a93015d4c
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2026-02-11 17:00:31 +0100

    [FINANCEMENTS] Utilise le cache
    ... sur l'entrepôt des financements Grist

--- back/src/infra/entrepotFinancementGrist.ts
+++ back/src/infra/entrepotFinancementGrist.ts
@@ -4,79 +4,75 @@ import { Financement } from '../metier/financement';
 import { AdaptateurEnvironnement } from './adaptateurEnvironnement';
 import { ClientHttp } from './clientHttp';
 import { aseptiseListeGrist } from './grist';
+import { EntrepotGrist, ReponseGrist } from './entrepotGrist';
 
-export type RetourApiGrist = {
-  records: {
-    id: number;
-    fields: {
-      Nom_du_dispositif: string | null;
-      Financeur: string | null;
-      Entites_eligibles: string[] | null;
-      Perimetre_geographique: string[] | null;
-      Financement: string[] | null;
-      Objectifs: string | null;
-      Operations_eligibles: string | null;
-      Beneficiaire: string | null;
-      Montant: string | null;
-      Conditions: string | null;
-      Region: string | null;
-      Contact: string | null;
-      Source: string | null;
-    };
-  }[];
+export type FinancementGrist = {
+  id: number;
+  fields: {
+    Nom_du_dispositif: string | null;
+    Financeur: string | null;
+    Entites_eligibles: string[] | null;
+    Perimetre_geographique: string[] | null;
+    Financement: string[] | null;
+    Objectifs: string | null;
+    Operations_eligibles: string | null;
+    Beneficiaire: string | null;
+    Montant: string | null;
+    Conditions: string | null;
+    Region: string | null;
+    Contact: string | null;
+    Source: string | null;
+  };
 };
 
-export class EntrepotFinancementGrist implements EntrepotFinancement {
-  clientHttp: ClientHttp<RetourApiGrist>;
-  adaptateurEnvironnement: AdaptateurEnvironnement;
+export class EntrepotFinancementGrist
+  extends EntrepotGrist<FinancementGrist>
+  implements EntrepotFinancement
+{
   constructor({
     clientHttp = axios,
     adaptateurEnvironnement,
   }: {
-    clientHttp?: ClientHttp<RetourApiGrist>;
+    clientHttp: ClientHttp<ReponseGrist<FinancementGrist>>;
     adaptateurEnvironnement: AdaptateurEnvironnement;
   }) {
-    this.clientHttp = clientHttp;
-    this.adaptateurEnvironnement = adaptateurEnvironnement;
+    super(
+      clientHttp,
+      adaptateurEnvironnement.grist().urlFinancements(),
+      adaptateurEnvironnement.grist().cleApiFinancements(),
+      adaptateurEnvironnement.grist().dureeCacheEnSecondes()
+    );
   }
 
-  tous = async () => {
-    const urlDocFinancement = this.adaptateurEnvironnement
-      .grist()
-      .urlFinancements();
-    if (!urlDocFinancement) {
-      return [];
-    }
-    const cleApi = this.adaptateurEnvironnement.grist().cleApiFinancements();
-    const reponse = await this.clientHttp.get(urlDocFinancement, {
-      headers: { Authorization: `Bearer ${cleApi}` },
+  async parId(id: number): Promise<Financement | undefined> {
+    return (await this.tous()).find((financement) => financement.id === id);
+  }
+
+  async tous(): Promise<Financement[]> {
+    const financementsGrist = await this.appelleGrist();
+    return financementsGrist.records.map(this.convertisFinancementGrist);
+  }
+
+  private readonly convertisFinancementGrist = ({
+    fields,
+    id,
+  }: FinancementGrist) =>
+    new Financement({
+      id,
+      nom: fields.Nom_du_dispositif ?? '',
+      financeur: fields.Financeur ?? '',
+      typesDeFinancement: aseptiseListeGrist(fields.Financement),
+      entitesElligibles: aseptiseListeGrist(fields.Entites_eligibles),
+      perimetresGeographiques: aseptiseListeGrist(
+        fields.Perimetre_geographique
+      ),
+      objectifs: fields.Objectifs ?? '',
+      operationsEligibles: fields.Operations_eligibles ?? '',
+      benificiaires: fields.Beneficiaire ?? '',
+      montant: fields.Montant ?? '',
+      condition: fields.Conditions ?? '',
+      sources: fields.Source ? [fields.Source] : [],
+      contact: fields.Contact ?? '',
+      regions: fields.Region ? [fields.Region] : [],
     });
-
-    return reponse.data.records.map(
-      ({ fields, id }) =>
-        new Financement({
-          id,
-          nom: fields.Nom_du_dispositif ?? '',
-          financeur: fields.Financeur ?? '',
-          typesDeFinancement: aseptiseListeGrist(fields.Financement),
-          entitesElligibles: aseptiseListeGrist(fields.Entites_eligibles),
-          perimetresGeographiques: aseptiseListeGrist(
-            fields.Perimetre_geographique
-          ),
-          objectifs: fields.Objectifs ?? '',
-          operationsEligibles: fields.Operations_eligibles ?? '',
-          benificiaires: fields.Beneficiaire ?? '',
-          montant: fields.Montant ?? '',
-          condition: fields.Conditions ?? '',
-          sources: fields.Source ? [fields.Source] : [],
-          contact: fields.Contact ?? '',
-          regions: fields.Region ? [fields.Region] : [],
-        })
-    );
-  };
-
-  parId = async (id: number) => {
-    const tous = await this.tous();
-    return tous.find((f) => f.id === id);
-  };
 }
