0db46a54df974fd10567558e487c3ec84259a4dc
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2026-02-11 14:29:07 +0100

    [NIS2] Complète la liste des items des solutions

--- front/lib-svelte/src/nis2/Solutions.svelte
+++ front/lib-svelte/src/nis2/Solutions.svelte
@@ -1,19 +1,30 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
   import type { ItemCyber } from '../catalogue/Catalogue.types';
+  import {
+    chargeGuidesDansLeStore,
+    guidesStore,
+  } from '../catalogue/stores/guides/guides.store';
   import ActionParcoursAvecItems from '../parcours/ActionParcoursAvecItems.svelte';
   import ControleSegmente from '../navigation/ControleSegmente.svelte';
   import { versItemsCyberOuGuide } from '../parcours/itemDuParcours';
 
   export let itemsCyber: ItemCyber[];
 
-  const versMesItems = versItemsCyberOuGuide(itemsCyber, []);
+  onMount(chargeGuidesDansLeStore);
 
-  const actions = [
+  $: versMesItems = versItemsCyberOuGuide(itemsCyber, $guidesStore);
+
+  $: actions = [
     {
       id: 'comprendre',
       titre: 'Comprendre',
       explication: 'Se familiariser avec la menace cyber.',
-      items: versMesItems(['/services/mon-espace-nis2']),
+      items: versMesItems([
+        '/ressources/risques-cyber',
+        '/ressources/panorama',
+        '/guides/attaques-par-rancongiciels-tous-concernes',
+      ]),
       ancre: 'solutions&comprendre',
     },
     {
@@ -21,7 +32,7 @@
       titre: 'Se déclarer',
       explication:
         "Vérifiez si votre entité est concernée et procédez à son enregistrement auprès de l'ANSSI.",
-      items: versMesItems(['/services/NIS2']),
+      items: versMesItems(['/services/NIS2', '/services/mon-espace-nis2']),
       ancre: 'solutions&se-declarer',
     },
     {
@@ -34,22 +45,33 @@
         '/services/ads',
         '/services/conseil-technique',
         '/ressources/revue-presse',
+        '/guides/la-cybersecurite-pour-les-tpepme-en-treize-questions',
+        '/guides/la-methode-ebios-risk-manager-le-guide',
       ]),
       ancre: 'solutions&reduire-risques',
     },
     {
       id: 'se-preparer',
       titre: 'Se préparer',
-      explication: 'Signalez à l’ANSSI vos incidents de sécurité.',
-      items: versMesItems(['/services/assistance-reponse-incidents']),
+      explication:
+        'Supervisez et préparez votre organisation à l’éventualité d’une attaque.',
+      items: versMesItems([
+        '/guides/organiser-un-exercice-de-gestion-de-crise-cyber',
+        '/guides/la-supervision-de-securite-les-cles-de-decision',
+      ]),
       ancre: 'solutions&se-preparer',
     },
     {
-      id: 'declarer-incident',
-      titre: 'Déclarer un incident',
-      explication: 'Signalez à l’ANSSI vos incidents de sécurité.',
-      items: versMesItems(['/services/assistance-reponse-incidents']),
-      ancre: 'solutions&declarer-incident',
+      id: 'reagir-incident',
+      titre: 'Réagir à un incident',
+      explication:
+        'Agissez et signalez à l’ANSSI vos incidents de sécurité et sollicitez une assistance.',
+      items: versMesItems([
+        '/services/assistance-reponse-incidents',
+        '/guides/cyberattaques-et-remediation-les-cles-de-decision',
+        '/guides/anticiper-et-gerer-sa-communication-de-crise-cyber',
+      ]),
+      ancre: 'solutions&reagir-incident',
     },
   ];
 </script>
