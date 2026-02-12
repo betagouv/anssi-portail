3ba55ce8a1af8949b0a8b04a4972b3c906e106be
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2026-02-12 16:20:30 +0100

    [COMPARAISON] Appelle le comparateur des guides
    ... et crée un fichier Markdown

--- comparaison-grist/src/guides.ts
+++ comparaison-grist/src/guides.ts
@@ -1,11 +1,47 @@
 import fs from 'node:fs';
+import { ConsignateurDeComparaisonDeGuides } from './infrastructure/consignateurDeComparaisonDeGuides';
+import { ComparateurDeGuides } from './metier/guides/comparateurDeGuides';
+import { Guide } from './metier/guides/guide.type';
 
 const summaryFile = process.env.GITHUB_STEP_SUMMARY ?? 'rapport-guides.txt';
 
-const differences =
-  `## 📊 Table Diff\n\n` +
-  `| Row | Status | Changed Columns |\n` +
-  `|-----|--------|----------------|\n` +
-  `| 1 | modifié | Documents |\n`;
+// Pour tester le temps du dévelppement
+const guideZeroTrust: Guide = {
+  id: 'zero-trust',
+  nom: 'Zero Trust',
+  description:
+    '<p>Avec l’accroissement des usages liés au télétravail, ...</p>',
+  nomImage: 'anssi-fondamentaux-zero-trust-v1_publication',
+  langue: 'FR',
+  collections: ['Les essentiels'],
+  documents: [
+    {
+      libelle: 'Les Fondamentaux de l&#039;ANSSI - Zero Trust - v1.0',
+      nomFichier: 'anssi-fondamentaux-zero-trust-v1.0.pdf',
+    },
+  ],
+  dateMiseAJour: '20 Juin 2025',
+  datePublication: '20 Juin 2025',
+  thematique: 'Les essentiels',
+  besoins: ['REAGIR', 'SE_FORMER'],
+};
 
-fs.appendFileSync(summaryFile, differences);
+const comparateurDeGuides = new ComparateurDeGuides(
+  {
+    tous: async () => [guideZeroTrust],
+  },
+  {
+    tous: async () => [],
+  }
+);
+
+await comparateurDeGuides.chargeLesDonnees();
+const comparaison = await comparateurDeGuides.compare();
+
+const consignateurDeComparaisonDeGuides =
+  new ConsignateurDeComparaisonDeGuides();
+
+const markdown =
+  consignateurDeComparaisonDeGuides.consigneComparaison(comparaison);
+
+fs.appendFileSync(summaryFile, markdown);
