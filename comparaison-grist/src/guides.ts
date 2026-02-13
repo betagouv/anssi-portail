be27657efe6f021e29045c6371c233405f33d666
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2026-02-13 11:42:56 +0100

    [COMPARAISON] Branche le comparateur aux Grist

--- comparaison-grist/src/guides.ts
+++ comparaison-grist/src/guides.ts
@@ -1,38 +1,23 @@
 import fs from 'node:fs';
 import { ConsignateurDeComparaisonDeGuides } from './infrastructure/consignateurDeComparaisonDeGuides';
 import { ComparateurDeGuides } from './metier/guides/comparateurDeGuides';
-import { Guide } from './metier/guides/guide.type';
+import { EntrepotGuideGrist } from './infrastructure/entrepotGuideGrist';
+import { adaptateurEnvironnement } from './infrastructure/adaptateurEnvironnement';
+import axios from 'axios';
 
 const summaryFile = process.env.GITHUB_STEP_SUMMARY ?? 'rapport-guides.md';
 
-// Pour tester le temps du dévelppement
-const guideZeroTrust: Guide = {
-  id: 'zero-trust',
-  nom: 'Zero Trust',
-  description:
-    '<p>Avec l’accroissement des usages liés au télétravail, ...</p>',
-  nomImage: 'anssi-fondamentaux-zero-trust-v1_publication',
-  langue: 'FR',
-  collections: ['Les essentiels'],
-  documents: [
-    {
-      libelle: 'Les Fondamentaux de l&#039;ANSSI - Zero Trust - v1.0',
-      nomFichier: 'anssi-fondamentaux-zero-trust-v1.0.pdf',
-    },
-  ],
-  dateMiseAJour: '20 Juin 2025',
-  datePublication: '20 Juin 2025',
-  thematique: 'Les essentiels',
-  besoins: ['REAGIR', 'SE_FORMER'],
-};
-
 const comparateurDeGuides = new ComparateurDeGuides(
-  {
-    tous: async () => [guideZeroTrust],
-  },
-  {
-    tous: async () => [],
-  }
+  new EntrepotGuideGrist(
+    axios,
+    adaptateurEnvironnement.grist().urlGuideSource(),
+    adaptateurEnvironnement.grist().cleApiSource()
+  ),
+  new EntrepotGuideGrist(
+    axios,
+    adaptateurEnvironnement.grist().urlGuideCible(),
+    adaptateurEnvironnement.grist().cleApiCible()
+  )
 );
 
 await comparateurDeGuides.chargeLesDonnees();
