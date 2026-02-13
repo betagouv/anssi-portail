363634faf67c691c45ec3b63aba66e7364e8d870
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-02-13 10:37:28 +0100

    [COMPARAISON] Fais ressortir les guides modifiés

--- comparaison-grist/src/infrastructure/consignateurDeComparaisonDeGuides.ts
+++ comparaison-grist/src/infrastructure/consignateurDeComparaisonDeGuides.ts
@@ -7,6 +7,9 @@ export class ConsignateurDeComparaisonDeGuides {
       ...comparaison.suppressions.map((suppression) =>
         this.construisUneLigneSuppression(suppression)
       ),
+      ...comparaison.modifications.map((modification) =>
+        this.construisUneLigneModification(modification)
+      ),
     ].join('\n');
     return `<table>
 <thead>
@@ -61,6 +64,51 @@ ${this.construisUneCelluleAvecLeContenu('-', guide.collections.join(', '))}
 ${this.construisUneCelluleAvecLeContenu('-', guide.besoins.join(', '))}
 </tr>
 `;
+  }
+
+  private construisUneLigneModification({
+    cible,
+    source,
+  }: {
+    source: Guide;
+    cible: Guide;
+  }) {
+    return `<tr>
+${this.construisUneCelluleDeDiff(source.id, cible.id)}
+${this.construisUneCelluleDeDiff(source.nom, cible.nom)}
+${this.construisUneCelluleDeDiff(source.thematique, cible.thematique)}
+${this.construisUneCelluleDeDiff(source.datePublication, cible.datePublication)}
+${this.construisUneCelluleDeDiff(source.dateMiseAJour, cible.dateMiseAJour)}
+${this.construisUneCelluleDeDiff(source.description, cible.description)}
+${this.construisUneCelluleDeDiff(source.nomImage ?? '', cible.nomImage ?? '')}
+${this.construisUneCelluleDeDiff(source.documents.map((document) => `${document.libelle} : ${document.nomFichier}`).join('\n'), cible.documents.map((document) => `${document.libelle} : ${document.nomFichier}`).join('\n'))}
+${this.construisUneCelluleDeDiff(source.langue, cible.langue)}
+${this.construisUneCelluleDeDiff(source.collections.join(', '), cible.collections.join(', '))}
+${this.construisUneCelluleDeDiff(source.besoins.join(', '), cible.besoins.join(', '))}
+</tr>
+`;
+  }
+
+  private construisUneCelluleDeDiff(
+    contenuSource: string,
+    contenuCible: string
+  ) {
+    if (contenuCible === contenuSource) {
+      return `<td>
+
+\`\`\`diff
+${contenuSource}
+\`\`\`
+</td>`;
+    }
+
+    return `<td>
+
+\`\`\`diff
+- ${contenuSource}
++ ${contenuCible}
+\`\`\`
+</td>`;
   }
 
   private construisUneCelluleAvecLeContenu(prefix: '+' | '-', contenu: string) {
