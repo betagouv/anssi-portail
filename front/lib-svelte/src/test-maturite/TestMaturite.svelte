6b516d6532ccebfbab42f5723fe40e4b3ee878fa
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2026-02-13 12:12:46 +0100

    [SOIN] Extrais l’introduction du test
    ...dans un composant

--- front/lib-svelte/src/test-maturite/TestMaturite.svelte
+++ front/lib-svelte/src/test-maturite/TestMaturite.svelte
@@ -6,6 +6,7 @@
   import Etapier from '../ui/Etapier.svelte';
   import Hero from '../ui/Hero.svelte';
   import ComparaisonTest from './ComparaisonTest.svelte';
+  import IntroductionTestMaturite from './IntroductionTestMaturite.svelte';
   import PartageTest from './PartageTest.svelte';
   import ResultatsTestMaturite from './ResultatsTestMaturite.svelte';
   import { enregistreIdResultatTestPourRevendication } from './resultatTest';
@@ -87,10 +88,6 @@
     window.location.href = `/resultats-session-groupe?code=${codeSessionGroupe}`;
   }
 
-  function debuteTeste() {
-    introFaite = true;
-  }
-
   let montreProposition = false;
   $: {
     const avecPropositions = etapesTestMaturite.filter(
@@ -116,19 +113,6 @@
     }
     organisateurSession = parametres.has('organisateur');
   });
-
-  const aDejaUnTest = derived<typeof profilStore, boolean>(
-    profilStore,
-    ($profilStore, set) => {
-      if ($profilStore) {
-        axios
-          .get('/api/resultats-test/dernier')
-          .then(() => set(true))
-          .catch(() => set(false));
-      }
-    },
-    false
-  );
 
   $: enSessionGroupe = !!codeSessionGroupe;
   $: organisateurSessionGroupe = enSessionGroupe && organisateurSession;
@@ -215,7 +199,8 @@
                   </label>
 
                   <label>
-                    Dans quelle région / territoire se trouve votre organisation&nbsp;?
+                    Dans quelle région / territoire se trouve votre
+                    organisation&nbsp;?
                     <SelectRegion bind:region />
                   </label>
 
@@ -259,55 +244,7 @@
             </div>
           </div>
         {:else}
-          {#if $aDejaUnTest}
-            <lab-anssi-lien
-              href="/ma-maturite"
-              titre="Retour"
-              icone="arrow-go-back-line"
-              positionIcone="gauche"
-            ></lab-anssi-lien>
-          {/if}
-          <div class="contenu-test">
-            <div class="introduction">
-              <h2>Quelle est la maturité cyber de votre organisation ?</h2>
-              <p>
-                La maturité cyber <b>reflète</b> le niveau global de prise en
-                compte des enjeux de cybersécurité par une organisation.
-                Répondez à
-                <b>6 questions</b>
-                pour obtenir votre évaluation <b>indicative</b>.
-              </p>
-              <input
-                type="button"
-                class="bouton primaire taille-moyenne"
-                value="Débuter le test"
-                on:click={debuteTeste}
-              />
-              <div class="acces-session-groupe">
-                <p>
-                  Vous souhaitez réaliser ce test à plusieurs, lors d’un atelier
-                  ou d’un événement ? Lancez une session de groupe pour
-                  recueillir et comparer les résultats des participants.
-                </p>
-                <a href="/session-groupe" class="lien"
-                  >Accéder à l’espace de session de groupe</a
-                >
-              </div>
-              <div class="note">
-                Le résultat obtenu est une évaluation indicative basée sur un
-                modèle élaboré par l’ANSSI. La maturité cyber n’est pas une
-                évaluation du niveau de sécurité des systèmes d’information
-                d’une organisation mais de sa posture à l’égard des enjeux
-                cyber.
-              </div>
-            </div>
-            <div class="illustration">
-              <img
-                src="/assets/images/test-maturite/illustration-prise-en-compte-risque.svg"
-                alt=""
-              />
-            </div>
-          </div>
+          <IntroductionTestMaturite bind:introFaite />
         {/if}
       </div>
     </section>
@@ -321,11 +258,3 @@
   {/if}
 {/if}
 
-<style lang="scss">
-  .acces-session-groupe {
-    p {
-      font-weight: bold;
-      margin-bottom: 8px;
-    }
-  }
-</style>
