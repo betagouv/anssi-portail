fe252c2f76c34c598ffa6e4cbc0791a29d8b7e73
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2026-02-20 16:26:58 +0100

    [EXIGENCES] Récupère les exigences depuis l’API

--- front/lib-svelte/src/nis2/ExigencesNis2.svelte
+++ front/lib-svelte/src/nis2/ExigencesNis2.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+  import axios from 'axios';
+  import { onMount } from 'svelte';
+
   type CategorieEntite = 'EntiteEssentielle' | 'EntiteImportante';
 
   const labelCategorieEntite = (categorie: CategorieEntite) =>
@@ -21,39 +24,14 @@
     entitesCible: CategorieEntite[];
   };
 
-  const exigencesNis2: ExigenceNis2[] = [
-    {
-      reference: '1.1-EI/EE',
-      entitesCible: ['EntiteEssentielle', 'EntiteImportante'],
-      objectifSecurite:
-        "Objectif de sécurité 1: Recensement des systèmes d'information",
-      thematique: 'Recensement des SI',
-      contenu:
-        'L’entité liste l’ensemble de ses activités et services, y compris les activités et services qui ne correspondent pas aux critères pour lesquels l’entité est devenue une entité importante ou essentielle (par exemple : une entité essentielle au titre d’une activité exploitation d’un oléoduc liste, en plus des activités et services participant à l’exploitation de l’oléoduc, tous les autres activités et services qu’elle fournit).\n' +
-        'Pour chaque entrée de cette liste, l’entité :\n' +
-        "•\tidentifie un responsable de l’activité ou du service (par exemple le chef de service auquel est rattachée l'activité ou le service, un directeur métier, la direction générale, etc) ;\n" +
-        '•\tliste les systèmes d’information les supportant.',
-    },
-    {
-      reference: '1.2-EI/EE',
-      entitesCible: ['EntiteEssentielle', 'EntiteImportante'],
-      objectifSecurite:
-        "Objectif de sécurité 1: Recensement des systèmes d'information",
-      thematique: 'Recensement des SI',
-      contenu:
-        'L’entité précise dans la liste prévue au (a) les systèmes d’information qui ne sont exposés à aucun des risques mentionnés à l’alinéa 2 de l’objectif de sécurité.\n' +
-        'L’entité renseigne les justifications de ces choix.',
-    },
-    {
-      reference: '2.A.2-EE',
-      entitesCible: ['EntiteEssentielle'],
-      objectifSecurite:
-        "Objectif de sécurité 2: Mise en œuvre d'un cadre de gouvernance de la sécurité numérique",
-      thematique: 'Rôles et responsabilités',
-      contenu:
-        'Il désigne au moins une personne qui le conseille et l’accompagne dans l’exercice de cette responsabilité. Cette personne est le point de contact privilégié de l’Agence nationale de la sécurité des systèmes d’information pour tous les sujets relatifs à la sécurité numérique (incidents de sécurité, communications de l’ANSSI sur les sujets relatifs aux entités essentielles)',
-    },
-  ];
+  let exigencesNis2: ExigenceNis2[] = [];
+
+  onMount(async () => {
+    const axiosResponse = await axios.get<ExigenceNis2[]>(
+      '/api/exigences-nis2'
+    );
+    exigencesNis2 = axiosResponse.data;
+  });
 </script>
 
 <dsfr-container>
