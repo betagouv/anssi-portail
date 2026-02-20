2c270519523f0e3728a63dcdf427c157ad2f416d
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2026-02-20 12:11:27 +0100

    [EXIGENCES NIS2] Structure et dynamise les exigences

--- front/lib-svelte/src/nis2/ExigencesNis2.svelte
+++ front/lib-svelte/src/nis2/ExigencesNis2.svelte
@@ -1,12 +1,33 @@
 <script lang="ts">
+  type CategorieEntite = 'EntiteEssentielle' | 'EntiteImportante';
+
+  const labelCategorieEntite = (categorie: CategorieEntite) =>
+    ({
+      EntiteImportante: 'EI',
+      EntiteEssentielle: 'EE',
+    })[categorie];
+
+  const couleurCategorieEntite = (categorie: CategorieEntite) =>
+    ({
+      EntiteImportante: 'green-archipel',
+      EntiteEssentielle: 'green-bourgeon',
+    })[categorie];
+
   type ExigenceNis2 = {
     reference: string;
+    objectifSecurite: string;
+    thematique: string;
     contenu: string;
+    entitesCible: CategorieEntite[];
   };
 
   const exigencesNis2: ExigenceNis2[] = [
     {
       reference: '1.1-EI/EE',
+      entitesCible: ['EntiteEssentielle', 'EntiteImportante'],
+      objectifSecurite:
+        "Objectif de sécurité 1: Recensement des systèmes d'information",
+      thematique: 'Recensement des SI',
       contenu:
         'L’entité liste l’ensemble de ses activités et services, y compris les activités et services qui ne correspondent pas aux critères pour lesquels l’entité est devenue une entité importante ou essentielle (par exemple : une entité essentielle au titre d’une activité exploitation d’un oléoduc liste, en plus des activités et services participant à l’exploitation de l’oléoduc, tous les autres activités et services qu’elle fournit).\n' +
         'Pour chaque entrée de cette liste, l’entité :\n' +
@@ -15,9 +36,22 @@
     },
     {
       reference: '1.2-EI/EE',
+      entitesCible: ['EntiteEssentielle', 'EntiteImportante'],
+      objectifSecurite:
+        "Objectif de sécurité 1: Recensement des systèmes d'information",
+      thematique: 'Recensement des SI',
       contenu:
         'L’entité précise dans la liste prévue au (a) les systèmes d’information qui ne sont exposés à aucun des risques mentionnés à l’alinéa 2 de l’objectif de sécurité.\n' +
         'L’entité renseigne les justifications de ces choix.',
+    },
+    {
+      reference: '2.A.2-EE',
+      entitesCible: ['EntiteEssentielle'],
+      objectifSecurite:
+        "Objectif de sécurité 2: Mise en œuvre d'un cadre de gouvernance de la sécurité numérique",
+      thematique: 'Rôles et responsabilités',
+      contenu:
+        'Il désigne au moins une personne qui le conseille et l’accompagne dans l’exercice de cette responsabilité. Cette personne est le point de contact privilégié de l’Agence nationale de la sécurité des systèmes d’information pour tous les sujets relatifs à la sécurité numérique (incidents de sécurité, communications de l’ANSSI sur les sujets relatifs aux entités essentielles)',
     },
   ];
 </script>
@@ -46,23 +80,16 @@
         <tr>
           <td>
             <dsfr-badges-group
-              badges={[
-                { label: 'EI', accent: 'green-archipel' },
-                { label: 'EE', accent: 'green-bourgeon' },
-              ]}
+              badges={exigence.entitesCible.map((categorie) => ({
+                label: labelCategorieEntite(categorie),
+                accent: couleurCategorieEntite(categorie),
+              }))}
             ></dsfr-badges-group>
             <dsfr-tags-group
               tags={[
-                {
-                  label:
-                    "Objectif de sécurité 1 : Recensement des systèmes d'information",
-                },
-                {
-                  label: 'Recensement des SI',
-                },
-                {
-                  label: exigence.reference,
-                },
+                { label: exigence.objectifSecurite },
+                { label: exigence.thematique },
+                { label: exigence.reference },
               ]}
               size="sm"
               groupMarkup="div"
