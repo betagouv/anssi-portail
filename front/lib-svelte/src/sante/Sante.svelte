d69a1e1638c80819cd73c5ea4dc63922373f2c7b
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2026-02-26 16:08:51 +0100

    [SANTE] Branche le composant sur l'API

--- front/lib-svelte/src/sante/Sante.svelte
+++ front/lib-svelte/src/sante/Sante.svelte
@@ -6,35 +6,26 @@
     documents: { nom: string; etat: Etat }[];
     images: Record<TailleImage, Etat>;
   };
+  type SanteGuides = {
+    guidesAvecProbleme: SanteGuide[];
+    guidesEnBonneSante: SanteGuide[];
+  };
 </script>
 
 <script lang="ts">
+  import axios from 'axios';
+  import { onMount } from 'svelte';
   import TableSanteGuides from './TableSanteGuides.svelte';
 
-  const guidesAvecDocumentsManquants: SanteGuide[] = [
-    {
-      id: 'Transition-post-quantique-protocole-SSHv2',
-      documents: [{ nom: 'transition_post_quantique_ssh_v2.pdf', etat: 'ko' }],
-      images: { '234': 'ok', '588': 'ko', origine: 'ok' },
-    },
-    {
-      id: 'cyberattaques-et-remediation-preparer-la-remedation',
-      documents: [
-        { nom: 'preparation_remediation_fr-web.pdf', etat: 'ko' },
-        { nom: 'plan_remediation-modele.xlsx', etat: 'ok' },
-        { nom: 'plan_remediation-modele.odt', etat: 'ko' },
-      ],
-      images: { '234': 'ok', '588': 'ok', origine: 'ok' },
-    },
-  ];
+  let guidesAvecDocumentsManquants: SanteGuide[] = [];
 
-  const guidesOk: SanteGuide[] = [
-    {
-      id: 'xxx',
-      documents: [{ nom: 'xxx.pdf', etat: 'ok' }],
-      images: { '234': 'ok', '588': 'ok', origine: 'ok' },
-    },
-  ];
+  let guidesOk: SanteGuide[] = [];
+
+  onMount(async () => {
+    const reponse = await axios.get<SanteGuides>('/api/sante-guides');
+    guidesOk = reponse.data.guidesEnBonneSante;
+    guidesAvecDocumentsManquants = reponse.data.guidesAvecProbleme;
+  });
 </script>
 
 <dsfr-container>
