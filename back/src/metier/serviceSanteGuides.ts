4b3e44001e22d911ac3c9c7976f8bf651ebeefe5
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2026-02-26 16:36:02 +0100

    [SANTE] Calcule la santé des guides

--- back/src/metier/serviceSanteGuides.ts
+++ back/src/metier/serviceSanteGuides.ts
@@ -1,18 +1,45 @@
 import { Guide } from './guide';
 
+export type Etat = 'ok' | 'ko';
+
+type SanteDocumentGuide = {
+  nom: string;
+  etat: Etat;
+};
+type TailleImage = '234' | '588' | 'origine';
+
+type SanteImagesGuide = Record<TailleImage, Etat>;
+
+type SanteGuide = {
+  images: SanteImagesGuide;
+  id: string;
+  documents: SanteDocumentGuide[];
+};
+
 export type ServiceSanteGuides = {
   calculeSante: (guides: Guide[]) => {
-    guidesAvecProbleme: any[];
-    guidesEnBonneSante: any[];
+    guidesAvecProbleme: SanteGuide[];
+    guidesEnBonneSante: SanteGuide[];
   };
 };
 
 export const fabriqueServiceSanteGuides = (): ServiceSanteGuides => {
   return {
-    calculeSante: () => {
+    calculeSante: (guides: Guide[]) => {
       return {
         guidesAvecProbleme: [],
-        guidesEnBonneSante: [],
+        guidesEnBonneSante: guides.map((guide) => ({
+          id: guide.id,
+          documents: guide.documents.map((document) => ({
+            nom: document.nomFichier,
+            etat: 'ok',
+          })),
+          images: {
+            '234': 'ok',
+            '588': 'ok',
+            origine: 'ok',
+          },
+        })),
       };
     },
   };
