00c599ba87d0bf47ac7f6862285e60fc52b2e0ca
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2026-03-03 10:13:50 +0100

    [EXIGENCES NIS2] Bascule en affichage large lors de la comparaison

---
+++ front/lib-svelte/src/ui/ConteneurLarge.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+
+  type Mode = 'STANDARD' | 'LARGE';
+  type Props = {
+    mode?: Mode;
+    children: Snippet;
+  };
+  const { mode = 'STANDARD', children }: Props = $props();
+</script>
+
+{#if mode === 'STANDARD'}
+  <dsfr-container>
+    {@render children()}
+  </dsfr-container>
+{:else}
+  <div class="conteneur-large">
+    {@render children()}
+  </div>
+{/if}
+
+<style lang="scss">
+  dsfr-container {
+    padding-bottom: 72px;
+  }
+
+  .conteneur-large {
+    padding: 24px 24px 72px;
+  }
+</style>
