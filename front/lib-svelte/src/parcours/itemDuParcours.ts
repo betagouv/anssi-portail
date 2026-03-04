929fa1edd21f9367f3e03e7186c1f4c7218de8a6
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2026-03-04 10:23:25 +0100

    [NIS 2] Affiche des tags spécifiques sur ADS et Silene
    ...« Entités régulées » et « Entités publiques ».

--- front/lib-svelte/src/parcours/itemDuParcours.ts
+++ front/lib-svelte/src/parcours/itemDuParcours.ts
@@ -2,14 +2,19 @@ import type { ItemCyber } from '../catalogue/Catalogue.types';
 import type { Guide } from '../catalogue/Guide.types';
 
 export const versItemsCyberOuGuide =
-  (itemsCyber: ItemCyber[], guides: Guide[]) => (idsItem: string[]) =>
-    idsItem.reduce(
-      (accumulateur, id) => {
+  (itemsCyber: ItemCyber[], guides: Guide[]) =>
+  (referencesItem: (string | { id: string; tagsSpecifiques: string[] })[]) =>
+    referencesItem.reduce(
+      (accumulateur, reference) => {
+        const { id, tagsSpecifiques } =
+          typeof reference === 'string'
+            ? { id: reference, tagsSpecifiques: [] }
+            : reference;
         const item =
           itemsCyber.find((itemCyber) => itemCyber.id === id) ??
           guides.find((guide) => guide.id === id);
         if (item) {
-          accumulateur.push({ ...item, tagsSpecifiques:["NIS2"] });
+          accumulateur.push({ ...item, tagsSpecifiques });
         }
         return accumulateur;
       },
