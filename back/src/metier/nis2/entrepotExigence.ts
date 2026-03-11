3dc9c7af6a8e0420a5b6b65d885a41d51e9ad4f9
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2026-03-11 11:34:00 +0100

    [SOIN] Retire les surcharges inutiles

--- back/src/metier/nis2/entrepotExigence.ts
+++ back/src/metier/nis2/entrepotExigence.ts
@@ -1,24 +1,6 @@
-import {
-  Exigence,
-  ExigenceAE,
-  ExigenceISO,
-  ExigenceNIS2,
-  Referentiel,
-} from './exigence';
+import { Exigence, Referentiel } from './exigence';
 
 export interface EntrepotExigence {
-  parReferentiel(
-    referentiel: 'NIS2',
-    referentielCible?: Referentiel
-  ): Promise<ExigenceNIS2[]>;
-  parReferentiel(
-    referentiel: 'ISO',
-    referentielCible?: Referentiel
-  ): Promise<ExigenceISO[]>;
-  parReferentiel(
-    referentiel: 'AE',
-    referentielCible?: Referentiel
-  ): Promise<ExigenceAE[]>;
   parReferentiel(
     referentiel: Referentiel,
     referentielCible?: Referentiel
