b08b6235de39f05960c05437ed6317f0ecfc477d
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-03-11 16:34:36 +0100

    [EXIGENCES NIS2] Permets de sélectionner CyFun23

---
+++ front/lib-svelte/src/nis2/tableaux/CelluleExigenceCyFun23.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import {
+    formateContenuExigence,
+    type ExigenceCyFun23,
+  } from '../exigence.type';
+
+  export let exigence: ExigenceCyFun23;
+</script>
+
+<td>
+  <dsfr-tag label={exigence.reference}></dsfr-tag>
+  <div class="texte-detail-sm">
+    <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+    {@html formateContenuExigence(exigence)}
+  </div>
+</td>
+
+<style lang="scss">
+  td {
+    padding: 0.5rem 1rem;
+    border: 1px solid black;
+
+    .texte-detail-sm {
+      margin: 8px 0 0;
+
+      :global(p) {
+        margin: 0;
+      }
+    }
+  }
+</style>
