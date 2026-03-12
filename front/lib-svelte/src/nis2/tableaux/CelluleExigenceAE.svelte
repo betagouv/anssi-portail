b9496cd4d998bbac3ce33c7077f3a3fe52e469d8
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-03-12 10:37:28 +0100

    [SOIN] Isole le formattage du contenu des exigences
    ...au sein d'un composant svelte dédié

--- front/lib-svelte/src/nis2/tableaux/CelluleExigenceAE.svelte
+++ front/lib-svelte/src/nis2/tableaux/CelluleExigenceAE.svelte
@@ -1,28 +1,18 @@
 <script lang="ts">
-  import { formateContenuExigence, type ExigenceAE } from '../exigence.type';
+  import { type ExigenceAE } from '../exigence.type';
+  import ContenuExigenceFormate from './ContenuExigenceFormate.svelte';
 
   export let exigence: ExigenceAE;
 </script>
 
 <td>
   <dsfr-tag label={exigence.reference}></dsfr-tag>
-  <div class="texte-detail-sm">
-    <!-- eslint-disable-next-line svelte/no-at-html-tags -->
-    {@html formateContenuExigence(exigence)}
-  </div>
+  <ContenuExigenceFormate {exigence} />
 </td>
 
 <style lang="scss">
   td {
     padding: 0.5rem 1rem;
     border: 1px solid black;
-
-    .texte-detail-sm {
-      margin: 8px 0 0;
-
-      :global(p) {
-        margin: 0;
-      }
-    }
   }
 </style>
--- front/lib-svelte/src/nis2/tableaux/CelluleExigenceCyFun23.svelte
+++ front/lib-svelte/src/nis2/tableaux/CelluleExigenceCyFun23.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
-  import {
-    badgesExigence,
-    formateContenuExigence,
-    type ExigenceCyFun23,
-  } from '../exigence.type';
+  import { badgesExigence, type ExigenceCyFun23 } from '../exigence.type';
+  import ContenuExigenceFormate from './ContenuExigenceFormate.svelte';
 
   export let exigence: ExigenceCyFun23;
 </script>
@@ -11,23 +8,12 @@
 <td>
   <dsfr-badges-group badges={badgesExigence(exigence)}></dsfr-badges-group>
   <dsfr-tag label={exigence.reference}></dsfr-tag>
-  <div class="texte-detail-sm">
-    <!-- eslint-disable-next-line svelte/no-at-html-tags -->
-    {@html formateContenuExigence(exigence)}
-  </div>
+  <ContenuExigenceFormate {exigence} />
 </td>
 
 <style lang="scss">
   td {
     padding: 0.5rem 1rem;
     border: 1px solid black;
-
-    .texte-detail-sm {
-      margin: 8px 0 0;
-
-      :global(p) {
-        margin: 0;
-      }
-    }
   }
 </style>
