5beac9b01c78f8c63e8c5cc9a0e6b0bde94eb829
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2026-03-12 17:32:02 +0100

    [NIS2] Précise qu'il n'y a aucun résultat
    ... lorsqu'il n'y a rien à afficher

--- front/lib-svelte/src/nis2/tableaux/configuration.type.ts
+++ front/lib-svelte/src/nis2/tableaux/configuration.type.ts
@@ -8,13 +8,13 @@ import type {
 export type Comparaison =
   | `COMPARAISON_NIS2_${ReferentielSelectionne}`
   | `COMPARAISON_${ReferentielSelectionne}_NIS2`;
+
 export type ConfigurationTableauComparaison = Record<
   Comparaison,
-  | {
-      titreColonneSource: string;
-      titreColonneCible: string;
-      colonneSource: Snippet<[Exigence]>;
-      colonneCible: Snippet<[ExigenceComparee[]]>;
-    }
-  | undefined
+  {
+    titreColonneSource: string;
+    titreColonneCible: string;
+    colonneSource: Snippet<[Exigence]>;
+    colonneCible: Snippet<[ExigenceComparee[]]>;
+  }
 >;
