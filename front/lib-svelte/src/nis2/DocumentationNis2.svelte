d03400117c62527f2f4de8a22efae652d2641f64
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-03-16 16:07:40 +0100

    [NIS2] Retire des contenus de la doc
    ...à savoir la plaquette et la vidéo

--- front/lib-svelte/src/nis2/DocumentationNis2.svelte
+++ front/lib-svelte/src/nis2/DocumentationNis2.svelte
@@ -2,40 +2,6 @@
 
 <dsfr-container id="documentation">
   <div class="contenu">
-    <h2>Plaquette de présentation de la directive NIS 2</h2>
-    <p>
-      <dsfr-link
-        href="https://messervicescyber-ressources.cellar-c2.services.clever-cloud.com/Plaquette_NIS2_2024.pdf"
-        label="Télécharger la plaquette"
-        download
-        blank
-      ></dsfr-link>
-    </p>
-    <img src="/assets/images/plaquette-nis2.avif" alt="Plaquette NIS 2" />
-
-    <h2>Vidéo de présentation de la directive NIS 2</h2>
-    <div class="video">
-      <!-- Les sous-titres sont intégrés dans la vidéo -->
-      <video
-        controls
-        preload="metadata"
-        poster="https://messervicescyber-ressources.cellar-c2.services.clever-cloud.com/focus-nis2-episode2.avif"
-      >
-        <source
-          src="https://messervicescyber-ressources.cellar-c2.services.clever-cloud.com/focus-nis2-episode2.av1.mp4"
-          type="video/mp4; codecs=av01.0.05M.08"
-        />
-        <source
-          src="https://messervicescyber-ressources.cellar-c2.services.clever-cloud.com/focus-nis2-episode2.webm"
-          type="video/webm; codecs='vp9,opus'"
-        />
-        <source
-          src="https://messervicescyber-ressources.cellar-c2.services.clever-cloud.com/focus-nis2-episode2.mp4"
-          type="video/mp4; codecs=avc1.42E01E"
-        />
-      </video>
-    </div>
-
     <div class="faq">
       <hgroup>
         <h2>Foire aux questions</h2>
@@ -81,23 +47,12 @@
         margin-bottom: 48px;
       }
 
-      .video {
-        background: black;
-
-        video {
-          aspect-ratio: 16 / 10;
-          height: 100%;
-          width: 100%;
-        }
-      }
-
       @include a-partir-de(lg) {
         max-width: taille-pour-colonnes(8);
       }
       .faq {
         display: flex;
         flex-direction: column;
-        margin-top: 48px;
         gap: 32px;
 
         h2 {
