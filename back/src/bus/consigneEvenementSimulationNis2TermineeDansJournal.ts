16c145861b94d35e1ea4e6ae1c5d3d12cf1829bd
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2026-03-24 17:26:53 +0100

    Câble dans `POST /api/simulateur-nis2` l'abonné qui consigne dans le journal

---
+++ back/src/bus/consigneEvenementSimulationNis2TermineeDansJournal.ts
@@ -0,0 +1,21 @@
+import { AdaptateurHorloge } from '../infra/adaptateurHorloge';
+import { AdaptateurJournal } from '../infra/adaptateurJournal';
+import { SimulationNis2Terminee } from './evenements/simulationNis2Terminee';
+
+export const consigneEvenementSimulationNis2TermineeDansJournal = ({
+  adaptateurJournal,
+  adaptateurHorloge,
+}: {
+  adaptateurJournal: AdaptateurJournal;
+  adaptateurHorloge: AdaptateurHorloge;
+}) => {
+  return async function (evenement: SimulationNis2Terminee) {
+    await adaptateurJournal.consigneEvenement({
+      donnees: {
+        question1EstTrue: evenement.question1
+      },
+      type: 'SIMULATION_NIS2_TERMINEE',
+      date: adaptateurHorloge.maintenant(),
+    });
+  };
+};
