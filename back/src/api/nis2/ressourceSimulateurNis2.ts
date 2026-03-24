c889516a05c7634c9b63103c7114b310a290f1c1
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2026-03-24 16:48:20 +0100

    Publie un événement sur le bus, depuis `POST /api/simulateur-nis2`

--- back/src/api/nis2/ressourceSimulateurNis2.ts
+++ back/src/api/nis2/ressourceSimulateurNis2.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import { ConfigurationServeur } from '../configurationServeur';
+import { SimulationNis2Terminee } from '../../bus/evenements/simulationNis2Terminee';
 
-export const ressourceSimulateurNis2 = () => {
+export const ressourceSimulateurNis2 = ({ busEvenements }: ConfigurationServeur) => {
   const routeur = Router();
 
   routeur.post('/', async (requete, reponse) => {
+    await busEvenements.publie(new SimulationNis2Terminee());
     reponse.status(201).json(requete.body);
   });
 
---
+++ back/src/bus/evenements/simulationNis2Terminee.ts
@@ -0,0 +1,1 @@
+export class SimulationNis2Terminee {}
