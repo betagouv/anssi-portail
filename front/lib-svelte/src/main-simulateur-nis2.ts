82247e64e1295bb7f64236c877bafe313a7e0b5b
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2026-03-24 15:18:31 +0100

    Ajoute la coquille Svelte du simulateur NIS2

---
+++ front/lib-svelte/src/main-simulateur-nis2.ts
@@ -0,0 +1,4 @@
+import { mount } from 'svelte';
+import SimulateurNis2 from './nis2-simulateur/SimulateurNis2.svelte';
+
+mount(SimulateurNis2, { target: document.getElementById('simulateur-nis2')! });
