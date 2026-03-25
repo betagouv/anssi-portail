080c093b3bafda4b91902b1525961d08933462ce
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2026-03-25 18:14:59 +0100

    Crée <PrecedentSuivant /> pour mutualiser

---
+++ front/lib-svelte/src/nis2-simulateur/etapes/PrecedentSuivant.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import { clic } from '../../directives/actions.svelte';
+
+  interface Props {
+    message: string;
+    onsuivant: () => void;
+    suivantdisabled: boolean;
+  }
+
+  let props: Props = $props();
+</script>
+
+<div class="actions">
+  <p>{props.message}</p>
+  <dsfr-button disabled={props.suivantdisabled} use:clic={props.onsuivant}>
+    Suivant
+  </dsfr-button>
+</div>
+
+<style lang="scss">
+  .actions {
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    justify-content: end;
+    gap: 24px;
+
+    p {
+      font-size: .90rem;
+      color: #929292;
+    }
+  }
+</style>
