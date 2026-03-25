c377678d3d025295e2c99fbe3a47c064b0286d91
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2026-03-25 11:41:13 +0100

    Rajoute 3 étapes au simulateur

--- front/lib-svelte/src/nis2-simulateur/stores/questionnaire.store.ts
+++ front/lib-svelte/src/nis2-simulateur/stores/questionnaire.store.ts
@@ -1,29 +1,48 @@
-import { writable } from "svelte/store";
-import type { ActionQuestionnaire } from "./actions";
-import type { EtatQuestionnaire } from "../../../../../back/src/metier/nis2-simulateur/EtatQuestionnaire";
-import { EtatQuestionnaireVide } from "../../../../../back/src/metier/nis2-simulateur/EtatQuestionnaire";
+import { writable } from 'svelte/store';
+import type { ActionQuestionnaire } from './actions';
+import type { EtatQuestionnaire } from '../../../../../back/src/metier/nis2-simulateur/EtatQuestionnaire';
+import { EtatQuestionnaireVide } from '../../../../../back/src/metier/nis2-simulateur/EtatQuestionnaire';
 
 const etatInitial = () => EtatQuestionnaireVide;
 
 const { set, subscribe, update } = writable<EtatQuestionnaire>(etatInitial());
 
 const reset = () => {
-  set(etatInitial())
+  set(etatInitial());
 };
 
 const repond = (reponse: ActionQuestionnaire) => {
-  if (reponse.type === "VALIDE_ETAPE_PREALABLE") {
+  if (reponse.type === 'VALIDE_ETAPE_PREALABLE') {
     set({
       ...etatInitial(),
-      etapeCourante: "designationOperateurServicesEssentiels",
-    })
-  }
-  else if (reponse.type === "VALIDE_ETAPE_DESIGNATION") {
+      etapeCourante: 'designationOperateurServicesEssentiels',
+    });
+  } else if (reponse.type === 'VALIDE_ETAPE_DESIGNATION') {
     update((etat) => ({
       ...etat,
-      etapeCourante: "appartenanceUnionEuropeenne",
+      etapeCourante: 'appartenanceUnionEuropeenne',
       designationOperateurServicesEssentiels: reponse.designations,
-    }))
+    }));
+  } else if (reponse.type === 'VALIDE_ETAPE_APPARTENANCE_UE') {
+    update((etat) => ({
+      ...etat,
+      etapeCourante: 'typeStructure',
+      appartenancePaysUnionEuropeenne: reponse.appartenances,
+    }));
+  } else if (reponse.type === 'VALIDE_ETAPE_TYPE_STRUCTURE') {
+    update((etat) => ({
+      ...etat,
+      etapeCourante: 'tailleEntitePrivee',
+      typeStructure: reponse.types,
+    }));
+  } else if (reponse.type === 'VALIDE_ETAPE_TAILLE_ENTITE_PRIVEE') {
+    update((etat) => ({
+      ...etat,
+      etapeCourante: 'secteursActivite',
+      trancheNombreEmployes: reponse.nombreEmployes,
+      trancheChiffreAffaire: reponse.chiffreAffaire,
+      trancheBilanFinancier: reponse.bilanFinancier,
+    }));
   }
 };
 
@@ -31,4 +50,4 @@ export const questionnaireStore = {
   subscribe,
   repond,
   reset,
-}
+};
