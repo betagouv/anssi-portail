51f610fbf607bb659007ab3f25303a334d910f34
Xavier Maso <xavier.maso@beta.gouv.fr>
2026-03-25 10:40:22 +0100

    Implémente le changement d'étape dans l'interface

--- front/lib-svelte/src/nis2-simulateur/SimulateurNis2.svelte
+++ front/lib-svelte/src/nis2-simulateur/SimulateurNis2.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
   import * as api from './simulateurNi2.api';
+  import { questionnaireStore } from './stores/questionnaire.store';
   import { clic } from '../directives/actions.svelte';
 </script>
 
 <dsfr-container>
+  {#if ($questionnaireStore.etapeCourante === "prealable")}
+    <h1 use:clic={() => {
+      questionnaireStore.repond({type: "VALIDE_ETAPE_PREALABLE"});
+    }}>Étape préalable</h1>
+  {:else if ($questionnaireStore.etapeCourante === "designationOperateurServicesEssentiels")}
+    <h1>Désignation</h1>
+  {/if}
+
   <h1>SIMULATEUR NIS 2</h1>
 
   <dsfr-button
