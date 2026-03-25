b00993e56d581aea5122521730be56c0a20891f9
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2026-03-25 16:47:21 +0100

    Intègre le front de l'étape préalable

---
+++ front/lib-svelte/src/nis2-simulateur/etapes/Etape.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  let { children } = $props();
+</script>
+
+<div class="etape">
+  {@render children()}
+</div>
+
+<style lang="scss">
+  @use '../../../../assets/styles/responsive' as *;
+  @use '../../../../assets/styles/grille' as *;
+
+  .etape {
+    box-sizing: border-box;
+    background: #f6f6f6;
+    padding: 40px taille-pour-colonnes(1);
+    margin: 40px auto;
+
+    @include a-partir-de(lg) {
+      max-width: taille-pour-colonnes(8);
+    }
+  }
+</style>
