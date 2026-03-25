2c1755a73ddf28de98aa00bf99cf004d9b53f032
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2026-03-25 17:59:28 +0100

    Intègre l'étape d'appartenance UE

--- front/lib-svelte/src/nis2-simulateur/SimulateurNis2.svelte
+++ front/lib-svelte/src/nis2-simulateur/SimulateurNis2.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
   import { questionnaireStore } from './stores/questionnaire.store';
   import {
+    valideEtapeAppartenanceUE,
     valideEtapeDesignation,
     valideEtapePrealable,
   } from './stores/actions';
   import EtapePrealable from './etapes/EtapePrealable.svelte';
   import Hero from '../ui/Hero.svelte';
   import EtapeDesignation from './etapes/EtapeDesignation.svelte';
+  import EtapeAppartenanceUE from './etapes/EtapeAppartenanceUE.svelte';
 </script>
 
 <Hero
@@ -25,6 +27,11 @@
     <EtapeDesignation
       onsuivant={(reponse) =>
         questionnaireStore.repond(valideEtapeDesignation([reponse]))}
+    />
+  {:else if $questionnaireStore.etapeCourante === 'appartenanceUnionEuropeenne'}
+    <EtapeAppartenanceUE
+      onsuivant={(reponse) =>
+        questionnaireStore.repond(valideEtapeAppartenanceUE([reponse]))}
     />
   {/if}
 </dsfr-container>
