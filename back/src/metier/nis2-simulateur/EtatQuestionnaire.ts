416b28c9a1bb8c5b1d8dae5a39b4b92128d86784
Xavier Maso <xavier.maso@beta.gouv.fr>
2026-03-25 11:20:33 +0100

    Implémente la réponse à la question désignation
    Par importation massive des fichiers existant côté projet NIS2...
    Parce qu'on sait qu'on en aura besoin dans l'implémentation de la suite
    du simulateur.

---
+++ back/src/metier/nis2-simulateur/EtatQuestionnaire.ts
@@ -0,0 +1,49 @@
+import { TypeEtape } from "./InformationsEtape";
+import {
+  AppartenancePaysUnionEuropeenne,
+  DesignationOperateurServicesEssentiels,
+  TrancheBilanFinancier,
+  TrancheChiffreAffaire,
+  TrancheNombreEmployes,
+  TypeEntitePublique,
+  TypeStructure,
+} from "./ChampsSimulateur.definitions";
+import { SecteurActivite } from "./SecteurActivite.definitions";
+import { SousSecteurActivite } from "./SousSecteurActivite.definitions";
+import { Activite } from "./Activite.definitions";
+
+export type EtatQuestionnaire = {
+  etapeCourante: TypeEtape;
+  designationOperateurServicesEssentiels: DesignationOperateurServicesEssentiels[];
+  appartenancePaysUnionEuropeenne: AppartenancePaysUnionEuropeenne[];
+  typeStructure: TypeStructure[];
+  trancheNombreEmployes: TrancheNombreEmployes[];
+  trancheChiffreAffaire: TrancheChiffreAffaire[];
+  trancheBilanFinancier: TrancheBilanFinancier[];
+  secteurActivite: SecteurActivite[];
+  sousSecteurActivite: SousSecteurActivite[];
+  activites: Activite[];
+  typeEntitePublique: TypeEntitePublique[];
+  localisationFournitureServicesNumeriques: AppartenancePaysUnionEuropeenne[];
+  paysDecisionsCyber: AppartenancePaysUnionEuropeenne[];
+  paysOperationsCyber: AppartenancePaysUnionEuropeenne[];
+  paysPlusGrandNombreSalaries: AppartenancePaysUnionEuropeenne[];
+};
+
+export const EtatQuestionnaireVide: EtatQuestionnaire = {
+  etapeCourante: "prealable",
+  designationOperateurServicesEssentiels: [],
+  appartenancePaysUnionEuropeenne: [],
+  typeStructure: [],
+  trancheNombreEmployes: [],
+  trancheChiffreAffaire: [],
+  trancheBilanFinancier: [],
+  secteurActivite: [],
+  sousSecteurActivite: [],
+  activites: [],
+  typeEntitePublique: [],
+  localisationFournitureServicesNumeriques: [],
+  paysDecisionsCyber: [],
+  paysOperationsCyber: [],
+  paysPlusGrandNombreSalaries: [],
+};
--- front/lib-svelte/src/nis2-simulateur/stores/questionnaire.store.ts
+++ front/lib-svelte/src/nis2-simulateur/stores/questionnaire.store.ts
@@ -1,22 +1,34 @@
 import { writable } from "svelte/store";
+import type { ActionQuestionnaire } from "./actions";
+import type { EtatQuestionnaire } from "../../../../../back/src/metier/nis2-simulateur/EtatQuestionnaire";
+import { EtatQuestionnaireVide } from "../../../../../back/src/metier/nis2-simulateur/EtatQuestionnaire";
 
-type EtatQuestionnaire = {
-  etapeCourante: "prealable" | "designationOperateurServicesEssentiels",
+const etatInitial = () => EtatQuestionnaireVide;
+
+const { set, subscribe, update } = writable<EtatQuestionnaire>(etatInitial());
+
+const reset = () => {
+  set(etatInitial())
 };
 
-const { set, subscribe } = writable<EtatQuestionnaire>({
-  etapeCourante: "prealable",
-});
-
-const repond = (reponse: { type: string, }) => {
+const repond = (reponse: ActionQuestionnaire) => {
   if (reponse.type === "VALIDE_ETAPE_PREALABLE") {
     set({
+      ...etatInitial(),
       etapeCourante: "designationOperateurServicesEssentiels",
     })
+  }
+  else if (reponse.type === "VALIDE_ETAPE_DESIGNATION") {
+    update((etat) => ({
+      ...etat,
+      etapeCourante: "appartenanceUnionEuropeenne",
+      designationOperateurServicesEssentiels: reponse.designations,
+    }))
   }
 };
 
 export const questionnaireStore = {
   subscribe,
   repond,
+  reset,
 }
