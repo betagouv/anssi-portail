fea76c383647381f8122b7260788e47e920d2308
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2026-03-25 14:19:53 +0100

    Termine les étapes du simulateur

--- front/lib-svelte/src/nis2-simulateur/stores/SecteurActivite.predicats.ts
+++ front/lib-svelte/src/nis2-simulateur/stores/SecteurActivite.predicats.ts
@@ -3,6 +3,8 @@ import type {
   SecteurComposite,
 } from '../../../../../back/src/metier/nis2-simulateur/SecteurActivite.definitions';
 import { ValeursSecteursComposites } from '../../../../../back/src/metier/nis2-simulateur/SecteurActivite.valeurs';
+import type { Activite } from '../../../../../back/src/metier/nis2-simulateur/Activite.definitions';
+import { contientUnParmi } from '../../../../../back/src/metier/nis2-simulateur/commun.predicats';
 
 export const estUnSecteurAvecDesSousSecteurs = (
   secteur: string
@@ -11,3 +13,32 @@ export const estUnSecteurAvecDesSousSecteurs = (
 
 export const estSecteurAutre = (secteur: SecteurActivite) =>
   secteur.startsWith('autre');
+
+export const doitPasserParLocalisationFournitureServicesNumeriques = (
+  activites: Activite[],
+) =>
+  contientUnParmi(
+    "fournisseurReseauxCommunicationElectroniquesPublics",
+    "fournisseurServiceCommunicationElectroniquesPublics",
+  )(activites);
+
+export const doitPasserParLocalisationEtablissementPrincipal = (
+  secteurs: SecteurActivite[],
+  activites: Activite[],
+) => {
+  const ticOuFournisseurNumerique = contientUnParmi(
+    "gestionServicesTic",
+    "fournisseursNumeriques",
+  )(secteurs);
+
+  const activiteFournisseurServiceNumerique = contientUnParmi(
+    "registresNomsDomainesPremierNiveau",
+    "fournisseurServicesDNS",
+    "fournisseurServicesInformatiqueNuage",
+    "fournisseurServiceCentresDonnees",
+    "fournisseurReseauxDiffusionContenu",
+    "fournisseurServicesEnregristrementNomDomaine",
+  )(activites);
+
+  return ticOuFournisseurNumerique || activiteFournisseurServiceNumerique;
+};
