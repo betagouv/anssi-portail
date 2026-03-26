9651d94338f1d4638e05670ea6a708d52e1da750
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-03-26 14:58:59 +0100

    [GESTION GUIDES] Saisis le libelle du lien du document

--- back/tests/api/guides/ressourceDocumentsGuide.spec.ts
+++ back/tests/api/guides/ressourceDocumentsGuide.spec.ts
@@ -16,6 +16,7 @@ describe('La ressource de gestion des documents des guides', () => {
     it('Répond 201', async () => {
       const reponse = await request(serveur)
         .post('/api/guides/zero-trust/documents')
+        .field('libelleDuLien', 'Cliquez pour télécharger le document')
         .attach('document-guide', Buffer.from('une-texte'), 'document.pdf');
 
       assert.equal(reponse.status, 201);
@@ -23,7 +24,17 @@ describe('La ressource de gestion des documents des guides', () => {
 
     describe('avec un corps de requête', () => {
       it('rejette les requêtes sans fichier', async () => {
-        const reponse = await request(serveur).post('/api/guides/zero-trust/documents');
+        const reponse = await request(serveur)
+          .post('/api/guides/zero-trust/documents')
+          .field('libelleDuLien', 'Cliquez pour télécharger le document');
+
+        assert.equal(reponse.status, 400);
+      });
+
+      it('rejette les requêtes sans libelle de lien', async () => {
+        const reponse = await request(serveur)
+          .post('/api/guides/zero-trust/documents')
+          .attach('document-guide', Buffer.from('une-texte'), 'document.pdf');
 
         assert.equal(reponse.status, 400);
       });
