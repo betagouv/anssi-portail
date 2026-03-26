2ed693bd2d1ae90b502e620b0756a152f68b50f6
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-03-26 15:41:50 +0100

    [GESTION GUIDES] Propose la recherche vague d'un id de guide

---
+++ front/lib-svelte/src/gestion-guides/SelectionIdentifiantGuide.svelte
@@ -0,0 +1,120 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import {
+    chargeGuidesDansLeStore,
+    guidesStore,
+  } from '../catalogue/stores/guides/guides.store';
+  import { clic } from '../directives/actions.svelte';
+  import ChampTexte from '../ui/ChampTexte.svelte';
+
+  type Props = {
+    valeur: string | undefined;
+  };
+
+  let { valeur = $bindable() }: Props = $props();
+
+  let saisie: string = $state('');
+  let listeIdentifiants: string[] = $state([]);
+  let minuteur: ReturnType<typeof setTimeout>;
+  let suggestions: string[] = $state([]);
+  let suggestionsVisibles = $state(false);
+
+  const avecTemporisation = (fonction: () => Promise<void>) => {
+    clearTimeout(minuteur);
+    minuteur = setTimeout(async () => {
+      await fonction();
+    }, 500);
+  };
+
+  const rechercheVague = function (source: string, terme: string) {
+    const tasDeFoin = source.toLowerCase();
+    let n = -1,
+      lettre;
+    const lettresRecherchees = terme.toLowerCase().replaceAll(/\s+/g, '-');
+    for (let i = 0; (lettre = lettresRecherchees[i]); i++) {
+      if (!~(n = tasDeFoin.indexOf(lettre, n + 1))) return false;
+    }
+    return true;
+  };
+
+  const rechercheSuggestions = async () => {
+    if (saisie.length === 0) {
+      valeur = undefined;
+    }
+    if (saisie.length < 2) {
+      suggestionsVisibles = false;
+      suggestions = [];
+      return;
+    }
+
+    suggestions = listeIdentifiants.filter((id) => rechercheVague(id, saisie));
+    suggestionsVisibles = suggestions.length > 0;
+  };
+
+  const choisisIdentifiant = (identifiantGuide: string) => {
+    valeur = identifiantGuide;
+    saisie = identifiantGuide;
+    suggestionsVisibles = false;
+  };
+
+  onMount(async () => {
+    await chargeGuidesDansLeStore();
+    listeIdentifiants = $guidesStore
+      .map((guide) => guide.id.replace('/guides/', ''))
+      .sort();
+  });
+</script>
+
+<div class="conteneur-selection-organisation">
+  <ChampTexte
+    id="selection-id-guide"
+    nom="organisation"
+    bind:valeur={saisie}
+    on:input={() => avecTemporisation(rechercheSuggestions)}
+    aideSaisie="ex : guide-pour-une-formation-sur-la-cybersecurite-des-systemes-industriels"
+    autocomplete="off"
+  />
+  <div class="liste-suggestions" class:visible={suggestionsVisibles}>
+    {#each suggestions as suggestion (suggestion)}
+      <div
+        class="option"
+        role="button"
+        tabindex="0"
+        use:clic={() => {
+          choisisIdentifiant(suggestion);
+        }}
+      >
+        <div>{suggestion}</div>
+      </div>
+    {/each}
+  </div>
+</div>
+
+<style lang="scss">
+  .conteneur-selection-organisation {
+    position: relative;
+  }
+
+  .liste-suggestions {
+    display: none;
+    position: absolute;
+    background: white;
+    width: calc(100% - 34px);
+    /* 34px = paddings gauche et droite + bords = 2 x 16 + 2 x 1 */
+    z-index: 1;
+    border-bottom-left-radius: 5px;
+    border-bottom-right-radius: 5px;
+    transform: translateY(-5px);
+    padding: 0 16px;
+  }
+
+  .visible {
+    display: block;
+    border: 1px solid var(--jaune-msc);
+  }
+
+  .option {
+    padding: 4px 0;
+    cursor: pointer;
+  }
+</style>
