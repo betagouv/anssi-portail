b8c9e3499dddca66cafb9193f85c3e09bcefeefe
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2026-03-26 16:34:29 +0100

    Passe les descriptions en HTML
    … plutôt qu'en markdown qui nécessiterait d'ajouter des paquets npm pour être rendu correctement.

--- front/lib-svelte/src/nis2-simulateur/etapes/DescriptionActivite.svelte
+++ front/lib-svelte/src/nis2-simulateur/etapes/DescriptionActivite.svelte
@@ -2,6 +2,10 @@
   import type { Activite } from '../../../../../back/src/metier/nis2-simulateur/Activite.definitions';
   import { listeDescriptionsActivites } from '../../../../../back/src/metier/nis2-simulateur/ListeDescriptionsActivites';
 
+  /* eslint-disable svelte/no-at-html-tags */
+  // Le @html est utilisé sur des valeurs venant de `listeDescriptionsActivites`.
+  // Pas sur des entrées d'utilisateurs.
+
   interface Props {
     activite: Activite;
   }
@@ -9,10 +13,24 @@
   let props: Props = $props();
 
   const descriptions = $derived(listeDescriptionsActivites[props.activite]);
+
 </script>
 
 {#each descriptions as { titre, description }, i (i)}
-  <dsfr-highlight text={description} size="sm">
+  <dsfr-highlight text="abc" size="sm">
     <b slot="title">{titre}</b>
+    <div class="description-msc" slot="text">
+      {@html description}</div>
   </dsfr-highlight>
 {/each}
+
+
+<style lang="scss">
+  .description-msc {
+    margin-bottom: 12px;
+  }
+
+  dsfr-highlight:last-child {
+    margin-bottom: 28px;
+  }
+</style>
