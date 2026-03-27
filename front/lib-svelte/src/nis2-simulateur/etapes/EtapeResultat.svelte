30e4b283afe5a30c108429056237139ea14e160c
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2026-03-27 09:29:08 +0100

    Intègre une étape Résultat en coquille vide

---
+++ front/lib-svelte/src/nis2-simulateur/etapes/EtapeResultat.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import type { EtatQuestionnaire } from '../../../../../back/src/metier/nis2-simulateur/EtatQuestionnaire';
+
+  interface Props {
+    reponses: EtatQuestionnaire;
+  }
+
+  let props: Props = $props();
+</script>
+
+<h1>RESULTAT !</h1>
+
+<pre>{JSON.stringify(props.reponses, null, 2)}</pre>
+
+<style lang="scss">
+</style>
