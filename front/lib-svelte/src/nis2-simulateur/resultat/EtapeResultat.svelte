3ad49946419f18f6d97d017117c5fba125519291
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2026-03-27 16:30:23 +0100

    Intègre <PointsAttention /> dans l'étape Résultat

--- front/lib-svelte/src/nis2-simulateur/resultat/EtapeResultat.svelte
+++ front/lib-svelte/src/nis2-simulateur/resultat/EtapeResultat.svelte
@@ -4,6 +4,7 @@
   import TamponResultat from './TamponResultat.svelte';
   import { evalueEligibilite } from '../../../../../back/src/metier/nis2-simulateur/questionnaire/evalueEligibilite';
   import SpecificationsCompletes from '../../../../../back/src/metier/nis2-simulateur/questionnaire/specifications-completes.csv?raw';
+  import PointsAttention from './PointsAttention.svelte';
 
   interface Props {
     reponses: EtatQuestionnaire;
@@ -22,6 +23,11 @@
 
 <div class="resultat">
   <TamponResultat {resultat} />
+
+  <PointsAttention
+    resumes={resultat.pointsAttention.resumes}
+    precisions={resultat.pointsAttention.precisions}
+  />
 </div>
 
 <style lang="scss">
@@ -32,6 +38,9 @@
     box-sizing: border-box;
     padding: 40px taille-pour-colonnes(1);
     margin: 40px auto;
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
 
     @include a-partir-de(lg) {
       max-width: taille-pour-colonnes(8);
