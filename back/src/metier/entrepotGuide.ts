9051962d4c04778a48a40bb898dd133f84accce6
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2026-03-30 15:04:07 +0200

    [SOIN] Corrige erreur de typo

--- back/src/metier/entrepotGuide.ts
+++ back/src/metier/entrepotGuide.ts
@@ -1,7 +1,7 @@
 import { Guide } from './guide';
 
 export interface EntrepotGuide {
-  parCollections(colelctions: string[]): Promise<Guide[]>;
+  parCollections(collections: string[]): Promise<Guide[]>;
   parId(id: string): Promise<Guide | undefined>;
   tous: () => Promise<Guide[]>;
 }
