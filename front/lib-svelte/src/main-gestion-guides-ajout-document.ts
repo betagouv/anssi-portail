6dc570a0d53b3a4d9d5384b091f88a26107034c2
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2026-03-31 10:13:11 +0200

    [SOIN] Adapte l'inclusion du composant Svelte

--- front/lib-svelte/src/main-gestion-guides-ajout-document.ts
+++ front/lib-svelte/src/main-gestion-guides-ajout-document.ts
@@ -2,5 +2,5 @@ import { mount } from 'svelte';
 import AjoutDocument from './gestion-guides/AjoutDocument.svelte';
 
 mount(AjoutDocument, {
-  target: document.getElementById('ajout-document')!,
+  target: document.getElementById('gestion-guides-ajout-document')!,
 });
