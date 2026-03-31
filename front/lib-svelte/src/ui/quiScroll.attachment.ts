bb39d6e1fc4e4845240ae94e0ac19bab7348d369
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2026-03-31 17:04:30 +0200

    Évite `requestAnimationFrame`
    … qui semble un peu trop "jeu vidéo".

--- front/lib-svelte/src/ui/quiScroll.attachment.ts
+++ front/lib-svelte/src/ui/quiScroll.attachment.ts
@@ -1,11 +1,12 @@
 import type { Attachment } from 'svelte/attachments';
+import { tick } from 'svelte';
 
 export const quiScroll: Attachment = (element) => {
-  requestAnimationFrame(() => {
+  tick().then(() =>
     element.scrollIntoView({
       behavior: 'smooth',
       block: 'start',
       inline: 'nearest',
-    });
-  });
+    })
+  );
 };
