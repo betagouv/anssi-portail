10e8df76d1e77f2a5d8c3dee8f0e2f4cd65c8c95
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-04-01 16:00:09 +0200

    Corrige le typage Sentry

--- front/lib-svelte/src/main-sentry.ts
+++ front/lib-svelte/src/main-sentry.ts
@@ -1,11 +1,18 @@
 import * as Sentry from '@sentry/browser';
 
-const dsn = document.getElementById('script-sentry').dataset.dsn;
-const environment = document.getElementById('script-sentry').dataset.environnement;
+const dsn = document?.getElementById('script-sentry')?.dataset.dsn;
+const environment =
+  document?.getElementById('script-sentry')?.dataset.environnement;
 
 // Voir l'issue https://github.com/axios/axios/issues/6209#issuecomment-2299747509
-const avantEnvoiSentry = (evenement, detail) => {
-  if (detail?.originalException?.code === 'ECONNABORTED') {
+const avantEnvoiSentry = (
+  evenement: Sentry.Event,
+  detail: Sentry.EventHint
+) => {
+  const originalException = detail?.originalException as
+    | { code: string }
+    | undefined;
+  if (originalException?.code === 'ECONNABORTED') {
     return null;
   }
   return evenement;
