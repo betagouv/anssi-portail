40f433570a1cfb16743688372a8a62ccea187fba
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2026-04-01 16:31:48 +0200

    [INFOLETTRE] Valide la requête

--- back/src/api/ressourceAbonnementInfolettre.ts
+++ back/src/api/ressourceAbonnementInfolettre.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { ConfigurationServeur } from './configurationServeur';
+import { schemaAbonnementInfolettre } from './ressourceAbonnementInfolettre.schema';
+import { valideCorpsRequete } from './zod';
 
 export const ressourceAbonnementInfolettre = ({ adaptateurEmail }: ConfigurationServeur) => {
   const routeur = Router();
-  routeur.post('/', async (requete, reponse) => {
+  routeur.post('/', valideCorpsRequete(schemaAbonnementInfolettre), async (requete, reponse) => {
     await adaptateurEmail.inscrisAInfolettre(requete.body.email);
     reponse.sendStatus(201);
   });
