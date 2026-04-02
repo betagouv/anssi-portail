2864c4bdc8981883bde1ffacb9d85a35f1fb3a95
Xavier Maso <xavier.maso@beta.gouv.fr>
2026-04-02 11:46:44 +0200

    [INFOLETTRE] Valide le formulaire
    ... à la main, car `formulaire.estValide` ne tient pas bien compte des
    valeurs manquantes dans les éléments contenu dans un shadow DOM.

--- front/lib-svelte/src/abonnement-infolettre/AbonnementInfolettre.svelte
+++ front/lib-svelte/src/abonnement-infolettre/AbonnementInfolettre.svelte
@@ -1,42 +1,69 @@
 <script lang="ts">
+  import { clic } from '../directives/actions.svelte';
   import FilAriane from '../ui/FilAriane.svelte';
+  import Formulaire from '../ui/Formulaire.svelte';
+
+  let mail: string | null = null;
+  let infolettreAcceptee = false;
+  let erreurValidation = false;
+
+  const soumetsFormulaire = () => {
+    if (!infolettreAcceptee || !mail) {
+      erreurValidation = true;
+      return;
+    }
+    console.log('envoi du formulaire');
+  };
 </script>
 
 <dsfr-container>
   <FilAriane feuille="Newsletter MesServicesCyber" />
 
-  <form>
-    <hgroup>
-      <h2>
-        <lab-anssi-icone nom="mail-line" taille="lg"
-        ></lab-anssi-icone>Abonnement à la newsletter
-      </h2>
-      <p class="texte-standard-md">
-        Directive NIS 2, guides de l’ANSSI : recevez gratuitement toute
-        l’actualité de MesServicesCyber.
-      </p>
-    </hgroup>
+  <Formulaire>
+    <div class="formulaire">
+      <hgroup>
+        <h2>
+          <lab-anssi-icone nom="mail-line" taille="lg"
+          ></lab-anssi-icone>Abonnement à la newsletter
+        </h2>
+        <p class="texte-standard-md">
+          Directive NIS 2, guides de l’ANSSI : recevez gratuitement toute
+          l’actualité de MesServicesCyber.
+        </p>
+      </hgroup>
 
-    <dsfr-input
-      id="email-infolettre"
-      label="Adresse email"
-      errorMessage="Cette information est obligatoire."
-      hint="Exemple : nom@exemple.fr"
-      required
-    ></dsfr-input>
+      <dsfr-input
+        id="email-infolettre"
+        label="Adresse email"
+        errorMessage="Cette information est obligatoire."
+        hint="Exemple : nom@exemple.fr"
+        value={mail}
+        onvaluechanged={(e: CustomEvent) => (mail = e.detail)}
+        status={mail || !erreurValidation ? 'default' : 'error'}
+        required
+      ></dsfr-input>
 
-    <dsfr-checkbox
-      id="acceptation-infolettre"
-      name="acceptation-infolettre"
-      errorMessage="Cette information est obligatoire."
-      hint="La politique de confidentialité est disponible ici."
-      required
-    >
-      <span>J'accepte de recevoir la newsletter de MesServicesCyber.</span>
-    </dsfr-checkbox>
+      <dsfr-checkbox
+        id="acceptation-infolettre"
+        name="acceptation-infolettre"
+        errorMessage="Cette information est obligatoire."
+        hint="La politique de confidentialité est disponible ici."
+        value={infolettreAcceptee}
+        onvaluechanged={(e: CustomEvent) => (infolettreAcceptee = e.detail)}
+        status={infolettreAcceptee || !erreurValidation ? 'default' : 'error'}
+        required
+      >
+        <span>J'accepte de recevoir la newsletter de MesServicesCyber.</span>
+      </dsfr-checkbox>
 
-    <dsfr-button label="S'abonner à la newsletter" kind="primary"></dsfr-button>
-  </form>
+      <dsfr-button
+        label="S'abonner à la newsletter"
+        kind="primary"
+        type="submit"
+        use:clic={soumetsFormulaire}
+      ></dsfr-button>
+    </div>
+  </Formulaire>
 </dsfr-container>
 
 <style lang="scss">
@@ -51,7 +78,7 @@
     padding: 1rem 0 3rem;
   }
 
-  form {
+  .formulaire {
     background-color: var(--background-alt-blue-france);
     display: flex;
     flex-direction: column;
