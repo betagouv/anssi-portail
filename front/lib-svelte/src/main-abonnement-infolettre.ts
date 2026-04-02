abffe1c69d12d9c6f2aeb52355ffbbda547f8202
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2026-04-02 10:13:39 +0200

    [INFOLETTRE] Ajoute une page d’abonnement

---
+++ front/lib-svelte/src/main-abonnement-infolettre.ts
@@ -0,0 +1,6 @@
+import { mount } from 'svelte';
+import AbonnementInfolettre from './abonnement-infolettre/AbonnementInfolettre.svelte';
+
+mount(AbonnementInfolettre, {
+  target: document.getElementById('abonnement-infolettre')!,
+});
