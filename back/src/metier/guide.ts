bcfc17e30ac9139f4f47042c4b998058ee4fcb1a
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-04-10 15:01:46 +0200

    [SOIN][GUIDES] Inverse le contrôle de la sauvegarde ... en déplaçnt la logique de persistance dans la classe `Guide` plutôt que dans la ressource HTTP

--- back/src/metier/guide.ts
+++ back/src/metier/guide.ts
@@ -1,5 +1,6 @@
 import { BesoinCyber } from './besoinCyber';
 import { EntrepotGuide } from './entrepotGuide';
+import { EntrepotGuideTravail } from './entrepotGuideTravail';
 
 export class Guide {
   id: string;
@@ -54,6 +55,10 @@ export class Guide {
     if (!this.possedeLeDocument(nomFichier)) return;
     this.listeDocuments = this.listeDocuments.filter((document) => document.nomFichier !== nomFichier);
     this.nomsAnciensDocuments.push(nomFichier);
+  };
+
+  sauvegarde = async (entrepotGuideTravail: EntrepotGuideTravail): Promise<void> => {
+    await entrepotGuideTravail.sauvegardeDocuments(this.id, this.listeDocuments, this.nomsAnciensDocuments);
   };
 }
 
