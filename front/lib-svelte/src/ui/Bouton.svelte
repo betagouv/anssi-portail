50f89dbf87c7bf291fa1ea1225352e084aad3b75
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-04-21 09:47:23 +0200

    [DSFR] Change le composant bouton
    ...pour utiliser le dsfr-button

--- front/lib-svelte/src/ui/Bouton.svelte
+++ front/lib-svelte/src/ui/Bouton.svelte
@@ -5,52 +5,22 @@
   export let actif: boolean = true;
   export let enCoursEnvoi: boolean = false;
   export let boutonSoumission: boolean = true;
-  export let classe: string = '';
   export let icone: '' | 'partager' = '';
+
+  const kind = type === 'primaire' ? 'primary' : 'secondary';
+  const boutonType = boutonSoumission ? 'submit' : 'button';
+  const hasIcon = !!icone;
+  const iconName = icone === 'partager' ? 'share-line' : '';
 </script>
 
-<button
-  class={`bouton ${type} ${classe} ${icone} ${taille ? `bouton-${taille}` : ''}`}
-  class:avecIcone={!!icone}
-  type={boutonSoumission ? 'submit' : 'button'}
-  on:click
+<dsfr-button
+  label={titre}
+  {kind}
+  size={taille}
+  type={boutonType}
   disabled={!actif || enCoursEnvoi}
-  class:en-cours-chargement={enCoursEnvoi}
->
-  {titre}
-</button>
-
-<style>
-  .bouton {
-    align-items: center;
-    gap: 8px;
-  }
-
-  .bouton-md {
-    padding: 8px 16px;
-    justify-content: center;
-    align-items: center;
-    font-size: 1rem;
-    font-style: normal;
-    font-weight: 500;
-    line-height: 1.5rem;
-  }
-
-  .avecIcone:before {
-    content: '';
-    display: inline-block;
-    background-repeat: no-repeat;
-    background-size: contain;
-    width: 20px;
-    height: 20px;
-    line-height: 20px;
-  }
-
-  .avecIcone {
-    text-wrap: nowrap;
-  }
-
-  .partager:before {
-    background-image: url('/assets/images/icone-partager.svg');
-  }
-</style>
+  {hasIcon}
+  icon={iconName}
+  iconPlace="left"
+  on:click
+></dsfr-button>
