72db72571d3626b9f98bfda5a7acb316bc8f27da
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2026-04-23 15:04:08 +0200

    [PROPHYLAXIE] Décode les entrées des ressources back
    ... en prévision de la suppression de l'aseptisation des entrées

--- back/src/api/favoris/ressourceFavoris.ts
+++ back/src/api/favoris/ressourceFavoris.ts
@@ -1,7 +1,8 @@
-import { ConfigurationServeur } from '../configurationServeur';
 import { Request, Response, Router } from 'express';
 import { check } from 'express-validator';
+import { decode } from 'html-entities';
 import { MiseAJourFavorisUtilisateur } from '../../bus/miseAJourFavorisUtilisateur';
+import { ConfigurationServeur } from '../configurationServeur';
 import { filetRouteAsynchrone } from '../middleware';
 
 const ressourceFavoris = ({
@@ -21,8 +22,7 @@ const ressourceFavoris = ({
     [check('idItemCyber').not().isEmpty().withMessage("L'idItemCyber est invalide")],
     middleware.valide(),
     filetRouteAsynchrone(async (requete: Request, reponse: Response) => {
-      let idItemCyber = requete.body.idItemCyber;
-      idItemCyber = idItemCyber.replaceAll('&#x2F;', '/');
+      const idItemCyber = decode(requete.body.idItemCyber);
       const utilisateur = requete.utilisateur;
       await entrepotFavori.ajoute({
         idItemCyber,
