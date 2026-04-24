802b4afb79c2f27b00e31da34a941879794ae4c4
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-04-24 17:11:28 +0200

    [DSFR][CARTE] Adapte le catalogue

--- front/lib-svelte/src/catalogue/CarteItem.svelte
+++ front/lib-svelte/src/catalogue/CarteItem.svelte
@@ -1,36 +1,96 @@
 <script lang="ts">
+  import BoutonFavori from '../favoris/BoutonFavori.svelte';
   import type { ItemCyber } from './Catalogue.types';
   import type { Guide } from './Guide.types';
-  import ContenuCarteItem from './ContenuCarteItem.svelte';
+  import HeaderBadge from './HeaderBadge.svelte';
 
   export let item: ItemCyber | Guide;
   export let avecBoutonFavori: boolean = false;
-  export let modeLargeur: 'automatique' | 'fixe' = 'fixe';
+
+  const LONGUEUR_MAX_DESCRIPTION = 54;
+
+  const estGuide = item.type === 'Guide';
+
+  // Calcul du lien
+  const lien = estGuide ? item.id : (item.lienInterne ?? (item as ItemCyber).lienExterne);
+  const nouvelOnglet = !estGuide && !item.lienInterne;
+  const sansLien = !lien;
+
+  // Contenu de la carte
+  const titre = estGuide ? item.nom : item.description;
+  const titreCoupe = titre.length > LONGUEUR_MAX_DESCRIPTION ? titre.slice(0, LONGUEUR_MAX_DESCRIPTION) + '…' : titre;
+
+  const image = estGuide
+    ? (item as Guide).illustration.petite
+    : `/assets/images/illustrations-services/${(item as ItemCyber).illustration}`;
+
+  const altImage = estGuide ? 'Illustration du guide' : 'Illustration du service';
+  const detailHaut = estGuide ? (item as Guide).thematique : item.nom;
+  const detailBas = estGuide ? (item as Guide).dateMiseAJourFormatee : undefined;
+
+  // Props analytics
+  const dataSource = estGuide ? 'Guide' : 'Catalogue';
+  const dataCible = item.nom;
+
+  // Badge de type pour ItemCyber
+  const libelleBadge = () => {
+    if (estGuide) return undefined;
+    const itemCyber = item as ItemCyber;
+    switch (itemCyber.typologie) {
+      case 'service':
+        return 'Service';
+      case 'outil':
+        return 'Outil';
+      case 'contenu':
+        return 'Contenu';
+    }
+    return undefined;
+  };
 </script>
 
-{#if item.type === 'Guide'}
-  <a class="carte guide {modeLargeur}" href={item.id} data-source="Guide" data-cible={item.nom}>
-    <ContenuCarteItem item={{ ...item, description: item.nom }} {avecBoutonFavori} />
-  </a>
-{:else if item.lienInterne || item.lienExterne}
-  <a
-    class="carte {item.typologie} {modeLargeur}"
-    target={item.lienInterne ? '' : '_blank'}
-    href={item.lienInterne ?? item.lienExterne}
-    class:lien-externe-produit={!item.lienInterne}
-    data-source="Catalogue"
-    data-cible={item.nom}
+<div>
+  <dsfr-card
+    title={titreCoupe}
+    hasDetailStart={!!detailHaut}
+    detailStart={detailHaut}
+    hasDetailEnd={!!detailBas || item.sources || item.tagsSpecifiques}
+    detailEnd={detailBas}
+    src={image}
+    alt={altImage}
+    href={lien}
+    blank={nouvelOnglet}
+    noLink={sansLien}
+    hasHeaderBadge
+    actionMarkup="a"
+    markup="h3"
+    data-source={dataSource}
+    data-cible={dataCible}
   >
-    <ContenuCarteItem {item} {avecBoutonFavori} />
-  </a>
-{:else}
-  <div class="carte {item.typologie} {modeLargeur}">
-    <ContenuCarteItem {item} {avecBoutonFavori} />
-  </div>
-{/if}
+    <div slot="headerbadges">
+      <HeaderBadge {item} libelleBadge={libelleBadge()} />
+    </div>
 
-<style lang="scss">
-  .carte.automatique {
-    width: auto;
-  }
-</style>
+    {#if item.sources || item.tagsSpecifiques}
+      <div slot="contentend">
+        {#if item.tagsSpecifiques}
+          <dsfr-tags-group
+            tags={item.tagsSpecifiques.map((tag) => ({ label: tag, 'has-icon': true, icon: 'government-line' }))}
+            size="sm"
+            groupMarkup="span"
+            hasIcon
+          ></dsfr-tags-group>
+        {/if}
+        {#if item.sources}
+          <dsfr-tags-group tags={item.sources.map((source) => ({ label: source }))} size="sm" groupMarkup="span"
+          ></dsfr-tags-group>
+        {/if}
+      </div>
+    {/if}
+  </dsfr-card>
+  {#if avecBoutonFavori}
+    <div>
+      <BoutonFavori idItem={item.id} />
+      Ajouter aux Favoris
+    </div>
+  {/if}
+</div>
---
+++ front/lib-svelte/src/catalogue/HeaderBadge.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import BadgesDeCollections from './guides/BadgesDeCollections.svelte';
+  import type { ItemCyber } from './Catalogue.types';
+  import type { Guide } from './Guide.types';
+
+  export let item: ItemCyber | Guide;
+  export let libelleBadge: 'Outil' | 'Service' | 'Contenu' | undefined;
+
+  const couleurlibelle = {
+    Outil: 'pink-tuile',
+    Contenu: 'blue-ecume',
+    Service: 'yellow-tournesol',
+  };
+
+  const couleur = libelleBadge ? couleurlibelle[libelleBadge] : '';
+</script>
+
+{#if item.type === 'Guide'}
+  <BadgesDeCollections guide={item as Guide} />
+{:else}
+  <dsfr-badge label={libelleBadge} size="sm" type="accent" accent={couleur}></dsfr-badge>
+{/if}
