6aa638bfb1639e1541878a34e293847f95c739ff
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-04-28 10:15:01 +0200

    [DSFR] Retravaille le bouton des favoris

--- front/lib-svelte/src/favoris/BoutonFavori.svelte
+++ front/lib-svelte/src/favoris/BoutonFavori.svelte
@@ -3,15 +3,19 @@
   import type { IdItem } from '../catalogue/Catalogue.types';
   import { favorisStore } from '../stores/favoris.store';
   import { profilStore } from '../stores/profil.store';
+  import Bouton from '../ui/Bouton.svelte';
 
   export let idItem: IdItem;
+  export let avecTexte: boolean = false;
+  export let taille: 'sm' | 'md' = 'md';
   export let avecBordure: boolean = false;
 
   $: estFavori = $favorisStore.includes(idItem);
-
-  $: cheminIcone = `/assets/images/icone-favori-${estFavori ? 'plein' : 'vide'}.svg`;
-
+  $: libelle = estFavori ? 'Retirer le favori' : 'Ajouter en favori';
   $: titre = $profilStore ? '' : 'Connectez-vous pour profiter des favoris';
+  $: icone = estFavori ? 'heart-fill' : 'heart-line';
+  $: iconeSeule = !avecTexte || !$profilStore;
+  $: type = (avecBordure ? 'tertiaire' : 'tertiaire-sans-bordure') as 'tertiaire' | 'tertiaire-sans-bordure';
 
   const actionSurClick = async () => {
     const idFavori = idItem;
@@ -31,42 +35,4 @@
   };
 </script>
 
-<button on:click|preventDefault={actionSurClick} title={titre} class:actif={$profilStore} class:bordure={avecBordure}>
-  <img src={cheminIcone} alt="Favori" />
-</button>
-
-<style lang="scss">
-  @use '../../../assets/styles/responsive' as *;
-
-  button {
-    background: transparent;
-    cursor: pointer;
-    border: none;
-    min-width: 40px;
-    height: 40px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 8px;
-    transition: transform 0.2s ease-in-out;
-    pointer-events: initial;
-
-    &.bordure {
-      border: 1px solid var(--border-default-grey);
-      border-radius: 8px;
-
-      &:hover {
-        background-color: var(--background-default-grey-hover);
-      }
-    }
-
-    &.actif:hover:not(.bordure) {
-      transform: scale(1.3);
-    }
-
-    img {
-      width: 20px;
-      height: 20px;
-    }
-  }
-</style>
+<Bouton {libelle} {titre} {iconeSeule} {icone} {type} surClic={actionSurClick} {taille} />
