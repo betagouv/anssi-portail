34acb6ee5bab440623e8be60a94711b899a8c03c
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2026-05-04 17:43:33 +0200

    [ENTETE] Utilise les composants DSFR partout
    Utilise les composants :
    - header : pour l'entête globale
    - user : pour le "menu" utilisateur
    - button : pour les boutons
    - navigation : pour la barre de navigation
    
    Les composants header et navigation ont été modifié dans l'UI-Kit pour les intégrer correctement.

--- front/lib-svelte/src/identification/ZoneIdentification.svelte
+++ front/lib-svelte/src/identification/ZoneIdentification.svelte
@@ -1,28 +1,41 @@
 <script lang="ts">
-  import { slide } from 'svelte/transition';
-  import LienNavigation from '../navigation/LienNavigation.svelte';
   import { profilStore } from '../stores/profil.store';
 
-  export let estMobile = false;
+  const menu = [{ label: 'Services ANSSI utilisés', href: '/services-anssi' }];
 </script>
 
-<div class="conteneur-zone-indentification" class:mobile={estMobile}>
+<div class="conteneur-zone-indentification">
   {#if $profilStore === undefined}
-    <div class="profil-deconnecte">
-      <a href="/inscription" class="lien-inscription">S’inscrire</a>
-      <a href="/connexion" class="lien-connexion">Se connecter</a>
-    </div>
+    <dsfr-button kind="tertiary-no-outline" markup="a" href="/inscription" size="sm">S’inscrire</dsfr-button>
+    <dsfr-button kind="tertiary-no-outline" markup="a" href="/connexion" size="sm" has-icon icon="user-line">
+      Se connecter
+    </dsfr-button>
   {:else}
-    <details>
-      <summary class="libelle-profil">
-        {$profilStore.prenom}
-        {$profilStore.nom}
-      </summary>
-      <div class="contenu" transition:slide>
-        <LienNavigation href="/services-anssi/" label="Services ANSSI utilisés" dansMenuDeroulant />
-      </div>
-    </details>
-    <a class="deconnexion" href="/oidc/deconnexion">Se déconnecter</a>
+    <dsfr-user
+      kind="tertiary-no-outline"
+      expandable
+      has-icon
+      icon="account-circle-line"
+      buttonLabel="{$profilStore.prenom} {$profilStore.nom}"
+      id="utilisateur"
+      button-id="utilisateur-btn"
+      collapse-id="nav-utilisateur"
+      hasLogout
+      links={menu}
+    >
+      <dsfr-button
+        slot="logout"
+        centered
+        kind="tertiary-no-outline"
+        markup="a"
+        href="/oidc/deconnexion"
+        size="sm"
+        has-icon
+        icon="logout-box-r-line"
+      >
+        Se déconnecter
+      </dsfr-button>
+    </dsfr-user>
   {/if}
 </div>
 
@@ -32,101 +45,11 @@
   * {
     box-sizing: border-box;
   }
-  .conteneur-zone-indentification:not(.mobile) {
-    display: none;
 
+  .conteneur-zone-indentification {
     @include a-partir-de(lg) {
       display: flex;
       align-items: center;
-
-      details {
-        position: relative;
-        summary {
-          cursor: pointer;
-          color: #000091;
-
-          &:after {
-            content: '';
-            display: block;
-            width: 16px;
-            height: 16px;
-            mask-image: url('/assets/images/icone-chevron-bas.svg');
-            transition: transform 0.15s linear;
-            background-color: #000091;
-          }
-        }
-
-        &[open] summary:after {
-          transform: rotate(-180deg);
-        }
-
-        .contenu {
-          position: absolute;
-          top: 100%;
-          width: 320px;
-          background-color: var(--background-default-grey);
-          z-index: 5;
-          box-shadow: 0 4px 12px 0 rgba(0, 0, 18, 0.16);
-        }
-      }
     }
-  }
-
-  .conteneur-zone-indentification.mobile {
-    border-bottom: 1px solid var(--border-default-grey);
-    padding-bottom: 12px;
-
-    display: block;
-
-    details {
-      display: none;
-    }
-
-    a {
-      border: none;
-    }
-  }
-
-  .profil-deconnecte,
-  .libelle-profil {
-    display: flex;
-    gap: 8px;
-    align-items: center;
-    padding: 4px 8px;
-    color: #000091;
-
-    &:before {
-      width: 16px;
-      height: 16px;
-      line-height: 16px;
-    }
-  }
-
-  .profil-deconnecte > a,
-  .libelle-profil {
-    padding: 4px 12px;
-    color: #000091;
-  }
-
-  .profil-deconnecte > a.lien-connexion:before {
-    content: url('/assets/images/icone-profil-non-connecte.svg');
-  }
-
-  .deconnexion {
-    display: flex;
-    gap: 8px;
-    align-items: center;
-    padding: 4px 12px;
-    color: #000091;
-    &:before {
-      width: 16px;
-      height: 16px;
-      line-height: 16px;
-      content: url('/assets/images/icone-profil-deconnecte.svg');
-    }
-  }
-
-  .libelle-profil:before {
-    content: url('/assets/images/icone-profil-connecte.svg');
   }
 </style>
--- front/lib-svelte/src/navigation/Navigation.svelte
+++ front/lib-svelte/src/navigation/Navigation.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  import NavigationBureau from './NavigationBureau.svelte';
-  import NavigationMobile from './NavigationMobile.svelte';
+  import { profilStore } from '../stores/profil.store';
 
-  let estBureau = false;
+  let estBureau = $state(false);
   onMount(() => {
     const mql = window.matchMedia('(min-width: 992px)');
     mql.addEventListener('change', (e: MediaQueryListEvent) => {
@@ -11,10 +10,82 @@
     });
     estBureau = mql.matches;
   });
+
+  let estConnecte = () => !!$profilStore;
+
+  const menu = $derived([
+    ...(estConnecte()
+      ? [{ label: 'Maturité cyber', id: 'nav-maturite', type: 'link', href: '/ma-maturite' }]
+      : [{ label: 'Test de maturité cyber', id: 'nav-test-maturite', type: 'link', href: '/test-maturite' }]),
+    {
+      label: 'Catalogue et sélections',
+      id: 'nav-catalogue',
+      type: 'menu',
+      collapsable: true,
+      collapseId: 'nav-catalogue',
+      items: [
+        { label: 'Le catalogue cyber complet', id: 'nav-catalogue-1', type: 'link', href: '/catalogue' },
+        { label: 'Les services pour se lancer', id: 'nav-catalogue-2', type: 'link', href: '/parcours-debutter' },
+        { label: 'Les services pour approfondir', id: 'nav-catalogue-3', type: 'link', href: '/parcours-approfondir' },
+      ],
+    },
+    { label: 'Directive NIS 2', id: 'nav-nis2', type: 'link', href: '/nis2' },
+    ...(estConnecte() ? [{ label: 'Favoris', id: 'nav-favoris', type: 'link', href: '/favoris' }] : []),
+    {
+      label: 'Contacts utiles',
+      id: 'nav-contacts',
+      type: 'menu',
+      collapsable: true,
+      collapseId: 'nav-contacts',
+      items: [
+        { label: 'Contacts cyber', id: 'nav-contacts-1', type: 'link', href: '/contacts' },
+        {
+          label: 'Prestataires qualifiés et labellisés',
+          id: 'nav-contacts-2',
+          type: 'link',
+          href: '/prestataires-labellises',
+        },
+      ],
+    },
+    { label: 'Financements', id: 'nav-financements', type: 'link', href: '/financements' },
+    {
+      label: 'Promouvoir',
+      id: 'nav-promouvoir',
+      type: 'menu',
+      collapsable: true,
+      collapseId: 'nav-promouvoir',
+      items: [
+        {
+          label: 'Promouvoir MesServicesCyber',
+          id: 'nav-promouvoir-1',
+          type: 'link',
+          href: '/promouvoir-messervicescyber',
+        },
+        {
+          label: 'Promouvoir le diagnostic cyber',
+          id: 'nav-promouvoir-2',
+          type: 'link',
+          href: '/promouvoir-diagnostic-cyber',
+        },
+      ],
+    },
+  ]);
 </script>
 
-{#if estBureau}
-  <NavigationBureau />
-{:else}
-  <NavigationMobile />
-{/if}
+<dsfr-navigation items={menu}>
+  {#if estBureau}
+    <div slot="afternavigation" class="centre">
+      <lab-anssi-mes-services-cyber-lien-diagnostic-cyber lien="/cyberdepart">
+      </lab-anssi-mes-services-cyber-lien-diagnostic-cyber>
+    </div>
+  {/if}
+</dsfr-navigation>
+
+<style lang="scss">
+  .centre {
+    display: flex;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+  }
+</style>
