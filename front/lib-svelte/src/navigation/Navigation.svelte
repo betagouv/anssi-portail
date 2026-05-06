0b1dedc6aa74a9d16275af6b138bea3c538d0c09
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2026-05-06 15:53:32 +0200

    [NAVIGATION] Affiche l'élément de navigation courant
    ... qui avait "sauté" suite à la migration vers les Webcomponent de l'UI Kit (composants DSFR)

--- front/lib-svelte/src/navigation/Navigation.svelte
+++ front/lib-svelte/src/navigation/Navigation.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { onMount } from 'svelte';
+  import { creeLienContactsUtiles } from '../contacts/contacts';
   import { profilStore } from '../stores/profil.store';
 
   let estBureau = $state(false);
@@ -13,59 +14,123 @@
 
   let estConnecte = () => !!$profilStore;
 
+  const cheminRelatif = window.location.pathname;
+
   const menu = $derived([
     ...(estConnecte()
-      ? [{ label: 'Maturité cyber', id: 'nav-maturite', type: 'link', href: '/ma-maturite' }]
-      : [{ label: 'Test de maturité cyber', id: 'nav-test-maturite', type: 'link', href: '/test-maturite' }]),
+      ? [
+          {
+            label: 'Maturité cyber',
+            id: 'nav-maturite',
+            type: 'link',
+            href: '/ma-maturite',
+            active: cheminRelatif === '/ma-maturite',
+          },
+        ]
+      : [
+          {
+            label: 'Test de maturité cyber',
+            id: 'nav-test-maturite',
+            type: 'link',
+            href: '/test-maturite',
+            active: cheminRelatif === '/test-maturite',
+          },
+        ]),
     {
       label: 'Catalogue et sélections',
       id: 'nav-catalogue',
       type: 'menu',
       collapsable: true,
       collapseId: 'nav-catalogue',
+      active: ['/catalogue', '/parcours-debuter', '/parcours-approfondir'].includes(cheminRelatif),
       items: [
-        { label: 'Le catalogue cyber complet', id: 'nav-catalogue-1', type: 'link', href: '/catalogue' },
-        { label: 'Les services pour se lancer', id: 'nav-catalogue-2', type: 'link', href: '/parcours-debuter' },
-        { label: 'Les services pour approfondir', id: 'nav-catalogue-3', type: 'link', href: '/parcours-approfondir' },
+        {
+          label: 'Le catalogue cyber complet',
+          id: 'nav-catalogue-1',
+          type: 'link',
+          href: '/catalogue',
+          active: cheminRelatif === '/catalogue',
+        },
+        {
+          label: 'Les services pour se lancer',
+          id: 'nav-catalogue-2',
+          type: 'link',
+          href: '/parcours-debuter',
+          active: cheminRelatif === '/parcours-debuter',
+        },
+        {
+          label: 'Les services pour approfondir',
+          id: 'nav-catalogue-3',
+          type: 'link',
+          href: '/parcours-approfondir',
+          active: cheminRelatif === '/parcours-approfondir',
+        },
       ],
     },
-    { label: 'Directive NIS 2', id: 'nav-nis2', type: 'link', href: '/nis2' },
-    ...(estConnecte() ? [{ label: 'Favoris', id: 'nav-favoris', type: 'link', href: '/favoris' }] : []),
+    { label: 'Directive NIS 2', id: 'nav-nis2', type: 'link', href: '/nis2', active: cheminRelatif === '/nis2' },
+    ...(estConnecte()
+      ? [
+          {
+            label: 'Favoris',
+            id: 'nav-favoris',
+            type: 'link',
+            href: '/favoris',
+            active: cheminRelatif === '/favoris',
+          },
+        ]
+      : []),
     {
       label: 'Contacts utiles',
       id: 'nav-contacts',
       type: 'menu',
       collapsable: true,
       collapseId: 'nav-contacts',
+      active: ['/contacts/', '/prestataires-labellises'].includes(cheminRelatif),
       items: [
-        { label: 'Contacts cyber', id: 'nav-contacts-1', type: 'link', href: '/contacts' },
+        {
+          label: 'Contacts cyber',
+          id: 'nav-contacts-1',
+          type: 'link',
+          href: creeLienContactsUtiles($profilStore),
+          active: cheminRelatif === '/contacts/',
+        },
         {
           label: 'Prestataires qualifiés et labellisés',
           id: 'nav-contacts-2',
           type: 'link',
           href: '/prestataires-labellises',
+          active: cheminRelatif === '/prestataires-labellises',
         },
       ],
     },
-    { label: 'Financements', id: 'nav-financements', type: 'link', href: '/financements' },
+    {
+      label: 'Financements',
+      id: 'nav-financements',
+      type: 'link',
+      href: '/financements',
+      active: cheminRelatif === '/financements',
+    },
     {
       label: 'Promouvoir',
       id: 'nav-promouvoir',
       type: 'menu',
       collapsable: true,
       collapseId: 'nav-promouvoir',
+      active: ['/promouvoir-messervicescyber', '/promouvoir-diagnostic-cyber'].includes(cheminRelatif),
       items: [
         {
           label: 'Promouvoir MesServicesCyber',
           id: 'nav-promouvoir-1',
           type: 'link',
           href: '/promouvoir-messervicescyber',
+          active: cheminRelatif === '/promouvoir-messervicescyber',
         },
         {
           label: 'Promouvoir le diagnostic cyber',
           id: 'nav-promouvoir-2',
           type: 'link',
           href: '/promouvoir-diagnostic-cyber',
+          active: cheminRelatif === '/promouvoir-diagnostic-cyber',
         },
       ],
     },
