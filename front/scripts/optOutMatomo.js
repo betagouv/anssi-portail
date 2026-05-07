d2a595bb7ffcbc3d55d3750d7cfc655215cd1ee6
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2026-05-07 16:18:25 +0200

    [DSFR] Remplace la case à cocher de la confidentialité

--- front/scripts/optOutMatomo.js
+++ front/scripts/optOutMatomo.js
@@ -2,7 +2,7 @@ const elementConsentement = () => document.getElementById('consentement-matomo')
 
 const modifieLabelConsentement = () => {
   const optOutMatomo = localStorage.getItem('optOutMatomo') === 'true';
-  document.getElementById('label-consentement-matomo').innerHTML = optOutMatomo
+  elementConsentement().label = optOutMatomo
     ? 'Vous n’êtes actuellement pas suivi(e). Cochez cette case pour ne plus être exclu(e).'
     : 'Vous êtes suivi(e) de manière anonyme. Décochez cette case pour vous exclure du suivi.';
 };
@@ -15,7 +15,7 @@ const chargeEtatInitial = () => {
 
 window.addEventListener('DOMContentLoaded', chargeEtatInitial);
 
-elementConsentement().addEventListener('change', () => {
+elementConsentement().addEventListener('valuechanged', () => {
   const optOutMatomo = !elementConsentement().checked;
   localStorage.setItem('optOutMatomo', optOutMatomo);
   modifieLabelConsentement();
