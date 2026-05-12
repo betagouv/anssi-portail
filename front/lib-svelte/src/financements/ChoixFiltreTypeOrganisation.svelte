1c20bae34b7fb7d6653a067e7e8fb6ac48cbe804
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2026-05-12 15:09:11 +0200

    [DSFR] Remplace le filtre de type d’organisation
    ...pour les financements

---
+++ front/lib-svelte/src/financements/ChoixFiltreTypeOrganisation.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { rechercheParTypeOrganisation } from './stores/rechercheParTypeOrganisation.store';
+
+  let { valeur, libelle }: { valeur: string; libelle: string } = $props();
+  let actif = $derived($rechercheParTypeOrganisation.includes(valeur));
+</script>
+
+<div class="choix">
+  <dsfr-checkbox
+    id={valeur}
+    label={libelle}
+    checked={actif}
+    onvaluechanged={(e: CustomEvent) => {
+      if (e.detail) {
+        $rechercheParTypeOrganisation = [...$rechercheParTypeOrganisation, valeur];
+      } else {
+        $rechercheParTypeOrganisation = $rechercheParTypeOrganisation.filter((v) => v !== valeur);
+      }
+    }}
+  ></dsfr-checkbox>
+</div>
