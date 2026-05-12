07f598b2d882a9d6acf52bfc0631df697574fe1a
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2026-05-12 09:55:17 +0200

    [DSFR] Remplace le filtre de typologie
    ...sur le catalogue

--- front/lib-svelte/src/catalogue/FiltreTypologie.svelte
+++ front/lib-svelte/src/catalogue/FiltreTypologie.svelte
@@ -1,24 +1,11 @@
 <script lang="ts">
   import { Typologie } from './Catalogue.types';
-  import { rechercheParTypologie } from './stores/rechercheParTypologie.store';
-  import { nombreResultats } from './stores/nombreResultats.store';
+  import ChoixFiltreTypologie from './ChoixFiltreTypologie.svelte';
 </script>
 
 <fieldset>
   <legend>Type</legend>
-  <label>
-    <input type="checkbox" value={Typologie.SERVICE} bind:group={$rechercheParTypologie} />
-    <span class="libelle"><img src="/assets/images/icone-service.svg" alt="Logo service" />Services</span>
-    <span class="compte">{$nombreResultats.parTypologie[Typologie.SERVICE]}</span>
-  </label>
-  <label>
-    <input type="checkbox" value={Typologie.OUTIL} bind:group={$rechercheParTypologie} />
-    <span class="libelle"><img src="/assets/images/icone-outil.svg" alt="Logo outils" />Outils</span>
-    <span class="compte">{$nombreResultats.parTypologie[Typologie.OUTIL]}</span>
-  </label>
-  <label>
-    <input type="checkbox" value={Typologie.CONTENU} bind:group={$rechercheParTypologie} />
-    <span class="libelle"><img src="/assets/images/icone-contenu.svg" alt="Logo contenus" />Contenus</span>
-    <span class="compte">{$nombreResultats.parTypologie[Typologie.CONTENU]}</span>
-  </label>
+  <ChoixFiltreTypologie valeur={Typologie.SERVICE} libelle="Services" icone="icone-service" />
+  <ChoixFiltreTypologie valeur={Typologie.OUTIL} libelle="Outils" icone="icone-outil" />
+  <ChoixFiltreTypologie valeur={Typologie.CONTENU} libelle="Contenus" icone="icone-contenu" />
 </fieldset>
