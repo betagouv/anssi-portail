f86ea598f2c31c0d979eb025a4fa9e8e341cdd44
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2026-05-19 15:52:47 +0200

    [DSFR] Utilise le contrôle segmenté dans les pages parcours

--- front/scripts/parcours.js
+++ front/scripts/parcours.js
@@ -1,14 +1,36 @@
-import { elementLePlusVisible } from './scroll.js';
+document.addEventListener('readystatechange', () => {
+  if (document.readyState !== 'complete') return;
 
-window.addEventListener('scroll', () => {
-  const sections = document.getElementsByClassName('action');
-  const laPlusVisible = elementLePlusVisible([...sections], window.innerHeight);
+  const controleSegmente = document.querySelector('dsfr-segmented');
+  const sections = [...document.querySelectorAll('.action')];
+  if (!controleSegmente || sections.length === 0) return;
 
-  if (!laPlusVisible) return;
+  const selectionneLaSection = (idx, updateHash = false, scroll = false) => {
+    const section = sections[idx];
+    if (!section) return;
+    controleSegmente.value = idx;
+    if (scroll) section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  };
 
-  document.querySelectorAll('.sommaire a').forEach((element) => element.classList.remove('actif'));
+  let index = 0;
 
-  const idPourSurlignage = `#${laPlusVisible.id}`;
-  const cible = document.querySelector(`.sommaire a[href='${idPourSurlignage}']`);
-  cible.classList.add('actif');
+  controleSegmente.addEventListener('valuechanged', (event) => {
+    index = event.detail;
+    selectionneLaSection(index, true, true);
+  });
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          index = sections.indexOf(entry.target);
+          selectionneLaSection(index);
+        }
+      });
+    },
+    { rootMargin: '-20% 0% -80% 0%' }
+  );
+
+  sections.forEach((section) => observer.observe(section));
+  controleSegmente.value = index;
 });
