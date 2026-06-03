f3eeba4dee263f3f53a08fafafe3bca224ebbddc
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2026-06-03 17:51:15 +0200

    [PARCOURS SECURISATION] Affiche les cartes des mesures

---
+++ front/lib-svelte/src/parcours-securisation/CarteMesure.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import Lien from '../ui/Lien.svelte';
+  import type { Mesure } from './mesure';
+
+  interface Props {
+    mesure: Mesure;
+  }
+
+  const { mesure }: Props = $props();
+</script>
+
+<dsfr-card
+  title={mesure.phraseAccroche}
+  description={mesure.titre}
+  enlarge={false}
+  no-link={true}
+  has-description="true"
+  has-buttons={true}
+  src={`/assets/images/parcours-securisation/mesure-${mesure.id}.svg`}
+>
+  <div slot="buttonsgroup" class="actions-carte">
+    <Lien apparence="bouton" href={`/mesures/${mesure.id}`} type="primaire" libelle="Prendre en compte" etire={true}
+    ></Lien>
+  </div>
+</dsfr-card>
+
+<style lang="scss">
+  .actions-carte {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
--- front/lib-svelte/src/parcours-securisation/ModuleCyberdepart.svelte
+++ front/lib-svelte/src/parcours-securisation/ModuleCyberdepart.svelte
@@ -1,6 +1,24 @@
 <script lang="ts">
   import Heros from '../ui/Heros.svelte';
+  import GrilleCartesMesures from './GrilleCartesMesures.svelte';
   import Progression from './Progression.svelte';
+
+  const mesure = {
+    id: 'CONTINU.1',
+    phrase_accroche: 'Évitez de tout perdre en cas de cyberattaque 😩',
+    titre: 'Sauvegarder régulièrement les données, vérifier et protéger ces sauvegardes.',
+  };
+  const mesure2 = {
+    id: 'AUTH.5',
+    phrase_accroche: 'Empêchez qu’un compte soit utilisé, même si le mot de passe a fuité 💨',
+    titre:
+      "Activer la vérification en deux étapes ou un autre moyen de renforcement de la sécurité de l'accès aux comptes ",
+  };
+
+  const mesures = [mesure, mesure2];
+  const idMesuresMisesEnAvant = ['CONTINU.1'];
+  const mesuresMisesEnAvant = $derived(mesures.filter((m) => idMesuresMisesEnAvant.includes(m.id)));
+  const autresMesures = $derived(mesures.filter((m) => !idMesuresMisesEnAvant.includes(m.id)));
 </script>
 
 <Heros
@@ -19,8 +37,30 @@
   <Progression></Progression>
 </dsfr-container>
 
+<dsfr-container class="actions-en-avant">
+  <h4>🚀 Lancez-vous avec ces premières actions</h4>
+  <GrilleCartesMesures mesures={mesuresMisesEnAvant}></GrilleCartesMesures>
+</dsfr-container>
+
+<dsfr-container class="autres-actions">
+  <GrilleCartesMesures mesures={autresMesures}></GrilleCartesMesures>
+</dsfr-container>
+
 <style lang="scss">
   dsfr-container.progression {
-    margin-block: 2rem;
+    padding-block: 2rem;
+  }
+
+  dsfr-container.actions-en-avant {
+    background-color: var(--background-alt-blue-france);
+    padding-block: 3rem;
+
+    h4 {
+      margin-bottom: 2rem;
+    }
+  }
+
+  .autres-actions {
+    padding-block: 3rem;
   }
 </style>
