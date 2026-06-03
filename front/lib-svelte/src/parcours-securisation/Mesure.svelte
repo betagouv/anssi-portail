b1763c31c65b17671ab381a55802cf6bd6dbfa25
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-06-03 15:25:03 +0200

    [MESURES] Affiche les explications
    ...après les avoir aseptisées

--- front/lib-svelte/src/parcours-securisation/Mesure.svelte
+++ front/lib-svelte/src/parcours-securisation/Mesure.svelte
@@ -3,6 +3,7 @@
   import { onMount } from 'svelte';
   import FilAriane from '../ui/FilAriane.svelte';
   import Heros from '../ui/Heros.svelte';
+  import { aseptiseHtml } from '../utils/aseptisationDuHtml';
   import type { Mesure } from './mesure';
 
   let mesure: Mesure | undefined = undefined;
@@ -39,8 +40,27 @@
   </Heros>
 
   <dsfr-container>
-    <div>A faire...</div>
+    <section class="presentation">
+      <h2>Présentation</h2>
+      {@html explications}
+    </section>
   </dsfr-container>
+
+  {#if mesure.risques.length > 0}
+    <dsfr-container>
+      <section class="risques">
+        <h2>Risques evites</h2>
+        <ul class="risques-list">
+          {#each mesure.risques as risque}
+            <li>
+              <strong>{risque.libelle}</strong>
+              <p>{risque.description}</p>
+            </li>
+          {/each}
+        </ul>
+      </section>
+    </dsfr-container>
+  {/if}
 {:else}
   <dsfr-container>Chargement... </dsfr-container>
 {/if}
