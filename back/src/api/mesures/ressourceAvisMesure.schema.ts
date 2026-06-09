85cee360e7614cf7018c54b726e6d453e30f482d
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2026-06-09 10:19:00 +0200

    [MESURES] Limite les commentaires
    ... à 1000 caractères

--- back/src/api/mesures/ressourceAvisMesure.schema.ts
+++ back/src/api/mesures/ressourceAvisMesure.schema.ts
@@ -2,5 +2,5 @@ import z from 'zod';
 
 export const schemaRessourceAvisMesure = z.strictObject({
   retour: z.enum(['POSITIF', 'NEGATIF'], 'Le retour doit être "POSITIF" ou "NEGATIF"'),
-  commentaire: z.string().optional(),
+  commentaire: z.string().max(1000, 'Le commentaire doit contenir au plus 1000 caractères').optional(),
 });
