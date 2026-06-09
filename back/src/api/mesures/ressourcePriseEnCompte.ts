ffa2fc4e624034bb73ef5608451cdfcad7126343
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-06-09 11:12:12 +0200

    [PARCOURS SECURISATION] Crée une prise en compte

--- back/src/api/mesures/ressourcePriseEnCompte.ts
+++ back/src/api/mesures/ressourcePriseEnCompte.ts
@@ -2,15 +2,30 @@ import { Request, Response, Router } from 'express';
 import { filetRouteAsynchrone } from '../middleware';
 import { corpsVide, valideCorpsRequete } from '../zod';
 import { ConfigurationServeur } from '../configurationServeur';
+import { Utilisateur } from '../../metier/utilisateur';
+import { PriseEnCompte } from '../../metier/PriseEnCompte';
 
-export const ressourcePriseEnCompte = ({ middleware }: ConfigurationServeur) => {
+export const ressourcePriseEnCompte = ({
+  middleware,
+  entrepotUtilisateur,
+  adaptateurHachage,
+  entrepotMesure,
+  entrepotPriseEnCompte,
+}: ConfigurationServeur) => {
   const routeur = Router();
 
   routeur.post(
     '/:idMesure/prise-en-compte',
     middleware.verifieJWT,
     valideCorpsRequete(corpsVide),
-    filetRouteAsynchrone(async (_requete: Request, reponse: Response) => {
+    middleware.ajouteUtilisateurARequete(entrepotUtilisateur, adaptateurHachage),
+    filetRouteAsynchrone(async (requete: Request, reponse: Response) => {
+      const utilisateur = requete.utilisateur as Utilisateur;
+      const mesure = await entrepotMesure.parId(requete.params.idMesure as string);
+      if (!mesure) {
+        return reponse.sendStatus(404);
+      }
+      await entrepotPriseEnCompte.ajoute(new PriseEnCompte(utilisateur, mesure));
       return reponse.sendStatus(201);
     })
   );
--- back/tests/api/mesures/ressourcePriseEnCompte.spec.ts
+++ back/tests/api/mesures/ressourcePriseEnCompte.spec.ts
@@ -1,17 +1,36 @@
+import { Express } from 'express';
 import assert from 'node:assert';
-import { describe, it } from 'node:test';
+import { beforeEach, describe, it } from 'node:test';
 import request from 'supertest';
 import { creeServeur } from '../../../src/api/msc';
-import { configurationDeTestDuServeur } from '../fauxObjets';
+import { EntrepotMesureMemoire } from '../../persistance/entrepotMesureMemoire';
+import { EntrepotPriseEnCompteMemoire } from '../../persistance/EntrepotPriseEnCompteMemoire';
 import { encodeSession } from '../cookie';
-import { jeanneDupont } from '../objetsPretsALEmploi';
+import { configurationDeTestDuServeur } from '../fauxObjets';
+import { jeanneDupont, mesureAuthentA2Etapes } from '../objetsPretsALEmploi';
+import { EntrepotUtilisateur } from '../../../src/metier/entrepotUtilisateur';
+import { EntrepotUtilisateurMemoire } from '../../persistance/entrepotUtilisateurMemoire';
 
 describe("La ressource de prise en compte d'une mesure", () => {
+  let serveur: Express;
+  let entrepotPriseEnCompte: EntrepotPriseEnCompteMemoire;
+  let entrepotMesure: EntrepotMesureMemoire;
+  let entrepotUtilisateur: EntrepotUtilisateur;
+  beforeEach(() => {
+    entrepotPriseEnCompte = new EntrepotPriseEnCompteMemoire();
+    entrepotMesure = new EntrepotMesureMemoire();
+    entrepotUtilisateur = new EntrepotUtilisateurMemoire();
+    serveur = creeServeur({
+      ...configurationDeTestDuServeur,
+      entrepotPriseEnCompte,
+      entrepotMesure,
+      entrepotUtilisateur,
+    });
+  });
+
   describe('sur une requête POST', () => {
     describe("d'un utilisateur anonyme", () => {
       it('réponds 401', async () => {
-        const serveur = creeServeur(configurationDeTestDuServeur);
-
         const reponse = await request(serveur).post('/api/mesures/AUTH.5/prise-en-compte');
 
         assert.equal(reponse.status, 401);
@@ -19,14 +38,34 @@ describe("La ressource de prise en compte d'une mesure", () => {
     });
     describe("d'un utilisateur connecté", () => {
       const cookieJeanneDupont = encodeSession({ email: jeanneDupont.email, token: 'valide' });
-      it('réponds 201', async () => {
-        const serveur = creeServeur(configurationDeTestDuServeur);
+      const mesure = mesureAuthentA2Etapes();
 
+      beforeEach(async () => {
+        await entrepotMesure.ajoute(mesure);
+        await entrepotUtilisateur.ajoute(jeanneDupont);
+      });
+
+      it('réponds 201', async () => {
         const reponse = await request(serveur)
           .post('/api/mesures/AUTH.5/prise-en-compte')
           .set('Cookie', cookieJeanneDupont);
 
         assert.equal(reponse.status, 201);
+      });
+
+      it('ajoute une prise en compte', async () => {
+        await request(serveur).post('/api/mesures/AUTH.5/prise-en-compte').set('Cookie', cookieJeanneDupont);
+
+        const priseEnComptePersistee = await entrepotPriseEnCompte.pour(jeanneDupont, mesure);
+        assert.notEqual(priseEnComptePersistee, undefined);
+      });
+
+      it("réponds 404 si la mesure n'existe pas", async () => {
+        const reponse = await request(serveur)
+          .post('/api/mesures/mesureinconnue/prise-en-compte')
+          .set('Cookie', cookieJeanneDupont);
+
+        assert.equal(reponse.status, 404);
       });
     });
   });
