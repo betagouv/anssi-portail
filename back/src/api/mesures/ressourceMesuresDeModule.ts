6d7b7d6149c3cd448ef96229c4ebb1efdf8dcb7b
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2026-06-10 17:04:40 +0200

    [PARCOURS SECURISATION] Retourne la prise en compte des mesures
    ...depuis la liste des mesures d’un module

--- back/src/api/mesures/ressourceMesuresDeModule.ts
+++ back/src/api/mesures/ressourceMesuresDeModule.ts
@@ -1,18 +1,35 @@
 import { Request, Response, Router } from 'express';
+import { Utilisateur } from '../../metier/utilisateur';
 import { ConfigurationServeur } from '../configurationServeur';
 import { filetRouteAsynchrone } from '../middleware';
 import { corpsVide, valideCorpsRequete } from '../zod';
+import { mesurePresentation } from './mesurePresentation';
 
-const ressourceMesuresDeModule = ({ entrepotMesure, middleware }: ConfigurationServeur) => {
+const ressourceMesuresDeModule = ({
+  entrepotMesure,
+  entrepotUtilisateur,
+  entrepotPriseEnCompte,
+  adaptateurHachage,
+  middleware,
+}: ConfigurationServeur) => {
   const routeur = Router();
 
   routeur.get(
     '/',
     middleware.verifieJWT,
+    middleware.ajouteUtilisateurARequete(entrepotUtilisateur, adaptateurHachage),
     valideCorpsRequete(corpsVide),
     filetRouteAsynchrone(async (_requete: Request, reponse: Response) => {
       const mesures = await entrepotMesure.tous();
-      reponse.status(200).send(mesures.toSorted((a, b) => a.ordre - b.ordre));
+      const utilisateur = _requete.utilisateur as Utilisateur;
+      const mesuresPresentation = await Promise.all(
+        mesures.map(async (mesure) => {
+          const priseEnCompte = await entrepotPriseEnCompte.pour(utilisateur, mesure);
+          return mesurePresentation(mesure, priseEnCompte);
+        })
+      );
+      const mesuresTries = mesuresPresentation.toSorted((a, b) => a.ordre - b.ordre);
+      reponse.status(200).send(mesuresTries);
     })
   );
 
