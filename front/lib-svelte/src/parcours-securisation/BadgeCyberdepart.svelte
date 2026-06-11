37366322f1de96d813570f3a6424f64d28c6a338
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2026-06-11 16:35:52 +0200

    [PARCOURS SECURISATION] Adapte le badge
    ... aux grands écrans

--- front/lib-svelte/src/parcours-securisation/BadgeCyberdepart.svelte
+++ front/lib-svelte/src/parcours-securisation/BadgeCyberdepart.svelte
@@ -47,22 +47,41 @@
 </div>
 
 <style lang="scss">
+  @use '../../../assets/styles/responsive' as *;
   .badge {
     border: 1px solid var(--border-default-grey);
-    display: flex;
+    display: grid;
     flex-direction: column;
     padding: 1.5rem;
+    grid-template-areas: 'image' 'titre' 'description' 'actions';
+
+    @include a-partir-de(md) {
+      column-gap: 1.5rem;
+      grid-template-areas: 'image titre' 'image description' 'image actions';
+    }
+
     img {
       margin-bottom: 1.5rem;
+      grid-area: image;
+      justify-self: center;
     }
     h6 {
+      grid-area: titre;
       margin-bottom: 1rem;
+    }
+    p {
+      grid-area: description;
     }
 
     .actions {
+      grid-area: actions;
       display: flex;
       flex-direction: column;
       gap: 1rem;
+
+      @include a-partir-de(md) {
+        flex-direction: row;
+      }
     }
   }
 </style>
