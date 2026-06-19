121943c947bfedc29a004d3e08339a0f8d903236
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2026-06-19 15:10:38 +0200

    [SOIN] Centralise le calcul de la cible
    ... de déblocage du badge

--- back/src/metier/module.ts
+++ back/src/metier/module.ts
@@ -1,6 +1,14 @@
+import { Mesure } from './mesure';
+
 export class Module {
+  mesures: Mesure[] = [];
+
   constructor(
     readonly id: number,
     readonly nom: string
   ) {}
+
+  cibleDéblocageBadgeCyberdépart() {
+    return Math.floor(this.mesures.length * 0.8);
+  }
 }
