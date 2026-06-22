426df171a312ddf934a1652bf4feae8eb729329d
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-06-22 16:02:48 +0200

    [SOIN] Permets de récupérer un module
    ...correspondant à une mesure dans l'entrepôt

--- back/src/metier/EntrepotModule.ts
+++ back/src/metier/EntrepotModule.ts
@@ -1,5 +1,7 @@
+import { Mesure } from './mesure';
 import { Module } from './module';
 
 export interface EntrepôtModule {
+  pourLaMesure(mesure: Mesure): Promise<Module>;
   parId(id: number): Promise<Module | undefined>;
 }
--- back/tests/persistance/EntrepôtModuleMémoire.ts
+++ back/tests/persistance/EntrepôtModuleMémoire.ts
@@ -1,4 +1,5 @@
 import { EntrepôtModule } from '../../src/metier/EntrepotModule';
+import { Mesure } from '../../src/metier/mesure';
 import { Module } from '../../src/metier/module';
 import { EntrepotMemoire } from './entrepotMemoire';
 
@@ -8,5 +9,10 @@ export class EntrepôtModuleMémoire extends EntrepotMemoire<Module> implements 
       throw new Error('pas un nombre');
     }
     return this.entites.find((module) => module.id === id);
+  }
+
+  async pourLaMesure(mesure: Mesure): Promise<Module> {
+    const module = this.entites.find((module) => module.mesures.some((m) => m.id === mesure.id));
+    return module!;
   }
 }
