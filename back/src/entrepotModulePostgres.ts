1cbf73eb008890aaf880f4c7b554ed0fa7373034
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2026-06-23 10:44:11 +0200

    [PARCOURS SECURISATION] Retourne un module depuis une mesure

--- back/src/entrepotModulePostgres.ts
+++ back/src/entrepotModulePostgres.ts
@@ -21,15 +21,27 @@ export class EntrepôtModulePostgres implements EntrepôtModule {
       return undefined;
     }
 
-    const mesuresDuModuleLues = await this.knex<MesurePersistee>('mesures').where({ id_module: id });
+    return await this.réhydrate(moduleLu);
+  }
+
+  async pourLaMesure(mesure: Mesure): Promise<Module> {
+    const moduleLu = await this.knex<ModulePersisté>('modules')
+      .join('mesures', 'mesures.id_module', 'modules.id')
+      .where({ 'mesures.id': mesure.id })
+      .select('modules.*')
+      .first();
+    if (!moduleLu) {
+      throw new Error(`Module non trouvé pour la mesure ${mesure.id}`);
+    }
+    return await this.réhydrate(moduleLu);
+  }
+
+  private async réhydrate(moduleLu: ModulePersisté) {
+    const mesuresDuModuleLues = await this.knex<MesurePersistee>('mesures').where({ id_module: moduleLu.id });
 
     const mesuresDuModule = await Promise.all(mesuresDuModuleLues.map((m) => this.entrepôtMesure.parId(m.id)));
     const module = new Module(moduleLu.id, moduleLu.nom);
     module.mesures = mesuresDuModule.filter((m) => !!m);
     return module;
-  }
-
-  pourLaMesure(_mesure: Mesure): Promise<Module> {
-    throw new Error('Method not implemented.');
   }
 }
