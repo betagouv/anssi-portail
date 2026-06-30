55617165d73c4b08afffa3632de4a6f8446145af
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-06-30 11:21:17 +0200

    [SSR] Construis un premier composant en mode serveur

--- front/lib-svelte/src/main-entreprises.ts
+++ front/lib-svelte/src/main-entreprises.ts
@@ -1,6 +1,6 @@
-import { mount } from 'svelte';
+import { hydrate } from 'svelte';
 import Page from './protection/entreprises/PresentationEntreprises.svelte';
 
-mount(Page, {
+hydrate(Page, {
   target: document.getElementById('entreprises')!,
 });
