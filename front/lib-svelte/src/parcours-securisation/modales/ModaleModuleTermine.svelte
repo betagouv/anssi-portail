dd0f0e87be6a8f87a71c72ab1be395d281e609c7
jpaulineau <jpaulineau@ippon.fr>
2026-07-07 15:46:19 +0200

    [PARCOURS SECURISATION] encourage l'utilisateur a continuer
    ...sur des modules complementaires

--- front/lib-svelte/src/parcours-securisation/modales/ModaleModuleTermine.svelte
+++ front/lib-svelte/src/parcours-securisation/modales/ModaleModuleTermine.svelte
@@ -11,21 +11,24 @@
 </script>
 
 <Modale bind:estOuverte>
-  <h2 class="fr-h4">🎉 Bravo ! Vous avez complété Cyberdépart</h2>
+  <h2 class="fr-h4">🎉 Bravo !</h2>
   <div class="corps">
     <p class="texte-standard-md">
-      En prenant votre Cyberdépart vous avez mis en place des actions pour réduire les risques les plus courants.
-      Cependant il est essentiel d’aller plus loin pour renforcer votre cybersécurité dans la durée.
+      Vous avez appliqué les 13 mesures du Cyberdépart. Vous avez posé les bases de votre cybersécurité.
     </p>
-    <h3 class="fr-h6">Accédez au parcours avancé</h3>
     <p class="texte-standard-md">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent lectus mi, tristique pulvinar posuere eget,
-      pretium ut quam. Donec convallis facilisis erat, viverra consectetur orci. Nunc hendrerit scelerisque neque et
-      feugiat.<br /><br /> Praesent facilisis, ipsum at eleifend auctor, mi lorem porta massa, nec rutrum nisi massa in augue.
+      <strong>Continuez votre progression&nbsp;:</strong> 5 modules complémentaires vous attendent pour renforcer la protection
+      de votre organisation contre d'autres risques courants.
     </p>
   </div>
   {#snippet actions()}
-    <Lien apparence="bouton" href="/parcours-complet" libelle="Accéder au parcours complet" />
+    <Lien
+      apparence="bouton"
+      href="/parcours-complet"
+      libelle="Poursuivre le parcours"
+      iconeADroite
+      icone="arrow-right-circle-line"
+    />
     <Bouton libelle="Plus tard" type="secondaire" surClic={() => (estOuverte = false)} />
   {/snippet}
 </Modale>
