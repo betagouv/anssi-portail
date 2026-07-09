3f55d7a66192e6a27d0b02ce65d1eb79e046955c
Cédric Prezelin <cedric.prezelin.ext@beta.gouv.fr>
2026-07-09 17:52:56 +0200

    [SOIN] remplace les <slot> dépréciés pour rendre du contenu parent

--- front/lib-svelte/src/ui/Accordeon.svelte
+++ front/lib-svelte/src/ui/Accordeon.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
+  import type { Snippet } from 'svelte';
+
   type Props = {
     libelle: string;
     niveauTitre?: 1 | 2 | 3 | 4 | 5;
     id?: string;
     estOuvert?: boolean;
+    children?: Snippet;
   };
-  const { libelle, niveauTitre = 3, id, estOuvert }: Props = $props();
+  const { libelle, niveauTitre = 3, id, estOuvert, children }: Props = $props();
 </script>
 
 <dsfr-accordion {id} title-markup-level={niveauTitre} label={libelle} is-expanded={estOuvert}>
-  <slot />
+  {@render children?.()}
 </dsfr-accordion>
