a910ee4ca62fb6e88e04e0f501bf40695232faa3
jpaulineau <jpaulineau@ippon.fr>
2026-07-09 14:56:45 +0200

    [PARCOURS SECURISATION] Adapte l'affichage aux tablettes

--- front/lib-svelte/src/parcours-securisation/ParcoursComplet.svelte
+++ front/lib-svelte/src/parcours-securisation/ParcoursComplet.svelte
@@ -1,6 +1,31 @@
 <script lang="ts">
   import Heros from '../ui/Heros.svelte';
   import Lien from '../ui/Lien.svelte';
+  const modules = [
+    {
+      description: 'Prenez votre cyber départ..',
+      titre: 'Cyberdépart: 13 actions pour se lancer',
+      libelleLien: 'Prendre mon Cyberdépart',
+    },
+    {
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tellus nibh, faucibus sed elit quis, aliquet malesuada augue.',
+      titre: "Aggravation des conséquences d'un incident",
+      libelleLien: 'Accéder aux mesures',
+    },
+    {
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tellus nibh, faucibus sed elit quis, aliquet malesuada augue.',
+      titre: 'Exploitation de faille dans la défense',
+      libelleLien: 'Accéder aux mesures',
+    },
+    {
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tellus nibh, faucibus sed elit quis, aliquet malesuada augue.',
+      titre: 'Incapacité de réagir',
+      libelleLien: 'Accéder aux mesures',
+    },
+  ];
 </script>
 
 <Heros
@@ -16,36 +41,26 @@
 ></Heros>
 <dsfr-container>
   <div class="grille">
-    <dsfr-card
-      description="Prenez votre cyber départ..."
-      enlarge={false}
-      has-buttons={true}
-      has-description="true"
-      no-link={true}
-      src="/assets/images/image-generique.avif"
-      title="Cyberdépart: 13 actions pour se lancer"
-    >
-      <div slot="buttonsgroup" class="actions-carte">
-        <Lien apparence="bouton" href="#" type="primaire" libelle="Prendre mon Cyberdépart" etire={true} />
-      </div>
-    </dsfr-card>
-    <dsfr-card
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tellus nibh, faucibus sed elit quis, aliquet malesuada augue."
-      enlarge={false}
-      has-buttons={true}
-      has-description="true"
-      no-link={true}
-      src="/assets/images/image-generique.avif"
-      title="Aggravation des conséquences d'un incident"
-    >
-      <div slot="buttonsgroup" class="actions-carte">
-        <Lien apparence="bouton" href="#" type="primaire" libelle="Accéder aux mesures" etire={true} />
-      </div>
-    </dsfr-card>
+    {#each modules as module (module.titre)}
+      <dsfr-card
+        description={module.description}
+        enlarge={false}
+        has-buttons={true}
+        has-description="true"
+        no-link={true}
+        src="/assets/images/image-generique.avif"
+        title={module.titre}
+      >
+        <div slot="buttonsgroup" class="actions-carte">
+          <Lien apparence="bouton" href="#" type="primaire" libelle={module.libelleLien} etire={true} />
+        </div>
+      </dsfr-card>
+    {/each}
   </div>
 </dsfr-container>
 
 <style lang="scss">
+  @use '../../../assets/styles/responsive' as *;
   .actions-carte {
     display: flex;
     flex-direction: column;
@@ -53,8 +68,12 @@
 
   .grille {
     display: grid;
-    grid-template-columns: 1fr;
+    --colonnes: 1;
+    grid-template-columns: repeat(var(--colonnes), 1fr);
     gap: 1rem;
     padding-bottom: 1rem;
+    @include a-partir-de(md) {
+      --colonnes: 2;
+    }
   }
 </style>
