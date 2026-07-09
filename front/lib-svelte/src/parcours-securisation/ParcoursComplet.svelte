be99432e9502fcad376d5c58d8f58008603521f4
jpaulineau <jpaulineau@ippon.fr>
2026-07-09 15:47:40 +0200

    [PARCOURS SECURISATION] Mets en avant le module Cyberdépart

--- front/lib-svelte/src/parcours-securisation/ParcoursComplet.svelte
+++ front/lib-svelte/src/parcours-securisation/ParcoursComplet.svelte
@@ -41,20 +41,23 @@
 ></Heros>
 <dsfr-container>
   <div class="grille">
-    {#each modules as module (module.titre)}
-      <dsfr-card
-        description={module.description}
-        enlarge={false}
-        has-buttons={true}
-        has-description="true"
-        no-link={true}
-        src="/assets/images/image-generique.avif"
-        title={module.titre}
-      >
-        <div slot="buttonsgroup" class="actions-carte">
-          <Lien apparence="bouton" href="#" type="primaire" libelle={module.libelleLien} etire={true} />
-        </div>
-      </dsfr-card>
+    {#each modules as module, index (module.titre)}
+      <div class="carte" class:mise-en-avant={index === 0}>
+        <p class="texte-standard-md">Notre sélection pour vous lancer</p>
+        <dsfr-card
+          description={module.description}
+          enlarge={false}
+          has-buttons={true}
+          has-description="true"
+          no-link={true}
+          src="/assets/images/image-generique.avif"
+          title={module.titre}
+        >
+          <div slot="buttonsgroup" class="actions-carte">
+            <Lien apparence="bouton" href="#" type="primaire" libelle={module.libelleLien} etire={true} />
+          </div>
+        </dsfr-card>
+      </div>
     {/each}
   </div>
 </dsfr-container>
@@ -73,10 +76,65 @@
     gap: 1rem;
     padding-bottom: 1rem;
     @include a-partir-de(md) {
+      gap: 1.5rem;
       --colonnes: 2;
+      padding-bottom: 3rem;
     }
     @include a-partir-de(lg) {
       --colonnes: 3;
+      padding-bottom: 4.5rem;
+    }
+  }
+
+  .carte {
+    display: flex;
+    flex-direction: column;
+    gap: 0.75rem;
+    dsfr-card {
+      flex: 1;
+    }
+    p {
+      display: none;
+    }
+    &:nth-child(2) {
+      @include a-partir-de(md) {
+        padding-top: 3rem;
+      }
+    }
+    &:nth-child(3) {
+      @include a-partir-de(lg) {
+        padding-top: 3rem;
+      }
+    }
+    &.mise-en-avant {
+      position: relative;
+      padding-top: 0.75rem;
+      margin-bottom: 1rem;
+      p {
+        display: block;
+        text-align: center;
+        margin-bottom: 0;
+        font-weight: bold;
+        z-index: 1;
+      }
+
+      @include a-partir-de(md) {
+        margin-bottom: 0;
+      }
+      &:before {
+        content: '';
+        background-color: var(--background-contrast-blue-france);
+        position: absolute;
+        top: 0rem;
+        bottom: -1rem;
+        right: -1rem;
+        left: -1rem;
+        @include a-partir-de(md) {
+          bottom: -0.75rem;
+          right: -0.75rem;
+          left: -0.75rem;
+        }
+      }
     }
   }
 </style>
