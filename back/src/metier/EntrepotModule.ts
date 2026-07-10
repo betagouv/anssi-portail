058b8341c951290011b1bc97a191606a1964dc69
jpaulineau <jpaulineau@ippon.fr>
2026-07-10 10:22:42 +0200

    [PARCOURS SECURISATION] Renvoie les mesures du parcours complet

--- back/src/metier/EntrepotModule.ts
+++ back/src/metier/EntrepotModule.ts
@@ -2,6 +2,7 @@ import { Mesure } from './mesure.js';
 import { Module } from './module.js';
 
 export interface EntrepôtModule {
+  tous(): Promise<Module[]>;
   pourLaMesure(mesure: Mesure): Promise<Module>;
   parId(id: number): Promise<Module | undefined>;
 }
