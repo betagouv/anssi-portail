30ca3158e056319cb56e031983f446e4a94aa825
jpaulineau <jpaulineau@ippon.fr>
2026-07-10 09:50:16 +0200

    [PARCOURS SECURISATION] Interdis l'accès au parcours complet des utilisateur non connectés

--- back/tests/api/mesures/ressourceParcoursComplet.spec.ts
+++ back/tests/api/mesures/ressourceParcoursComplet.spec.ts
@@ -1,19 +1,31 @@
 import assert from 'node:assert';
-import { describe, it } from 'node:test';
+import { beforeEach, describe, it } from 'node:test';
 import request from 'supertest';
 import { creeServeur } from '../../../src/api/msc.js';
 import { AdaptateurEnvironnement } from '../../../src/infra/adaptateurEnvironnement.js';
 import { configurationDeTestDuServeur, fauxAdaptateurEnvironnement } from '../fauxObjets.js';
+import { encodeSession } from '../cookie.js';
+import { jeanneDupont } from '../objetsPretsALEmploi.js';
+import { Express } from 'express';
 
 describe('La ressource du parcours complet', () => {
   describe('sur requête GET', () => {
+    let serveur: Express;
+    let cookieDeJeanneDupont: string;
+
+    beforeEach(() => {
+      serveur = creeServeur(configurationDeTestDuServeur);
+
+      cookieDeJeanneDupont = encodeSession({
+        email: jeanneDupont.email,
+        token: 'valide',
+      });
+    });
+
     it('retourne 200', async () => {
-      const serveur = creeServeur(configurationDeTestDuServeur);
+      const reponse = await request(serveur).get('/api/parcours/complet').set('Cookie', cookieDeJeanneDupont);
 
-      const reponse = await request(serveur).get('/api/parcours/complet');
-
-      const statut = reponse.status;
-      assert.equal(statut, 200);
+      assert.equal(reponse.status, 200);
     });
 
     it('retourne 404 si la fonctionnalité est désactivée', async () => {
@@ -30,11 +42,19 @@ describe('La ressource du parcours complet', () => {
         ...configurationDeTestDuServeur,
         adaptateurEnvironnement,
       };
-      const serveur = creeServeur(configuration);
+      const serveurSansLaRessource = creeServeur(configuration);
+
+      const reponse = await request(serveurSansLaRessource).get('/api/parcours/complet');
+
+      assert.equal(reponse.status, 404);
+    });
+
+    it("retourne 401 si l'utilisateur n'est pas connecté", async () => {
+      const serveur = creeServeur(configurationDeTestDuServeur);
 
       const reponse = await request(serveur).get('/api/parcours/complet');
 
-      assert.equal(reponse.status, 404);
+      assert.equal(reponse.status, 401);
     });
   });
 });
