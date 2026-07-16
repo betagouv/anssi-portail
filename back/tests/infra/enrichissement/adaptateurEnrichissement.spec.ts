3e3ab4903fe93e0bcf1f09cf4a6a700a39ea3e5f
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-07-16 10:58:47 +0200

    [SOIN] Simplifie les tests des liens canoniques

--- back/tests/infra/enrichissement/adaptateurEnrichissement.spec.ts
+++ back/tests/infra/enrichissement/adaptateurEnrichissement.spec.ts
@@ -1,25 +1,31 @@
-import { describe, it } from 'node:test';
-import { fabriqueAdaptateurEnrichissement } from '../../../src/infra/enrichissement/adaptateurEnrichissement.js';
+import { beforeEach, describe, it } from 'node:test';
+import {
+  AdaptateurEnrichissement,
+  fabriqueAdaptateurEnrichissement,
+} from '../../../src/infra/enrichissement/adaptateurEnrichissement.js';
 import { fauxAdaptateurEnvironnement, fauxFournisseurDeChemin } from '../../api/fauxObjets.js';
 import { EntrepotGuideMemoire } from '../../persistance/entrepotGuideMemoire.js';
 import { EntrepotExigenceMemoire } from '../../persistance/entrepotExigenceMemoire.js';
 import assert from 'node:assert';
 
 describe("L'adaptateur qui enrichie le html servi", () => {
-  describe('sait faire le lien canonique', () => {
-    it("lorsqu'on sert une page financement", async () => {
-      const adaptateurEnrichissement = await fabriqueAdaptateurEnrichissement(
-        fauxAdaptateurEnvironnement,
-        fauxFournisseurDeChemin,
-        new EntrepotGuideMemoire(),
-        new EntrepotExigenceMemoire()
-      );
+  let adaptateurEnrichissement: AdaptateurEnrichissement;
 
-      const htmlFactice = `
+  beforeEach(async () => {
+    adaptateurEnrichissement = await fabriqueAdaptateurEnrichissement(
+      fauxAdaptateurEnvironnement,
+      fauxFournisseurDeChemin,
+      new EntrepotGuideMemoire(),
+      new EntrepotExigenceMemoire()
+    );
+  });
+
+  const fabriqueHtmlFactice = (lienCanonique: string) => {
+    return `
 <!doctype html>
 <html lang="en">
   <head>
-    <link rel="canonical" href="http://localhost:3000/financements">
+    <link rel="canonical" href="${lienCanonique}">
     <title>titre</title>
     <link nonce="%%NONCE%%" />
   </head>
@@ -30,36 +36,22 @@ describe("L'adaptateur qui enrichie le html servi", () => {
   </body>
 </html>
       `;
+  };
+
+  describe('sait modifier le lien canonique', () => {
+    it("lorsqu'on sert une page financement", async () => {
+      const htmlFactice = fabriqueHtmlFactice('http://localhost:3000/financements');
 
       const rendu = await adaptateurEnrichissement.enrichisAvecComposants(htmlFactice, '/financements/1');
+
       assert.match(rendu, /<link rel="canonical" href="http:\/\/localhost:3000\/financements\/1">/);
     });
 
     it("lorsqu'on sert une page de guide", async () => {
-      const adaptateurEnrichissement = await fabriqueAdaptateurEnrichissement(
-        fauxAdaptateurEnvironnement,
-        fauxFournisseurDeChemin,
-        new EntrepotGuideMemoire(),
-        new EntrepotExigenceMemoire()
-      );
-
-      const htmlFactice = `
-<!doctype html>
-<html lang="en">
-  <head>
-    <link rel="canonical" href="http://localhost:3000/guides">
-    <title>titre</title>
-    <link nonce="%%NONCE%%" />
-  </head>
-  <body>
-    Ce fichier est utilisé pour renvoyer un contenu HTML factice pour les tests.
-    <script nonce="%%NONCE%%"></script>
-    <script src="/un-script.js?version=%%VERSION%%"></script>
-  </body>
-</html>
-      `;
+      const htmlFactice = fabriqueHtmlFactice('http://localhost:3000/guides');
 
       const rendu = await adaptateurEnrichissement.enrichisAvecComposants(htmlFactice, '/guides/identifiant-dun-guide');
+
       assert.match(rendu, /<link rel="canonical" href="http:\/\/localhost:3000\/guides\/identifiant-dun-guide">/);
     });
   });
