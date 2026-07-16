8266ca7c38bc9b93afd1de5a97246a765672ad4a
jpaulineau <jpaulineau@ippon.fr>
2026-07-16 16:43:01 +0200

    [PARCOURS SECURISATION] Limite l'affichage des interlocuteurs au parcours complet

--- front/lib-svelte/src/parcours-securisation/InterlocuteursParcoursSecurisation.svelte
+++ front/lib-svelte/src/parcours-securisation/InterlocuteursParcoursSecurisation.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
   import CarrouselDesInterlocuteurs from '../interlocuteurs/CarrouselDesInterlocuteurs.svelte';
+  import { profilStore } from '../stores/profil.store';
 </script>
 
-<dsfr-container>
-  <CarrouselDesInterlocuteurs mode="compact" />
-</dsfr-container>
+{#if $profilStore?.parcoursSecurisation.parcoursActuel === 'complet'}
+  <dsfr-container>
+    <CarrouselDesInterlocuteurs mode="compact" />
+  </dsfr-container>
+{/if}
 
 <style lang="scss">
   dsfr-container {
     padding-top: 3rem;
+    background-color: var(--background-alt-grey);
   }
 </style>
