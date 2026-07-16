26d4020a6e332ab223caabc59872ce8301a1b310
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-07-16 17:35:34 +0200

    [SSR] Adapte le titre de la page financement
    côté serveur

--- back/tests/infra/enrichissement/adaptateurEnrichissement.spec.ts
+++ back/tests/infra/enrichissement/adaptateurEnrichissement.spec.ts
@@ -7,21 +7,23 @@ import { fauxAdaptateurEnvironnement, fauxFournisseurDeChemin } from '../../api/
 import { EntrepotGuideMemoire } from '../../persistance/entrepotGuideMemoire.js';
 import { EntrepotExigenceMemoire } from '../../persistance/entrepotExigenceMemoire.js';
 import assert from 'node:assert';
-import { guideDevsecops } from '../../api/objetsPretsALEmploi.js';
+import { financementCyberPME, guideDevsecops } from '../../api/objetsPretsALEmploi.js';
 import { EntrepotFinancementMemoire } from '../../persistance/entrepotFinancementMemoire.js';
 
 describe("L'adaptateur qui enrichie le html servi", () => {
   let adaptateurEnrichissement: AdaptateurEnrichissement;
   let entrepôtGuide: EntrepotGuideMemoire;
+  let entrepôtFinancement: EntrepotFinancementMemoire;
 
   beforeEach(async () => {
     entrepôtGuide = new EntrepotGuideMemoire();
+    entrepôtFinancement = new EntrepotFinancementMemoire();
     adaptateurEnrichissement = await fabriqueAdaptateurEnrichissement(
       fauxAdaptateurEnvironnement,
       fauxFournisseurDeChemin,
       entrepôtGuide,
       new EntrepotExigenceMemoire(),
-      new EntrepotFinancementMemoire()
+      entrepôtFinancement
     );
   });
 
@@ -69,6 +71,15 @@ describe("L'adaptateur qui enrichie le html servi", () => {
       const rendu = await adaptateurEnrichissement.enrichisAvecComposants(htmlFactice, '/guides/devsecops');
 
       assert.match(rendu, /<title>DevSecOps | MesServicesCyber<\/title>/);
+    });
+
+    it("lorsqu'on sert une page de financement", async () => {
+      await entrepôtFinancement.ajoute(financementCyberPME);
+      const htmlFactice = fabriqueHtmlFactice('http://localhost:3000/financements');
+
+      const rendu = await adaptateurEnrichissement.enrichisAvecComposants(htmlFactice, '/financements/1');
+
+      assert.match(rendu, /<title>Cyber PME | MesServicesCyber<\/title>/);
     });
   });
 });
