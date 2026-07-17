38e0c2ac62c51149e56396ab87b1c4453517acae
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2026-07-17 15:17:24 +0200

    [DSFR] Crée le composant Tuile en Svelte
    ... et utilise le `<dsfr-tile>` de l'UI Kit.
    Pas d'intéraction dans cette première version.

---
+++ front/lib-svelte/src/ui/Tuile.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  type Props = {
+    titre: string;
+    pictogram?: string;
+  };
+  const { titre, pictogram }: Props = $props();
+</script>
+
+<dsfr-tile title={titre} hasDescription={$$slots.description} {pictogram} actionMarkup="false">
+  <slot name="pictogram" slot="pictogram"></slot>
+  <slot name="description" slot="description"></slot>
+</dsfr-tile>
