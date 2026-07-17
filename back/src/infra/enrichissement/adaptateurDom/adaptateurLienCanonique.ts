a35eb9e9280c5e0c7671cbc8a002b8625d2e2068
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-07-17 11:39:03 +0200

    [SOIN] Rends les liens canoniques paramétrables

--- back/src/infra/enrichissement/adaptateurDom/adaptateurLienCanonique.ts
+++ back/src/infra/enrichissement/adaptateurDom/adaptateurLienCanonique.ts
@@ -2,6 +2,13 @@ import { JSDOM } from 'jsdom';
 import { AdaptateurDom } from './adaptateurDom.js';
 
 export class AdaptateurLienCanonique implements AdaptateurDom {
+  private readonly motifRoutes: RegExp;
+
+  constructor(private readonly routesRacines: string[]) {
+    const alternatives = routesRacines.map((route) => route.replace(/^\//, '')).join('|');
+    this.motifRoutes = new RegExp(`/(${alternatives})$`);
+  }
+
   adapte(dom: JSDOM, routeDemandée: string) {
     const lienCanonique = dom.window.document.querySelector('link[rel="canonical"]');
     if (!lienCanonique) {
@@ -9,7 +16,7 @@ export class AdaptateurLienCanonique implements AdaptateurDom {
     }
 
     const href = lienCanonique.getAttribute('href') ?? '';
-    const nouveauHref = href.replace(/\/(financements|guides)$/, routeDemandée);
+    const nouveauHref = href.replace(this.motifRoutes, routeDemandée);
     lienCanonique.setAttribute('href', nouveauHref);
   }
 }
