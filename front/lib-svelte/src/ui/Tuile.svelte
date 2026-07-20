c9a3b602264ef7dd6edc328b412fb4a9c8b5b905
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2026-07-20 09:52:19 +0200

    [DSFR] Améliore la tuile Svelte
    - ajoute la description texte qui s'affiche en-dessous (la description en slot s'affiche au-dessus)
    - ajoute le mode horizontal, le mode élargi et la taille
    - ajoute un mode "compact" qui modifie les autres propriétés directement (horizontal, taille) si elles ne sont pas assignées depuis le parent
    - ajoute le mode lien avec son href

--- front/lib-svelte/src/ui/Tuile.svelte
+++ front/lib-svelte/src/ui/Tuile.svelte
@@ -1,12 +1,57 @@
 <script lang="ts">
+  import { untrack } from 'svelte';
+
+  // ATTENTION: Le `slot` description n'a pas le style "fr-tile_desc" et donc ne s'affiche pas au
+  // "bon" endroit : il est au-dessus du titre au lieu d'être en-dessous.
+  // Ce comportement est voulu sur les tuiles de la page Statistiques mais pas dans
+  // CarrouselDesInterlocuteurs où la description est en-dessous.
+  //
+  // Utilisez le `<slot>` pour mettre la description au-dessus et l'attribut `descriptionTexte`
+  // pour le placer en-dessous !
+
   type Props = {
+    descriptionTexte?: string;
+    élargi?: boolean;
+    horizontal?: boolean;
+    href?: string;
+    compact?: boolean;
     titre: string;
-    pictogram?: string;
+    pictogramme?: string;
+    taille?: 'sm' | 'md';
   };
-  const { titre, pictogram }: Props = $props();
+
+  let {
+    descriptionTexte: descriptionTexteInitiale,
+    élargi: enlarge = false,
+    horizontal,
+    href,
+    compact: compactInitial,
+    titre: title,
+    pictogramme: pictogram,
+    taille: size,
+  }: Props = $props();
+
+  const compact = untrack(() => compactInitial);
+  const descriptionTexte = untrack(() => descriptionTexteInitiale);
+
+  const hasDescription = ($$slots.description || descriptionTexte) && !compact;
+  const actionMarkup = $derived(href ? 'a' : false);
+
+  horizontal ??= compact;
+  size ??= compact ? 'sm' : undefined; // garde la taille par défaut
 </script>
 
-<dsfr-tile title={titre} hasDescription={$$slots.description} {pictogram} actionMarkup="false">
+<dsfr-tile
+  {title}
+  description={descriptionTexte}
+  {hasDescription}
+  {enlarge}
+  {pictogram}
+  {horizontal}
+  {size}
+  {actionMarkup}
+  {href}
+>
   <slot name="pictogram" slot="pictogram"></slot>
   <slot name="description" slot="description"></slot>
 </dsfr-tile>
