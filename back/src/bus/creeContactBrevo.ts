9e56ff1cc3eccf6d8b6838459e5bd938c6e330f4
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-07-20 14:58:20 +0200

    [PIXEL] Enregistre le consentement dans Brevo

--- back/src/bus/creeContactBrevo.ts
+++ back/src/bus/creeContactBrevo.ts
@@ -2,7 +2,7 @@ import { AdaptateurEmail } from '../metier/adaptateurEmail.js';
 import { CompteCree } from './evenements/compteCree.js';
 
 export const creeContactBrevo = ({ adaptateurEmail }: { adaptateurEmail: AdaptateurEmail }) => {
-  return async function ({ email, nom, prenom, infoLettre, telephone }: CompteCree) {
-    await adaptateurEmail.creeContactBrevo({ email, nom, prenom, infoLettre, telephone });
+  return async function ({ email, nom, prenom, infoLettre, pixelDeSuiviAccepté, telephone }: CompteCree) {
+    await adaptateurEmail.creeContactBrevo({ email, nom, prenom, infoLettre, pixelDeSuiviAccepté, telephone });
   };
 };
