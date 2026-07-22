f1a3ec50c39905d058219ef80659833bf0db512d
Cédric Prezelin <cedric.prezelin.ext@beta.gouv.fr>
2026-07-22 17:22:36 +0200

    [PARCOURS SÉCURISATION] Complète le contenu de la modale de tutoriel

--- front/lib-svelte/src/parcours-securisation/TutorielMesure.svelte
+++ front/lib-svelte/src/parcours-securisation/TutorielMesure.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Lien from '../ui/Lien.svelte';
   import Modale from '../ui/Modale.svelte';
   import type { Mesure } from './mesure';
 
@@ -30,9 +31,35 @@
     >
       <dsfr-badge slot="badgesgroup" label="Tutoriel" type="accent" accent="purple-glycine"></dsfr-badge>
     </dsfr-card>
+
     <Modale bind:estOuverte={étatDesModales[index]}>
-      {tutoriel.titre}</Modale
-    >
+      <h4>
+        {tutoriel.titre}
+      </h4>
+
+      {#if tutoriel.description}
+        <p class="texte-standard-md">{tutoriel.description}</p>
+      {/if}
+
+      <h5 class="fr-h6">Étapes générales</h5>
+
+      <ol>
+        {#each tutoriel.étapes as étape, index (index)}
+          <!-- eslint-disable-next-line svelte/no-at-html-tags-->
+          <li>{@html étape}</li>
+        {/each}
+      </ol>
+
+      {#if tutoriel.note}
+        <h5 class="fr-h6">Remarque</h5>
+        <p class="texte-standard-md">{tutoriel.note}</p>
+      {/if}
+
+      {#if tutoriel.lienPourAllerPlusLoin}
+        <h5 class="fr-h6">Pour aller plus loin</h5>
+        <Lien href={tutoriel.lienPourAllerPlusLoin.url} libelle={tutoriel.lienPourAllerPlusLoin.libelle}></Lien>
+      {/if}
+    </Modale>
   {/each}
   {#if mesure.liens?.length > 0}
     <div class="section-aide">
