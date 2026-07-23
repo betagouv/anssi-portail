0a89ba42405bda045ced21fac4c1185111194a93
Cédric Prezelin <cedric.prezelin.ext@beta.gouv.fr>
2026-07-23 09:31:45 +0200

    [SOIN] Extrais la modale de tutoriel

--- front/lib-svelte/src/parcours-securisation/TutorielMesure.svelte
+++ front/lib-svelte/src/parcours-securisation/TutorielMesure.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-  import Lien from '../ui/Lien.svelte';
-  import Modale from '../ui/Modale.svelte';
   import type { Mesure } from './mesure';
+  import ModaleTutoriel from './ModaleTutoriel.svelte';
 
   const { mesure }: { mesure: Mesure } = $props();
   // svelte-ignore state_referenced_locally
@@ -32,34 +31,7 @@
       <dsfr-badge slot="badgesgroup" label="Tutoriel" type="accent" accent="purple-glycine"></dsfr-badge>
     </dsfr-card>
 
-    <Modale bind:estOuverte={étatDesModales[index]}>
-      <h4>
-        {tutoriel.titre}
-      </h4>
-
-      {#if tutoriel.description}
-        <p class="texte-standard-md">{tutoriel.description}</p>
-      {/if}
-
-      <h5 class="fr-h6">Étapes générales</h5>
-
-      <ol>
-        {#each tutoriel.étapes as étape, index (index)}
-          <!-- eslint-disable-next-line svelte/no-at-html-tags-->
-          <li>{@html étape}</li>
-        {/each}
-      </ol>
-
-      {#if tutoriel.note}
-        <h5 class="fr-h6">Remarque</h5>
-        <p class="texte-standard-md">{tutoriel.note}</p>
-      {/if}
-
-      {#if tutoriel.lienPourAllerPlusLoin}
-        <h5 class="fr-h6">Pour aller plus loin</h5>
-        <Lien href={tutoriel.lienPourAllerPlusLoin.url} libelle={tutoriel.lienPourAllerPlusLoin.libelle}></Lien>
-      {/if}
-    </Modale>
+    <ModaleTutoriel {tutoriel} bind:estOuverte={étatDesModales[index]} />
   {/each}
   {#if mesure.liens?.length > 0}
     <div class="section-aide">
