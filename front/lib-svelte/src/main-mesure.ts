3accbab7c6b2e528fb73e3dd5f6b037299520cae
Cédric Prezelin <cedric.prezelin.ext@beta.gouv.fr>
2026-07-23 09:39:43 +0200

    [SOIN] Créé un dossier dédié aux mesures

--- front/lib-svelte/src/main-mesure.ts
+++ front/lib-svelte/src/main-mesure.ts
@@ -1,5 +1,5 @@
 import { mount } from 'svelte';
-import Mesure from './parcours-securisation/Mesure.svelte';
+import Mesure from './parcours-securisation/mesure/Mesure.svelte';
 
 mount(Mesure, {
   target: document.getElementById('mesure')!,
