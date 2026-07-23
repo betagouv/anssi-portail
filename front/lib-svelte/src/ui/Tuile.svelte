ea95d563409e4290e37db67ecb219b7cf35dae75
Cédric Prezelin <cedric.prezelin.ext@beta.gouv.fr>
2026-07-23 12:19:18 +0200

    [SOIN] Nettoie le composant Tuile

--- front/lib-svelte/src/ui/Tuile.svelte
+++ front/lib-svelte/src/ui/Tuile.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import type { Snippet } from 'svelte';
   import { untrack } from 'svelte';
 
   // ATTENTION: Le `slot` description n'a pas le style "fr-tile_desc" et donc ne s'affiche pas au
@@ -6,56 +7,59 @@
   // Ce comportement est voulu sur les tuiles de la page Statistiques mais pas dans
   // CarrouselDesInterlocuteurs où la description est en-dessous.
   //
-  // Utilisez le `<slot>` pour mettre la description au-dessus et l'attribut `descriptionTexte`
+  // Utilisez le snippet `description` pour mettre la description au-dessus et l'attribut `descriptionTexte`
   // pour le placer en-dessous !
 
   type Props = {
-    descriptionTexte?: string;
+    description?: Snippet | string;
     élargi?: boolean;
     horizontal?: boolean;
     href?: string;
     compact?: boolean;
+    illustration?: Snippet;
     titre: string;
     pictogramme?: string;
     taille?: 'sm' | 'md';
   };
 
   let {
-    descriptionTexte: descriptionTexteInitiale,
+    description: descriptionInitiale,
     élargi: enlarge = false,
     horizontal,
     href,
     compact: compactInitial,
+    illustration,
     titre: title,
     pictogramme: pictogram,
     taille: size,
   }: Props = $props();
 
   const compact = untrack(() => compactInitial);
-  const descriptionTexte = untrack(() => descriptionTexteInitiale);
+  const description = untrack(() => typeof descriptionInitiale === 'string' && descriptionInitiale);
+  const descriptionSnippet = untrack(() => typeof descriptionInitiale !== 'string' && descriptionInitiale);
 
-  const hasDescription = ($$slots.description || descriptionTexte) && !compact;
+  const hasDescription = $derived(Boolean(descriptionInitiale) && !compact);
   const actionMarkup = $derived(href ? 'a' : false);
 
   horizontal ??= compact;
   size ??= compact ? 'sm' : undefined; // garde la taille par défaut
 </script>
 
-<dsfr-tile
-  {title}
-  description={descriptionTexte}
-  {hasDescription}
-  {enlarge}
-  {pictogram}
-  {horizontal}
-  {size}
-  {actionMarkup}
-  {href}
->
-  <slot name="pictogram" slot="pictogram"></slot>
-  <slot name="description" slot="description"></slot>
+<dsfr-tile {title} {description} {hasDescription} {enlarge} {pictogram} {horizontal} {size} {actionMarkup} {href}>
+  {#if illustration}
+    <span slot="pictogram">{@render illustration()}</span>
+  {/if}
+  {#if descriptionSnippet}
+    <span slot="description">{@render descriptionSnippet()}</span>
+  {/if}
   <hgroup slot="seo">
     <h3><a {href}>{title}</a></h3>
-    <p>{descriptionTexte}</p>
+    {#if hasDescription}
+      {#if descriptionSnippet}
+        {@render descriptionSnippet()}
+      {:else}
+        <p>{description}</p>
+      {/if}
+    {/if}
   </hgroup>
 </dsfr-tile>
