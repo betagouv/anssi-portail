bcc2d94a5182c640c102a9babc6389fba12f78f0
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-07-28 15:33:16 +0200

    [DA] Utilise le Héros riche sur les entreprises

--- front/lib-svelte/src/protection/entreprises/PresentationEntreprises.svelte
+++ front/lib-svelte/src/protection/entreprises/PresentationEntreprises.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+  import { afficheNouvelleDA } from '$plateforme/environnement';
+  import Alternatives from '../../ui/Alternatives.svelte';
   import { fabriqueFilAriane } from '../../ui/filAriane';
   import FilAriane, { type Props as PropriétésFilAriane } from '../../ui/FilAriane.svelte';
   import Heros from '../../ui/Heros.svelte';
+  import HerosRiche from '../../ui/HerosRiche.svelte';
   import Proteger from '../Proteger.svelte';
 
   const propriétésFilAriane: PropriétésFilAriane = {
@@ -10,29 +13,45 @@
   };
 </script>
 
-<Heros
-  cacheActions
-  cacheIllustration={false}
-  cacheTags
-  description="Toutes les entreprises sont exposées au risque de cyberattaques. En 2025, les TPE/PME et ETI ont notamment
+<Alternatives affichageAlternatif={afficheNouvelleDA}>
+  {#snippet défaut()}
+    <Heros
+      cacheActions
+      cacheIllustration={false}
+      cacheTags
+      description="Toutes les entreprises sont exposées au risque de cyberattaques. En 2025, les TPE/PME et ETI ont notamment
       représenté 48&nbsp;% des victimes de rançongiciels ou ransowmare représentant les premières entités visées."
-  format="heros"
-  illustrationAlt=""
-  illustrationSource="/assets/images/personne-avec-laptop-entoure-de-diagrammes.svg"
-  theme="sombre"
-  titre="Protéger mon entreprise contre les cyberattaques"
-  segmentsFilAriane={fabriqueFilAriane(propriétésFilAriane)}
->
-  {#snippet preambule()}
-    <h1 class="alternatif-xs">Protéger mon entreprise contre les cyberattaques</h1>
-    <p class="texte-chapo-xl">
-      Toutes les entreprises sont exposées au risque de cyberattaques. En 2025, les TPE/PME et ETI ont notamment
-      représenté 48&nbsp;% des victimes de rançongiciels ou ransowmare représentant les premières entités visées.
-    </p>
+      format="heros"
+      illustrationAlt=""
+      illustrationSource="/assets/images/personne-avec-laptop-entoure-de-diagrammes.svg"
+      theme="sombre"
+      titre="Protéger mon entreprise contre les cyberattaques"
+      segmentsFilAriane={fabriqueFilAriane(propriétésFilAriane)}
+    >
+      {#snippet preambule()}
+        <h1 class="alternatif-xs">Protéger mon entreprise contre les cyberattaques</h1>
+        <p class="texte-chapo-xl">
+          Toutes les entreprises sont exposées au risque de cyberattaques. En 2025, les TPE/PME et ETI ont notamment
+          représenté 48&nbsp;% des victimes de rançongiciels ou ransowmare représentant les premières entités visées.
+        </p>
+      {/snippet}
+      {#snippet filAriane()}
+        <FilAriane {...propriétésFilAriane} />
+      {/snippet}
+    </Heros>
   {/snippet}
-  {#snippet filAriane()}
-    <FilAriane {...propriétésFilAriane} />
+  {#snippet alternatif()}
+    <HerosRiche
+      description="Toutes les entreprises sont exposées au risque de cyberattaques. En 2025, les TPE/PME et ETI ont notamment
+      représenté 48&nbsp;% des victimes de rançongiciels ou ransowmare représentant les premières entités visées."
+      propriétésFilAriane={{ ...propriétésFilAriane, fondSombre: false }}
+      titre="Protéger mon entreprise contre les cyberattaques"
+    >
+      {#snippet illustration()}
+        <img src="/assets/images/personne-avec-laptop-entoure-de-diagrammes.svg" alt="" />
+      {/snippet}
+    </HerosRiche>
   {/snippet}
-</Heros>
+</Alternatives>
 
 <Proteger origine="landing-entreprises" />
