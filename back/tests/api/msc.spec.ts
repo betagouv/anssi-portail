8b959384f0911856145c61d7c694741f54612544
Cédric Prezelin <cedric.prezelin.ext@beta.gouv.fr>
2026-07-29 17:58:26 +0200

    [SOIN] Simplifie la gestion des redirections

--- back/tests/api/msc.spec.ts
+++ back/tests/api/msc.spec.ts
@@ -4,6 +4,7 @@ import request from 'supertest';
 import { creeServeur } from '../../src/api/msc.js';
 import { configurationDeTestDuServeur, fauxFournisseurDeChemin } from './fauxObjets.js';
 import { join } from 'node:path';
+import { HttpStatusCode } from 'axios';
 
 describe('La configuration de notre serveur', () => {
   it('compresse les réponses', async () => {
@@ -24,5 +25,41 @@ describe('La configuration de notre serveur', () => {
 
     assert.equal(entetes['content-encoding'], 'br');
     assert.equal(entetes['vary'], 'Accept-Encoding');
+  });
+
+  it('redirige vers une url sans slashs finaux', async () => {
+    const serveur = creeServeur(configurationDeTestDuServeur);
+
+    const réponse = await request(serveur).get('/catalogue/');
+
+    assert.equal(réponse.statusCode, HttpStatusCode.PermanentRedirect);
+    assert.equal(réponse.headers.location, '/catalogue');
+  });
+
+  it('redirige vers une url avec paramètres de requête sans slashs finaux', async () => {
+    const serveur = creeServeur(configurationDeTestDuServeur);
+
+    const réponse = await request(serveur).get('/test/?param=value');
+
+    assert.equal(réponse.statusCode, HttpStatusCode.PermanentRedirect);
+    assert.equal(réponse.headers.location, '/test?param=value');
+  });
+
+  it("redirige vers l'URL voulue sans slash final", async () => {
+    const serveur = creeServeur(configurationDeTestDuServeur);
+
+    const réponse = await request(serveur).get('/guides');
+
+    assert.equal(réponse.statusCode, HttpStatusCode.MovedPermanently);
+    assert.equal(réponse.headers.location, '/catalogue');
+  });
+
+  it("redirige vers l'URL voulue avec slash final", async () => {
+    const serveur = creeServeur(configurationDeTestDuServeur);
+
+    const réponse = await request(serveur).get('/guides/');
+
+    assert.equal(réponse.statusCode, HttpStatusCode.MovedPermanently);
+    assert.equal(réponse.headers.location, '/catalogue');
   });
 });
