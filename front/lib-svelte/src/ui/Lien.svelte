0d619d1b43f92e663e1cc756e4a347f134f384ae
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-08-04 17:49:55 +0200

    [PARCOURS SECURISATION] Trace les clics depuis la vues de mesures
    ...afin de mesurer l'impact de cette vue, et de son usage

--- front/lib-svelte/src/ui/Lien.svelte
+++ front/lib-svelte/src/ui/Lien.svelte
@@ -57,12 +57,19 @@
         }[type]
       : 'primary'
   );
-  const traceClic = () => {
+  const traceClicVersMatomo = () => {
     const cible = href.startsWith('/') ? `${window.location.protocol}//${window.location.host}${href}` : href;
-    window._paq?.push(['trackLink', cible, telechargement ? 'download' : 'link']);
+    window._paq?.push([
+      'trackLink',
+      cible,
+      telechargement ? 'download' : 'link',
+      {
+        dimension2: window.location.href,
+      },
+    ]);
   };
   const auClic = (e: MouseEvent | KeyboardEvent) => {
-    traceClic();
+    traceClicVersMatomo();
     surClic?.(e);
   };
 </script>
