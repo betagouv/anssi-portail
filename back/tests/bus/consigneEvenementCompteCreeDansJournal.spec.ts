5033c03bc51656b317c1d7c70991e6828eb61d79
Cédric Prezelin <cedric.prezelin.ext@beta.gouv.fr>
2026-08-05 14:24:44 +0200

    [SOIN] Rends la structure du test extensible

--- back/tests/bus/consigneEvenementCompteCreeDansJournal.spec.ts
+++ back/tests/bus/consigneEvenementCompteCreeDansJournal.spec.ts
@@ -8,11 +8,11 @@ import { AdaptateurHachage } from '../../src/infra/adaptateurHachage.js';
 import { fauxAdaptateurHachage } from '../api/fauxObjets.js';
 
 describe("L'abonnement qui consigne la création d'un compte utilisateur dans le journal", () => {
-  it('consigne un évènement de NouvelUtilisateurInscrit', async () => {
-    let evenementRecu;
+  it("consigne un évènement avec les données de l'utilisateur", async () => {
+    let évènementReçu;
     const adaptateurJournal: AdaptateurJournal = {
-      consigneEvenement: async (donneesEvenement: unknown) => {
-        evenementRecu = donneesEvenement;
+      consigneEvenement: async (donnéesÉvÈnement: unknown) => {
+        évènementReçu = donnéesÉvÈnement;
       },
     };
     const adaptateurHorloge: AdaptateurHorloge = {
@@ -38,10 +38,9 @@ describe("L'abonnement qui consigne la création d'un compte utilisateur dans le
       })
     );
 
-    assert.deepEqual(evenementRecu, {
-      type: 'NOUVEL_UTILISATEUR_INSCRIT',
-      donnees: { idUtilisateur: 'u1@mail.com-hacheHMAC' },
-      date: new Date('2025-03-10'),
-    });
+    assert.notEqual(évènementReçu, undefined);
+    assert.equal(évènementReçu!.type, 'NOUVEL_UTILISATEUR_INSCRIT');
+    assert.equal(évènementReçu!.donnees?.idUtilisateur, 'u1@mail.com-hacheHMAC');
+    assert.deepEqual(évènementReçu!.date, new Date('2025-03-10'));
   });
 });
