1fb7a6783a0f34f9e5d885ebcde5230bd2cc5366
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2026-08-10 12:13:02 +0200

    [DA] Utilise un dsfr-callout pour le composant BasculeParcoursComplet

--- front/lib-svelte/src/parcours-securisation/BasculeParcoursComplet.svelte
+++ front/lib-svelte/src/parcours-securisation/BasculeParcoursComplet.svelte
@@ -2,55 +2,25 @@
   import Lien from '../ui/Lien.svelte';
 </script>
 
-<div class="encart">
-  <h4>Protégez votre organisation dans la durée</h4>
-  <p class="texte-standard-md">
-    Passer au niveau supérieur et continuez à progresser avec des mesures plus avancées pour renforcer vos pratiques,
-    mieux structurer vos actions et protéger votre organisation dans la durée.
-  </p>
-  <div class="lien">
+<dsfr-callout
+  text="Passer au niveau supérieur et continuez à progresser avec des mesures plus avancées pour renforcer vos pratiques,
+    mieux structurer vos actions et protéger votre organisation dans la durée."
+  title="Protégez votre organisation dans la durée"
+  title-markup="h4"
+  button-label="En savoir plus"
+  icon="info-line"
+  accent="blue-ecume"
+  has-title
+  has-button
+>
+  <div slot="button">
     <Lien
       apparence="bouton"
-      libelle="Continuer à renforcer ma sécurité"
+      libelle="Poursuivre le parcours"
       etire
-      icone="arrow-right-line"
+      icone="arrow-right-circle-line"
       iconeADroite
       href="/parcours-complet"
     />
   </div>
-</div>
-
-<style lang="scss">
-  @use '../../../assets/styles/responsive' as *;
-  .encart {
-    display: grid;
-    padding: 1.5rem;
-    background-color: var(--background-alt-blue-france);
-    grid-template-areas: 'titre' 'description' 'action';
-
-    @include a-partir-de(lg) {
-      grid-template-areas: 'titre action' 'description action';
-      column-gap: 1.5rem;
-    }
-
-    h4 {
-      margin-bottom: 1rem;
-      grid-area: titre;
-    }
-
-    p {
-      grid-area: description;
-    }
-
-    .lien {
-      grid-area: action;
-      display: flex;
-      flex-direction: column;
-      @include a-partir-de(md) {
-        flex-direction: row;
-        align-items: center;
-        padding: 10px;
-      }
-    }
-  }
-</style>
+</dsfr-callout>
