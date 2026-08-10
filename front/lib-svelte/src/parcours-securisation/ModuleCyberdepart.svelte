0ce0b1764ad67705c9536156cf9c28771d36abf1
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2026-08-10 11:43:24 +0200

    [WORDING] Modifie le texte du toaster de mesure prise en compte

--- front/lib-svelte/src/parcours-securisation/ModuleCyberdepart.svelte
+++ front/lib-svelte/src/parcours-securisation/ModuleCyberdepart.svelte
@@ -28,7 +28,7 @@
     const reponse = await axios.get<ModuleRéponseApi>(`/api/modules/1`);
     module = reponse.data;
     if (sessionStorage.getItem('mesure-prise-en-compte') === 'true') {
-      toasterStore.succes('Mesure déclarée prise en compte', 'Mesure déclarée prise en compte');
+      toasterStore.succes('Mesure déclarée prise en compte', 'Votre progression a été mise à jour.');
       sessionStorage.removeItem('mesure-prise-en-compte');
     }
     if (sessionStorage.getItem('badge-cyberdepart-debloque') === 'true') {
