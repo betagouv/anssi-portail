09584b4ddff69b8437d241ca6bedb252337960a2
Stéfanie LOISELEUR <stefanie.loiseleur@beta.gouv.fr>
2026-08-11 14:12:40 +0200

    Ajoute une ligne horizontale sous l'étapier

--- front/lib-svelte/src/ui/Etapier.svelte
+++ front/lib-svelte/src/ui/Etapier.svelte
@@ -27,3 +27,14 @@
   hideDetails={cacheTitreEtapeSuivante}
 >
 </dsfr-stepper>
+
+<hr />
+
+<style>
+  hr {
+    margin-block: 2rem;
+    height: 1px;
+    border: 0;
+    background-color: var(--border-default-grey);
+  }
+</style>
