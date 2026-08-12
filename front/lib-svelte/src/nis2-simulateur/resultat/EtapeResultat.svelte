58a8cf57bd191e334e1ba74b5b872200a9cbbc17
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-08-12 09:41:20 +0200

    [CORRECTION] Affiche la description et retire la hauteur mimnimale

--- front/lib-svelte/src/nis2-simulateur/resultat/EtapeResultat.svelte
+++ front/lib-svelte/src/nis2-simulateur/resultat/EtapeResultat.svelte
@@ -34,12 +34,14 @@
       <dsfr-card
         title="Accéder à la liste d'exigences applicables à ReCyF (NIS&nbsp;2)"
         description="Découvrez la liste d'exigences applicables à ReCyF (NIS&nbsp;2) pour atteindre les objectifs de sécurité fixés par NIS&nbsp;2 ainsi que l'outil de comparaison de référentiels."
+        hasDescription
         href="/nis2#exigences"
         enlarge="true"
       ></dsfr-card>
       <dsfr-card
         title="S'abonner à la Newsletter MesServicesCyber"
         description="Restez informé des dernières actualités de la directive NIS&nbsp;2 et autres actualités."
+        hasDescription
         href="/abonnement-infolettre?adresseRetour=/nis2"
         enlarge="true"
       ></dsfr-card>
@@ -68,6 +70,10 @@
       flex-direction: column;
       gap: 16px;
       margin-top: 36px;
+
+      dsfr-card {
+        min-height: auto;
+      }
 
       @include a-partir-de(lg) {
         flex-direction: row;
