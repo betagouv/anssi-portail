8d6e455f83c2e895a60a55de6a1a22c666ffbabc
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-08-13 11:22:23 +0200

    Permet de mettre en pause les animations via un bouton

--- front/lib-svelte/src/accueil/HerosAccueil.svelte
+++ front/lib-svelte/src/accueil/HerosAccueil.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Bouton from '../ui/Bouton.svelte';
   import Lien from '../ui/Lien.svelte';
   import MachineAEcrireRotative from '../ui/MachineAEcrireCyclique.svelte';
   import Surlignage from '../ui/Surlignage.svelte';
@@ -12,13 +13,20 @@
     'mon entreprise',
     'ma collectivité',
   ];
+
+  let enPause = $state(false);
+  const libelléPause = $derived(enPause ? 'Lancer les animations' : 'Mettre les animations en pause');
+
+  const basculePause = () => {
+    enPause = !enPause;
+  };
 </script>
 
 <dsfr-container>
   <div class="conteneur-heros">
     <div class="contenu">
       <div class="surtitre fr-h4">
-        <MachineAEcrireRotative {phrasesAnimées} préfixe="Je protège " />
+        <MachineAEcrireRotative {phrasesAnimées} préfixe="Je protège " {enPause} />
       </div>
       <h1 class="alternatif-md">
         Agissez pour votre <Surlignage>cybersécurité&nbsp;!</Surlignage>
@@ -37,7 +45,17 @@
       </div>
     </div>
     <div class="illustration">
-      <IllustrationAnimee />
+      <IllustrationAnimee {enPause} />
+    </div>
+    <div class="controle-animation">
+      <Bouton
+        type="secondaire-inverse"
+        icone={enPause ? 'play-circle-line' : 'pause-circle-line'}
+        iconeSeule
+        libelle={libelléPause}
+        titre={libelléPause}
+        surClic={basculePause}
+      />
     </div>
   </div>
 </dsfr-container>
@@ -51,12 +69,23 @@
   }
 
   .conteneur-heros {
+    position: relative;
     display: grid;
     align-content: center;
     grid-template-areas:
       'contenu'
       'illustration';
     padding-block: 6rem;
+
+    .controle-animation {
+      position: absolute;
+      right: 0;
+      bottom: 24px;
+
+      @media (prefers-reduced-motion: reduce) {
+        display: none;
+      }
+    }
 
     @include a-partir-de(lg) {
       grid-template-areas: 'contenu illustration';
