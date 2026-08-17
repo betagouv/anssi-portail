926fa6b0679068c43d284d851b7044148727bc05
jpaulineau <jpaulineau@ippon.fr>
2026-08-17 11:23:52 +0200

    [SOIN] Conditionne l'affichage de la section tutoriel
    ...à la présence de liens ou de tutoriels

--- front/lib-svelte/src/parcours-securisation/mesure/TutorielMesure.svelte
+++ front/lib-svelte/src/parcours-securisation/mesure/TutorielMesure.svelte
@@ -1,47 +1,53 @@
 <script lang="ts">
+  import { untrack } from 'svelte';
   import type { Mesure } from './../mesure';
   import ModaleTutoriel from './ModaleTutoriel.svelte';
 
   const { mesure }: { mesure: Mesure } = $props();
-  // svelte-ignore state_referenced_locally
-  let étatDesModales: boolean[] = $state(Array(mesure.tutoriels.length).fill(false));
+  let étatDesModales: boolean[] = $state(untrack(() => Array(mesure.tutoriels.length).fill(false)));
+  const aDesTutoriels: boolean = $derived(mesure.tutoriels.length > 0);
+  const aDesLiens: boolean = $derived(mesure.liens?.length > 0);
 
   const selectionneLeTutoriel = (index: number) => {
     étatDesModales[index] = true;
   };
 </script>
 
-<div class="contenu-section">
-  <h2>Comment faire concrètement</h2>
-  {#each mesure.tutoriels as tutoriel, index (index)}
-    <dsfr-card
-      hasBadge
-      title={tutoriel.titre}
-      horizontal
-      horizontalProportion="tier"
-      size="lg"
-      actionMarkup="button"
-      role="button"
-      tabindex={null}
-      onkeypress={(event: KeyboardEvent) => {
-        if (event.code === 'Enter' || event.code === 'Space') selectionneLeTutoriel(index);
-      }}
-      onclick={() => selectionneLeTutoriel(index)}
-    >
-      <dsfr-badge slot="badgesgroup" label="Tutoriel" type="accent" accent="purple-glycine"></dsfr-badge>
-    </dsfr-card>
+{#if aDesTutoriels || aDesLiens}
+  <div class="contenu-section">
+    {#if aDesTutoriels}
+      <h2>Comment faire concrètement</h2>
+    {/if}
+    {#each mesure.tutoriels as tutoriel, index (index)}
+      <dsfr-card
+        hasBadge
+        title={tutoriel.titre}
+        horizontal
+        horizontalProportion="tier"
+        size="lg"
+        actionMarkup="button"
+        role="button"
+        tabindex={null}
+        onkeypress={(event: KeyboardEvent) => {
+          if (event.code === 'Enter' || event.code === 'Space') selectionneLeTutoriel(index);
+        }}
+        onclick={() => selectionneLeTutoriel(index)}
+      >
+        <dsfr-badge slot="badgesgroup" label="Tutoriel" type="accent" accent="purple-glycine"></dsfr-badge>
+      </dsfr-card>
 
-    <ModaleTutoriel {tutoriel} bind:estOuverte={étatDesModales[index]} />
-  {/each}
-  {#if mesure.liens?.length > 0}
-    <div class="section-aide">
-      <p class="texte-article-lg">Pour aller plus loin</p>
-      {#each mesure.liens as lien (lien.libelle)}
-        <msc-lien href={lien.url} libelle={lien.libelle}></msc-lien>
-      {/each}
-    </div>
-  {/if}
-</div>
+      <ModaleTutoriel {tutoriel} bind:estOuverte={étatDesModales[index]} />
+    {/each}
+    {#if aDesLiens}
+      <div class="section-aide">
+        <p class="texte-article-lg">Pour aller plus loin</p>
+        {#each mesure.liens as lien (lien.libelle)}
+          <msc-lien href={lien.url} libelle={lien.libelle}></msc-lien>
+        {/each}
+      </div>
+    {/if}
+  </div>
+{/if}
 
 <style lang="scss">
   .contenu-section {
