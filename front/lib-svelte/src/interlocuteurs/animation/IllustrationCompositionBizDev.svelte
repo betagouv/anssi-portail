b4b59e96204db0e5a31bb3590332c8fefc76cc14
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-08-17 17:27:07 +0200

    Ajoute les animations sur le bloc de présentation des BizDev

---
+++ front/lib-svelte/src/interlocuteurs/animation/IllustrationCompositionBizDev.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import IllustrationAnimee from '../../accueil/animation/IllustrationAnimee.svelte';
+  import CompositionBizDev from './CompositionBizDev.svelte';
+
+  const scènes = [CompositionBizDev];
+</script>
+
+<IllustrationAnimee {scènes} étiquette="Floriane Gallego et Pierre Vernier, chargés de développement." />
