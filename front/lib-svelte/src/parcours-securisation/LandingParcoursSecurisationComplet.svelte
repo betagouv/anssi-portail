e25c43eec3ea607ff5093385c02f62675caac17c
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2026-08-17 11:39:57 +0200

    Coupe les mots sur le mobile

--- front/lib-svelte/src/parcours-securisation/LandingParcoursSecurisationComplet.svelte
+++ front/lib-svelte/src/parcours-securisation/LandingParcoursSecurisationComplet.svelte
@@ -118,6 +118,9 @@
 </section>
 
 <style lang="scss">
+  .section-hero-et-tuiles {
+    word-break: break-word;
+  }
   .section-tuiles {
     lab-anssi-carrousel-tuiles {
       margin-block-start: -2.5rem;
