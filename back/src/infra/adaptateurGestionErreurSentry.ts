1038cc9ce753c91ff45b284b496e6592073bd859
Cédric Prezelin <cedric.prezelin.ext@beta.gouv.fr>
2026-08-18 16:37:22 +0200

    [RENOVATE] Update dependency @sentry/node to v10

--- back/src/infra/adaptateurGestionErreurSentry.ts
+++ back/src/infra/adaptateurGestionErreurSentry.ts
@@ -1,39 +1,39 @@
 import * as Sentry from '@sentry/node';
-import { Express, NextFunction, Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import expressIpFilter from 'express-ipfilter';
 import { adaptateurEnvironnement } from './adaptateurEnvironnement.js';
+import { HttpStatusCode } from '@anssi-portail/axios';
 
 const { IpDeniedError } = expressIpFilter;
 
 export interface AdaptateurGestionErreur {
-  initialise(applicationExpress: Express): void;
+  initialise(): void;
   controleurErreurs(erreur: Error, requete: Request, reponse: Response, suite: NextFunction): void;
 }
 
 export const adaptateurGestionErreurSentry: AdaptateurGestionErreur = {
-  initialise: (applicationExpress: Express) => {
+  initialise: () => {
     const config = adaptateurEnvironnement.sentry();
 
     Sentry.init({
       dsn: config.dsn(),
       environment: config.environnement(),
-      integrations: [
-        new Sentry.Integrations.Express({ app: applicationExpress }),
-        new Sentry.Integrations.Postgres(),
-        ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
-      ],
+      integrations: [...Sentry.getAutoPerformanceIntegrations()],
     });
     Sentry.setTag('msc-source', 'backend');
-
-    applicationExpress.use(Sentry.Handlers.requestHandler());
-    applicationExpress.use(Sentry.Handlers.tracingHandler());
   },
   controleurErreurs: (erreur: Error, requete: Request, reponse: Response, suite: NextFunction) => {
     if (erreur instanceof IpDeniedError) {
-      reponse.status(401);
+      reponse.status(HttpStatusCode.Unauthorized);
       reponse.end();
     } else {
-      Sentry.Handlers.errorHandler()(erreur, requete, reponse, suite);
+      const gestionnaireErreurSentry = Sentry.expressErrorHandler();
+      gestionnaireErreurSentry(
+        erreur,
+        requete as Parameters<typeof gestionnaireErreurSentry>[1],
+        reponse as unknown as Parameters<typeof gestionnaireErreurSentry>[2],
+        suite
+      );
     }
   },
 };
