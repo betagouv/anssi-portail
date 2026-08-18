4cf39bccd93771b2856b220a355fdab3999a4214
jpaulineau <jpaulineau@ippon.fr>
2026-08-18 16:26:15 +0200

    [PARCOURS SECURISATION] Uniformise les sources
    ...entre les événements d'inscription et de parcours

--- front/scripts/connexion.js
+++ front/scripts/connexion.js
@@ -1,10 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
   sessionStorage.removeItem('pagePostConnexion');
+  const paramètresRecherche = new URLSearchParams(window.location.search);
 
-  const urlRedirection = new URL(window.location.href).searchParams.get('urlRedirection');
+  const urlRedirection = paramètresRecherche.get('urlRedirection');
   if (!urlRedirection) return;
 
   const urlPostConnexion = new URL(urlRedirection, window.location.origin);
+  const pageSource = urlPostConnexion.searchParams.get('pageSource') || window.location.href;
+  sessionStorage.setItem('pageSource', pageSource);
   if (urlPostConnexion.origin === window.location.origin) {
     sessionStorage.setItem('pagePostConnexion', urlPostConnexion.href);
   }
--- front/scripts/metadonneesConnexion.js
+++ front/scripts/metadonneesConnexion.js
@@ -1,6 +1,5 @@
 (function () {
   try {
-    sessionStorage.setItem('pageSource', window.location.href);
     const paramètresRecherche = new URLSearchParams(window.location.search);
     const campagne = paramètresRecherche.get('mtm_campaign') || paramètresRecherche.get('utm_campaign');
     if (campagne) {
