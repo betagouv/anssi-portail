f9fe70bf70d9ed6aee300b07afd99535ce82c8ed
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2026-08-18 10:59:51 +0200

    Ajoute l'illustration animée de la page "Associations"

---
+++ front/lib-svelte/src/protection/animation/IllustrationHerosAssociations.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import IllustrationAnimee from '../../accueil/animation/IllustrationAnimee.svelte';
+  import HerosAssociations from './HerosAssociations.svelte';
+
+  const scènes = [HerosAssociations];
+</script>
+
+<IllustrationAnimee
+  {scènes}
+  étiquette="Des bénévoles d'une association réunis en cercle. Bon réflexe : protéger les adhérents et les bénévoles."
+/>
