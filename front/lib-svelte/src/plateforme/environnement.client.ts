dcd649088e332020e566caa40209dd4becdd8741
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2026-08-24 16:54:31 +0200

    [DA] Retire le feature flag du front

--- front/lib-svelte/src/plateforme/environnement.client.ts
+++ front/lib-svelte/src/plateforme/environnement.client.ts
@@ -1,3 +1,2 @@
 export const estServeur = false;
-export const afficheNouvelleDA = import.meta.env.FEATURE_FLAG_NOUVELLE_DA === 'true';
 export const afficheParcoursSecurisation = import.meta.env.FEATURE_FLAG_PARCOURS_SECURISATION === 'true';
--- front/lib-svelte/src/plateforme/environnement.serveur.ts
+++ front/lib-svelte/src/plateforme/environnement.serveur.ts
@@ -1,3 +1,2 @@
 export const estServeur = true;
-export const afficheNouvelleDA = process.env.FEATURE_FLAG_NOUVELLE_DA === 'true';
 export const afficheParcoursSecurisation = process.env.FEATURE_FLAG_PARCOURS_SECURISATION === 'true';
