263fa9e37a7dd593a82d809b3da668743656cf49
Cédric Prezelin <cedric.prezelin.ext@beta.gouv.fr>
2026-08-26 18:41:40 +0200

    [SOIN] Migre FermetureSurClicEnDehors vers Svelte 5

--- front/lib-svelte/src/ui/FermetureSurClicEnDehors.svelte
+++ front/lib-svelte/src/ui/FermetureSurClicEnDehors.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
-  export let elements: HTMLElement[];
-  export let doitEtreOuvert: boolean;
+  import { clic } from '../directives/actions.svelte';
 
-  const gereClic = (e: MouseEvent) => {
+  interface Props {
+    elements: HTMLElement[];
+    doitEtreOuvert: boolean;
+  }
+
+  let { elements, doitEtreOuvert = $bindable() }: Props = $props();
+
+  const gereClic = (e: MouseEvent | KeyboardEvent) => {
     const clicInterieur =
       elements.includes(e.target as HTMLElement) || elements.some((el) => el.contains(e.target as Node));
     if (!clicInterieur) doitEtreOuvert = false;
   };
 </script>
 
-<svelte:body on:click={gereClic} />
+<svelte:body use:clic={gereClic} />
