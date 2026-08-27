e4352343dfd948bc0deea90bf6382c2014182564
jpaulineau <jpaulineau@ippon.fr>
2026-08-27 17:29:56 +0200

    [SOIN] Extrais le composant du retour aux modules

---
+++ front/lib-svelte/src/parcours-securisation/LienRetourAuxModules.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { profilStore } from '../stores/profil.store';
+  import Lien from '../ui/Lien.svelte';
+</script>
+
+{#if $profilStore?.parcoursSecurisation.parcoursActuel === 'complet'}
+  <Lien href="/parcours-complet" libelle="Revenir aux modules" icone="arrow-go-back-line"></Lien>
+{/if}
