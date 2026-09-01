a367436932281a32a39bd49da2708e04db3c08a5
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2026-09-01 12:08:38 +0200

    [MINI TESTS] Affiche les réactions sous la carte Mini-Test Vrai/Faux

--- front/lib-svelte/src/mini-tests/MiniTests.svelte
+++ front/lib-svelte/src/mini-tests/MiniTests.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { PropriétésFilAriane } from '../ui/filAriane';
   import HerosRiche from '../ui/HerosRiche.svelte';
+  import Reactions from '../ui/Reactions.svelte';
   import IllustrationHeroMiniTest from './IllustrationHeroMiniTest.svelte';
   import TestVraiFauxAnime from './TestVraiFauxAnime.svelte';
 
@@ -8,6 +9,7 @@
     feuille: 'Faire le test !',
   };
   let survol = $state(false);
+  let réactions = $state({ '❤️': 0, '👍': 0, '🔥': 0 });
 </script>
 
 <HerosRiche
@@ -26,30 +28,37 @@
 
 <dsfr-container>
   <div class="cartes">
-    <!-- svelte-ignore a11y_no_static_element_interactions -->
-    <dsfr-card
-      actionMarkup="a"
-      data-cible="VraiFaux"
-      data-source="MiniTest"
-      enlarge
-      hasDescription={false}
-      href="/mini-tests/vrai-faux"
-      markup="h3"
-      size="sm"
-      title="Cyberattaques : saurez-vous démêler le vrai du faux ?"
-      onmouseenter={() => (survol = true)}
-      onmouseleave={() => (survol = false)}
-    >
-      <div slot="image" class="image">
-        <TestVraiFauxAnime {survol} />
-      </div>
-
-      <div slot="seo">
-        <h3>
-          <a href="/mini-tests/vrai-faux">Cyberattaques : saurez-vous démêler le vrai du faux ?</a>
-        </h3>
-      </div>
-    </dsfr-card>
+    <div class="carte">
+      <!-- svelte-ignore a11y_no_static_element_interactions -->
+      <dsfr-card
+        actionMarkup="a"
+        data-cible="VraiFaux"
+        data-source="MiniTest"
+        enlarge
+        hasDescription={false}
+        href="/mini-tests/vrai-faux"
+        markup="h3"
+        size="sm"
+        title="Cyberattaques : saurez-vous démêler le vrai du faux ?"
+        onmouseenter={() => (survol = true)}
+        onmouseleave={() => (survol = false)}
+      >
+        <div slot="image" class="image">
+          <TestVraiFauxAnime {survol} />
+        </div>
+        <div slot="seo">
+          <h3>
+            <a href="/mini-tests/vrai-faux">Cyberattaques : saurez-vous démêler le vrai du faux ?</a>
+          </h3>
+        </div>
+      </dsfr-card>
+      <Reactions
+        bind:réactions
+        clé="évaluation-vrai-faux"
+        urlDePost="/api/reactions"
+        variant="tertiaire-sans-bordure"
+      />
+    </div>
   </div>
 </dsfr-container>
 
@@ -66,13 +75,20 @@
       grid-template-columns: repeat(3, 1fr);
     }
 
-    dsfr-card {
-      --background-default-grey: var(--background-alt-pink-macaron);
-      --background-default-grey-hover: var(--background-alt-pink-macaron-hover);
-      .image {
-        display: grid;
-        place-items: center;
-        aspect-ratio: 16/9;
+    .carte {
+      border: 1px solid var(--border-default-grey);
+      display: grid;
+
+      dsfr-card {
+        --background-default-grey: var(--background-alt-pink-macaron);
+        --background-default-grey-hover: var(--background-alt-pink-macaron-hover);
+        margin: -1px;
+
+        .image {
+          display: grid;
+          place-items: center;
+          aspect-ratio: 16/9;
+        }
       }
     }
   }
