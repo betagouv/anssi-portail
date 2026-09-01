ea63eb2993f6dccc82333f4bc89020355a96a27d
jpaulineau <jpaulineau@ippon.fr>
2026-09-01 17:51:36 +0200

    [MINI-TESTS] Ajoute le fil d'Ariane au quiz vrai faux
    ...et gère la césure

--- front/lib-svelte/src/mini-tests/vrai-faux/QuizVraiFaux.svelte
+++ front/lib-svelte/src/mini-tests/vrai-faux/QuizVraiFaux.svelte
@@ -1,5 +1,11 @@
-<script lang="ts"></script>
+<script lang="ts">
+  import FilAriane from '../../ui/FilAriane.svelte';
+</script>
 
-<main>
+<dsfr-container>
+  <FilAriane
+    feuille="Cyber&shy;attaques&nbsp;: saurez-vous démêler le vrai du faux ?"
+    branche={{ nom: 'Faire le test !', lien: '/mini-tests' }}
+  />
   <h1>Vrai ou faux</h1>
-</main>
+</dsfr-container>
