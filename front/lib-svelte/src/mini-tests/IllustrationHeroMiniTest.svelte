4f7c47c4f761f35634b61da4a25c8a8a65957515
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2026-09-01 09:25:10 +0200

    [MINI TESTS] Ajoute le héros

---
+++ front/lib-svelte/src/mini-tests/IllustrationHeroMiniTest.svelte
@@ -0,0 +1,91 @@
+<script lang="ts">
+  type Props = {
+    large?: boolean;
+  };
+  const { large }: Props = $props();
+</script>
+
+<div class="composition" class:large>
+  <img
+    class="illustration"
+    src="/assets/images/groupe-regardant-test-maturite.avif"
+    alt="Groupe regardant la présentation du test maturité"
+  />
+
+  <div class="manette">
+    <lab-anssi-icone nom="gamepad-fill" taille="lg"></lab-anssi-icone>
+  </div>
+  <div class="plante">
+    <lab-anssi-icone nom="plant-fill" taille="lg"></lab-anssi-icone>
+  </div>
+  <img class="boucle" src="/assets/images/decorations/boucle.svg" alt="" />
+  <img class="ellipse" src="/assets/images/decorations/ellipse.svg" alt="" />
+</div>
+
+<style lang="scss">
+  .composition {
+    aspect-ratio: 4 / 3;
+    display: grid;
+    place-items: center;
+    position: relative;
+
+    > .illustration {
+      border-radius: 12px;
+      height: 100%;
+      width: 70%;
+      object-fit: cover;
+    }
+
+    &.large {
+      aspect-ratio: 70 / 39;
+
+      .manette,
+      .boucle,
+      .ellipse {
+        transform: scale(90%);
+      }
+
+      .manette {
+        right: 50px;
+      }
+    }
+
+    .manette {
+      position: absolute;
+      right: 12%;
+      top: 4%;
+      padding: 1rem;
+      border-radius: 999px;
+      background-color: var(--background-default-grey);
+      color: var(--text-title-blue-france);
+      box-shadow: 0 6px 18px 0 rgba(0, 0, 18, 0.16);
+    }
+
+    .plante {
+      position: absolute;
+      left: 10%;
+      bottom: 10%;
+      padding: 1rem;
+      border-radius: 999px;
+      background-color: var(--background-default-grey);
+      color: var(--text-title-blue-france);
+      box-shadow: 0 6px 18px 0 rgba(0, 0, 18, 0.16);
+    }
+
+    .boucle {
+      top: 25%;
+      color: var(--text-title-blue-france);
+      position: absolute;
+      left: 6%;
+      width: 30%;
+      transform: rotate(70deg);
+    }
+
+    .ellipse {
+      top: calc(25% + 100px);
+      position: absolute;
+      left: 8%;
+      width: 15px;
+    }
+  }
+</style>
--- front/lib-svelte/src/mini-tests/MiniTests.svelte
+++ front/lib-svelte/src/mini-tests/MiniTests.svelte
@@ -1,9 +1,28 @@
 <script lang="ts">
+  import type { PropriétésFilAriane } from '../ui/filAriane';
+  import HerosRiche from '../ui/HerosRiche.svelte';
+  import IllustrationHeroMiniTest from './IllustrationHeroMiniTest.svelte';
   import PlanteAnimee from './PlanteAnimee.svelte';
   import TestVraiFauxAnime from './TestVraiFauxAnime.svelte';
+
+  const propriétésFilAriane: PropriétésFilAriane = {
+    feuille: 'Faire le test !',
+  };
 </script>
 
-Des tests gratuits pour mieux maîtriser les risques cyber
+<HerosRiche
+  {propriétésFilAriane}
+  description="Évaluez votre situation et comprenez mieux les enjeux cyber de votre organisation, simplement et rapidement."
+  variante="cafe-creme"
+  class="avec-image-fond"
+>
+  {#snippet titreHtml()}
+    Des tests gratuits pour mieux maîtriser les risques cyber
+  {/snippet}
+  {#snippet illustration()}
+    <IllustrationHeroMiniTest />
+  {/snippet}
+</HerosRiche>
 
 <PlanteAnimee />
 <TestVraiFauxAnime />
