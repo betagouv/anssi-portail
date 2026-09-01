0d763d1822b30de201d3958f33689c4f97c1922e
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-09-01 17:48:03 +0200

    [MINI-TESTS] Renvoie la première question vrai-faux

--- back/src/api/mini-tests/vrai-faux/ressourceVraiFaux.ts
+++ back/src/api/mini-tests/vrai-faux/ressourceVraiFaux.ts
@@ -9,7 +9,23 @@ export const ressourceVraiFaux = () => {
     '/',
     valideCorpsRequete(corpsVide),
     filetRouteAsynchrone(async (_requete, reponse) => {
-      reponse.sendStatus(HttpStatusCode.Ok);
+      reponse.status(HttpStatusCode.Ok).send([
+        {
+          idQuestion: 'idQuestion1',
+          libellé: {
+            emoji: '🏢',
+            texte: 'Les grandes entreprises sont les principales victimes des rançongiciels, pas les PME et TPE.',
+          },
+          idéeReçue:
+            'FAUX. Les grandes entreprises sont les principales victimes des rançongiciels, pas les PME et TPE.',
+          explications: `
+Les PME, TPE et ETI sont la catégorie la plus touchée.
+En 2025, parmi les victimes d'attaques par rançongiciel portées à la connaissance de l'ANSSI, les PME, TPE et ETI représentent 37 % des cas — c'est la catégorie la plus affectée. Les attaques cybercriminelles ciblent indistinctement la plupart des secteurs et zones géographiques, de façon opportuniste.
+`,
+          source: 'ANSSI, Panorama de la cybermenace 2025, section 1.A — pages 10-11.',
+          estVraie: false,
+        },
+      ]);
     })
   );
   return routeur;
--- back/tests/api/mini-tests/vrai-faux/ressourceVraiFaux.spec.ts
+++ back/tests/api/mini-tests/vrai-faux/ressourceVraiFaux.spec.ts
@@ -18,5 +18,30 @@ describe('La ressource du questionnaire Vrai-Faux', () => {
 
       assert.equal(reponse.status, HttpStatusCode.Ok);
     });
+
+    it('retourne une liste de questions', async () => {
+      const reponse = await request(serveur).get('/api/mini-tests/vrai-faux');
+
+      assert.equal(reponse.body.length, 1);
+      assert.equal(reponse.body[0].idQuestion, 'idQuestion1');
+      assert.equal(reponse.body[0].libellé.emoji, '🏢');
+      assert.equal(
+        reponse.body[0].libellé.texte,
+        'Les grandes entreprises sont les principales victimes des rançongiciels, pas les PME et TPE.'
+      );
+      assert.equal(
+        reponse.body[0].idéeReçue,
+        'FAUX. Les grandes entreprises sont les principales victimes des rançongiciels, pas les PME et TPE.'
+      );
+      assert.equal(
+        reponse.body[0].explications,
+        `
+Les PME, TPE et ETI sont la catégorie la plus touchée.
+En 2025, parmi les victimes d'attaques par rançongiciel portées à la connaissance de l'ANSSI, les PME, TPE et ETI représentent 37 % des cas — c'est la catégorie la plus affectée. Les attaques cybercriminelles ciblent indistinctement la plupart des secteurs et zones géographiques, de façon opportuniste.
+`
+      );
+      assert.equal(reponse.body[0].source, 'ANSSI, Panorama de la cybermenace 2025, section 1.A — pages 10-11.');
+      assert.equal(reponse.body[0].estVraie, false);
+    });
   });
 });
