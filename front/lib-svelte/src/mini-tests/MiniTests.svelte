4fd49b5dc0f53b934f00206567691351fc895456
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2026-09-01 08:55:37 +0200

    [MINI TESTS] Ajoute une animation de cartes
    ...qui servira pour le bouton du mini test Vrai/Faux

--- front/lib-svelte/src/mini-tests/MiniTests.svelte
+++ front/lib-svelte/src/mini-tests/MiniTests.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import PlanteAnimee from './PlanteAnimee.svelte';
+  import TestVraiFauxAnime from './TestVraiFauxAnime.svelte';
 </script>
 
 Des tests gratuits pour mieux maîtriser les risques cyber
 
 <PlanteAnimee />
+<TestVraiFauxAnime />
