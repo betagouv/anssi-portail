e540285a98b3e8fe3cd42caccd47a36d7647c339
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-09-02 10:38:27 +0200

    [MINI-TESTS] Valide les réponses reçues

--- back/tests/api/mini-tests/vrai-faux/ressourceReponsesVraiFaux.spec.ts
+++ back/tests/api/mini-tests/vrai-faux/ressourceReponsesVraiFaux.spec.ts
@@ -16,9 +16,42 @@ describe('La ressource des réponses aux questionnaire Vrai-Faux', () => {
   });
   describe('sur une requête POST', () => {
     it('répond un 201', async () => {
-      const reponse = await request(serveur).post('/api/mini-tests/vrai-faux/reponses');
+      const reponse = await request(serveur).post('/api/mini-tests/vrai-faux/reponses').send({
+        idQuestion: 'idQuestion1',
+        réponseUtilisateur: true,
+        idCorrélation: '1234567890',
+      });
 
       assert.equal(reponse.status, HttpStatusCode.Created);
+    });
+
+    describe('répond un 400', () => {
+      it("si auncun identifiant de question n'est fourni", async () => {
+        const reponse = await request(serveur).post('/api/mini-tests/vrai-faux/reponses').send({
+          réponseUtilisateur: true,
+          idCorrélation: '1234567890',
+        });
+
+        assert.equal(reponse.status, HttpStatusCode.BadRequest);
+      });
+
+      it("si auncune réponse utilisateur n'est fournie", async () => {
+        const reponse = await request(serveur).post('/api/mini-tests/vrai-faux/reponses').send({
+          idQuestion: 'idQuestion1',
+          idCorrélation: '1234567890',
+        });
+
+        assert.equal(reponse.status, HttpStatusCode.BadRequest);
+      });
+
+      it("si auncun identifiant de corrélation n'est fourni", async () => {
+        const reponse = await request(serveur).post('/api/mini-tests/vrai-faux/reponses').send({
+          idQuestion: 'idQuestion1',
+          réponseUtilisateur: true,
+        });
+
+        assert.equal(reponse.status, HttpStatusCode.BadRequest);
+      });
     });
   });
 });
