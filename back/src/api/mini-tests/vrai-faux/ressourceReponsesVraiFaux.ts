9e6cf6decdf33200ae932ab27bb1b7dac5a72afa
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-09-02 12:00:34 +0200

    [MINI-TESTS] Publie un événement lorsqu'on reçoit une réponse

--- back/src/api/mini-tests/vrai-faux/ressourceReponsesVraiFaux.ts
+++ back/src/api/mini-tests/vrai-faux/ressourceReponsesVraiFaux.ts
@@ -1,19 +1,28 @@
 import { HttpStatusCode } from '@anssi-portail/axios';
 import { Router } from 'express';
 import z from 'zod';
+import { RéponsesVraieFausseSoumise } from '../../../bus/evenements/reponsesVraieFausseSoumise.js';
 import { ConfigurationServeur } from '../../configurationServeur.js';
 import { filetRouteAsynchrone } from '../../middlewares/middleware.js';
 import { valideCorpsRequete } from '../../zod.js';
 import { schemaPostRéponsesVraiFaux } from './ressourceReponsesVraiFaux.schemas.js';
 import CorpsDeRequeteTypee = Express.CorpsDeRequeteTypee;
 
-export const ressourceRéponsesVraiFaux = (_config: ConfigurationServeur) => {
+export const ressourceRéponsesVraiFaux = ({ busEvenements }: ConfigurationServeur) => {
   const routeur = Router();
   routeur.post(
     '/',
     valideCorpsRequete(schemaPostRéponsesVraiFaux),
-    filetRouteAsynchrone(async (_requête: CorpsDeRequeteTypee<z.infer<typeof schemaPostRéponsesVraiFaux>>, reponse) => {
-      reponse.sendStatus(HttpStatusCode.Created);
+    filetRouteAsynchrone(async (requête: CorpsDeRequeteTypee<z.infer<typeof schemaPostRéponsesVraiFaux>>, reponse) => {
+      await busEvenements.publie(
+        new RéponsesVraieFausseSoumise({
+          idCorrélation: requête.body.idCorrélation,
+          idQuestion: requête.body.idQuestion,
+          réponseCorrecte: requête.body.réponseUtilisateur,
+        })
+      );
+
+      return reponse.sendStatus(HttpStatusCode.Created);
     })
   );
   return routeur;
