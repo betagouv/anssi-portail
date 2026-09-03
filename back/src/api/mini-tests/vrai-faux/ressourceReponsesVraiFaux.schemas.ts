c87a9759aa4d2f131fcd178a56097fa00ddeec86
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-09-03 17:09:14 +0200

    [MINI-TESTS] Restreint la taille des informations postables
    ...lorsque l'on fourni une réponse de questionnaire vrai-faux

--- back/src/api/mini-tests/vrai-faux/ressourceReponsesVraiFaux.schemas.ts
+++ back/src/api/mini-tests/vrai-faux/ressourceReponsesVraiFaux.schemas.ts
@@ -1,7 +1,7 @@
 import * as z from 'zod';
 
 export const schemaPostRéponsesVraiFaux = z.strictObject({
-  idQuestion: z.string(),
+  idQuestion: z.string().max(100),
   réponseUtilisateur: z.boolean(),
-  idCorrélation: z.string(),
+  idCorrélation: z.string().max(100),
 });
