52a413960c6c1bca563007821b7d67d7395f37b8
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2026-09-08 11:11:43 +0200

    [EXPOSITION] Utilise une notice
    ...pour l’avertissement

--- front/lib-svelte/src/ui/Notice.svelte
+++ front/lib-svelte/src/ui/Notice.svelte
@@ -8,11 +8,24 @@
       libelle: string;
     };
     titre: string;
+    type?: 'attention' | 'info';
+    aUneIcone?: boolean;
   };
-  const { description, estRejetable, lien, titre }: Props = $props();
+  const { description, estRejetable, lien, titre, type, aUneIcone = false }: Props = $props();
+
+  const typeDsfr = $derived.by(() => {
+    switch (type) {
+      case 'attention':
+        return 'warning';
+      default:
+        return undefined;
+    }
+  });
 </script>
 
 <dsfr-notice
+  type={typeDsfr}
+  has-icon={aUneIcone || undefined}
   desc={description}
   dismissible={estRejetable ? true : undefined}
   hasDescription={description ? true : undefined}
