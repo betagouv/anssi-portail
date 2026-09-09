9da58df89d78f1617ee21f9be889529b6f298680
Cédric Prezelin <cedric.prezelin.ext@beta.gouv.fr>
2026-09-09 14:50:22 +0200

    [SOIN] Ajoute le suffixe (EN) au titre HTML pour les guides anglais

--- back/src/infra/enrichissement/adaptateurDom/adaptateurTitre.ts
+++ back/src/infra/enrichissement/adaptateurDom/adaptateurTitre.ts
@@ -6,9 +6,20 @@ export class AdaptateurTitre implements AdaptateurDom {
   constructor(private readonly résolveurDePage: RésolveurDePage) {}
 
   async adapte(dom: JSDOM, routeDemandée: string) {
-    const guideTrouvé = await this.résolveurDePage.guide(routeDemandée);
-    const financementTrouvé = await this.résolveurDePage.financement(routeDemandée);
-    const nouveauTitre = guideTrouvé?.nom ?? financementTrouvé?.nom;
+    const promesseGuide = this.résolveurDePage.guide(routeDemandée);
+    const promesseFinancement = this.résolveurDePage.financement(routeDemandée);
+
+    await Promise.all([promesseGuide, promesseFinancement]);
+    const guideTrouvé = await promesseGuide;
+    const financementTrouvé = await promesseFinancement;
+
+    let nouveauTitre: string | undefined;
+
+    if (guideTrouvé) {
+      nouveauTitre = guideTrouvé.langue === 'EN' ? `${guideTrouvé.nom} (EN)` : guideTrouvé.nom;
+    } else {
+      nouveauTitre = financementTrouvé?.nom;
+    }
 
     const titre = dom.window.document.getElementsByTagName('title').item(0);
     if (titre && nouveauTitre) {
