9ed615f9b476cbf9a29eb5913049895380512afc
Cédric Prezelin <cedric.prezelin.ext@beta.gouv.fr>
2026-09-09 09:23:31 +0200

    [SOIN] Ignore tous les espaces sans chasse dans la recherche du catalogue

--- front/lib-svelte/src/catalogue/stores/rechercheTextuelle.store.ts
+++ front/lib-svelte/src/catalogue/stores/rechercheTextuelle.store.ts
@@ -11,7 +11,10 @@ export const rechercheTextuelle = {
   ok: (item: ItemCyber | Guide) => {
     if (!get(rechercheTextuelle)) return true;
     return (
-      item.nom.replace('&ZeroWidthSpace;', '').toLowerCase().includes(get(rechercheTextuelle).toLowerCase()) ||
+      item.nom
+        .replace(/&ZeroWidthSpace;|\u200B/g, '')
+        .toLowerCase()
+        .includes(get(rechercheTextuelle).toLowerCase()) ||
       (item.description ?? '').toLowerCase().includes(get(rechercheTextuelle).toLowerCase())
     );
   },
