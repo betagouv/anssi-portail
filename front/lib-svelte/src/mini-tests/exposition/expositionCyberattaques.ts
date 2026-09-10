bc30c88dbe04dce1686d987f2e48b66e8ad33778
jpaulineau <jpaulineau@ippon.fr>
2026-09-10 17:06:28 +0200

    [EXPOSITION] Change le texte de l'encart DDoS

--- front/lib-svelte/src/mini-tests/exposition/expositionCyberattaques.ts
+++ front/lib-svelte/src/mini-tests/exposition/expositionCyberattaques.ts
@@ -388,7 +388,7 @@ export function resumeInsight(id: IdMenace, reponses: ReponsesExposition): Resum
   return {
     indicateur: 'DDoS',
     enHausse: false,
-    texte: "mode d'attaque le plus fréquent en 2025",
+    texte: "mode d'attaque à des fins de destabilisation",
     source: 'ANSSI',
   };
 }
