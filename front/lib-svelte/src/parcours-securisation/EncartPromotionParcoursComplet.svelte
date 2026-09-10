534718c383e1be7217e78644f8f57d52447c4a07
jpaulineau <jpaulineau@ippon.fr>
2026-09-10 16:54:08 +0200

    [PARCOURS SECURISATION] Change le libellé du contenu ReCyf

--- front/lib-svelte/src/parcours-securisation/EncartPromotionParcoursComplet.svelte
+++ front/lib-svelte/src/parcours-securisation/EncartPromotionParcoursComplet.svelte
@@ -32,7 +32,7 @@
     {
       titre: 'Basé sur ReCyf',
       description:
-        "Simplifiée et accompagnée d'explications pédagogiques, chaque mesure est issue du référentiel cyber français (ReCyF) pensé pour faciliter la mise en conformité avec NIS 2.",
+        "Simplifiée et accompagnée d'explications pédagogiques, chaque mesure prend racine dans le référentiel cyber français (ReCyF)",
       id: 'recyf',
       rich: true,
     },
--- front/lib-svelte/src/parcours-securisation/LandingParcoursSecurisationComplet.svelte
+++ front/lib-svelte/src/parcours-securisation/LandingParcoursSecurisationComplet.svelte
@@ -29,7 +29,7 @@
     {
       titre: 'Basé sur ReCyF',
       description:
-        "Simplifiée et accompagnée d'explications pédagogiques, chaque mesure est issue du référentiel cyber français (ReCyF) pensé pour faciliter la mise en conformité avec NIS 2.",
+        "Simplifiée et accompagnée d'explications pédagogiques, chaque mesure prend racine dans le référentiel cyber français (ReCyF)",
       illustration: IllustrationRecyf,
     },
     {
