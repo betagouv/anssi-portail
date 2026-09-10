1250409c050eac8b23a8b86689e98905fe294e2a
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2026-09-10 10:58:39 +0200

    [EXPOSITION] Publie les réponses au questionnaire

--- front/lib-svelte/src/passerelles/mini-tests/publicationRéponses.ts
+++ front/lib-svelte/src/passerelles/mini-tests/publicationRéponses.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import type { TypeOrganisation, Secteur, FacteurAggravant } from '../../mini-tests/exposition/expositionCyberattaques';
 
 export const publieRéponseQuestionnaireVraiFaux = async (réponse: {
   idCorrélation: string;
@@ -6,4 +7,12 @@ export const publieRéponseQuestionnaireVraiFaux = async (réponse: {
   réponseUtilisateur: boolean;
 }): Promise<void> => {
   await axios.post('/api/mini-tests/vrai-faux/reponses', réponse);
+};
+
+export const publieRéponseQuestionnaireExposition = async (réponse: {
+  typeOrganisation: TypeOrganisation;
+  secteur?: Secteur;
+  facteursAggravant: FacteurAggravant[];
+}): Promise<void> => {
+  await axios.post('/api/mini-tests/exposition/tests', réponse);
 };
