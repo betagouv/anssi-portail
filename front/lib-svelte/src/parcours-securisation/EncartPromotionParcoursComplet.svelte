85f41c1d7e4ca5a0db4b7e5caf16c28a020cac8f
Cédric Prezelin <cedric.prezelin.ext@beta.gouv.fr>
2026-09-10 16:13:37 +0200

    [PARCOURS SÉCURISATION] Corrige les affichages d'encart

--- front/lib-svelte/src/parcours-securisation/EncartPromotionParcoursComplet.svelte
+++ front/lib-svelte/src/parcours-securisation/EncartPromotionParcoursComplet.svelte
@@ -101,9 +101,16 @@
 </dsfr-container>
 
 <style lang="scss">
+  @use '../../../assets/styles/responsive.scss' as *;
+
   [slot='media'] {
+    display: none;
     max-width: 486px;
     width: 100%;
+
+    @include a-partir-de(md) {
+      display: block;
+    }
   }
 
   .image-animee,
