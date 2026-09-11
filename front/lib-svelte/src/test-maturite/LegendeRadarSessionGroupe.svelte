9eb5034ecf6aeb37ff8e447cf1c8b48f26c21db4
jpaulineau <jpaulineau@ippon.fr>
2026-09-11 10:46:01 +0200

    [LINT] Adapte les composants dont le style est statique et connu

--- front/lib-svelte/src/test-maturite/LegendeRadarSessionGroupe.svelte
+++ front/lib-svelte/src/test-maturite/LegendeRadarSessionGroupe.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-  import { couleursDeNiveau, niveauxMaturite } from '../niveaux-maturite/NiveauxMaturite.donnees';
+  import { niveauxMaturite } from '../niveaux-maturite/NiveauxMaturite.donnees';
 </script>
 
 <div class="legende">
   {#each niveauxMaturite as niveau (niveau.id)}
-    <div class="ligne-legende ligne-legende-{niveau.id}" style="--couleur-puce: {couleursDeNiveau[niveau.id]}">
+    <div class="ligne-legende ligne-legende-{niveau.id}">
       <span class="libelle">{niveau.label}</span>
     </div>
   {/each}
@@ -31,5 +31,25 @@
     border-radius: 7px;
     content: '';
     background-color: var(--couleur-puce);
+  }
+
+  .ligne-legende-insuffisant {
+    --couleur-puce: #5c68e5;
+  }
+
+  .ligne-legende-emergent {
+    --couleur-puce: #82b5f2;
+  }
+
+  .ligne-legende-intermediaire {
+    --couleur-puce: #28598f;
+  }
+
+  .ligne-legende-confirme {
+    --couleur-puce: #30a7ae;
+  }
+
+  .ligne-legende-optimal {
+    --couleur-puce: #81eef5;
   }
 </style>
