59be5834b232d1aea38e7cc603e2e165f2b2a493
jpaulineau <jpaulineau@ippon.fr>
2026-09-15 16:09:19 +0200

    [REFLEXES CYBER] Contrains l'utilisation d'un uuidv7
    ...dans les identifiants de corrélation

--- back/src/api/mini-tests/vrai-faux/ressourceReponsesVraiFaux.schemas.ts
+++ back/src/api/mini-tests/vrai-faux/ressourceReponsesVraiFaux.schemas.ts
@@ -3,5 +3,5 @@ import * as z from 'zod';
 export const schemaPostRéponsesVraiFaux = z.strictObject({
   idQuestion: z.string().max(100),
   réponseUtilisateur: z.boolean(),
-  idCorrélation: z.string().max(100),
+  idCorrélation: z.uuidv7(),
 });
