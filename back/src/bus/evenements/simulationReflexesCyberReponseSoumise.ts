62185d2ae7e40a27f0e2d3934497b53fb99add24
jpaulineau <jpaulineau@ippon.fr>
2026-09-15 16:33:14 +0200

    [SOIN] Extrais les types métier du test Réflexes Cyber

--- back/src/bus/evenements/simulationReflexesCyberReponseSoumise.ts
+++ back/src/bus/evenements/simulationReflexesCyberReponseSoumise.ts
@@ -1,3 +1,4 @@
+import { Réflexe, Rôle, Scénario } from '../../metier/mini-tests/reflexes-cyber/reflexesCyber.js';
 import { CodeRegion } from '../../metier/referentielRegions.js';
 import { CodeSecteur } from '../../metier/referentielSecteurs.js';
 import { CodeTrancheEffectif } from '../../metier/referentielTranchesEffectifEtablissement.js';
@@ -5,10 +6,10 @@ import { EvenementDuBus } from '../busEvenements.js';
 
 export class SimulationRéflexesCyberRéponseSoumise implements EvenementDuBus {
   readonly idCorrélation: string;
-  readonly idScénario: 'entreprise' | 'collectivité';
-  readonly idRôle: 'direction' | 'si' | 'communication' | 'juridique' | 'rh' | 'relations';
+  readonly idScénario: Scénario;
+  readonly idRôle: Rôle;
   readonly numéroÉvènement: number;
-  readonly réflexe: 'bon' | 'mauvais' | 'aucun';
+  readonly réflexe: Réflexe;
   readonly email?: string;
   readonly codeRegion?: CodeRegion;
   readonly codeSecteur?: CodeSecteur;
@@ -25,10 +26,10 @@ export class SimulationRéflexesCyberRéponseSoumise implements EvenementDuBus {
     codeTrancheEffectif,
   }: {
     idCorrélation: string;
-    idScénario: 'entreprise' | 'collectivité';
-    idRôle: 'direction' | 'si' | 'communication' | 'juridique' | 'rh' | 'relations';
+    idScénario: Scénario;
+    idRôle: Rôle;
     numéroÉvènement: number;
-    réflexe: 'bon' | 'mauvais' | 'aucun';
+    réflexe: Réflexe;
     email?: string;
     codeRegion?: CodeRegion;
     codeSecteur?: CodeSecteur;
--- back/src/bus/evenements/simulationReflexesCyberTermine.ts
+++ back/src/bus/evenements/simulationReflexesCyberTermine.ts
@@ -1,3 +1,4 @@
+import { Rôle, Scénario } from '../../metier/mini-tests/reflexes-cyber/reflexesCyber.js';
 import { CodeRegion } from '../../metier/referentielRegions.js';
 import { CodeSecteur } from '../../metier/referentielSecteurs.js';
 import { CodeTrancheEffectif } from '../../metier/referentielTranchesEffectifEtablissement.js';
@@ -5,8 +6,8 @@ import { EvenementDuBus } from '../busEvenements.js';
 
 export class SimulationRéflexesCyberTerminé implements EvenementDuBus {
   readonly idCorrélation: string;
-  readonly idScénario: 'entreprise' | 'collectivité';
-  readonly idRôle: 'direction' | 'si' | 'communication' | 'juridique' | 'rh' | 'relations';
+  readonly idScénario: Scénario;
+  readonly idRôle: Rôle;
   readonly email?: string;
   readonly codeRegion?: CodeRegion;
   readonly codeSecteur?: CodeSecteur;
@@ -21,8 +22,8 @@ export class SimulationRéflexesCyberTerminé implements EvenementDuBus {
     codeTrancheEffectif,
   }: {
     idCorrélation: string;
-    idScénario: 'entreprise' | 'collectivité';
-    idRôle: 'direction' | 'si' | 'communication' | 'juridique' | 'rh' | 'relations';
+    idScénario: Scénario;
+    idRôle: Rôle;
     email?: string;
     codeRegion?: CodeRegion;
     codeSecteur?: CodeSecteur;
