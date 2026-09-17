291f4309e36f4bb21e380005440b4a8280d2a481
jpaulineau <jpaulineau@ippon.fr>
2026-09-17 11:57:57 +0200

    [WEBC] Ajoute l'entête autonome sur le test vrai faux

--- front/lib-svelte/src/mini-tests/vrai-faux/LandingVraiFaux.svelte
+++ front/lib-svelte/src/mini-tests/vrai-faux/LandingVraiFaux.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   import Bouton from '../../ui/Bouton.svelte';
+  import EnteteAutonome from '../../ui/EnteteAutonome.svelte';
   import type { PropriétésFilAriane } from '../../ui/filAriane';
-
   import HerosRiche from '../../ui/HerosRiche.svelte';
+
   import Lien from '../../ui/Lien.svelte';
 
   import IllustrationHerosVraiFaux from './IllustrationHerosVraiFaux.svelte';
@@ -22,35 +23,55 @@
   );
 </script>
 
-<HerosRiche
-  {propriétésFilAriane}
-  description=""
-  variante="cafe-creme"
-  class="avec-image-fond"
-  badges={[{ label: '⏱️ 3 minutes', accent: 'green-bourgeon' }]}
-  {urlBase}
->
-  {#snippet titreHtml()}
-    Cyber&shy;attaques&nbsp;: saurez-vous démêler le vrai du faux ?
-  {/snippet}
-  {#snippet illustration()}
-    <figure class="illustration">
-      <IllustrationHerosVraiFaux {urlBase} />
-    </figure>
-  {/snippet}
+{#snippet herosRiche()}
+  <HerosRiche
+    {propriétésFilAriane}
+    description=""
+    variante="cafe-creme"
+    class="avec-image-fond"
+    badges={[{ label: '⏱️ 3 minutes', accent: 'green-bourgeon' }]}
+    {urlBase}
+  >
+    {#snippet titreHtml()}
+      Cyber&shy;attaques&nbsp;: saurez-vous démêler le vrai du faux ?
+    {/snippet}
+    {#snippet illustration()}
+      <figure class="illustration">
+        <IllustrationHerosVraiFaux {urlBase} />
+      </figure>
+    {/snippet}
 
-  {#snippet actions()}
-    {#if mode === 'autonome'}
-      <Bouton libelle="Démarrer le quiz" taille="lg" surClic={suivant}></Bouton>
-    {:else}
-      <Lien apparence="bouton" libelle="Démarrer le quiz" taille="lg" href="/vrai-faux/quiz" />
-    {/if}
-  {/snippet}
-</HerosRiche>
+    {#snippet actions()}
+      {#if mode === 'autonome'}
+        <Bouton libelle="Démarrer le quiz" taille="lg" surClic={suivant}></Bouton>
+      {:else}
+        <Lien apparence="bouton" libelle="Démarrer le quiz" taille="lg" href="/vrai-faux/quiz" />
+      {/if}
+    {/snippet}
+  </HerosRiche>
+{/snippet}
+
+{#if mode === 'autonome'}
+  <div class="entete">
+    <dsfr-container>
+      <EnteteAutonome {urlBase} />
+    </dsfr-container>
+    {@render herosRiche()}
+  </div>
+{:else}
+  {@render herosRiche()}
+{/if}
 
 <style lang="scss">
   .illustration {
     margin: 0 auto;
     width: 100%;
+  }
+
+  .entete {
+    display: flex;
+    flex-direction: column;
+    gap: 1.5rem;
+    background-color: var(--brown-cafe-creme-975-75);
   }
 </style>
