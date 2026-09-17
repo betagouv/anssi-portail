27c06b2307e9656434b4333b791a9309961b6a53
jpaulineau <jpaulineau@ippon.fr>
2026-09-17 10:43:33 +0200

    [SOIN] Extrait l'entete autonome de la demande de diag simplifié
    ...afin de pouvoir l'utiliser dans les webc

--- front/lib-svelte/src/demande-aide-mon-aide-cyber/FormulaireDemandeSimplifiee.svelte
+++ front/lib-svelte/src/demande-aide-mon-aide-cyber/FormulaireDemandeSimplifiee.svelte
@@ -14,6 +14,7 @@
   import ConfirmationCreationDemandeAide from './ConfirmationCreationDemandeAide.svelte';
   import type { CorpsAPIDemandeAide } from './DonneesFormulaireDemandeAide';
   import { récupèreStatistiquesMSC, type Statistiques } from '../passerelles/statistiquesMSC';
+  import EnteteAutonome from '../ui/EnteteAutonome.svelte';
 
   interface Props {
     mode?: 'autonome';
@@ -149,30 +150,7 @@
         <Alerte type="ERREUR" titre="Une erreur est survenue" message={erreurs} />
       {/if}
       {#if mode === 'autonome'}
-        <div class="entete-principale">
-          <a href={urlBase}>
-            <img
-              src={`${urlBase}/assets/images/marianne-dark.svg`}
-              class="marianne"
-              alt="Marianne"
-              crossorigin="anonymous"
-            />
-          </a>
-          <a href="https://cyber.gouv.fr" aria-label="Site de l’ANSSI">
-            <img
-              class="logo-anssi"
-              src={`${urlBase}/assets/images/logo-anssi.svg`}
-              alt="Logo ANSSI"
-              crossorigin="anonymous"
-            />
-          </a>
-          <a href={urlBase} class="titre-msc" aria-label="Site MesServicesCyber">
-            <div class="titre-site-msc">
-              <p class="site-msc">MesServicesCyber</p>
-              <p class="organisation-anssi">Innovation ANSSI</p>
-            </div>
-          </a>
-        </div>
+        <EnteteAutonome {urlBase} />
       {/if}
     </div>
   </Formulaire>
@@ -221,50 +199,5 @@
 
   .confirmation {
     padding-bottom: 1.5rem;
-  }
-
-  .entete-principale {
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    margin-top: 1.5rem;
-
-    .titre-msc {
-      display: flex;
-    }
-
-    a.titre-msc {
-      text-decoration: none;
-    }
-
-    .marianne {
-      width: 67px;
-      height: 67px;
-      transform: translateX(-9px);
-    }
-
-    .logo-anssi {
-      width: 49.5px;
-      height: 49.5px;
-      margin: 9px;
-    }
-
-    .titre-site-msc {
-      p {
-        margin: 0;
-      }
-      .site-msc {
-        color: var(--text-title-grey);
-        font-size: 1rem;
-        font-weight: 700;
-        line-height: 1.4rem;
-        min-height: auto;
-      }
-      .organisation-anssi {
-        color: var(--text-default-grey);
-        font-size: 0.75rem;
-        line-height: 1.125rem;
-      }
-    }
   }
 </style>
---
+++ front/lib-svelte/src/ui/EnteteAutonome.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+  interface Props {
+    urlBase: string;
+  }
+
+  let { urlBase }: Props = $props();
+</script>
+
+<div class="entete-principale">
+  <a href={urlBase}>
+    <img src={`${urlBase}/assets/images/marianne-dark.svg`} class="marianne" alt="Marianne" crossorigin="anonymous" />
+  </a>
+  <a href="https://cyber.gouv.fr" aria-label="Site de l’ANSSI">
+    <img class="logo-anssi" src={`${urlBase}/assets/images/logo-anssi.svg`} alt="Logo ANSSI" crossorigin="anonymous" />
+  </a>
+  <a href={urlBase} class="titre-msc" aria-label="Site MesServicesCyber">
+    <div class="titre-site-msc">
+      <p class="site-msc">MesServicesCyber</p>
+      <p class="organisation-anssi">Innovation ANSSI</p>
+    </div>
+  </a>
+</div>
+
+<style lang="scss">
+  .entete-principale {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    margin-top: 1.5rem;
+
+    .titre-msc {
+      display: flex;
+    }
+
+    a.titre-msc {
+      text-decoration: none;
+    }
+
+    .marianne {
+      width: 67px;
+      height: 67px;
+      transform: translateX(-9px);
+    }
+
+    .logo-anssi {
+      width: 49.5px;
+      height: 49.5px;
+      margin: 9px;
+    }
+
+    .titre-site-msc {
+      p {
+        margin: 0;
+      }
+      .site-msc {
+        color: var(--text-title-grey);
+        font-size: 1rem;
+        font-weight: 700;
+        line-height: 1.4rem;
+        min-height: auto;
+      }
+      .organisation-anssi {
+        color: var(--text-default-grey);
+        font-size: 0.75rem;
+        line-height: 1.125rem;
+      }
+    }
+  }
+</style>
