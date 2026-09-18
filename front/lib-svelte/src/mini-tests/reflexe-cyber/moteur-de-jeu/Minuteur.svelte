48520fa98c3f407856b555a5d3b482d6a43622dd
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2026-09-18 11:15:24 +0200

    [REFLEXE CYBER] Anime le minuteur

--- front/lib-svelte/src/mini-tests/reflexe-cyber/moteur-de-jeu/Minuteur.svelte
+++ front/lib-svelte/src/mini-tests/reflexe-cyber/moteur-de-jeu/Minuteur.svelte
@@ -1,9 +1,33 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
+  import { Tween } from 'svelte/motion';
+
+  const décrément = 1000; // en millisecondes
+
+  let secondesRestantes = new Tween(30, { duration: décrément });
+
+  let palier: 'fini' | 'moins-10-secondes' | 'moins-15-secondes' | 'moins-30-secondes' = $derived.by(() => {
+    if (secondesRestantes.current <= 0) return 'fini';
+    if (secondesRestantes.current <= 10) return 'moins-10-secondes';
+    if (secondesRestantes.current <= 15) return 'moins-15-secondes';
+    return 'moins-30-secondes';
+  });
+
+  onMount(() => {
+    const intervale = setInterval(() => {
+      if (palier !== 'fini') {
+        secondesRestantes.target = secondesRestantes.current - 1;
+      } else {
+        clearInterval(intervale);
+      }
+    }, décrément);
+    return () => clearInterval(intervale);
+  });
 </script>
 
 <div class="minuteur">
-  <p>Temps restant&nbsp;: <strong>30 secondes</strong></p>
-  <progress value="25" max="30"></progress>
+  <p>Temps restant&nbsp;: <strong>{Math.max(0, Math.floor(secondesRestantes.current))} secondes</strong></p>
+  <progress class={palier} value={secondesRestantes.current} max="30">{secondesRestantes.current}</progress>
 </div>
 
 <style lang="scss">
@@ -34,13 +58,25 @@
       }
 
       &::-webkit-progress-value {
-        background: var(--border-default-blue-france);
+        background: var(--couleur);
         border-radius: 999px;
       }
 
       &::-moz-progress-bar {
-        background: var(--border-default-blue-france);
+        background: var(--couleur);
         border-radius: 999px;
+      }
+
+      &.moins-30-secondes {
+        --couleur: var(--border-default-blue-france);
+      }
+
+      &.moins-15-secondes {
+        --couleur: var(--background-flat-warning);
+      }
+
+      &.moins-10-secondes {
+        --couleur: var(--background-flat-error);
       }
     }
   }
