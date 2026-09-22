52f430ebebd9125e3f6fe46e21acaea03aef43bd
jpaulineau <jpaulineau@ippon.fr>
2026-09-22 14:36:59 +0200

    [BADGE CYBERDEPART] Ajoute le feature flag d'activation du badge Cyberdépart

--- front/lib-svelte/src/env.d.ts
+++ front/lib-svelte/src/env.d.ts
@@ -2,6 +2,7 @@
 
 interface ImportMetaEnv {
   readonly FEATURE_FLAG_PARCOURS_SECURISATION: string;
+  readonly FEATURE_FLAG_BADGE_CYBERDEPART: string;
 }
 interface ImportMeta {
   readonly env: ImportMetaEnv;
--- front/lib-svelte/src/plateforme/environnement.serveur.ts
+++ front/lib-svelte/src/plateforme/environnement.serveur.ts
@@ -1,2 +1,4 @@
 export const estServeur = true;
 export const afficheParcoursSecurisation = process.env.FEATURE_FLAG_PARCOURS_SECURISATION === 'true';
+
+export const afficheBadgeCyberdépart = process.env.FEATURE_FLAG_BADGE_CYBERDEPART === 'true';
