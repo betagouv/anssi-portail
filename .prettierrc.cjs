4f76defe5a4dcf7023f32d57ad4da391c127a9b2
Cédric Prezelin <cedric.prezelin.ext@beta.gouv.fr>
2026-09-22 17:54:55 +0200

    [OUTILLAGE] Configure le tri automatique des imports

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,3 +1,5 @@
+const importOrder = ['<BUILTIN_MODULES>', '<THIRD_PARTY_MODULES>', '^\\$plateforme/', '^[.]'];
+
 /**
  * @see https://prettier.io/docs/configuration
  * @type {import("prettier").Config}
@@ -6,11 +8,17 @@ module.exports = {
   printWidth: 120,
   singleQuote: true,
   trailingComma: 'es5',
-  plugins: ['prettier-plugin-svelte'],
+  plugins: ['@ianvs/prettier-plugin-sort-imports', 'prettier-plugin-svelte'],
+  importOrder: [],
+  importOrderTypeScriptVersion: '6.0.3',
   overrides: [
     {
+      files: '**/*.{js,jsx,mjs,cjs,ts,tsx,mts,cts}',
+      options: { importOrder },
+    },
+    {
       files: '*.svelte',
-      options: { parser: 'svelte' },
+      options: { parser: 'svelte', importOrder },
     },
   ],
 };
