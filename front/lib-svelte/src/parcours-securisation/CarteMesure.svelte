9512437bee4ca1430d1e087c129220cdaa144489
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2026-09-23 11:51:47 +0200

    [PARCOURS SECURISATION] Permets d’utiliser des vignettes animées
    ...en tant que composants svelte qui seront dans le sous-dossier animation/mesures et qui auront comme nom MesureXXX5 si la mesure est XXX.5

--- front/lib-svelte/src/parcours-securisation/CarteMesure.svelte
+++ front/lib-svelte/src/parcours-securisation/CarteMesure.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import type { Component } from 'svelte';
   import Lien from '../ui/Lien.svelte';
   import type { Mesure } from './mesure';
 
@@ -11,8 +12,18 @@
   const libelleDeLien = $derived(mesure.estPriseEnCompte ? 'Accéder au détail' : "Passer à l'action");
   const titre = $derived(mesure.phraseAccroche || mesure.titre);
   const description = $derived(mesure.phraseAccroche ? mesure.titre : undefined);
+  let survol = $state(false);
+
+  const vignettesAnimées = import.meta.glob<{ default: Component<{ survol: boolean }> } | undefined>(
+    './animation/mesures/Mesure*.svelte'
+  );
+
+  const vignetteAnimée = $derived.by(async () => {
+    return (await vignettesAnimées[`./animation/mesures/Mesure${mesure.id.replaceAll('.', '')}.svelte`]?.())?.default;
+  });
 </script>
 
+<!-- svelte-ignore a11y_no_static_element_interactions -->
 <dsfr-card
   {description}
   enlarge={false}
@@ -23,6 +34,8 @@
   src={`/assets/images/parcours-securisation/mesure-${mesure.id}.avif`}
   title={titre}
   size="sm"
+  onmouseenter={() => (survol = true)}
+  onmouseleave={() => (survol = false)}
 >
   <div slot="headerbadges">
     <dsfr-badge
@@ -38,10 +51,24 @@
     <Lien apparence="bouton" href={`/mesures/${mesure.id}`} type={typeDeLien} libelle={libelleDeLien} etire={true}
     ></Lien>
   </div>
+
+  {#await vignetteAnimée}
+    Chargement…
+  {:then VignetteAnimée}
+    {#if VignetteAnimée}
+      <div slot="image" class="image">
+        <VignetteAnimée {survol} />
+      </div>
+    {/if}
+  {/await}
 </dsfr-card>
 
 <style lang="scss">
   .actions-carte {
+    display: flex;
+    flex-direction: column;
+  }
+  .image {
     display: flex;
     flex-direction: column;
   }
